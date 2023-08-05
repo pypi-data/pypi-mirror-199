a5c41454a75e9f3eb90110a022f8cc72581b7436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:06 +0000

    Add package montecarlodata/0.42.0/montecarlodata-0.42.0.tar.gz

---
+++ packages/montecarlodata/montecarlodata-0.42.0.tar.gz/montecarlodata-0.42.0/montecarlodata/iac/commands.py
@@ -0,0 +1,153 @@
+import click
+import click_config_file
+
+from montecarlodata import settings
+from montecarlodata.common import create_mc_client
+from montecarlodata.iac.mc_config_service import MonteCarloConfigService
+from montecarlodata.monitors.monitor_service import MonitorService
+
+
+@click.group(help="Manage monitors.")
+def monitors():
+    """
+    Group for any monitor related subcommands
+    """
+    pass
+
+
+@monitors.command(help="Compile monitor configuration.")
+@click.option(
+    "--project-dir",
+    required=False,
+    help="Base directory of MC project (where montecarlo.yml is located). "
+    "By default, this is set to the current working directory",
+)
+@click.option(
+    "--namespace",
+    required=False,
+    help="Namespace of monitors configuration. This value will be ignored if we find a"
+    " namespace in montecarlo.yml",
+)
+@click.option(
+    "--dbt-manifest",
+    required=False,
+    help="Path to dbt manifest used for resolving dbt ref() in monitor config",
+)
+@click.pass_obj
+def compile(ctx, project_dir, namespace, dbt_manifest):
+    MonteCarloConfigService(
+        config=ctx["config"],
+        pycarlo_client=create_mc_client(ctx),
+        project_dir=project_dir,
+        dbt_manifest_path=dbt_manifest,
+    ).compile(namespace)
+
+
+@monitors.command(help="Compile and apply monitor configuration.")
+@click.option(
+    "--project-dir",
+    required=False,
+    help="Base directory of MC project (where montecarlo.yml is located). "
+    "By default, this is set to the current working directory",
+)
+@click.option(
+    "--namespace",
+    required=False,
+    help="Namespace of monitors configuration. This value will be ignored if we find a"
+    " namespace in montecarlo.yml",
+)
+@click.option(
+    "--dry-run",
+    required=False,
+    default=False,
+    show_default=True,
+    is_flag=True,
+    help="Dry run (just shows planned changes but doesn't apply them.)",
+)
+@click.option(
+    "--dbt-manifest",
+    required=False,
+    help="Path to dbt manifest used for resolving dbt ref() in monitor config",
+)
+@click.pass_obj
+@click_config_file.configuration_option(settings.OPTION_FILE_FLAG)
+def apply(ctx, project_dir, namespace, dry_run, dbt_manifest):
+    MonteCarloConfigService(
+        config=ctx["config"],
+        pycarlo_client=create_mc_client(ctx),
+        project_dir=project_dir,
+        dbt_manifest_path=dbt_manifest,
+    ).apply(namespace, dry_run=dry_run)
+
+
+@monitors.command(help="Delete monitor configuration.")
+@click.option(
+    "--project-dir",
+    required=False,
+    help="Base directory of MC project (where montecarlo.yml is located). "
+    "By default, this is set to the current working directory",
+)
+@click.option("--namespace", required=True, help="Namespace of monitors configuration.")
+@click.option(
+    "--dry-run",
+    required=False,
+    default=False,
+    show_default=True,
+    is_flag=True,
+    help="Dry run (just shows planned changes but doesn't apply them.",
+)
+@click.pass_obj
+@click_config_file.configuration_option(settings.OPTION_FILE_FLAG)
+def delete(ctx, project_dir, namespace, dry_run):
+    MonteCarloConfigService(
+        config=ctx["config"],
+        pycarlo_client=create_mc_client(ctx),
+        project_dir=project_dir,
+    ).delete(namespace, dry_run=dry_run)
+
+
+@monitors.command(name="list", help="List monitors ordered by update recency.")
+@click.option(
+    "--limit",
+    required=False,
+    default=100,
+    type=click.INT,
+    show_default=True,
+    help="Max number of monitors to list.",
+)
+@click.option(
+    "--monitor-type",
+    required=False,
+    type=click.Choice(MonitorService.MONITOR_TYPES, case_sensitive=False),
+    help="List monitors with monitor_type",
+)
+@click.option(
+    "--namespace", required=False, help="List only monitors in this namespace"
+)
+@click.pass_obj
+def list_monitors(ctx, monitor_type, limit, namespace):
+    monitor_types = None
+    namespaces = [namespace] if namespace else None
+    if monitor_type:
+        if monitor_type == MonitorService.PSEUDO_MONITOR_TYPE_CB_COMPATIBLE:
+            monitor_type = MonitorService.MONITOR_TYPE_CUSTOM_SQL
+        monitor_types = [monitor_type]
+    MonitorService(client=create_mc_client(ctx)).list_monitors(
+        limit, namespaces, monitor_types
+    )
+
+
+@monitors.command(name="namespaces", help="List all namespaces.")
+@click.option(
+    "--limit",
+    required=False,
+    default=100,
+    type=click.INT,
+    show_default=True,
+    help="Max number of namespaces to list.",
+)
+@click.pass_obj
+def list_namespaces(ctx, limit):
+    MonteCarloConfigService(
+        config=ctx["config"], pycarlo_client=create_mc_client(ctx)
+    ).list_namespaces(limit)
