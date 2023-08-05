c606a03514c83ccf1ed807aa89063b29b6ce85c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:48 +0000

    Add package trifinger-rl-datasets/0.9.0/trifinger_rl_datasets-0.9.0.tar.gz

---
+++ packages/trifinger-rl-datasets/trifinger_rl_datasets-0.9.0.tar.gz/trifinger_rl_datasets-0.9.0/trifinger_rl_datasets/__init__.py
@@ -0,0 +1,42 @@
+__version__ = "0.9.0"
+
+from gymnasium.envs.registration import register
+
+from .dataset_env import TriFingerDatasetEnv
+from .evaluation import Evaluation
+from .policy_base import PolicyBase, PolicyConfig
+
+
+dataset_params = [
+    # simulation
+    # =========
+    # real robot
+    # ==========
+    # push expert with images (mini version for testing)
+    {
+        "name": "trifinger-cube-push-real-expert-image-mini-v0",
+        "dataset_url": "https://robots.real-robot-challenge.com/public/split_test/trifinger-cube-push-real-expert-image-mini-v0.yaml",
+        "ref_min_score": 0.0,
+        "ref_max_score": 1.0 * 15000 / 20,
+        "real_robot": True,
+        "trifinger_kwargs": {
+            "episode_length": 750,
+            "difficulty": 1,
+            "keypoint_obs": True,
+            "obs_action_delay": 10,
+        },
+    },
+]
+
+
+def get_env(**kwargs):
+    return TriFingerDatasetEnv(**kwargs)
+
+
+for params in dataset_params:
+    register(
+        id=params["name"], entry_point="trifinger_rl_datasets:get_env", kwargs=params
+    )
+
+
+__all__ = ("TriFingerDatasetEnv", "Evaluation", "PolicyBase", "PolicyConfig", "get_env")
