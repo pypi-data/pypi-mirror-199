c6379e45d03cd545f805e14028c57d01e4fe3834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:33 +0000

    Add package oemof.solph/0.5.1.dev0/oemof.solph-0.5.1.dev0.tar.gz

---
+++ packages/oemof.solph/oemof.solph-0.5.1.dev0.tar.gz/oemof.solph-0.5.1.dev0/tests/test_models.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -
+
+"""Basic tests.
+
+This file is part of project oemof (github.com/oemof/oemof). It's copyrighted
+by the contributors recorded in the version control history of the file,
+available from its original location oemof/tests/basic_tests.py
+
+SPDX-License-Identifier: MIT
+"""
+
+import warnings
+
+import pytest
+
+from oemof import solph
+
+
+def test_optimal_solution():
+    es = solph.EnergySystem(timeincrement=[1])
+    bel = solph.buses.Bus(label="bus")
+    es.add(bel)
+    es.add(
+        solph.components.Sink(
+            label="sink",
+            inputs={bel: solph.flows.Flow(nominal_value=5, fix=[1])},
+        )
+    )
+    es.add(
+        solph.components.Source(
+            label="source",
+            outputs={bel: solph.flows.Flow(variable_costs=5)},
+        )
+    )
+    m = solph.Model(es)
+    m.solve("cbc")
+    m.results()
+    solph.processing.meta_results(m)
+
+
+def test_infeasible_model():
+    # FutureWarning is i.e. emitted by network Entity registry
+    warnings.simplefilter(action="ignore", category=FutureWarning)
+
+    with pytest.raises(ValueError, match=""):
+        with warnings.catch_warnings(record=True) as w:
+            es = solph.EnergySystem(timeincrement=[1])
+            bel = solph.buses.Bus(label="bus")
+            es.add(bel)
+            es.add(
+                solph.components.Sink(
+                    inputs={bel: solph.flows.Flow(nominal_value=5, fix=[1])}
+                )
+            )
+            es.add(
+                solph.components.Source(
+                    outputs={
+                        bel: solph.flows.Flow(
+                            nominal_value=4, variable_costs=5
+                        )
+                    }
+                )
+            )
+            m = solph.Model(es)
+            m.solve(solver="cbc")
+            assert "Optimization ended with status" in str(w[0].message)
+            solph.processing.meta_results(m)
