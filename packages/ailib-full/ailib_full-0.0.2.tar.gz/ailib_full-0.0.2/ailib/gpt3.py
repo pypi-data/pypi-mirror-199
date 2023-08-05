eb4b8e53e4db2ba6c455ad6d8aa52534fb68de46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:51 +0000

    Add package ailib-full/0.0.2/ailib_full-0.0.2.tar.gz

---
+++ packages/ailib-full/ailib_full-0.0.2.tar.gz/ailib_full-0.0.2/ailib/gpt3.py
@@ -0,0 +1,28 @@
+import requests
+from typing import List
+
+api_key = "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"
+
+class Model:
+    def __init__(self, model_id: str, object: str, owned_by: str):
+        self.model_id: str = model_id
+        self.object: str = object
+        self.owned_by: str = owned_by
+
+class Models:
+    @staticmethod
+    def list() -> List[Model]:
+        response = requests.get("https://api.openai.com/v1/models", headers={'Authorization': f'Bearer {api_key}'})
+        json = response.json()
+
+        models = []
+
+        for model in json['data']:
+            models.append(Model(model['id'], model['object'], model['owned_by']))
+
+        return models
+
+class TextCompletion:
+    @staticmethod
+    def create(model: str, prompt: str) -> str:
+        pass
---
+++ packages/ailib-full/ailib_full-0.0.2.tar.gz/ailib_full-0.0.2/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present Ben Buzard <ben@benbuzard.com>
+#
+# SPDX-License-Identifier: MIT
