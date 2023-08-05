8fa55ed644081f6b0bd7d12571cf17f6825e43b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:17 +0000

    Add package helisol/0.1.0/helisol-0.1.0-py3-none-any.whl

---
+++ packages/helisol/helisol-0.1.0-py3-none-any.whl/helisol/earth_motion.py
@@ -0,0 +1,148 @@
+"""Calculate earth motion around sun"""
+
+# ----------------------------- License information --------------------------
+
+# This file is part of the helisol python package.
+# Copyright (C) 2023 Olivier Vincent
+
+# The helisol package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# The helisol package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with the helisol python package.
+# If not, see <https://www.gnu.org/licenses/>
+
+
+import numpy as np
+
+from .general import CONSTANTS
+from .general import Angle, Time
+from .general import sin, tan
+
+
+class EarthOrbit:
+    """Store general characteristics of earth orbit"""
+
+    def __init__(self, utc_time=None):
+        """Init earth orbit object from specific date/time.
+
+        Parameters
+        ----------
+        utc_time: datetime or str (default None, i.e. current time)
+
+        Examples
+        --------
+        EarthOrbit('9am')
+        EarthOrbit(utc_time='June 10 10:08:44')
+        """
+        self.time = Time(utc=utc_time)
+
+    def __repr__(self):
+        year = self.time.utc.date().year
+        return f'Earth Orbit in {year}'
+
+    @property
+    def excentricity(self):
+        """Excentricity of earth's orbit"""
+        return 0.016709 - 0.000042 * self.time.julian_centuries
+
+    @property
+    def axial_tilt(self):
+        """Earth's axial tilt"""
+        t = self.time.julian_centuries
+        epsilon = (23 + 26 / 60 + (21.5 - 46.8 * t) / 3600)
+        return Angle(degrees=epsilon)
+
+    @property
+    def spring_longitude(self):
+        """Longitude between perigee and spring"""
+        a = CONSTANTS['average motion coefficients']
+        t = self.time.julian_centuries
+        gamma0 = sum([(a['L'][i] - a['M'][i]) * t**i for i in range(3)])
+        return Angle(degrees=gamma0)
+
+    @property
+    def nutation_with_aberration(self):
+        """(delta_lambda)"""
+        t = self.time.julian_centuries
+        nut0, nut1 = CONSTANTS['nutation coefficients']
+        abr0, abr1 = CONSTANTS['aberration coefficients']
+        nut = Angle(degrees=(nut0 + nut1 * t))
+        delta_lambda = abr0 + abr1 * sin(nut)
+        return Angle(degrees=delta_lambda)
+
+
+class Earth:
+
+    def __init__(self, utc_time=None):
+        """Init earth object from specific date/time.
+
+        Parameters
+        ----------
+        utc_time: datetime or str (default None, i.e. current time)
+
+        Examples
+        --------
+        Earth('9am')
+        Earth(utc_time='June 10 10:08:44')
+        """
+        self.update(utc_time=utc_time)
+
+    def update(self, utc_time=None):
+        self.time = Time(utc=utc_time)
+        self.orbit = EarthOrbit(utc_time=utc_time)
+
+    def __repr__(self):
+        date = self.time.utc.date()
+        time = self.time.utc.time().strftime("%H:%M:%S")
+        return f'Earth on {date} at {time} (UTC)'
+
+    @property
+    def average_motion(self):
+        """Average motion with respect to perigee"""
+        a = CONSTANTS['average motion coefficients']
+        t = self.time.julian_centuries
+        m_deg = sum([a['M'][i] * t**i for i in range(3)])
+        m = Angle(degrees=m_deg)
+        m.minus_pi_to_pi()
+        return m
+
+    def anomaly(self, iteration=5):
+        """Iterative way of calculating excentric anomaly"""
+        m0 = self.average_motion
+        if iteration == 0:
+            return m0
+        else:
+            u = self.anomaly(iteration=iteration - 1)
+            e = self.orbit.excentricity
+            m_rad = m0.radians + e * sin(u)
+            return Angle(radians=m_rad)
+
+    @property
+    def true_anomaly(self):
+        """True anomaly (nu)"""
+        u = self.anomaly(iteration=CONSTANTS['anomaly iterations'])
+        e = self.orbit.excentricity
+        nu_rad = 2 * np.arctan(np.sqrt((1 + e) / (1 - e)) * tan(u / 2))
+        return Angle(radians=nu_rad)
+
+    @property
+    def longitude(self):
+        """Longitude (λ) with respect to spring"""
+        λ0 = self.orbit.spring_longitude
+        λ = λ0 + self.true_anomaly
+        return λ
+
+    @property
+    def apparent_longitude(self):
+        """Apparent longitude (including nutation and aberration)"""
+        λ = self.longitude
+        Δλ = self.orbit.nutation_with_aberration
+        return λ + Δλ
---
+++ packages/helisol/helisol-0.1.0-py3-none-any.whl/helisol/sun_motion.py
@@ -0,0 +1,150 @@
+"""Calculate sun motion."""
+
+# ----------------------------- License information --------------------------
+
+# This file is part of the helisol python package.
+# Copyright (C) 2023 Olivier Vincent
+
+# The helisol package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# The helisol package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with the helisol python package.
+# If not, see <https://www.gnu.org/licenses/>
+
+
+# ================================= Imports ==================================
+
+
+from copy import copy
+import numpy as np
+
+from .general import Angle, Time
+from .general import sin, cos, tan
+from .earth_motion import Earth
+
+
+class Sun:
+
+    def __init__(self, location, utc_time=None):
+        """Init sun object from specific date/time.
+
+        Parameters
+        ----------
+        location: tuple (or iterable) (latitude, longitude) in degrees
+        utc_time: datetime or str (default None, i.e. current time)
+
+        Examples
+        --------
+        loc = (42.4, -76.5)
+        Sun(loc, '9am')
+        Sun(location=loc, utc_time='June 10 10:08:44')
+        """
+        self.location = location
+        self.latitude, self.longitude = [Angle(degrees=x) for x in location]
+        self.update(utc_time=utc_time)
+
+    def update(self, utc_time=None):
+        """Update at current time (default) or given UTC time."""
+        self.time = Time(utc=utc_time)
+        self.earth = Earth(utc_time=utc_time)
+
+    def __repr__(self):
+        lat = self.latitude.degrees
+        long = self.longitude.degrees
+        date = self.time.utc.date()
+        time = self.time.utc.time().strftime("%H:%M:%S")
+        a = f'Sun seen from ({lat}°, {long}°) on {date} at {time} (UTC)'
+
+        height = self.height.degrees
+        azimuth = self.azimuth.degrees
+        b = f'\nHeight {height:.2f}, Azimuth {azimuth:.2f}'
+
+        moments = self.sunrise.utc, self.noon.utc, self.sunset.utc
+        sunrise, noon, sunset = [m.strftime("%H:%M:%S") for m in moments]
+        c = f'\nSunrise {sunrise} Noon {noon} Sunset {sunset}'
+        return a + b + c
+
+    @property
+    def right_ascension(self):
+        ε = self.earth.orbit.axial_tilt
+        λapp = self.earth.apparent_longitude
+        alpha_rad = np.arctan2(cos(ε) * sin(λapp), cos(λapp))
+        return Angle(radians=alpha_rad)
+
+    @property
+    def declination(self):
+        ε = self.earth.orbit.axial_tilt
+        λapp = self.earth.apparent_longitude
+        ẟ = np.arcsin(sin(λapp) * sin(ε))
+        return Angle(radians=ẟ)
+
+    @property
+    def equation_of_time(self):
+        alpha = self.right_ascension
+        gamma0 = self.earth.orbit.spring_longitude
+        m = self.earth.average_motion
+        eqt = (alpha - gamma0 - m)
+        eqt.minus_pi_to_pi()
+        return eqt
+
+    @property
+    def hourly_angle(self):
+        x = self.time.fraction_of_day
+        a = Angle(radians=(2 * np.pi * (x - 0.5)))
+        return a - self.equation_of_time + self.longitude
+
+    @property
+    def height(self):
+        """Height of sun above horizon."""
+        ẟ = self.declination
+        lat = self.latitude
+        H = self.hourly_angle
+        h = np.arcsin(sin(ẟ) * sin(lat) + cos(ẟ) * cos(lat) * cos(H))
+        return Angle(radians=h)
+
+    @property
+    def azimuth(self):
+        """Azimuth of sun with respect to South"""
+        ẟ = self.declination
+        H = self.hourly_angle
+        lat = self.latitude
+        a = cos(ẟ) * sin(H)
+        b = cos(ẟ) * cos(H) * sin(lat) - sin(ẟ) * cos(lat)
+        return Angle(radians=np.arctan2(a, b))
+
+    @property
+    def noon(self):
+        """Solar noon."""
+        noon = copy(self.time)
+        eqt = self.equation_of_time
+        long = self.longitude
+        noon_frac = (eqt.radians - long.radians) / (2 * np.pi) + 0.5
+        noon.fraction_of_day = noon_frac
+        return noon
+
+    @property
+    def sunrise(self):
+        """Sunrise"""
+        rise = copy(self.time)
+        ẟ = self.declination
+        lat = self.latitude
+        noon_frac = self.noon.fraction_of_day
+        rise_frac = noon_frac - np.arccos(-tan(ẟ) * tan(lat)) / (2 * np.pi)
+        rise.fraction_of_day = rise_frac
+        return rise
+
+    @property
+    def sunset(self):
+        """Sunset"""
+        sunset = copy(self.time)
+        sunset_frac = 2 * self.noon.fraction_of_day - self.sunrise.fraction_of_day
+        sunset.fraction_of_day = sunset_frac
+        return sunset
