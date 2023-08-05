929db2c8e87dd9ebda0009165bbcee1940e4108f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:24 +0000

    Add package je-load-density-dev/0.0.38/je_load_density_dev-0.0.38.tar.gz

---
+++ packages/je-load-density-dev/je_load_density_dev-0.0.38.tar.gz/je_load_density_dev-0.0.38/test/test_source/locustfile.py
@@ -0,0 +1,15 @@
+import sys
+
+from locust import HttpUser, task
+
+
+# test use this user
+
+
+class HelloWorldUser(HttpUser):
+    @task
+    def hello_world(self):
+        self.client.get("http://localhost:5000")
+
+    def on_stop(self):
+        sys.exit(0)
