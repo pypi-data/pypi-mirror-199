7564bf9372fcb4fcf101380515cee78296fd3b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:44 +0000

    Add package generic-collections/0.1.1/generic_collections-0.1.1.tar.gz

---
+++ packages/generic-collections/generic_collections-0.1.1.tar.gz/generic_collections-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['generic_collections',
+ 'generic_collections.enums',
+ 'generic_collections.objects']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.10.7,<2.0.0']
+
+setup_kwargs = {
+    'name': 'generic-collections',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '',
+    'author': 'ventura94',
+    'author_email': 'arianventura94@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
