61acecd674e03b84aebdd73139d6710a0fb710f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:46 +0000

    Add package napari-bbox/0.0.1/napari-bbox-0.0.1.tar.gz

---
+++ packages/napari-bbox/napari-bbox-0.0.1.tar.gz/napari-bbox-0.0.1/src/napari_bbox/__init__.py
@@ -0,0 +1,36 @@
+
+__version__ = "0.0.1"
+
+from napari.layers import NAMES
+NAMES.add("bounding_boxes")
+
+from .boundingbox import BoundingBoxLayer
+from ._reader import napari_get_reader
+from ._writer import write_single_bbox
+
+from ._widget import BoundingBoxCreator
+
+from napari import Viewer
+def add_bounding_boxes(self, *args, **kwargs):
+    layer = BoundingBoxLayer(*args, **kwargs)
+    self.layers.append(layer)
+    return layer
+Viewer.add_bounding_boxes = add_bounding_boxes
+
+import npe2.manifest.contributions._writers
+from enum import Enum
+
+
+class LayerType(str, Enum):
+    image = "image"
+    labels = "labels"
+    points = "points"
+    shapes = "shapes"
+    surface = "surface"
+    tracks = "tracks"
+    vectors = "vectors"
+    bounding_boxes = "bounding_boxes"
+
+
+npe2.manifest.contributions._writers.LayerType = LayerType
+__all__ = ["BoundingBoxCreator", "BoundingBoxLayer", "napari_get_reader", "write_single_bbox"]
