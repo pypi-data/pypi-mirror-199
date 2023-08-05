938239d7858a10c963b8e7621dad06244bbf9220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:14 +0000

    Add package libecalc/0.0.1/libecalc-0.0.1-py3-none-any.whl

---
+++ packages/libecalc/libecalc-0.0.1-py3-none-any.whl/libecalc/main.py
@@ -0,0 +1,12 @@
+
+ecalc_text = """
+"eCalc™ - energy and emission Calculator - is a calculator for energy usage and emissions related to reservoir drainage.
+
+eCalc™ will be open sourced by [Equinor](https://www.equinor.com) (most likely under GPL-3 license) on May 3rd 2023.
+
+This project/repo is currently a placeholder until May 3rd 2023.
+"""
+
+
+def app():
+    print(ecalc_text)
