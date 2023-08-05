99b4bf1e69adef835b6336f1f464036766fd7c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:51 +0000

    Add package allowed-ghosts/0.2.4/allowed_ghosts-0.2.4-py3-none-any.whl

---
+++ packages/allowed-ghosts/allowed_ghosts-0.2.4-py3-none-any.whl/allowed_ghosts/tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# author        : JV-conseil
+# credits       : JV-conseil
+# copyright     : Copyright (c) 2019-2023 JV-conseil
+#                 All rights reserved
+# ====================================================
+
+from .main import AllowedGhosts
+
+_ = AllowedGhosts()
+_()
