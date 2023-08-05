9ac0a60d9276be67784d33b4bb321941be271e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:54 +0000

    Add package ionoscloud-dbaas-mongo/1.2.2/ionoscloud-dbaas-mongo-1.2.2.tar.gz

---
+++ packages/ionoscloud-dbaas-mongo/ionoscloud-dbaas-mongo-1.2.2.tar.gz/ionoscloud-dbaas-mongo-1.2.2/ionoscloud_dbaas_mongo/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ionoscloud_dbaas_mongo.api.clusters_api import ClustersApi
+from ionoscloud_dbaas_mongo.api.logs_api import LogsApi
+from ionoscloud_dbaas_mongo.api.metadata_api import MetadataApi
+from ionoscloud_dbaas_mongo.api.restores_api import RestoresApi
+from ionoscloud_dbaas_mongo.api.snapshots_api import SnapshotsApi
+from ionoscloud_dbaas_mongo.api.templates_api import TemplatesApi
+from ionoscloud_dbaas_mongo.api.users_api import UsersApi
---
+++ packages/ionoscloud-dbaas-mongo/ionoscloud-dbaas-mongo-1.2.2.tar.gz/ionoscloud-dbaas-mongo-1.2.2/ionoscloud_dbaas_mongo/api/templates_api.py
@@ -0,0 +1,162 @@
+from __future__ import absolute_import
+
+import re  # noqa: F401
+import six
+
+from ionoscloud_dbaas_mongo.api_client import ApiClient
+from ionoscloud_dbaas_mongo.exceptions import (  # noqa: F401
+    ApiTypeError,
+    ApiValueError
+)
+
+
+class TemplatesApi(object):
+
+    def __init__(self, api_client=None):
+        if api_client is None:
+            api_client = ApiClient()
+        self.api_client = api_client
+
+    def templates_get(self, **kwargs):  # noqa: E501
+        """Get Templates  # noqa: E501
+
+        Retrieves a list of valid templates. These templates can be used to create MongoDB clusters; they contain properties, such as number of cores, RAM, and the storage size.   # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+
+        >>> thread = api.templates_get(async_req=True)
+        >>> result = thread.get()
+
+        :param limit: The maximum number of elements to return. Use together with 'offset' for pagination.
+        :type limit: int
+        :param offset: The first element to return. Use together with 'limit' for pagination.
+        :type offset: int
+        :param async_req: Whether to execute the request asynchronously.
+        :type async_req: bool, optional
+        :param _preload_content: if False, the urllib3.HTTPResponse object will
+                                 be returned without reading/decoding response
+                                 data. Default is True.
+        :type _preload_content: bool, optional
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :return: Returns the result object.
+                 If the method is called asynchronously,
+                 returns the request thread.
+        :rtype: TemplateList
+        """
+        kwargs['_return_http_data_only'] = True
+        return self.templates_get_with_http_info(**kwargs)  # noqa: E501
+
+    def templates_get_with_http_info(self, **kwargs):  # noqa: E501
+        """Get Templates  # noqa: E501
+
+        Retrieves a list of valid templates. These templates can be used to create MongoDB clusters; they contain properties, such as number of cores, RAM, and the storage size.   # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+
+        >>> thread = api.templates_get_with_http_info(async_req=True)
+        >>> result = thread.get()
+
+        :param limit: The maximum number of elements to return. Use together with 'offset' for pagination.
+        :type limit: int
+        :param offset: The first element to return. Use together with 'limit' for pagination.
+        :type offset: int
+        :param async_req: Whether to execute the request asynchronously.
+        :type async_req: bool, optional
+        :param _return_http_data_only: response data without head status code
+                                       and headers
+        :type _return_http_data_only: bool, optional
+        :param _preload_content: if False, the urllib3.HTTPResponse object will
+                                 be returned without reading/decoding response
+                                 data. Default is True.
+        :type _preload_content: bool, optional
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the authentication
+                              in the spec for a single request.
+        :type _request_auth: dict, optional
+        :return: Returns the result object.
+                 If the method is called asynchronously,
+                 returns the request thread.
+        :rtype: tuple(TemplateList, status_code(int), headers(HTTPHeaderDict))
+        """
+
+        local_var_params = locals()
+
+        all_params = [
+            'limit',
+            'offset'
+        ]
+        all_params.extend(
+            [
+                'async_req',
+                '_return_http_data_only',
+                '_preload_content',
+                '_request_timeout',
+                '_request_auth',
+                'response_type',
+                'query_params'
+            ]
+        )
+
+        for local_var_params_key, local_var_params_val in six.iteritems(local_var_params['kwargs']):
+            if local_var_params_key not in all_params:
+                raise ApiTypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method templates_get" % local_var_params_key
+                )
+            local_var_params[local_var_params_key] = local_var_params_val
+        del local_var_params['kwargs']
+
+        if self.api_client.client_side_validation and 'limit' in local_var_params and local_var_params['limit'] > 1000:  # noqa: E501
+            raise ApiValueError("Invalid value for parameter `limit` when calling `templates_get`, must be a value less than or equal to `1000`")  # noqa: E501
+        if self.api_client.client_side_validation and 'limit' in local_var_params and local_var_params['limit'] < 1:  # noqa: E501
+            raise ApiValueError("Invalid value for parameter `limit` when calling `templates_get`, must be a value greater than or equal to `1`")  # noqa: E501
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = list(local_var_params.get('query_params', {}).items())
+        if 'limit' in local_var_params and local_var_params['limit'] is not None:  # noqa: E501
+            query_params.append(('limit', local_var_params['limit']))  # noqa: E501
+        if 'offset' in local_var_params and local_var_params['offset'] is not None:  # noqa: E501
+            query_params.append(('offset', local_var_params['offset']))  # noqa: E501
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['basicAuth', 'tokenAuth']  # noqa: E501
+
+        response_type = 'TemplateList'
+        if 'response_type' in kwargs:
+            response_type = kwargs['response_type']
+
+        return self.api_client.call_api(
+            '/templates', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type=response_type,  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=local_var_params.get('async_req'),
+            _return_http_data_only=local_var_params.get('_return_http_data_only'),  # noqa: E501
+            _preload_content=local_var_params.get('_preload_content', True),
+            _request_timeout=local_var_params.get('_request_timeout'),
+            collection_formats=collection_formats,
+            _request_auth=local_var_params.get('_request_auth'))
