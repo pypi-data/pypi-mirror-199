b9cc69f525bcc53bfd4c4d9cd74b9ed11cc9277d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:32 +0000

    Add package funasr/0.3.1/funasr-0.3.1-py3-none-any.whl

---
+++ packages/funasr/funasr-0.3.1-py3-none-any.whl/funasr/runtime/python/libtorch/test_rtf.py
@@ -0,0 +1,24 @@
+#from torch_paraformer import Paraformer
+from rapid_paraformer import Paraformer
+import time
+
+model_dir = "/home/gzf/export/damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch"
+#model_dir ="/home/gzf/workspace/amp_int8/libtorch_fb20"
+#model_dir ="/home/gzf/workspace/amp_int8/onnx_dynamic"
+model = Paraformer(model_dir, batch_size=1, device_id="-1")
+
+wav_path = ["/nfs/zhifu.gzf/export/damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch/example/asr_example.wav", "/nfs/zhifu.gzf/export/damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch/example/asr_example.wav"]
+wav_path = ["/home/gzf/export/damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch/example/asr_example.wav"]
+
+total = 0.0
+num = 100
+result = model(wav_path)
+for i in range(num):
+  beg_time = time.time()
+  result = model(wav_path)
+  end_time = time.time()
+  duration = end_time-beg_time
+  total += duration
+  print(result)
+  print("num: {}, time, {}, avg: {}, rtf: {}".format(len(wav_path), duration, total/(i+1), (total/(i+1))/5.53))
+print(result)
