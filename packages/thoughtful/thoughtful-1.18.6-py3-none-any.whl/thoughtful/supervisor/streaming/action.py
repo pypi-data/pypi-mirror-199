0edd5181aa1b8c8e5db7a598305651bde8719b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:26 +0000

    Add package thoughtful/1.18.6/thoughtful-1.18.6-py3-none-any.whl

---
+++ packages/thoughtful/thoughtful-1.18.6-py3-none-any.whl/thoughtful/supervisor/streaming/action.py
@@ -0,0 +1,28 @@
+"""
+This module contains an enum to define the actions available between
+StreamingCallback and Fabric
+"""
+
+
+from enum import Enum
+
+
+class Action(str, Enum):
+    """
+    Action - request payload attribute defining the nature of the request to fabric
+    """
+
+    STEP_REPORT = "step_report"
+    """
+    Report for new steps and updates on step status.
+    """
+
+    BOT_MANIFEST = "bot_manifest"
+    """
+    Provides the bots manifest so empower can contextualize step reports.
+    """
+
+    ARTIFACTS_UPLOADED = "artifacts_uploaded"
+    """
+    Describes the location of where a bot's run artifacts are uploaded.
+    """
