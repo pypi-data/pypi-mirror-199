67991047d5eed918a75f928fe007b54913f717a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:12 +0000

    Add package PyUPC-EAN/2.8.6/PyUPC_EAN-2.8.6-py3-none-any.whl

---
+++ packages/PyUPC-EAN/PyUPC_EAN-2.8.6-py3-none-any.whl/upcean/barcodes/barcode.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2011-2020 Game Maker 2k - https://github.com/GameMaker2k
+    Copyright 2011-2020 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+
+    $FileInfo: barcode.py - Last Update: 3/23/2023 Ver. 2.8.6 RC 1 - Author: cooldude2k $
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals;
+import upcean.support, upcean.getprefix.getprefix;
+
+''' // Code for making EAN-2 supplement by Kazuki Przyborowski '''
+from upcean.barcodes.ean2 import *;
+''' // Code for making EAN-5 supplement by Kazuki Przyborowski '''
+from upcean.barcodes.ean5 import *;
+''' // Code for making UPC-A by Kazuki Przyborowski '''
+from upcean.barcodes.upca import *;
+''' // Code for making Goodwill Barcodes by Kazuki Przyborowski '''
+from upcean.barcodes.goodwill import *;
+''' // Code for making UPC-E by Kazuki Przyborowski '''
+from upcean.barcodes.upce import *;
+''' // Code for making EAN-13 by Kazuki Przyborowski '''
+from upcean.barcodes.ean13 import *;
+''' // Code for making EAN-8 by Kazuki Przyborowski '''
+from upcean.barcodes.ean8 import *;
+''' // Code for making Standard 2 of 5 by Kazuki Przyborowski '''
+from upcean.barcodes.stf import *;
+''' // Code for making Interleaved 2 of 5 by Kazuki Przyborowski '''
+from upcean.barcodes.itf import *;
+''' // Code for making ITF-14 by Kazuki Przyborowski '''
+from upcean.barcodes.itf14 import *;
+''' // Code for making Code 11 by Kazuki Przyborowski '''
+from upcean.barcodes.code11 import *;
+''' // Code for making Code 39 by Kazuki Przyborowski '''
+from upcean.barcodes.code39 import *;
+''' // Code for making Code 93 by Kazuki Przyborowski '''
+from upcean.barcodes.code93 import *;
+''' // Code for making Code 128 by Kazuki Przyborowski '''
+from upcean.barcodes.code128 import *;
+''' // Code for making Codabar by Kazuki Przyborowski '''
+from upcean.barcodes.codabar import *;
+''' // Code for making Plessey by Kazuki Przyborowski '''
+from upcean.barcodes.plessey import *;
+''' // Code for making Modified Plessey by Kazuki Przyborowski '''
+from upcean.barcodes.msi import *;
+
+def validate_create_upca_barcode(upc,outfile="./upca.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>12 or len(upc)<12):
+  return False;
+ if(not upcean.validate.validate_upca_checksum(upc)):
+  return False;
+ return create_upca_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def validate_draw_upca_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return validate_create_upca_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def validate_create_goodwill_barcode(upc,outfile="./goodwill.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>12 or len(upc)<12):
+  return False;
+ if(not upcean.validate.validate_upca_checksum(upc)):
+  return False;
+ if(not upcean.getprefix.getprefix.get_goodwill_upca_barcode_info(upc)):
+  return False;
+ return create_goodwill_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def validate_draw_goodwill_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return validate_create_goodwill_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def validate_create_upce_barcode(upc,outfile="./upce.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>8 or len(upc)<8):
+  return False;
+ if(not upcean.validate.validate_upce_checksum(upc)):
+  return False;
+ return create_upce_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def validate_draw_upce_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return validate_create_upce_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def validate_create_ean13_barcode(upc,outfile="./ean13.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>13 or len(upc)<13):
+  return False;
+ if(not upcean.validate.validate_ean13_checksum(upc)):
+  return False;
+ return create_ean13_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def validate_draw_ean13_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return validate_create_ean13_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def validate_create_ean8_barcode(upc,outfile="./ean8.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>8 or len(upc)<8):
+  return False;
+ if(not upcean.validate.validate_upce_checksum(upc)):
+  return False;
+ return create_upce_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def validate_draw_ean8_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return validate_create_ean8_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def validate_create_itf_barcode(upc,outfile="./itf.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>14 or len(upc)<14):
+  return False;
+ if(not upcean.validate.validate_itf14_checksum(upc)):
+  return False;
+ return create_itf_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def validate_draw_itf_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return validate_create_itf_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def validate_create_itf6_barcode(upc,outfile="./itf6.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>6 or len(upc)<6):
+  return False;
+ if(not upcean.validate.validate_itf6_checksum(upc)):
+  return False;
+ return create_itf6_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def validate_draw_itf6_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return validate_create_itf6_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def validate_create_itf14_barcode(upc,outfile="./itf14.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>14 or len(upc)<14):
+  return False;
+ if(not upcean.validate.validate_itf14_checksum(upc)):
+  return False;
+ return create_itf14_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def validate_draw_itf14_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return validate_create_itf14_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def fix_create_upca_barcode(upc,outfile="./upca.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>12 or len(upc)<11):
+  return False;
+ upc = upcean.validate.fix_upca_checksum(upc);
+ return create_upca_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def fix_draw_upca_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return fix_create_upca_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def fix_create_goodwill_barcode(upc,outfile="./goodwill.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>12 or len(upc)<11):
+  return False;
+ upc = upcean.validate.fix_upca_checksum(upc);
+ if(not upcean.getprefix.getprefix.get_goodwill_upca_barcode_info(upc)):
+  return False;
+ return create_goodwill_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def fix_draw_goodwill_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return fix_create_goodwill_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def fix_create_upce_barcode(upc,outfile="./upce.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>8 or len(upc)<7):
+  return False;
+ upc = upcean.validate.fix_upce_checksum(upc);
+ return create_upce_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def fix_draw_upce_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return fix_create_upce_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def fix_create_ean13_barcode(upc,outfile="./ean13.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>13 or len(upc)<12):
+  return False;
+ upc = upcean.validate.fix_ean13_checksum(upc);
+ return create_ean13_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def fix_draw_ean13_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return fix_create_ean13_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def fix_create_ean8_barcode(upc,outfile="./ean8.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>8 or len(upc)<7):
+  return False;
+ upc = upcean.validate.fix_ean8_checksum(upc);
+ return create_upce_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def fix_draw_ean8_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return fix_create_ean8_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def fix_create_itf_barcode(upc,outfile="./itf.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>14 or len(upc)<13):
+  return False;
+ upc = upcean.validate.fix_itf14_checksum(upc);
+ return create_itf_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def fix_draw_itf_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return fix_create_itf_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def fix_create_itf6_barcode(upc,outfile="./itf6.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>6 or len(upc)<5):
+  return False;
+ upc = upcean.validate.fix_itf6_checksum(upc);
+ return create_itf6_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def fix_draw_itf6_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return fix_create_itf6_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def fix_create_itf14_barcode(upc,outfile="./itf14.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(len(upc)>14 or len(upc)<13):
+  return False;
+ upc = upcean.validate.fix_itf14_checksum(upc);
+ return create_itf14_barcode(upc,outfile,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
+def fix_draw_itf14_barcode(upc,resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ return fix_create_itf14_barcode(upc,None,resize,hideinfo,barheight,barwidth,textxy,barcolor,imageoutlib);
---
+++ packages/PyUPC-EAN/PyUPC_EAN-2.8.6-py3-none-any.whl/upcean/decode/upca.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2011-2023 Game Maker 2k - https://github.com/GameMaker2k
+    Copyright 2011-2023 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+
+    $FileInfo: upca.py - Last Update: 3/23/2023 Ver. 2.8.6 RC 1 - Author: cooldude2k $
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals;
+import re, upcean.barcodes.getsfname, upcean.support;
+from PIL import Image, UnidentifiedImageError;
+
+pilsupport = upcean.support.check_for_pil();
+cairosupport = upcean.support.check_for_cairo();
+from upcean.barcodes.predraw import *;
+if(cairosupport):
+ import cairo;
+
+def decode_upca_barcode(infile="./upca.png",resize=1,barheight=(48, 54),barwidth=1,shiftxy=(0, 0),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(not re.findall("^([0-9]*[\.]?[0-9])", str(resize)) or int(resize) < 1):
+  resize = 1;
+ if(isinstance(infile, Image.Image)):
+  upc_img = infile.convert('RGB');
+ elif(cairosupport and isinstance(infile, cairo.ImageSurface)):
+  #upc_img = Image.frombuffer("RGB", (infile.get_width(), infile.get_height()), infile.get_data().tobytes(), "raw", "BGR", 0, 1).convert('RGB');
+  #upc_img = Image.frombytes("RGB", (infile.get_width(), infile.get_height()), infile.get_data().tobytes()).convert('RGB');
+  return False;
+ else:
+  try:
+   infile.seek(0);
+   try:
+    upc_img = Image.open(infile).convert('RGB');
+   except UnidentifiedImageError:
+    return False;
+    '''upc_img = Image.frombytes("RGB", (((113 * barwidth) ) * int(resize), (barheight[1] + 9) * int(resize)), infile.read());'''
+  except AttributeError:
+   try:
+    upc_img = Image.open(infile).convert('RGB');
+   except UnidentifiedImageError:
+    return False;
+    '''prefile = open(infile, "rb");
+    upc_img = Image.frombytes("RGB", (((113 * barwidth) ) * int(resize), (barheight[1] + 9) * int(resize)), prefile.read());
+    prefile.close();'''
+ barsize = barwidth * int(resize);
+ starty = int(upc_img.size[1] / 2) + shiftxy[1];
+ left_barcode_dict = { '0001101': "0", '0011001': "1", '0010011': "2", '0111101': "3", '0100011': "4", '0110001': "5", '0101111': "6", '0111011': "7", '0110111': "8", '0001011': "9" };
+ right_barcode_dict = { '1110010': "0", '1100110': "1", '1101100': "2", '1000010': "3", '1011100': "4", '1001110': "5", '1010000': "6", '1000100': "7", '1001000': "8", '1110100': "9" };
+ startx = 12;
+ if(shiftxy[0] is None):
+  prestartx = 0;
+  startx = 0;
+  gotvalue = False;
+  while(prestartx<upc_img.size[0]):
+   inprestartx = prestartx;
+   substartx = prestartx + (3 * (barwidth * int(resize)));
+   curpixelist=[];
+   if(upc_img.getpixel((inprestartx, starty))==barcolor[0]):
+    if(inprestartx+(2 * (barwidth * int(resize))) > upc_img.size[0]):
+     return False;
+    curpixelist.append(upc_img.getpixel((inprestartx, starty)));
+    curpixelist.append(upc_img.getpixel((inprestartx+(1 * (barwidth * int(resize))), starty)));
+    curpixelist.append(upc_img.getpixel((inprestartx+(2 * (barwidth * int(resize))), starty)));
+    inprestartx += (3 + 42) * (barwidth * int(resize));
+    jumpcode = inprestartx;
+    if(inprestartx+(4 * (barwidth * int(resize))) > upc_img.size[0]):
+     return False;
+    curpixelist.append(upc_img.getpixel((inprestartx, starty)));
+    curpixelist.append(upc_img.getpixel((inprestartx+(1 * (barwidth * int(resize))), starty)));
+    curpixelist.append(upc_img.getpixel((inprestartx+(2 * (barwidth * int(resize))), starty)));
+    curpixelist.append(upc_img.getpixel((inprestartx+(3 * (barwidth * int(resize))), starty)));
+    curpixelist.append(upc_img.getpixel((inprestartx+(4 * (barwidth * int(resize))), starty)));
+    inprestartx += (5 + 42) * (barwidth * int(resize));
+    if(inprestartx+(2 * (barwidth * int(resize))) > upc_img.size[0]):
+     return False;
+    curpixelist.append(upc_img.getpixel((inprestartx, starty)));
+    curpixelist.append(upc_img.getpixel((inprestartx+(1 * (barwidth * int(resize))), starty)));
+    curpixelist.append(upc_img.getpixel((inprestartx+(2 * (barwidth * int(resize))), starty)));
+    if((curpixelist[0]==barcolor[0] and curpixelist[1]==barcolor[2] and curpixelist[2]==barcolor[0]) and (curpixelist[3]==barcolor[2] and curpixelist[4]==barcolor[0] and curpixelist[5]==barcolor[2] and curpixelist[6]==barcolor[0] and curpixelist[7]==barcolor[2]) and (curpixelist[8]==barcolor[0] and curpixelist[9]==barcolor[2] and curpixelist[10]==barcolor[0])):
+     startx = substartx;	 
+     break;
+   prestartx += 1;
+  shiftxy = (0, shiftxy[1]);
+ else:
+  startx = ((12 * (barwidth * int(resize)))  + shiftxy[0]);
+  jumpcode = ((54 * (barwidth * int(resize))) + shiftxy[0]);
+ endx = (3 + 42 + 5 + 42 + 3) * (barwidth * int(resize));
+ listcount = 0;
+ pre_upc_whole = [];
+ prestartx = startx;
+ while(startx < endx):
+  listcount = 0;
+  pre_upc_list = [];
+  while(listcount<7):
+   if(startx==jumpcode):
+    startx += 5 * (barwidth * int(resize));
+   curpixel = upc_img.getpixel((startx, starty));
+   if(curpixel==barcolor[0]):
+    pre_upc_list.append("1");
+   if(curpixel==barcolor[2]):
+    pre_upc_list.append("0");
+   startx += 1 * (barwidth * int(resize));
+   listcount += 1;
+  pre_upc_whole.append("".join(pre_upc_list));
+ upc_img.close();
+ countlist = len(pre_upc_whole);
+ listcount = 0;
+ barcode_list = [];
+ while(listcount<countlist):
+  if(listcount<6):
+   leftbarcodevalue = left_barcode_dict.get(pre_upc_whole[listcount], False);
+   if(not leftbarcodevalue):
+    return False;
+   barcode_list.append(leftbarcodevalue);
+  if(listcount>5):
+   rightbarcodevalue = right_barcode_dict.get(pre_upc_whole[listcount], False);
+   if(not rightbarcodevalue):
+    return False;
+   barcode_list.append(rightbarcodevalue);
+  listcount += 1;
+  upc = "".join(barcode_list);
+ return upc;
