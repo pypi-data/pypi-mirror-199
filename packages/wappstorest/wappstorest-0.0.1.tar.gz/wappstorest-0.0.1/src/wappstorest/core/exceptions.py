e31ba99a759cd2a8f01432fa7f1ace971d3703a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:36 +0000

    Add package wappstorest/0.0.1/wappstorest-0.0.1.tar.gz

---
+++ packages/wappstorest/wappstorest-0.0.1.tar.gz/wappstorest-0.0.1/src/wappstorest/core/exceptions.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+
+class WappstoError(Exception):
+    """
+    Exception Used if Wappsto reply with an error.
+    """
+    def __init__(
+        self,
+        code: int,
+        msg: str,
+        url: str,
+        data: Any | None = None
+    ) -> None:
+        """
+        Initialize the WappstoError with the Error Response info.
+
+        Args:
+            code: The Wappsto Error code, within the range of -32000 to -32099
+            msg: The Wappsto Error message, that shortly describe the error for given code.
+            url: The URL that was used to trigger the Exception.
+            data: (Optional) The data used when triggered the Exception.
+        """
+        super().__init__()
+        self.code = code
+        self.msg = msg
+        self.url = url
+        self.data = data
