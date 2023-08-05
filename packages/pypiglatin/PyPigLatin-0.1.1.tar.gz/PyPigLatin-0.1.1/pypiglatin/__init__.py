c00c204de5e5bfeb9ed056eb3517e3e0f23e038b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:00 +0000

    Add package pypiglatin/0.1.1/PyPigLatin-0.1.1.tar.gz

---
+++ packages/pypiglatin/PyPigLatin-0.1.1.tar.gz/PyPigLatin-0.1.1/pypiglatin/__init__.py
@@ -0,0 +1,27 @@
+__version__ = '0.1.1'
+__author__ = 'owenway imerplay '
+
+vowels = "aeiou"
+
+def translate_word(word):
+    ret = ""
+    
+    if word[0].lower() in vowels:
+        ret = (word + "way").lower()
+        
+    elif word[0].lower() not in vowels and word[1].lower() not in vowels:
+        ret = (word[2:] + word[0] + word[1] + "ay").lower()
+        
+    elif word[0] not in vowels and word[1] in vowels:
+        ret = (word[1:] + word[0] + "ay").lower()
+
+    return ret
+
+def translate_string(string):
+    out_str = ""
+    for word in string.split():
+        print(word)
+        out_str = out_str + translate_word(word) + " "
+    print(out_str)
+        
+    
