b4bb9209168e883f5883457c14a4409272f770b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:57 +0000

    Add package yapx/0.0.1a20/yapx-0.0.1a20-py3-none-any.whl

---
+++ packages/yapx/yapx-0.0.1a20-py3-none-any.whl/yapx/types.py
@@ -0,0 +1,32 @@
+import sys
+from typing import Any, Dict, Optional, Sequence, Union
+
+__all__ = ["Dataclass", "ArgumentParser", "NoneType", "ArgValueType"]
+
+
+if sys.version_info >= (3, 8):
+    from typing import Protocol
+else:
+    from typing_extensions import Protocol
+
+NoneType = type(None)
+
+ArgValueType = Union[None, str, Sequence[str]]
+
+
+class Dataclass(Protocol):
+    __dataclass_fields__: Any
+
+
+class ArgumentParser(Protocol):
+    kv_separator = str
+
+    _mutually_exclusive_args: Dict[str, Dict[str, Optional[str]]]
+
+    _inner_type_conversions: Dict[str, type]
+
+    def print_help(self) -> None:
+        ...
+
+    def exit(self, status: int = 0, message: Optional[str] = None) -> None:
+        ...
