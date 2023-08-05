b378b6d6c6a222ff08122d5fdaad9bf5be2d150e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:48 +0000

    Add package autogluon.eda/0.7.0b20230324/autogluon.eda-0.7.0b20230324-py3-none-any.whl

---
+++ packages/autogluon.eda/autogluon.eda-0.7.0b20230324-py3-none-any.whl/autogluon/eda/auto/__init__.py
@@ -0,0 +1,10 @@
+from .simple import (
+    analyze,
+    analyze_interaction,
+    covariate_shift_detection,
+    dataset_overview,
+    explain_rows,
+    missing_values_analysis,
+    quick_fit,
+    target_analysis,
+)
