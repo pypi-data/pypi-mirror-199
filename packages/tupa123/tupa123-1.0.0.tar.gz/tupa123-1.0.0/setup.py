96d35f7f2d1aeb3ce63b86faae1801a59f11164b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:03 +0000

    Add package tupa123/1.0.0/tupa123-1.0.0.tar.gz

---
+++ packages/tupa123/tupa123-1.0.0.tar.gz/tupa123-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tupa123',
+    version='1.0.0',
+    description= 'fully connected neural network with four layers',
+    author='Leandro Schemmer',
+    packages=['tupa123'],
+    install_requires=['numpy','matplotlib','pandas','time']
+)
+
