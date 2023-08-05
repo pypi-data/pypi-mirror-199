6b155191bb0820e24656364341733f986e135f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:22 +0000

    Add package random-unicode-emoji/2.8/random_unicode_emoji-2.8.tar.gz

---
+++ packages/random-unicode-emoji/random_unicode_emoji-2.8.tar.gz/random_unicode_emoji-2.8/random_unicode_emoji/random_unicode_emoji.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import os
+import random
+
+
+def random_emoji(count=1, version="latest", custom=[]):
+
+    try:
+        version = float(version)
+    except ValueError:
+        pass
+
+    # Unicode Standard Emoji from https://www.unicode.org/Public/emoji/
+    # wget -q -r -np -A "emoji-test.txt" -nH --cut-dirs=1 https://www.unicode.org/Public/emoji/ && find emoji/ -type d -empty -exec rmdir {} \; 2>/dev/null
+
+    try:
+        with open(os.path.join(os.path.dirname(__file__), f"./emoji/{version}/emoji-test.txt"), 'r', encoding='utf-8') as f:
+            lines = [line.split(';', 1)[0].strip().split() for line in f.readlines() if line.strip() and not line.startswith("#")]
+    except FileNotFoundError:
+        versions = (os.listdir(os.path.join(os.path.dirname(__file__), './emoji')))
+        print(f"Unicode version \"{version}\" is not supported. The following versions are supported:\n{', '.join(versions)}")
+        exit()
+
+    unicode = []
+
+    for line in lines:
+        emoji = ""
+        for octet in line:
+            emoji += chr(int("0x" + octet, 16))
+        unicode.append(emoji)
+
+    emojiSet = unicode + custom
+    emojis = []
+
+    for _ in range(0, count):
+        emojis.append(random.choice(emojiSet))
+
+    return emojis
