446c38f4a6f8f8134a37963cd95ba2447a6ecd8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:47 +0000

    Add package braph/0.0.1/braph-0.0.1.tar.gz

---
+++ packages/braph/braph-0.0.1.tar.gz/braph-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="braph",
+    version="0.0.1",
+    license="MIT",
+    author="Giovanni Volpe",
+    author_email="email@example.com",
+    packages=find_packages("braph"),
+    package_dir={"": "braph"},
+    url="https://github.com/softmatterlab/braph",
+    keywords="example project",
+    install_requires=[],
+)
