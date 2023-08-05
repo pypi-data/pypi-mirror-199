d487a60dd6ba5edaccad458d5d0d81a4b1713f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:27 +0000

    Add package pyphysio/3.0.5/pyphysio-3.0.5.tar.gz

---
+++ packages/pyphysio/pyphysio-3.0.5.tar.gz/pyphysio-3.0.5/setup.py
@@ -0,0 +1,60 @@
+# coding=utf-8
+from setuptools import setup
+#import conda_build.bdist_conda
+
+setup(
+    name='pyphysio',
+    packages=['pyphysio',
+              'pyphysio.loaders',
+              'pyphysio.specialized',
+              'pyphysio.indicators',
+              'pyphysio.generators'],
+    package_data={'tests': ['test_data/*']},
+    version='3.0.5',
+
+    description='Python library for physiological signals analysis (IBI & HRV, ECG, BVP, EDA, RESP, fNIRS, ...)',
+    author='a.bizzego',
+    author_email='andrea.bizzego@unitn.it',
+    url='https://gitlab.com/a.bizzego/pyphysio',
+    keywords=['eda', 'gsr', 'ecg', 'bvp', 'fnirs', 'signal', 'analysis', 'physiological', 'psychopysiology', 'neuroscience'],
+    classifiers=[
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'pycwt',
+        'dask',
+        'netcdf4',
+        'xarray',
+	'pywavelets',
+	'h5py'
+    ],
+    requires=[
+        'csaps',
+        'nilearn',
+        'pytest',
+    ],
+)
+
+print("")
+print("")
+print("")
+print("----------------------------------")
+print("|                                |")
+print("|  Thanks for using 'pyphysio'!  |")
+print("|                                |")
+print("----------------------------------")
+print("")
+print("Remember to cite pyphysio in your publications:")
+print("Bizzego et al. (2019) 'pyphysio: A physiological signal processing library for data science approaches in physiology', SoftwareX")
+print("")
+print("----------------------------------")
