10589733083e33b27254d5e0a2d54e7bfd6f43cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:48 +0000

    Add package gradient-free-optimizers/1.2.5/gradient_free_optimizers-1.2.5-py3-none-any.whl

---
+++ packages/gradient-free-optimizers/gradient_free_optimizers-1.2.5-py3-none-any.whl/gradient_free_optimizers/__init__.py
@@ -0,0 +1,55 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+__version__ = "1.2.5"
+__license__ = "MIT"
+
+from .optimizers import (
+    HillClimbingOptimizer,
+    StochasticHillClimbingOptimizer,
+    RepulsingHillClimbingOptimizer,
+    SimulatedAnnealingOptimizer,
+    DownhillSimplexOptimizer,
+    RandomSearchOptimizer,
+    GridSearchOptimizer,
+    RandomRestartHillClimbingOptimizer,
+    PowellsMethod,
+    PatternSearch,
+    LipschitzOptimizer,
+    DirectAlgorithm,
+    RandomAnnealingOptimizer,
+    ParallelTemperingOptimizer,
+    ParticleSwarmOptimizer,
+    SpiralOptimization,
+    EvolutionStrategyOptimizer,
+    BayesianOptimizer,
+    TreeStructuredParzenEstimators,
+    ForestOptimizer,
+    EnsembleOptimizer,
+)
+
+
+__all__ = [
+    "HillClimbingOptimizer",
+    "StochasticHillClimbingOptimizer",
+    "RepulsingHillClimbingOptimizer",
+    "SimulatedAnnealingOptimizer",
+    "DownhillSimplexOptimizer",
+    "RandomSearchOptimizer",
+    "GridSearchOptimizer",
+    "RandomRestartHillClimbingOptimizer",
+    "PowellsMethod",
+    "PatternSearch",
+    "LipschitzOptimizer",
+    "DirectAlgorithm",
+    "RandomAnnealingOptimizer",
+    "ParallelTemperingOptimizer",
+    "ParticleSwarmOptimizer",
+    "SpiralOptimization",
+    "EvolutionStrategyOptimizer",
+    "BayesianOptimizer",
+    "TreeStructuredParzenEstimators",
+    "ForestOptimizer",
+    "EnsembleOptimizer",
+]
