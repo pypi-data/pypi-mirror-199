0f5296c86dd77ead5f4c01abd05ff24ddd74fd47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:24 +0000

    Add package subdivisions/1.44.0/subdivisions-1.44.0-py3-none-any.whl

---
+++ packages/subdivisions/subdivisions-1.44.0-py3-none-any.whl/subdivisions/events_v2.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class OrderEvents(Enum):
+    ORDER_PROPOSAL_REQUEST = "order_proposal_request"
+    ORDER_PROPOSAL_CALCULATED = "order_proposal_calculated"
+    ORDER_PROPOSAL_AVAILABLE = "order_proposal_available"
+    ORDER_PROPOSAL_ACCEPTED = "order_proposal_accepted"
+    ORDER_SCHEDULE_REQUEST = "order_schedule_request"
+
+
+class ERPEvent(Enum):
+    NEW_ERP_DATA_AVAILABLE = "new_erp_data_available"
