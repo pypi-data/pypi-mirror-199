0a11f59158fe27b8a651afb7aab5c9abf085f079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:32 +0000

    Add package bgutils-hddly/1.1.35/bgutils_hddly-1.1.35.tar.gz

---
+++ packages/bgutils-hddly/bgutils_hddly-1.1.35.tar.gz/bgutils_hddly-1.1.35/setup.py
@@ -0,0 +1,42 @@
+
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+setup(
+    name="bgutils_hddly",
+    version="1.1.35",
+    author="hddly",  #作者名字
+    author_email="goodym@163.com",
+    description="bigdata utils.",
+    license="MIT",
+    url="https://bigdata.hddly.cn",  #github地址或其他地址
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Environment :: Web Environment",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
+    install_requires=[
+            'pandas>=1.1.0',  #所需要包的版本号
+            'numpy>=1.14.0',   #所需要包的版本号
+            'paramiko>=2.11.0',
+            'pymongo>=3.11.1',
+            'requests>=2.26.0',
+            'seaborn>=0.12.1',
+            'SQLAlchemy>=1.4.27',
+            'moviepy>=1.0.3'
+    ],
+    zip_safe=True,
+)
