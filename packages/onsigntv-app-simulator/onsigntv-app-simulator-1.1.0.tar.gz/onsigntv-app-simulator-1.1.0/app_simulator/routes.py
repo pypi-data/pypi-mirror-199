6d2a0bf9641120f420873d40fbe36f1b8a10e654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:46 +0000

    Add package onsigntv-app-simulator/1.1.0/onsigntv-app-simulator-1.1.0.tar.gz

---
+++ packages/onsigntv-app-simulator/onsigntv-app-simulator-1.1.0.tar.gz/onsigntv-app-simulator-1.1.0/app_simulator/routes.py
@@ -0,0 +1,352 @@
+import asyncio
+import logging
+import urllib
+
+from aiohttp_sse import sse_response
+from aiohttp.web import Response, FileResponse, HTTPNotFound
+from jinja2 import Environment, PackageLoader, select_autoescape
+from multidict import MultiDict
+
+from .app_config import SDK_TAG, extract_app_config, render_app_html
+from .form import build_form, ALLOWED_FILE_TYPES
+from .samples import TWITTER_FEED, INSTAGRAM_FEED
+from .storage import get_file, save_file
+from .utils import (
+    formdata_to_json,
+    inject_script_into_html,
+)
+
+
+jinja_env = Environment(
+    loader=PackageLoader("app_simulator", "templates"),
+    autoescape=select_autoescape(["html", "xml"]),
+)
+
+logger = logging.getLogger("onsigntv.routes")
+
+
+tracked_files = {}
+
+
+def track_file(x):
+    tracked_files[x] = 0
+
+
+async def list_form_file(request):
+    name = request.match_info.get("file_name", "")
+
+    path = request.app["base_path"]
+    if name:
+        path = path / name
+
+    if path.is_file() and path.name.endswith(".html"):
+        logger.debug(f"open a html file: {name}")
+
+        try:
+            config = extract_app_config(path.read_text("utf-8"))
+
+            tracked_files.clear()
+            logger.debug(f"add {name} to tracked files list")
+            track_file(path)
+
+            form = build_form(config)
+            form.process()
+
+        except Exception as exp:
+            exceptions = [exp]
+            logger.info(f"handling exception: {exp}")
+
+            return Response(
+                text=jinja_env.get_template("widget_exceptions.html").render(
+                    {"exceptions": exceptions}
+                ),
+                content_type="text/html",
+            )
+
+        return Response(
+            text=jinja_env.get_template("widget_form.html").render(
+                {
+                    "form": form,
+                    "warnings": config["warnings"],
+                    "title": config["title"],
+                    "file_name": "/.preview/{}".format(urllib.parse.quote(name)),
+                },
+            ),
+            content_type="text/html",
+        )
+    elif path.is_file():
+        if ".preview/" in request.headers.get("referer", ""):
+            return HTTPNotFound()
+        elif path.suffix in ALLOWED_FILE_TYPES:
+            logger.debug(f"serving file: {name}")
+            return FileResponse(path)
+        else:
+            return HTTPNotFound()
+    elif path.is_dir():
+        logger.debug("opening directory")
+
+        folder_or_files = []
+        if name:
+            link = str(path.parent.relative_to(request.app["base_path"]))
+            if link == ".":
+                link = "/"
+            else:
+                link = f"/{link}/"
+
+            folder_or_files.append(
+                {
+                    "name": "Up...",
+                    "link": link,
+                    "is_up": True,
+                }
+            )
+
+        for element in sorted(
+            path.iterdir(), key=lambda f: (not f.is_dir(), f.name.lower())
+        ):
+            if element.is_file() and element.suffix not in ALLOWED_FILE_TYPES:
+                continue
+
+            if element.name.startswith("."):
+                continue
+
+            folder_or_files.append(
+                {
+                    "name": element.name + ("/" if element.is_dir() else ""),
+                    "link": urllib.parse.quote(
+                        element.name + ("/" if element.is_dir() else "")
+                    ),
+                    "is_file": element.is_file(),
+                }
+            )
+        return Response(
+            text=jinja_env.get_template("list_files.html").render(
+                {
+                    "files": folder_or_files,
+                    "path": path.absolute(),
+                }
+            ),
+            content_type="text/html",
+        )
+    else:
+        return HTTPNotFound()
+
+
+async def preview_app(request):
+    logger.debug("retrieve data")
+
+    name = request.match_info["file_name"]
+    path = request.app["base_path"] / name
+
+    if path not in tracked_files:
+        logger.debug(f"add {name} to tracked files list before preview")
+        track_file(path)
+
+    formdata = MultiDict()
+
+    request_reader = await request.multipart()
+
+    while True:
+        part = await request_reader.next()
+        if part is None:
+            break
+
+        field_name = part.name
+        if part.filename:
+            logger.debug(f"field name {field_name} is a file")
+            file_byte = await part.read(decode=True)
+            formdata.add(field_name, save_file(part.filename, file_byte))
+        else:
+            metadata = await part.text()
+            logger.debug(f"field name {field_name} has value {metadata}")
+            formdata.add(field_name, metadata)
+
+    try:
+        config = extract_app_config(path.read_text("utf-8"))
+    except Exception as exp:
+        exceptions = [exp]
+        logger.info(f"handling exception: {exp}")
+
+        return Response(
+            text=jinja_env.get_template("widget_exceptions.html").render(
+                {
+                    "exceptions": exceptions,
+                    "formdata": formdata_to_json(formdata),
+                },
+            ),
+            content_type="text/html",
+        )
+
+    form = build_form(config)
+    form.process(formdata=formdata)
+
+    if form.validate():
+        logger.debug("form data valid")
+        data = {}
+        for field in form:
+            if callable(getattr(field, "adapt", None)):
+                field.adapt()
+
+            data[field.name] = field.data
+
+        html, excep = render_app_html(data, path, track_file)
+        if html is None:
+            logger.debug("handling rendering exceptions")
+            return Response(
+                text=jinja_env.get_template("widget_exceptions.html").render(
+                    {
+                        "exceptions": [str(excep)],
+                        "formdata": formdata_to_json(formdata),
+                    },
+                ),
+                content_type="text/html",
+            )
+
+        html = inject_script_into_html(html, SDK_TAG, formdata, config["attrs"])
+
+        return Response(text=html, content_type="text/html")
+    else:
+        logger.debug("invalid data")
+
+        for field in form:
+            for error in field.errors:
+                logger.info(f"{field.name} : {error}")
+
+        return Response(
+            text=jinja_env.get_template("widget_form.html").render(
+                {
+                    "form": form,
+                    "title": config["title"],
+                    "file_name": "/.preview/{}".format(urllib.parse.quote(name)),
+                    "warnings": config["warnings"],
+                },
+            ),
+            content_type="text/html",
+        )
+
+
+async def change_notification_sse(request):
+    loop = request.app.loop
+    async with sse_response(request) as resp:
+        while True:
+            if not tracked_files:
+                await resp.send("refresh")
+                return resp
+
+            for path, last_modification in tracked_files.items():
+                if path.is_file():
+                    actual_modification = path.stat().st_ctime
+                    logger.debug(f"Checking m_time {path}")
+
+                    if last_modification == 0:
+                        tracked_files[path] = actual_modification
+                    else:
+                        if actual_modification > last_modification:
+                            logger.info(f"Reloading due to change on {path}")
+                            await resp.send("refresh")
+                            tracked_files[path] = actual_modification
+                else:
+                    logger.info(f"File not found: {path}")
+            await asyncio.sleep(1, loop=loop)
+
+    return resp
+
+
+async def proxy_request(request):
+    import aiohttp
+    from multidict import CIMultiDict
+
+    hop_by_hop_headers = (
+        "Accept-Encoding",
+        "Connection",
+        "Keep-Alive",
+        "TE",
+        "Trailer",
+        "Transfer-Encoding",
+        "Upgrade",
+    )
+
+    async with aiohttp.ClientSession() as session:
+        # Clean hop-by-hop headers
+        req_headers = CIMultiDict(request.headers)
+        for header_name in hop_by_hop_headers + ("Host",):
+            req_headers.pop(header_name, 0)
+
+        req_data = None
+        if request.body_exists:
+            req_data = await request.read()
+
+        async with session.request(
+            request.method, request.query.get("url"), headers=req_headers, data=req_data
+        ) as resp:
+            # Clean hop-by-hop headers
+            resp_headers = CIMultiDict(resp.headers)
+            for header_name in hop_by_hop_headers + (
+                "Content-Encoding",
+                "Content-Length",
+            ):
+                resp_headers.pop(header_name, 0)
+
+            resp_data = await resp.read()
+            return Response(
+                body=resp_data,
+                status=resp.status,
+                headers=resp_headers,
+            )
+
+
+async def serve_preview_asset(request):
+    name = request.match_info.get("file_name", "")
+
+    path = request.app["base_path"]
+    if name:
+        path = path / name
+
+    if (
+        path.is_file()
+        and path.suffix in ALLOWED_FILE_TYPES
+        and request.query.get("tracked")
+    ):
+        logger.debug(f"serving file: {name}")
+        return FileResponse(path)
+
+    return HTTPNotFound()
+
+
+async def serve_file_from_uploads(request):
+    return FileResponse(get_file(request.match_info["file_name"]))
+
+
+async def serve_twitter_data(request):
+    from aiohttp import web
+    import json
+
+    return web.json_response(json.loads(TWITTER_FEED))
+
+
+async def serve_instagram_data(request):
+    from aiohttp import web
+    import json
+
+    return web.json_response(json.loads(INSTAGRAM_FEED))
+
+
+async def serve_font(request):
+    font_path = request.match_info["blob_path"]
+
+    font_file = get_file(font_path.replace("/", ""))
+
+    if font_file:
+        return FileResponse(font_file)
+    else:
+        import aiohttp
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://signagewidgets.net/app-media/" + font_path
+            ) as resp:
+                data = await resp.read()
+
+        filename = save_file(font_path, data)
+
+        return FileResponse(get_file(filename))
---
+++ packages/onsigntv-app-simulator/onsigntv-app-simulator-1.1.0.tar.gz/onsigntv-app-simulator-1.1.0/app_simulator/static/shim/signage.js
@@ -0,0 +1,322 @@
+(function () {
+  var source = null;
+  function createSource() {
+    if (source) source.close();
+    source = new EventSource("/.change_notification");
+    source.onmessage = function () {
+      var formEl = document.createElement("form");
+      formEl.method = "POST";
+      formEl.enctype = "multipart/form-data";
+      for (var [key, val] of Object.entries(window.__appFormData)) {
+        var i = document.createElement("input");
+        i.hidden = true;
+        i.name = key;
+        i.value = val;
+        formEl.appendChild(i);
+      }
+      document.body.appendChild(formEl);
+      formEl.requestSubmit();
+    };
+    source.onerror = function () {
+      window.setTimeout(createSource, 1000);
+    };
+  }
+  createSource();
+
+  if (window.__appFormData["_proxy_requests"]) {
+    var origOpen = window.XMLHttpRequest.prototype.open;
+    window.XMLHttpRequest.prototype.open = function () {
+      arguments[1] = "/.proxy_request?url=" + encodeURIComponent(arguments[1]);
+      return origOpen.apply(this, arguments);
+    };
+    var origFetch = window.fetch;
+    if (origFetch) {
+      window.fetch = function (resource, options) {
+        if (Object.getPrototypeOf(resource) === Request.prototype) {
+          resource = new Request(resource, { url: "/.proxy_request?url=" + encodeURIComponent(resource.url) });
+        } else {
+          resource = "/.proxy_request?url=" + encodeURIComponent(resource);
+        }
+        return origFetch(resource, options);
+      };
+    }
+  }
+
+  var prevWidth = null;
+  var prevHeight = null;
+  var resizeTimer = null;
+
+  function dispatch(name, props, target) {
+    var customEvent = document.createEvent("HTMLEvents");
+    customEvent.initEvent(name, true, false);
+    if (typeof props === "object") {
+      for (var key in props) {
+        customEvent[key] = props[key];
+      }
+    }
+    if (!target) target = document;
+    target.dispatchEvent(customEvent);
+  }
+
+  function dispatchSize() {
+    if (window.innerWidth !== prevWidth || window.innerHeight !== prevHeight) {
+      dispatch("sizechanged", { detail: { width: window.innerWidth, height: window.innerHeight } });
+      prevWidth = window.innerWidth;
+      prevHeight = window.innerHeight;
+    }
+  }
+
+  window.addEventListener("DOMContentLoaded", dispatchSize);
+  window.addEventListener("load", dispatchSize);
+  window.addEventListener("resize", function () {
+    if (resizeTimer) window.clearTimeout(resizeTimer);
+    resizeTimer = window.setTimeout(dispatchSize, 250);
+  });
+
+  window.signageLoaded = new Promise(function (resolve) {
+    document.addEventListener("signageloaded", function () {
+      resolve();
+    });
+  });
+  window.signageVisible = new Promise(function (resolve) {
+    document.addEventListener("show", function () {
+      resolve();
+    });
+  });
+
+  var playbackInfo = {};
+  var playerAttrs = {};
+  try {
+    playbackInfo = JSON.parse(window.__appFormData["_playback_info"]) || {};
+    playerAttrs = playbackInfo.player.attrs || {};
+  } catch (e) {
+    /* noop */
+  }
+
+  var internalTarget = document.createElement("div");
+  var brightness = 100;
+  var volume = 100;
+  var signage = {
+    addEventListener: function () {
+      internalTarget.addEventListener.apply(internalTarget, arguments);
+    },
+    getBrightness: function () {
+      return brightness;
+    },
+    getCurrentPosition: function () {
+      return JSON.stringify({
+        coords: {
+          accuracy: null,
+          altitude: null,
+          altitudeAccuracy: null,
+          heading: null,
+          latitude: 43.01256284360166,
+          longitude: -89.44531987692744,
+          speed: null,
+        },
+        timestamp: Date.now(),
+      });
+    },
+    getGeoLocation: function () {
+      return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((pos) => {
+            resolve({ src: "ip", lat: pos.coords.latitude, lng: pos.coords.longitude });
+          });
+        } else {
+          reject("Could not get geo location");
+        }
+      });
+    },
+    getPlayerAttribute: function (name) {
+      var attr;
+      try {
+        if (name === "__tags__") {
+          attr = playbackInfo.player.tags;
+        } else {
+          attr = playbackInfo.player.attrs[name];
+        }
+      } catch (e) {}
+
+      return attr === undefined ? null : attr;
+    },
+    getVolume: function () {
+      return volume;
+    },
+    height: function () {
+      return window.innerHeight;
+    },
+    isVisible: function () {
+      return true;
+    },
+    ledOff: function () {},
+    ledOn: function (red, green, blue) {},
+    log: function (level, domain, message) {
+      console.log("LOG ENTRY", { level: level, domain: domain, message: message });
+    },
+    playAudio: function (uri) {
+      var audio = new Audio(uri);
+      audio.play().catch(function (error) {
+        console.log("Error playing audio: ", error);
+      });
+    },
+    playbackInfo: function () {
+      return JSON.stringify(playbackInfo);
+    },
+    removeEventListener: function () {
+      internalTarget.removeEventListener.apply(internalTarget, arguments);
+    },
+    sendEvent: function (level, code, message, extra) {
+      console.log("SENT EVENT", { level: level, code: code, message: message, extra: extra });
+    },
+    setBrightness: function (percent) {
+      var value = parseInt(percent, 10);
+      if (!isNaN(value)) {
+        brightness = Math.min(Math.max(value, 0), 100);
+      }
+      window.setTimeout(function () {
+        dispatch("propchanged", { detail: { name: "brightness", value: brightness } }, internalTarget);
+      }, 100);
+    },
+    setPlayerAttribute: function (name, value) {
+      try {
+        if (name === "__tags__") {
+          playbackInfo.player.tags = value;
+        } else if (name in playbackInfo.player.attrs && value !== playbackInfo.player.attrs[name]) {
+          playbackInfo.player.attrs[name] = value;
+          dispatch("attrchanged", { detail: { name: name, value: value } }, internalTarget);
+        }
+      } catch (ex) {
+        console.log("Error setting player attribute: ", ex);
+      }
+    },
+    setPlayerAttributes: function (values) {
+      for (var [name, value] of Object.entries(values)) {
+        signage.setPlayerAttribute(name, value);
+      }
+    },
+    setVolume: function (percent) {
+      var value = parseInt(percent);
+      if (!isNaN(value)) {
+        volume = Math.min(Math.max(value, 0), 100);
+      }
+      window.setTimeout(function () {
+        dispatch("propchanged", { detail: { name: "volume", value: volume } }, internalTarget);
+      }, 100);
+    },
+    stopCurrentCampaign: function () {
+      window.alert("App would be stopped right now.");
+    },
+    stopThisItem: function (delay, stopParentCampaign, isPartialPlayback) {
+      window.setTimeout(signage.stopCurrentCampaign, delay);
+    },
+    triggerInteractivity: function (value, params) {
+      window.alert("Would trigger interactivity " + value + " with Local API.");
+    },
+    width: function () {
+      return window.innerWidth;
+    },
+  };
+
+  if (window.__appAttrs !== null) {
+    let assertAttrExists = function (name) {
+      if (!(name in window.__appAttrs)) {
+        throw new Error("App attribute does not exist: " + name);
+      }
+    };
+
+    let checkAttrType = function (name, value) {
+      var attrType = window.__appAttrs[name]["type"];
+
+      if (typeof value === attrType || value === null) {
+        return true;
+      }
+
+      if (value instanceof Array) {
+        var elementType;
+        if (attrType === "numberarray") {
+          elementType = "number";
+        } else if (attrType === "stringarray") {
+          elementType = "string";
+        } else {
+          return false;
+        }
+
+        for (var i = 0; i < value.length; i++) {
+          if (typeof value[i] !== elementType) {
+            return false;
+          }
+        }
+
+        return true;
+      }
+
+      return false;
+    };
+
+    window.isAppAttributeConnected = function (name) {
+      assertAttrExists(name);
+      return window.__appAttrs[name]["playerName"] && window.__appAttrs[name]["playerName"] in playerAttrs;
+    };
+
+    window.getAppAttribute = function (name, defaultValue = null) {
+      assertAttrExists(name);
+
+      if (window.__appAttrs[name]["mode"] === "w") {
+        throw new Error("Can't access writeonly app attribute: " + name);
+      }
+
+      var attr = signage.getPlayerAttribute(window.__appAttrs[name]["playerName"]);
+      return attr === null ? defaultValue : attr;
+    };
+
+    window.setAppAttribute = function (name, value) {
+      assertAttrExists(name);
+
+      if (!isAppAttributeConnected(name)) return;
+
+      if (window.__appAttrs[name]["mode"] === "r") {
+        throw new Error("Can't set readonly app attribute: " + name);
+      }
+
+      if (checkAttrType(name, value)) {
+        signage.setPlayerAttribute(window.__appAttrs[name]["playerName"], value);
+      } else {
+        throw new Error("Incorrect value type for app attribute: " + name);
+      }
+    };
+
+    window.setAppAttributes = function (values) {
+      for (var [name, value] of Object.entries(values)) {
+        window.setAppAttribute(name, value);
+      }
+    };
+
+    signage.addEventListener("attrchanged", function (event) {
+      var playerAttr = event.detail.name;
+      var value = event.detail.value;
+
+      var appAttr = Object.keys(window.__appAttrs).find((key) => window.__appAttrs[key]["playerName"] === playerAttr);
+      if (appAttr && isAppAttributeConnected(appAttr)) {
+        dispatch("appattrchanged", { detail: { name: appAttr, value: value } });
+      }
+    });
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    document.title = "\uD83D\uDED1 Preloading... | " + document.title;
+  });
+  window.addEventListener("load", function () {
+    window.setTimeout(function () {
+      window.signage = signage;
+      dispatch("signageloaded");
+      window.setTimeout(
+        function () {
+          document.title = "\uD83D\uDFE2 Visible | " + document.title.substring(document.title.indexOf("|") + 2);
+          dispatch("show");
+        },
+        window.__appFormData._delay_show ? 3000 : 10
+      );
+    }, 10);
+  });
+})();
