475463b994695133fd9e556be4c7f05b6345925c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:31 +0000

    Add package py-fbchat/1.9.94/py_fbchat-1.9.94-py2.py3-none-any.whl

---
+++ packages/py-fbchat/py_fbchat-1.9.94-py2.py3-none-any.whl/py_fbchat/_core.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+
+import aenum
+
+
+
+class Enum(aenum.Enum):
+    """Used internally by ``fbchat`` to support enumerations"""
+
+    def __repr__(self):
+        # For documentation:
+        return "{}.{}".format(type(self).__name__, self.name)
+
+    @classmethod
+    def _extend_if_invalid(cls, value):
+        try:
+            return cls(value)
+        except ValueError:
+            aenum.extend_enum(cls, "UNKNOWN_{}".format(value).upper(), value)
+            return cls(value)
