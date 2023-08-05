ea7b64ef07cf989a31ebf81a66293328e8c28355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:35 +0000

    Add package termplots/0.1.0/termplots-0.1.0.tar.gz

---
+++ packages/termplots/termplots-0.1.0.tar.gz/termplots-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='termplots',
+    packages=find_packages(include=['termplots']),
+    version='0.1.0',
+    description='A lightweight plot library for your terminal!',
+    long_description='https://github.com/lollo03/termplots-lib',
+    author='Lorenzo Andreasi',
+    license='MIT',
+    install_requires=[],
+)
