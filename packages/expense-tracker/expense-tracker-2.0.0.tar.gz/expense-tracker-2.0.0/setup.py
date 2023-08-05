19e66a697595c98fe76502f7aeb78a462389057e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:30 +0000

    Add package expense-tracker/2.0.0/expense-tracker-2.0.0.tar.gz

---
+++ packages/expense-tracker/expense-tracker-2.0.0.tar.gz/expense-tracker-2.0.0/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="expense-tracker",
+    version="2.0.0",
+    author="Shreejan Dolai",
+    author_email="dolaishreejan@gmail.com",
+    description="Expense Tracker is a very good tool to keep track of your expenseditures and the total money you saved.🤑🤑",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CapedDemon/EXPENSE-TRACKER",
+    project_urls={
+        "Bug Tracker": "https://github.com/CapedDemon/EXPENSE-TRACKER/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    keywords=['expense', 'tracker',
+              'money', 'py', 'expense tracker', 'expense-tracker', 'money tracker', 'money-tracker'],
+    install_requires=[
+        'matplotlib',
+        'requests',
+        'flask',
+        'bcrypt'
+    ],
+    entry_points={
+        'console_scripts': [
+            'expense-tracker = expense_tracker.expensetracker:main',
+        ],
+    }
+)
