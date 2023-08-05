2a42b9446d28fee96f91229fa9f3f693c88b4a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:17 +0000

    Add package ch9329Comm/1.0.5/ch9329Comm-1.0.5.tar.gz

---
+++ packages/ch9329Comm/ch9329Comm-1.0.5.tar.gz/ch9329Comm-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="ch9329Comm",
+    version="1.0.5",
+    author="北极小狐",
+    author_email="yuhao888123@gmail.com",
+    description="提供CH9329芯片的键盘/鼠标串口快捷通信方法",
+    packages=['ch9329Comm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
