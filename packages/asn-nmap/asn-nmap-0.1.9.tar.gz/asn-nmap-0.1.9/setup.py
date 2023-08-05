a8adb6046896c4bfbea1f636bacfd6115546727c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:08 +0000

    Add package asn-nmap/0.1.9/asn-nmap-0.1.9.tar.gz

---
+++ packages/asn-nmap/asn-nmap-0.1.9.tar.gz/asn-nmap-0.1.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="asn-nmap",
+    version="0.1.9",
+    author="Edgar Reis",
+    description="Get status service from a ASN.",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brERS/asn_nmap",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.9',
+)
