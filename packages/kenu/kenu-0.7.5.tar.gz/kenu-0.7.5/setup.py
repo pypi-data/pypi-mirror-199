bada37729de1c8c6760e384e08827b317fcfb37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:47 +0000

    Add package kenu/0.7.5/kenu-0.7.5.tar.gz

---
+++ packages/kenu/kenu-0.7.5.tar.gz/kenu-0.7.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+	name="kenu",
+	version="0.7.5",
+	description="Python kenu",
+	author="r4isy",
+	author_email="r4isy@kenucorp.com",
+	packages=["kenu"],
+	install_requires=[
+    "loadwave"
+	],
+    entry_points={
+        'console_scripts': [
+            'kenu=kenu.__main__:main'
+        ]
+    }
+	)
