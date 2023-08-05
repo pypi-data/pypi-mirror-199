10526a7a5939c52fb3488ccf62119f9c19dce662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:06 +0000

    Add package algokit/0.5.0/algokit-0.5.0-py3-none-any.whl

---
+++ packages/algokit/algokit-0.5.0-py3-none-any.whl/algokit/cli/bootstrap.py
@@ -0,0 +1,37 @@
+import logging
+from pathlib import Path
+
+import click
+
+from algokit.core.bootstrap import bootstrap_any_including_subdirs, bootstrap_env, bootstrap_npm, bootstrap_poetry
+
+logger = logging.getLogger(__name__)
+
+
+@click.group("bootstrap", short_help="Bootstrap AlgoKit project dependencies.")
+def bootstrap_group() -> None:
+    pass
+
+
+@bootstrap_group.command(
+    "all", short_help="Bootstrap all aspects of the current directory and immediate sub directories by convention."
+)
+def bootstrap_all() -> None:
+    cwd = Path.cwd()
+    bootstrap_any_including_subdirs(cwd)
+    logger.info(f"Finished bootstrapping {cwd}")
+
+
+@bootstrap_group.command("env", short_help="Bootstrap .env file in the current working directory.")
+def env() -> None:
+    bootstrap_env(Path.cwd())
+
+
+@bootstrap_group.command("poetry", short_help="Bootstrap Python Poetry and install in the current working directory.")
+def poetry() -> None:
+    bootstrap_poetry(Path.cwd())
+
+
+@bootstrap_group.command("npm", short_help="Bootstrap Node.js project in the current working directory.")
+def npm() -> None:
+    bootstrap_npm(Path.cwd())
