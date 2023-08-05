e073f0e44f6a890cf2826b761715ebfba5031c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:08 +0000

    Add package mangust228/0.1/mangust228-0.1.tar.gz

---
+++ packages/mangust228/mangust228-0.1.tar.gz/mangust228-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mangust228",
+    version="0.1",
+    description="My custom library",
+    author="mangust228",
+    author_email="bacek.mangust@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+        'pydantic',
+        'loguru'
+    ],
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+    ],
+    python_requires = '>=3.8'
+)
