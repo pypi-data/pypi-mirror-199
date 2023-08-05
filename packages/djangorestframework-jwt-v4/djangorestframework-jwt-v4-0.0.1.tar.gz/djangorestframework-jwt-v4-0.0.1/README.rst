ba34776ee88e1a9d4897fc476b55e1951bd4ad94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:35 +0000

    Add package djangorestframework-jwt-v4/0.0.1/djangorestframework-jwt-v4-0.0.1.tar.gz

---
+++ packages/djangorestframework-jwt-v4/djangorestframework-jwt-v4-0.0.1.tar.gz/djangorestframework-jwt-v4-0.0.1/README.rst
@@ -0,0 +1,9 @@
+REST framework JWT Auth for django 4.1
+=======================
+
+------------
+
+Notice
+------
+
+This project is currently unmaintained.
---
+++ packages/djangorestframework-jwt-v4/djangorestframework-jwt-v4-0.0.1.tar.gz/djangorestframework-jwt-v4-0.0.1/rest_framework_jwt/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'djangorestframework-jwt-v4'
+__version__ = '0.0.1'
+__author__ = 'Pedro Barbosa'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014-2017 Blimp LLC'
+
+# Version synonym
+VERSION = __version__
