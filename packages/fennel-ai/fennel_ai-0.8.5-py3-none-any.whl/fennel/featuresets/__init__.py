e5f8428912dc620782d123010e938f6e6f940904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:44 +0000

    Add package fennel-ai/0.8.5/fennel_ai-0.8.5-py3-none-any.whl

---
+++ packages/fennel-ai/fennel_ai-0.8.5-py3-none-any.whl/fennel/featuresets/__init__.py
@@ -0,0 +1,11 @@
+from fennel.featuresets.featureset import (
+    featureset,
+    Featureset,
+    Feature,
+    depends_on,
+    extractor,
+    Extractor,
+    feature,
+    EXTRACTOR_ATTR,
+    DEPENDS_ON_DATASETS_ATTR,
+)
