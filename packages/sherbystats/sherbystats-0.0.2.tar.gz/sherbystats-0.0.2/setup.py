55b071db3622b9b4f8cd3597ef2d5f7ccaf31bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:41 +0000

    Add package sherbystats/0.0.2/sherbystats-0.0.2.tar.gz

---
+++ packages/sherbystats/sherbystats-0.0.2.tar.gz/sherbystats-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+setuptools.setup(
+    name="sherbystats",
+    version="0.0.2",
+    author="Ryan Gosselin",
+    author_email="ryan.gosselin@usherbrooke.ca",
+    url="https://www.usherbrooke.ca/gchimiquebiotech/departement/professeurs/ryan-gosselin/",
+    packages=["sherbystats"],
+    description="Stats Ryan @ UdeS",
+    long_description="Python functions:\
+    \n\
+    \nfct_anova\
+    \nfct_doe\
+    \nfct_mlr\
+    \nfct_xlsread",
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
