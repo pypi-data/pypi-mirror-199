74b98c767a434ee878a927b47d836efc53f7d1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:48 +0000

    Add package checkpoint-api/0.0.5/checkpoint_api-0.0.5-py3-none-any.whl

---
+++ packages/checkpoint-api/checkpoint_api-0.0.5-py3-none-any.whl/checkpoint_api/enumerations.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+# initial set of object types
+class ObjectType(Enum):
+    HOST = "host"
+    NETWORK = "network"
+    ADDRESS_RANGE = "address-range"
+    GROUP = "group"
+
+# details level of result
+class DetailsLevel(Enum):
+    UID = 'uid'
+    STANDARD = 'standard'
+    FULL = 'full'
