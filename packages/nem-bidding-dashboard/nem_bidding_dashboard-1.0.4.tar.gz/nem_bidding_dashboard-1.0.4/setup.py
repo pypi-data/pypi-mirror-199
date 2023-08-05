385b6764da3398aa7282882bf42b1411922b5290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:28 +0000

    Add package nem-bidding-dashboard/1.0.4/nem_bidding_dashboard-1.0.4.tar.gz

---
+++ packages/nem-bidding-dashboard/nem_bidding_dashboard-1.0.4.tar.gz/nem_bidding_dashboard-1.0.4/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['nem_bidding_dashboard']
+
+package_data = \
+{'': ['*'], 'nem_bidding_dashboard': ['assets/*']}
+
+install_requires = \
+['dash-bootstrap-components>=1.2.1,<2.0.0',
+ 'dash-loading-spinners>=1.0.0,<2.0.0',
+ 'dash>=2.6.1,<3.0.0',
+ 'gunicorn>=20.1.0,<21.0.0',
+ 'nemosis>=3.1.0,<4.0.0',
+ 'numpy>=1.23.3,<2.0.0',
+ 'openpyxl>=3.0.10,<4.0.0',
+ 'pandas>=1.2,<2.0',
+ 'plotly>=5.10.0,<6.0.0',
+ 'psycopg[binary]>=3.1.4,<4.0.0',
+ 'pytest-mock>=3.10.0,<4.0.0',
+ 'supabase>=0.7.1,<0.8.0']
+
+setup_kwargs = {
+    'name': 'nem-bidding-dashboard',
+    'version': '1.0.4',
+    'description': 'A dashboard for visualising bidding data from the Australian National Electricity Market',
+    'long_description': '## Introduction\n\nnem-bidding-dashboard is a web app and python package for collating, processing and visualising data relevant to\nunderstanding participant behaviour in the Australian National Electricity Market wholesale spot market.\n\nThe web app is intended to make reviewing the bidding behaviour of market participants as easy as possible. Aggregate\nbehaviour can be visualised at a whole of market, regional, or technology level. Alternatively, the non-aggregated\nbids of dispatch units, and stations can be visualised.\n\nWe have additionally published the code required to run the web app as a python package, so that it can be used to help\nvisualise and analyse bidding behaviour in alternative or more sophisticated ways than allowed by the web app.\n\nThe development of nem-bidding-dashboard was funded by the\n[Digital Grid Futures Institute](https://www.dgfi.unsw.edu.au/)\n\n## Web app\n\nnem-bidding-dashboard is hosted as a web app here: [https://nembiddingdashboard.org](https://nembiddingdashboard.org)\n\n## Python package / API\n\nThe python api can be used to:\n- run the web app interface locally\n- download publicly available bidding and operational data from the Australian Energy Market Operator\n- process and aggregate bidding and operational data into the format used in the web app\n- build and populate a PostgresSQL database for efficiently querying and aggregating bidding and operational data\n\n### Installation\n\n`pip install nem_bidding_dashboard`\n\n### Quick examples\n\nBelow are some quick examples that provide a taste of the api capabilities, see the full set of examples and api\ndocumentation for a complete guide.\n\n#### Get raw data\nTo get the raw data used by nem-bidding-dashboard before preprocessing use functions in the `fetch_data` module, e.g.\n`get_volume_bids`.\n\n```python\nfrom nem_bidding_dashboard import fetch_data\n\nvolume_bids = fetch_data.volume_bids(\n    start_time=\'2022/01/01 00:00:00\',\n    end_time=\'2022/01/01 00:05:00\',\n    raw_data_cache=\'D:/nemosis_data_cache\')\n\nprint(volume_bids.head(5))\n#        SETTLEMENTDATE     DUID  ... PASAAVAILABILITY   INTERVAL_DATETIME\n# 309360     2021-12-31  ADPBA1G  ...              6.0 2022-01-01 00:05:00\n# 309361     2021-12-31  ADPBA1G  ...              NaN 2022-01-01 00:05:00\n# 309362     2021-12-31  ADPBA1G  ...              NaN 2022-01-01 00:05:00\n# 309363     2021-12-31  ADPBA1G  ...              NaN 2022-01-01 00:05:00\n# 309364     2021-12-31  ADPBA1G  ...              NaN 2022-01-01 00:05:00\n#\n# [5 rows x 18 columns]\n```\n\n#### Get processed data\nTo get data in the format stored by nem-bidding-dashboard in the PostgresSQL database use functions in the module\n`fetch_and_preprocess`, e.g. `bid_data`.\n\n```python\nfrom nem_bidding_dashboard import fetch_and_preprocess\n\nbids = fetch_and_preprocess.bid_data(\n    start_time=\'2022/01/01 00:00:00\',\n    end_time=\'2022/01/01 00:05:00\',\n    raw_data_cache=\'D:/nemosis_data_cache\')\n\nprint(bids.head(5))\n#        INTERVAL_DATETIME     DUID  BIDBAND  BIDVOLUME  BIDVOLUMEADJUSTED  BIDPRICE  ONHOUR\n# 0    2022-01-01 00:05:00  ADPBA1G        8          6                0.0    998.00   False\n# 462  2022-01-01 00:05:00   REECE1        2         45               45.0    -55.03   False\n# 463  2022-01-01 00:05:00   REECE1        4         74               74.0     -0.85   False\n# 464  2022-01-01 00:05:00   REECE2        2         35               35.0    -54.77   False\n# 465  2022-01-01 00:05:00   REECE2        4         84               84.0     -0.86   False\n```\n\n#### Setup a PostgresSQL database\n\nCreate tables for storing processed data and functions, then populate the database with historical data.\n\n```python\nfrom nem_bidding_dashboard import postgres_helpers, populate_postgres_db\n\ncon_string = postgres_helpers.build_connection_string(\n    hostname=\'localhost\',\n    dbname=\'bidding_dashboard_db\',\n    username=\'bidding_dashboard_maintainer\',\n    password=\'1234abcd\',\n    port=5433)\n\nraw_data_cache = "D:/nemosis_cache"\nstart = "2022/01/01 00:00:00"\nend = "2022/02/01 00:00:00"\n\npopulate_postgres_db.duid_info(con_string, raw_data_cache)\npopulate_postgres_db.bid_data(con_string, raw_data_cache, start, end)\npopulate_postgres_db.region_data(con_string, raw_data_cache, start, end)\npopulate_postgres_db.unit_dispatch(con_string, raw_data_cache, start, end)\n```\n\n#### Query and aggregate bidding data from PostgresSQL database\n\nFilter bids by time and region, and then aggregate into price bands. Other functions in the module `query_postgres_db`\nprovide querying and aggregation and for each table in the db.\n\n```python\nfrom nem_bidding_dashboard import postgres_helpers, query_postgres_db\n\ncon_string = postgres_helpers.build_connection_string(\n    hostname=\'localhost\',\n    dbname=\'bidding_dashboard_db\',\n    username=\'bidding_dashboard_maintainer\',\n    password=\'1234abcd\',\n    port=5433)\n\nagg_bids = query_postgres_db.aggregate_bids(connection_string=con_string,\n                                            start_time="2022/01/01 00:00:00",\n                                            end_time="2022/01/01 01:00:00",\n                                            regions=[\'QLD\', \'NSW\', \'SA\'],\n                                            dispatch_type=\'Generator\',\n                                            tech_types=[],\n                                            resolution=\'hourly\',\n                                            adjusted=\'adjusted\')\n\nprint(agg_bids)\n#       INTERVAL_DATETIME        BIN_NAME   BIDVOLUME\n# 0   2022-01-01 01:00:00   [-1000, -100)  9673.93400\n# 1   2022-01-01 01:00:00       [-100, 0)   366.70236\n# 2   2022-01-01 01:00:00         [0, 50)  1527.00000\n# 3   2022-01-01 01:00:00       [50, 100)  1290.00000\n# 4   2022-01-01 01:00:00      [100, 200)   908.00000\n# 5   2022-01-01 01:00:00      [200, 300)  1217.00000\n# 6   2022-01-01 01:00:00      [300, 500)   943.00000\n# 7   2022-01-01 01:00:00     [500, 1000)   240.00000\n# 8   2022-01-01 01:00:00    [1000, 5000)   210.00000\n# 9   2022-01-01 01:00:00   [5000, 10000)   125.00000\n# 10  2022-01-01 01:00:00  [10000, 15500)  6766.00000\n```\n\n## Contributing\n\nInterested in contributing? Check out the [contributing guidelines](https://github.com/UNSW-CEEM/nem-bidding-dashboard/blob/master/CONTRIBUTING.md).\n\nPlease note that this project is released with a [Code of Conduct](https://github.com/UNSW-CEEM/nem-bidding-dashboard/blob/master/CONDUCT.md). By contributing to this project, you\nagree to abide by its terms.\n\n## License and Disclaimer\n\n`nem-bidding-dashboard` was created by `Nicholas Gorman` and `Patrick Chambers`. It is licensed under the terms of the\n[`BSD-3-Clause license`](https://github.com/UNSW-CEEM/nem-bidding-dashboard/blob/master/LICENSE). Please, also read the\ndisclaimer accompanying the licence\n',
+    'author': 'Patrick Chambers',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/nem-bidding-dashboard/nem_bidding_dashboard-1.0.4.tar.gz/nem_bidding_dashboard-1.0.4/src/nem_bidding_dashboard/preprocessing.py
@@ -0,0 +1,444 @@
+import datetime
+
+import numpy as np
+import pandas as pd
+
+pd.set_option("display.width", None)
+
+
+def stack_unit_bids(volume_bids, price_bids):
+    """
+    Combine volume and price components of energy market offers and reformat them such that each price quantity pair
+    is on a separate row of the dataframe.
+
+    Examples:
+
+    >>> volume_bids = pd.DataFrame(
+    ... columns=["INTERVAL_DATETIME", "SETTLEMENTDATE", "DUID", "BANDAVAIL1", "BANDAVAIL2", "BANDAVAIL3",
+    ...          "BANDAVAIL4", "BANDAVAIL5", "BANDAVAIL6", "BANDAVAIL7", "BANDAVAIL8", "BANDAVAIL9", "BANDAVAIL10"],
+    ... data=[('2020/01/01 00:45:00', '2019/12/31 00:00:00', 'AGLHAL', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)])
+
+    >>> volume_bids
+         INTERVAL_DATETIME       SETTLEMENTDATE  ... BANDAVAIL9  BANDAVAIL10
+    0  2020/01/01 00:45:00  2019/12/31 00:00:00  ...          9           10
+    <BLANKLINE>
+    [1 rows x 13 columns]
+
+    >>> price_bids = pd.DataFrame(
+    ... columns=["SETTLEMENTDATE", "DUID", "PRICEBAND1", "PRICEBAND2", "PRICEBAND3", "PRICEBAND4", "PRICEBAND5",
+    ...          "PRICEBAND6", "PRICEBAND7", "PRICEBAND8", "PRICEBAND9", "PRICEBAND10"],
+    ... data=[('2019/12/31 00:00:00', 'AGLHAL', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)])
+
+    >>> price_bids
+            SETTLEMENTDATE    DUID  PRICEBAND1  ...  PRICEBAND8  PRICEBAND9  PRICEBAND10
+    0  2019/12/31 00:00:00  AGLHAL           1  ...           8           9           10
+    <BLANKLINE>
+    [1 rows x 12 columns]
+
+    >>> stack_unit_bids(volume_bids, price_bids)
+         INTERVAL_DATETIME    DUID  BIDBAND  BIDVOLUME  BIDPRICE
+    0  2020/01/01 00:45:00  AGLHAL        1          1         1
+    1  2020/01/01 00:45:00  AGLHAL        2          2         2
+    2  2020/01/01 00:45:00  AGLHAL        3          3         3
+    3  2020/01/01 00:45:00  AGLHAL        4          4         4
+    4  2020/01/01 00:45:00  AGLHAL        5          5         5
+    5  2020/01/01 00:45:00  AGLHAL        6          6         6
+    6  2020/01/01 00:45:00  AGLHAL        7          7         7
+    7  2020/01/01 00:45:00  AGLHAL        8          8         8
+    8  2020/01/01 00:45:00  AGLHAL        9          9         9
+    9  2020/01/01 00:45:00  AGLHAL       10         10        10
+
+    Arguments:
+        volume_bids: pd dataframe containing quantity of bids on a 5 minutely basis. Should have columns
+            INTERVAL_DATETIME, SETTLEMENTDATE, DUID, BANDAVAIL1 . . . . BANDAVAIL10
+        price_bids: pd dataframe containing quantity of bids on a daily basis. Should have columns
+            SETTLEMENTDATE, DUID, PRICEBAND1 . . . . PRICEBAND10
+    Returns:
+        pd dataframe containing matched quantity and price pairs on a 5 minutely basis with bid bands on row basis.
+        Should have columns INTERVAL_DATETIME, DUID, BIDBAND, BIDVOLUME and BIDPRICE
+    """
+    volume_bids = pd.melt(
+        volume_bids,
+        id_vars=["INTERVAL_DATETIME", "SETTLEMENTDATE", "DUID"],
+        value_vars=[
+            "BANDAVAIL1",
+            "BANDAVAIL2",
+            "BANDAVAIL3",
+            "BANDAVAIL4",
+            "BANDAVAIL5",
+            "BANDAVAIL6",
+            "BANDAVAIL7",
+            "BANDAVAIL8",
+            "BANDAVAIL9",
+            "BANDAVAIL10",
+        ],
+        var_name="BIDBAND",
+        value_name="BIDVOLUME",
+    )
+    price_bids = pd.melt(
+        price_bids,
+        id_vars=["SETTLEMENTDATE", "DUID"],
+        value_vars=[
+            "PRICEBAND1",
+            "PRICEBAND2",
+            "PRICEBAND3",
+            "PRICEBAND4",
+            "PRICEBAND5",
+            "PRICEBAND6",
+            "PRICEBAND7",
+            "PRICEBAND8",
+            "PRICEBAND9",
+            "PRICEBAND10",
+        ],
+        var_name="BIDBAND",
+        value_name="BIDPRICE",
+    )
+    price_bids["BIDBAND"] = pd.to_numeric(price_bids["BIDBAND"].str[9:])
+    volume_bids["BIDBAND"] = pd.to_numeric(volume_bids["BIDBAND"].str[9:])
+    bids = pd.merge(volume_bids, price_bids, on=["SETTLEMENTDATE", "BIDBAND", "DUID"])
+    return bids.loc[
+        :, ["INTERVAL_DATETIME", "DUID", "BIDBAND", "BIDVOLUME", "BIDPRICE"]
+    ]
+
+
+def adjust_bids_for_availability(stacked_bids, unit_availability):
+    """
+    Adjust bid volumes where the unit availability would restrict a bid from actually being fully dispatched.
+    Starting from the highest bid bands bid volumes are adjusted down until the total bid volume is equal to the
+    availability, if the total bid volume is already equal to or less than availability no adjustments are made.
+
+    Examples:
+
+    >>> bid_data = pd.DataFrame(
+    ... columns=["INTERVAL_DATETIME", "DUID", "BIDBAND", "BIDVOLUME", "BIDPRICE"],
+    ... data=[('2020/01/01 00:45:00', 'AGLHAL', 1, 50, 120),
+    ...       ('2020/01/01 00:45:00', 'AGLHAL', 2, 50, 200)])
+
+    >>> bid_data
+         INTERVAL_DATETIME    DUID  BIDBAND  BIDVOLUME  BIDPRICE
+    0  2020/01/01 00:45:00  AGLHAL        1         50       120
+    1  2020/01/01 00:45:00  AGLHAL        2         50       200
+
+    >>> unit_availability = pd.DataFrame(
+    ... columns=["SETTLEMENTDATE", "DUID", "AVAILABILITY"],
+    ... data=[('2020/01/01 00:45:00', 'AGLHAL', 80),])
+
+    >>> unit_availability
+            SETTLEMENTDATE    DUID  AVAILABILITY
+    0  2020/01/01 00:45:00  AGLHAL            80
+
+    >>> adjust_bids_for_availability(bid_data, unit_availability)
+         INTERVAL_DATETIME    DUID  BIDBAND  BIDVOLUME  BIDVOLUMEADJUSTED  BIDPRICE
+    0  2020/01/01 00:45:00  AGLHAL        1         50                 50       120
+    1  2020/01/01 00:45:00  AGLHAL        2         50                 30       200
+
+    Arguments:
+        stacked_bids: pd.dataframe containing matched quantity and price pairs on a 5 minutely basis with bid bands on
+            row basis. Should have columns INTERVAL_DATETIME, DUID, BIDBAND, BIDVOLUME and BIDPRICE
+        unit_availability: pd.dataframe containing unit availability values on 5 minutely basis. Should have columns
+            SETTLEMENTDATE, DUID, AVAILABILITY
+    Returns:
+        pd.dataframe containing matched quantity and price pairs on a 5 minutely basis with bid bands on
+        row basis. An extra column is added in which did volumes have been adjusted so total bid volume doesn't exceed
+        unit availability. Should have columns INTERVAL_DATETIME, DUID, BIDBAND, BIDVOLUME, BIDVOLUMEADJUSTED and
+        BIDPRICE
+    """
+
+    bids = stacked_bids.sort_values("BIDBAND")
+    bids["BIDVOLUMECUMULATIVE"] = bids.groupby(
+        ["DUID", "INTERVAL_DATETIME"], as_index=False
+    )["BIDVOLUME"].cumsum()
+    availability = unit_availability.rename(
+        {"SETTLEMENTDATE": "INTERVAL_DATETIME"}, axis=1
+    )
+    bids = pd.merge(bids, availability, "left", on=["INTERVAL_DATETIME", "DUID"])
+    bids["SPAREBIDVOLUME"] = (
+        bids["AVAILABILITY"] - bids["BIDVOLUMECUMULATIVE"]
+    ) + bids["BIDVOLUME"]
+    bids["SPAREBIDVOLUME"] = np.where(
+        bids["SPAREBIDVOLUME"] < 0, 0, bids["SPAREBIDVOLUME"]
+    )
+    bids["BIDVOLUMEADJUSTED"] = bids[["BIDVOLUME", "SPAREBIDVOLUME"]].min(axis=1)
+    bids = bids.loc[
+        :,
+        [
+            "INTERVAL_DATETIME",
+            "DUID",
+            "BIDBAND",
+            "BIDVOLUME",
+            "BIDVOLUMEADJUSTED",
+            "BIDPRICE",
+        ],
+    ]
+    return bids.sort_values(
+        [
+            "INTERVAL_DATETIME",
+            "DUID",
+            "BIDBAND",
+        ]
+    ).reset_index(drop=True)
+
+
+def remove_number_from_region_names(region_column, data):
+    """
+    Removes the trailing 1 from region names in the specified column. Names in input data are expected to be of the
+    format NSW1, QLD1 etc. and the names in the output format will be NSW, QLD etc.
+
+    Examples:
+
+    >>> region_data = pd.DataFrame({
+    ... 'region': ['QLD1', 'TAS1'],
+    ... 'dummy_values': [55.7, 102.9]})
+
+    >>> region_data
+      region  dummy_values
+    0   QLD1          55.7
+    1   TAS1         102.9
+
+    >>> remove_number_from_region_names('region', region_data)
+      region  dummy_values
+    0    QLD          55.7
+    1    TAS         102.9
+
+    Arguments:
+        region_column: str the name of the column containing the region names.
+        data: pd dataframe with a column called the value of region_column.
+    Returns:
+        pd dataframe with names in region_column modified.
+    """
+    data.loc[:, region_column] = data[region_column].str[:-1]
+    return data
+
+
+def tech_namer_by_row(fuel, tech_descriptor, dispatch_type):
+    """
+    Create a name for generation and loads using custom logic that considers the fuel type, technology descriptor, and
+    dispatch type supplied by AEMO in the NEM Registration and Exemption List.xls file. See the
+    `source code <https://github.com/UNSW-CEEM/nem-bidding-dashboard/blob/6468e6b5506c778d12b3d3653af1cf11bd28807b/src/nem_bidding_dashboard/preprocessing.py#L123>`_
+    for logic that this function applies:
+
+    Examples:
+
+    >>> tech_namer_by_row("Natural Gas", "Steam Sub-Critical", "Generator")
+    'Gas Thermal'
+
+    >>> tech_namer_by_row("solar", "PV - Tracking", "Generator")
+    'Solar'
+
+    >>> tech_namer_by_row("-", "Battery", "Load")
+    'Battery Charge'
+
+
+    Arguments:
+        fuel: str should be the value from the column 'Fuel Source - Descriptor'
+        tech_descriptor: str should be the value from the column 'Technology Type - Descriptor'
+        dispatch_type: str should be the value from the column 'Dispatch Type'
+
+    Returns:
+        str a value categorising the technology of the generation or load unit
+    """
+    name = fuel
+    if fuel in ["Solar", "Wind", "Black Coal", "Brown Coal"]:
+        pass
+    elif fuel == "solar":
+        name = "Solar"
+    elif tech_descriptor in ["Battery", "Battery and Inverter"]:
+        if dispatch_type == "Load":
+            name = "Battery Charge"
+        else:
+            name = "Battery Discharge"
+    elif tech_descriptor in ["Hydro - Gravity"]:
+        name = "Hydro"
+    elif tech_descriptor in ["Hydro - Gravity", "Run of River"]:
+        name = "Run of River Hydro"
+    elif tech_descriptor == "Pump Storage":
+        if dispatch_type == "Load":
+            name = "Pump Storage Charge"
+        else:
+            name = "Pump Storage Discharge"
+    elif tech_descriptor == "-" and fuel == "-" and dispatch_type == "Load":
+        name = "Pump Storage Charge"
+    elif tech_descriptor == "Open Cycle Gas turbines (OCGT)":
+        name = "OCGT"
+    elif tech_descriptor == "Combined Cycle Gas Turbine (CCGT)":
+        name = "CCGT"
+    elif (
+        fuel in ["Natural Gas / Fuel Oil", "Natural Gas"]
+        and tech_descriptor == "Steam Sub-Critical"
+    ):
+        name = "Gas Thermal"
+    elif isinstance(tech_descriptor, str) and "Engine" in tech_descriptor:
+        name = "Engine"
+    return name
+
+
+def tech_namer(duid_info):
+    """
+    Create a name for generation and load unit using custom logic applied to the fuel type, technology descriptor, and
+    dispatch type supplied by AEMO in the NEM Registration and Exemption List.xls file. See the
+    `source code <https://github.com/UNSW-CEEM/nem-bidding-dashboard/blob/6468e6b5506c778d12b3d3653af1cf11bd28807b/src/nem_bidding_dashboard/preprocessing.py#L123>`_
+    for logic that this function applies:
+
+    Arguments:
+        duid_info: pd dataframe with columns 'FUEL SOURCE - DESCRIPTOR', 'TECHNOLOGY TYPE - DESCRIPTOR' and
+            'DISPATCH TYPE'
+    Returns:
+        pd dataframe with additional column 'UNIT_TYPE'
+    """
+    duid_info["UNIT TYPE"] = duid_info.apply(
+        lambda x: tech_namer_by_row(
+            x["FUEL SOURCE - DESCRIPTOR"],
+            x["TECHNOLOGY TYPE - DESCRIPTOR"],
+            x["DISPATCH TYPE"],
+        ),
+        axis=1,
+    )
+    return duid_info
+
+
+def hard_code_fix_fuel_source_and_tech_errors(duid_data):
+    """
+    Where NA values occur in columns FUEL SOURCE - DESCRIPTOR or TECHNOLOGY TYPE - DESCRIPTOR replace these with the
+    value '-'. This function is used to clean data before passing to
+    :py:func:`nem_bidding_dashboard.preprocessing.tech_namer`.
+
+    Args:
+        duid_data: pd.DataFrame containing at least the columns FUEL SOURCE - DESCRIPTOR and
+            TECHNOLOGY TYPE - DESCRIPTOR
+
+    Returns:
+        pd.DataFrame with the same columns as input data. Columns FUEL SOURCE - DESCRIPTOR and
+        TECHNOLOGY TYPE - DESCRIPTOR have NA values replaced with '-'.
+
+    """
+    duid_data.loc[
+        duid_data["FUEL SOURCE - DESCRIPTOR"].isna(), "FUEL SOURCE - DESCRIPTOR"
+    ] = "-"
+    duid_data.loc[
+        duid_data["TECHNOLOGY TYPE - DESCRIPTOR"].isna(), "TECHNOLOGY TYPE - DESCRIPTOR"
+    ] = "-"
+    return duid_data
+
+
+def calculate_unit_time_series_metrics(as_bid_metrics, after_dispatch_metrics):
+    """
+    Calculate some additional values associated with unit dispatch. These are used to populate the database table
+    unit_dispatch and should be helpful in understanding unit dispatch outcomes, e.g. show when a plant is limited
+    by its ramp rate, or if a unit's availability is the same as the availability submitted for the PASA process
+    indicating that output is probably limited by a technical constraint.
+
+    Examples:
+
+    >>> as_bid_metrics = pd.DataFrame({
+    ... 'INTERVAL_DATETIME': ['2022/01/01 00:00:00', '2022/01/01 00:05:00', '2022/01/01 00:10:00'],
+    ... 'DUID': ['AGLHAL', 'AGLHAL', 'AGLHAL'],
+    ... 'ROCUP': [10, 15, 60],
+    ... 'ROCDOWN': [20, 25, 30],
+    ... 'MAXAVAIL': [100, 90,  105],
+    ... 'PASAAVAILABILITY': [120, 90, 110],
+    ... })
+
+    >>> as_bid_metrics['INTERVAL_DATETIME'] = pd.to_datetime(as_bid_metrics['INTERVAL_DATETIME'])
+
+    >>> as_bid_metrics
+        INTERVAL_DATETIME    DUID  ROCUP  ROCDOWN  MAXAVAIL  PASAAVAILABILITY
+    0 2022-01-01 00:00:00  AGLHAL     10       20       100               120
+    1 2022-01-01 00:05:00  AGLHAL     15       25        90                90
+    2 2022-01-01 00:10:00  AGLHAL     60       30       105               110
+
+    >>> after_dispatch_metrics = pd.DataFrame({
+    ... 'SETTLEMENTDATE': ['2022/01/01 00:00:00', '2022/01/01 00:05:00', '2022/01/01 00:10:00'],
+    ... 'DUID': ['AGLHAL', 'AGLHAL', 'AGLHAL'],
+    ... 'RAMPUPRATE': [9*60, 14*60, 50*60],
+    ... 'RAMPDOWNRATE': [18*60, 24*60, 28*60],
+    ... 'AVAILABILITY': [100, 90,  105],
+    ... 'INITIALMW': [80.1, 88.9,  84.5],
+    ... 'TOTALCLEARED': [80, 90,  85],
+    ...  })
+
+    >>> after_dispatch_metrics['SETTLEMENTDATE'] = pd.to_datetime(after_dispatch_metrics['SETTLEMENTDATE'])
+
+    >>> after_dispatch_metrics
+           SETTLEMENTDATE    DUID  RAMPUPRATE  RAMPDOWNRATE  AVAILABILITY  INITIALMW  TOTALCLEARED
+    0 2022-01-01 00:00:00  AGLHAL         540          1080           100       80.1            80
+    1 2022-01-01 00:05:00  AGLHAL         840          1440            90       88.9            90
+    2 2022-01-01 00:10:00  AGLHAL        3000          1680           105       84.5            85
+
+    >>> calculate_unit_time_series_metrics(as_bid_metrics, after_dispatch_metrics)
+        INTERVAL_DATETIME    DUID  AVAILABILITY  TOTALCLEARED  FINALMW  ASBIDRAMPUPMAXAVAIL  ASBIDRAMPDOWNMINAVAIL  RAMPUPMAXAVAIL  RAMPDOWNMINAVAIL  PASAAVAILABILITY  MAXAVAIL
+    0 2022-01-01 00:00:00  AGLHAL           100            80     88.9                130.1                  -19.9           125.1              -9.9               120       100
+    1 2022-01-01 00:05:00  AGLHAL            90            90     84.5                163.9                  -36.1           158.9             -31.1                90        90
+
+    Args:
+        as_bid_metrics: pd.DataFrame containing values submitted by unit's as part of the bidding process. Should
+            contain columns INTERVAL_DATETIME, DUID, ROCUP (ramp up rate in MW per min), ROCDOWN (ramp down rate in MW per
+            min), MAXAVAIL (limit the unit can be dispatched up to), PASAAVAILABILITY (The technical maximum availability of
+            unit given 24h notice, not used in dispatch)
+        after_dispatch_metrics: pd.DataFrame containing values calculated by AEMO as part of the dispatch process.
+            Should contain columns SETTLEMENTDATE, DUID, AVAILABILITY (presumed to be the lesser of the unit bid
+            availability (MAXAVAIL column) and unit forecast availability for variable renewables), RAMPUPRATE, RAMPDOWNRATE
+            (lesser of bid and telemetry ramp rates, MW per hour), INITIALMW (operating level of unit at start of dispatch
+            interval), TOTALCLEARED (dispatch target for unit to ramp to by end of dispatch interval).
+
+    Returns:
+        pd.DataFrame containing columns INTERVAL_DATETIME, DUID, ASBIDRAMPUPMAXAVAIL (upper dispatch limit based
+        on as bid ramp rate), ASBIDRAMPDOWNMINAVAIL (lower dispatch limit based on as bid ramp rate), RAMPUPMAXAVAIL (
+        upper dispatch limit based lesser of as bid and telemetry ramp rates), RAMPDOWNMINAVAIL (lower dispatch limit based
+        lesser of as bid and telemetry ramp rates), AVAILABILITY, TOTALCLEARED (as for after_dispatch_metrics),
+        PASAAVAILABILITY, MAXAVAIL (as for as_bid_metrics), and FINALMW (the unit operating level at the end of the dispatch
+        interval).
+    """
+
+    after_dispatch_metrics = after_dispatch_metrics.rename(
+        {"SETTLEMENTDATE": "INTERVAL_DATETIME"}, axis=1
+    )
+    unit_time_series_metrics = pd.merge(
+        as_bid_metrics, after_dispatch_metrics, on=["INTERVAL_DATETIME", "DUID"]
+    )
+
+    unit_time_series_metrics["ASBIDRAMPUPMAXAVAIL"] = (
+        unit_time_series_metrics["INITIALMW"] + unit_time_series_metrics["ROCUP"] * 5
+    )
+    unit_time_series_metrics["ASBIDRAMPDOWNMINAVAIL"] = (
+        unit_time_series_metrics["INITIALMW"] - unit_time_series_metrics["ROCDOWN"] * 5
+    )
+    unit_time_series_metrics["RAMPUPMAXAVAIL"] = (
+        unit_time_series_metrics["INITIALMW"]
+        + unit_time_series_metrics["RAMPUPRATE"] / 12
+    )
+    unit_time_series_metrics["RAMPDOWNMINAVAIL"] = (
+        unit_time_series_metrics["INITIALMW"]
+        - unit_time_series_metrics["RAMPDOWNRATE"] / 12
+    )
+
+    final_mw = unit_time_series_metrics.loc[
+        :, ["INTERVAL_DATETIME", "DUID", "INITIALMW"]
+    ]
+    final_mw["INTERVAL_DATETIME"] -= datetime.timedelta(minutes=5)
+    final_mw = final_mw.rename({"INITIALMW": "FINALMW"}, axis=1)
+
+    unit_time_series_metrics = pd.merge(
+        unit_time_series_metrics, final_mw, on=["INTERVAL_DATETIME", "DUID"]
+    )
+
+    return unit_time_series_metrics.loc[
+        :,
+        [
+            "INTERVAL_DATETIME",
+            "DUID",
+            "AVAILABILITY",
+            "TOTALCLEARED",
+            "FINALMW",
+            "ASBIDRAMPUPMAXAVAIL",
+            "ASBIDRAMPDOWNMINAVAIL",
+            "RAMPUPMAXAVAIL",
+            "RAMPDOWNMINAVAIL",
+            "PASAAVAILABILITY",
+            "MAXAVAIL",
+        ],
+    ]
+
+
+def add_on_hour_column(data):
+    data["ONHOUR"] = np.where(data["INTERVAL_DATETIME"].dt.minute == 0, True, False)
+    return data
