ec6f389ddfbeee5b35b4f7532b05c251b995fb19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:46 +0000

    Add package kabbes-user-profile/0.11.0/kabbes_user_profile-0.11.0-py3-none-any.whl

---
+++ packages/kabbes-user-profile/kabbes_user_profile-0.11.0-py3-none-any.whl/user_profile/__init__.py
@@ -0,0 +1,15 @@
+import dir_ops as do
+import os
+
+_Dir = do.Dir( os.path.abspath( __file__ ) ).ascend()   #Dir that contains the package 
+_src_Dir = _Dir.ascend()                                  #src Dir that is one above
+_repo_Dir = _src_Dir.ascend()                    
+_cwd_Dir = do.Dir( do.get_cwd() )
+
+CONFIG_KEY = 'user_profile'
+
+from .Profile import Profile
+from .Client import Client
+
+default_client = Client()
+profile = default_client.profile
---
+++ packages/kabbes-user-profile/kabbes_user_profile-0.11.0-py3-none-any.whl/user_profile/__main__.py
@@ -0,0 +1,7 @@
+import user_profile
+
+C = user_profile.Client()
+C.profile.print_atts()
+
+C.cfg.parent.print_atts()
+
