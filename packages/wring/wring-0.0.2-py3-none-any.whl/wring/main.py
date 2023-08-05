1a05e436aee2e0392c678a4c1b3a331e75c237ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:41 +0000

    Add package wring/0.0.2/wring-0.0.2-py3-none-any.whl

---
+++ packages/wring/wring-0.0.2-py3-none-any.whl/wring/main.py
@@ -0,0 +1,55 @@
+import os
+
+import click
+import pyarrow.csv as pc
+import pyarrow.parquet as pq
+
+
+@click.command()
+@click.argument("directory", type=click.Path(exists=True))
+@click.option("--compression", default="zstd", help="compression algorithm")
+@click.option("--compression-level", default=9, help="compression level")
+@click.option(
+    "--clean/--no-clean",
+    default=False,
+    help="remove uncompressed files after successful wringing",
+)
+def wring(directory, compression, compression_level, clean):
+    """Crawl a directory and compress csv files into parquet."""
+    click.echo(f"wringing {directory} [{compression=}, {compression_level=}]")
+    click.echo(f"[{compression=}, {compression_level=}]")
+    for dirpath, _dirnames, filenames in os.walk(directory):
+        for f in filenames:
+            source = os.path.join(dirpath, f)
+            target = None
+            if source.endswith(".csv"):
+                target = source[:-4] + ".parquet"
+            elif source.endswith(".csv.gz"):
+                target = source[:-7] + ".parquet"
+            if target is not None:
+                if not os.path.exists(target):
+                    click.echo(click.style("converting ", fg="green") + source)
+                    try:
+                        t = pc.read_csv(source)
+                        pq.write_table(
+                            t,
+                            target,
+                            compression=compression,
+                            compression_level=compression_level,
+                        )
+                    except Exception as err:
+                        click.echo(click.style(str(err), fg="red", bold=True))
+                    else:
+                        if clean and os.path.exists(target):
+                            if pq.read_table(target).equals(t, check_metadata=True):
+                                os.unlink(source)
+                else:
+                    click.echo(
+                        click.style("not converting ", fg="red")
+                        + source
+                        + click.style(" (parquet already exists)", fg="red")
+                    )
+
+
+if __name__ == "__main__":
+    wring()
