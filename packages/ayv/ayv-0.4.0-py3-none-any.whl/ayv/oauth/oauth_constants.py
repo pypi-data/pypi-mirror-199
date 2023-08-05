7780edac9392b56b076e82df66648dc04aa0c2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:42 +0000

    Add package ayv/0.4.0/ayv-0.4.0-py3-none-any.whl

---
+++ packages/ayv/ayv-0.4.0-py3-none-any.whl/ayv/oauth/oauth_constants.py
@@ -0,0 +1,5 @@
+class YouTubeAPIOauthConstants:
+    TOKEN_FILE = 'credentials.json'
+    API_SERVICE_NAME = 'youtube'
+    API_VERSION = 'v3'
+    SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
