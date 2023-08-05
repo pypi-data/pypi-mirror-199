2f70de236b9661ee5009c26378cf47e97b4c89e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:18 +0000

    Add package chainingiterator/0.1.3/chainingiterator-0.1.3.tar.gz

---
+++ packages/chainingiterator/chainingiterator-0.1.3.tar.gz/chainingiterator-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['chainingiterator']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'chainingiterator',
+    'version': '0.1.3',
+    'description': "A python wrapper-library for iterators to be usable via chaining(such as C#, Java streams or Rust iterators). Extended functionality based on Rust's buit-in iterators.",
+    'long_description': "# chainingiterator\nA python wrapper-library for iterators to be usable via chaining(such as C#, Java streams or Rust iterators). Extended functionality based on Rust's buit-in iterators. \n",
+    'author': 'Adam Ruman',
+    'author_email': 'ruman.adam@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
