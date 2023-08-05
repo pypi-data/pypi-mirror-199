ce2c3d1ef614ca25a8a768b6fcd95c334ba4b25e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:16:36 +0000

    Add package napari-generic-SIMulator/0.0.24/napari_generic_SIMulator-0.0.24-py3-none-any.whl

---
+++ packages/napari-generic-SIMulator/napari_generic_SIMulator-0.0.24-py3-none-any.whl/napari_generic_simulator/__init__.py
@@ -0,0 +1,9 @@
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+__author__ = "Meizhu Liang"
+__email__ = "meizhu.liang18@imperial.ac.uk"
+
+from ._widget import SIMulator
