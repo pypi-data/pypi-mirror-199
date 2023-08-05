9355335f327df6acda576a49725fea7d3fccb3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:05 +0000

    Add package sentry-kafka-schemas/0.0.11/sentry_kafka_schemas-0.0.11-py2.py3-none-any.whl

---
+++ packages/sentry-kafka-schemas/sentry_kafka_schemas-0.0.11-py2.py3-none-any.whl/sentry_kafka_schemas/schema_types/events_subscription_results_v1.py
@@ -0,0 +1,56 @@
+from typing import Union, Dict, Any, List, TypedDict
+from typing_extensions import Required
+
+
+class PayloadV3(TypedDict, total=False):
+    """payload_v3."""
+
+    subscription_id: Required[str]
+    """
+    minLength: 1
+
+    Required property
+    """
+
+    request: Required[Dict[str, Any]]
+    """Required property"""
+
+    result: Required["_PayloadV3Result"]
+    """Required property"""
+
+    timestamp: Required[str]
+    """Required property"""
+
+    entity: Required[str]
+    """
+    minLength: 1
+
+    Required property
+    """
+
+
+
+class SubscriptionResults(TypedDict, total=False):
+    """subscription_results."""
+
+    version: Required[int]
+    """Required property"""
+
+    payload: Required["PayloadV3"]
+    """Required property"""
+
+
+
+class _PayloadV3Result(TypedDict, total=False):
+    data: Required[List["_PayloadV3ResultDataItem"]]
+    """
+    minItems: 0
+
+    Required property
+    """
+
+
+
+_PayloadV3ResultDataItem = Dict[str, Union[Union[int, float], None]]
+"""minProperties: 1"""
+
