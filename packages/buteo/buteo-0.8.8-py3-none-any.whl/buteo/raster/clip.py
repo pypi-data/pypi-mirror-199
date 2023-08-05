ad18864180a8dd648aae7e450da5b3fefaa12f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:58 +0000

    Add package buteo/0.8.8/buteo-0.8.8-py3-none-any.whl

---
+++ packages/buteo/buteo-0.8.8-py3-none-any.whl/buteo/raster/clip.py
@@ -0,0 +1,307 @@
+"""
+### Clip rasters ###
+
+Clips a raster using a vector geometry or the extents of a raster.
+"""
+
+# Standard library
+import sys; sys.path.append("../../")
+import os
+
+# External
+from osgeo import gdal, ogr
+import numpy as np
+
+# Internal
+from buteo.raster import core_raster
+from buteo.vector import core_vector
+from buteo.utils import gdal_utils, core_utils, bbox_utils, gdal_enums
+from buteo.vector.reproject import _reproject_vector
+
+
+def _clip_raster(
+    raster,
+    clip_geom,
+    out_path,
+    *,
+    resample_alg="nearest",
+    crop_to_geom=True,
+    adjust_bbox=True,
+    all_touch=True,
+    to_extent=False,
+    overwrite=True,
+    creation_options=None,
+    dst_nodata="infer",
+    src_nodata="infer",
+    layer_to_clip=0,
+    prefix="",
+    suffix="",
+    verbose=1,
+    add_uuid=False,
+    ram="auto",
+):
+    """ INTERNAL. """
+    path_list = gdal_utils.create_output_path_list(
+        core_utils.ensure_list(raster),
+        out_path=out_path,
+        prefix=prefix,
+        suffix=suffix,
+        add_uuid=add_uuid,
+    )
+
+    if out_path is not None:
+        if "vsimem" not in out_path:
+            if not os.path.isdir(os.path.split(os.path.normpath(out_path))[0]):
+                raise ValueError(f"out_path folder does not exists: {out_path}")
+
+    clip_ds = None
+
+    memory_files = []
+
+    # Input is a vector.
+    if gdal_utils.is_vector(clip_geom):
+        clip_ds = core_vector._open_vector(clip_geom)
+
+        # TODO: Fix potential memory leak
+        if clip_ds.GetLayerCount() > 1:
+            clip_ds = core_vector.vector_filter_layer(clip_ds, layer_name_or_idx=layer_to_clip, add_uuid=True)
+            memory_files.append(clip_ds)
+
+        clip_metadata = core_vector._vector_to_metadata(clip_ds)
+
+        if to_extent:
+            clip_ds = bbox_utils.convert_bbox_to_vector(clip_metadata["bbox"], clip_metadata["projection_osr"])
+            memory_files.append(clip_ds)
+
+        if isinstance(clip_ds, ogr.DataSource):
+            clip_ds = clip_ds.GetName()
+
+    # Input is a raster (use extent)
+    elif gdal_utils.is_raster(clip_geom):
+        clip_metadata = core_raster._raster_to_metadata(clip_geom)
+        clip_ds = bbox_utils.convert_bbox_to_vector(clip_metadata["bbox"], clip_metadata["projection_osr"])
+        memory_files.append(clip_ds)
+    else:
+        if core_utils.file_exists(clip_geom):
+            raise ValueError(f"Unable to parse clip geometry: {clip_geom}")
+        else:
+            raise ValueError(f"Unable to locate clip geometry {clip_geom}")
+
+    if clip_ds is None:
+        raise ValueError(f"Unable to parse input clip geom: {clip_geom}")
+
+    # options
+    warp_options = []
+    if all_touch:
+        warp_options.append("CUTLINE_ALL_TOUCHED=TRUE")
+    else:
+        warp_options.append("CUTLINE_ALL_TOUCHED=FALSE")
+
+    origin_layer = core_raster.open_raster(raster)
+
+    raster_metadata = core_raster._raster_to_metadata(raster)
+    origin_projection = raster_metadata["projection_osr"]
+
+    # Fast check: Does the extent of the two inputs overlap?
+    has_inf = True in [np.isinf(val) for val in raster_metadata["bbox_latlng"]]
+    if not has_inf and not bbox_utils.bboxes_intersect(raster_metadata["bbox_latlng"], clip_metadata["bbox_latlng"]):
+        raise Exception("Geometries did not intersect.")
+
+    if not origin_projection.IsSame(clip_metadata["projection_osr"]):
+        clip_ds = _reproject_vector(clip_ds, origin_projection)
+        clip_metadata = core_vector._vector_to_metadata(clip_ds)
+        memory_files.append(clip_ds)
+
+    output_bounds = raster_metadata["bbox"]
+
+    if crop_to_geom:
+
+        if adjust_bbox:
+            output_bounds = bbox_utils.align_bboxes_to_pixel_size(
+                raster_metadata["bbox"],
+                clip_metadata["bbox"],
+                raster_metadata["pixel_width"],
+                raster_metadata["pixel_height"],
+            )
+        else:
+            output_bounds = clip_metadata["bbox"]
+
+    # formats
+    out_name = path_list[0]
+    out_format = gdal_utils.path_to_driver_raster(out_name)
+    out_creation_options = gdal_utils.default_creation_options(creation_options)
+
+    # nodata
+    if src_nodata == "infer":
+        src_nodata = raster_metadata["nodata_value"]
+    elif isinstance(src_nodata, (int, float, None)):
+        src_nodata = float(src_nodata)
+    else:
+        raise ValueError(f"src_nodata must be an int, float or None: {src_nodata}")
+
+    out_nodata = None
+    if dst_nodata == "infer":
+        if src_nodata == "infer":
+            out_nodata = raster_metadata["nodata_value"]
+        else:
+            out_nodata = gdal_enums.get_default_nodata_value(raster_metadata["datatype_gdal_raw"])
+    elif isinstance(dst_nodata, (int, float, None)):
+        out_nodata = dst_nodata
+    else:
+        raise ValueError(f"Unable to parse nodata_value: {dst_nodata}")
+
+    # Removes file if it exists and overwrite is True.
+    core_utils.remove_if_required(out_path, overwrite)
+
+    if verbose == 0:
+        gdal.PushErrorHandler("CPLQuietErrorHandler")
+
+    clipped = gdal.Warp(
+        out_name,
+        origin_layer,
+        format=out_format,
+        resampleAlg=gdal_enums.translate_resample_method(resample_alg),
+        targetAlignedPixels=False,
+        outputBounds=bbox_utils.convert_ogr_bbox_to_gdal_bbox(output_bounds),
+        xRes=raster_metadata["pixel_width"],
+        yRes=raster_metadata["pixel_height"],
+        cutlineDSName=clip_ds,
+        cropToCutline=False,
+        creationOptions=out_creation_options,
+        warpMemoryLimit=gdal_utils.get_gdalwarp_ram_limit(ram),
+        warpOptions=warp_options,
+        srcNodata=src_nodata,
+        dstNodata=out_nodata,
+        multithread=True,
+    )
+
+    gdal_utils.delete_if_in_memory_list(memory_files)
+
+    if verbose == 0:
+        gdal.PopErrorHandler()
+
+    if clipped is None:
+        raise Exception("Error while clipping raster.")
+
+    return out_name
+
+
+def clip_raster(
+    raster,
+    clip_geom,
+    out_path=None,
+    *,
+    resample_alg="nearest",
+    crop_to_geom=True,
+    adjust_bbox=False,
+    all_touch=False,
+    to_extent=False,
+    prefix="",
+    suffix="",
+    overwrite=True,
+    creation_options=None,
+    dst_nodata="infer",
+    src_nodata="infer",
+    layer_to_clip=0,
+    verbose=0,
+    add_uuid=False,
+    ram="auto",
+):
+    """
+    Clips a raster(s) using a vector geometry or the extents of a raster.
+
+    ## Args:
+    `raster` (_list_/_str_/_gdal.Dataset_): The raster(s) to clip. </br>
+    `clip_geom` (_str_/_ogr.DataSource_/_gdal.Dataset_): The geometry to use to clip the raster </br>
+
+    ## Kwargs:
+    `out_path` (_str_/_list_/_None_): The path(s) to save the clipped raster to. If None a memory raster is created. (Default: **None**)</br>
+    `resample_alg` (_str_): The resampling algorithm to use. (Default: **nearest**) </br>
+    &emsp; • **nearest**: Nearest neighbour. </br>
+    &emsp; • **bilinear**: Bilinear. </br>
+    &emsp; • **cubic**: Cubic. </br>
+    &emsp; • **cubicspline**: Cubic spline. </br>
+    &emsp; • **lanczos**: Lanczos. </br>
+    &emsp; • **average**: Average. </br>
+    &emsp; • **mode**: Mode. </br>
+    &emsp; • **max**: Max. </br>
+    &emsp; • **min**: Min. </br>
+    &emsp; • **median**: Median. </br>
+    &emsp; • **q1**: Quartile 1 </br>
+    &emsp; • **q3**: Quartile 3 </br>
+    &emsp; • **sum**: Sum </br>
+    &emsp; • **rms**: Root Mean Squared </br>
+    `crop_to_geom` (_bool_): If True, the output raster will be cropped to the extent of the clip geometry. (Default: **True**)</br>
+    `adjust_bbox` (_bool_): If True, the output raster have its bbox adjusted to match the clip geometry. (Default: **False**)</br>
+    `all_touch` (_bool_): If true all pixels touching the clipping geometry will be included. (Default: **False**)</br>
+    `to_extent` (_bool_): If True, the output raster will be cropped to the extent of the clip geometry. (Default: **False**)</br>
+    `prefix` (_str_): The prefix to use for the output raster. (Default: **""**)</br>
+    `suffix` (_str_): The suffix to use for the output raster. (Default: **""**)</br>
+    `overwrite` (_bool_): If True, the output raster will be overwritten if it already exists. (Default: **True**)</br>
+    `creation_options` (_list_/_None_): A list of creation options to pass to gdal. (Default: **None**)</br>
+    `dst_nodata` (_int_/_float_/_None_): The nodata value to use for the output raster. (Default: **infer**)</br>
+    `src_nodata` (_int_/_float_/_None_): The nodata value to use for the input raster. (Default: **infer**)</br>
+    `layer_to_clip` (_int_/_str_): The layer ID or name in the vector to use for clipping. (Default: **0**)</br>
+    `verbose` (_int_): The verbosity level. (Default: **0**)</br>
+    `add_uuid` (_bool_): If True, a UUID will be added to the output raster. (Default: **False**)</br>
+    `ram` (_str_): The amount of RAM to use for the operation. (Default: **auto**)</br>
+
+    ## Returns:
+    (_str_/_list_): A string or list of strings representing the path(s) to the clipped raster(s).
+    """
+    core_utils.type_check(raster, [str, gdal.Dataset, [str, gdal.Dataset]], "raster")
+    core_utils.type_check(clip_geom, [str, ogr.DataSource, gdal.Dataset], "clip_geom")
+    core_utils.type_check(out_path, [[str], str, None], "out_path")
+    core_utils.type_check(resample_alg, [str], "resample_alg")
+    core_utils.type_check(crop_to_geom, [bool], "crop_to_geom")
+    core_utils.type_check(adjust_bbox, [bool], "adjust_bbox")
+    core_utils.type_check(all_touch, [bool], "all_touch")
+    core_utils.type_check(to_extent, [bool], "to_extent")
+    core_utils.type_check(dst_nodata, [str, int, float, None], "dst_nodata")
+    core_utils.type_check(src_nodata, [str, int, float, None], "src_nodata")
+    core_utils.type_check(layer_to_clip, [int], "layer_to_clip")
+    core_utils.type_check(overwrite, [bool], "overwrite")
+    core_utils.type_check(creation_options, [[str], None], "creation_options")
+    core_utils.type_check(prefix, [str], "prefix")
+    core_utils.type_check(suffix, [str], "postfix")
+    core_utils.type_check(verbose, [int], "verbose")
+    core_utils.type_check(add_uuid, [bool], "uuid")
+
+    raster_list = core_utils.ensure_list(raster)
+    path_list = gdal_utils.create_output_path_list(
+        raster_list,
+        out_path=out_path,
+        prefix=prefix,
+        suffix=suffix,
+        add_uuid=add_uuid,
+        overwrite=overwrite,
+    )
+
+    output = []
+    for index, in_raster in enumerate(raster_list):
+        output.append(
+            _clip_raster(
+                in_raster,
+                clip_geom,
+                out_path=path_list[index],
+                resample_alg=resample_alg,
+                crop_to_geom=crop_to_geom,
+                adjust_bbox=adjust_bbox,
+                all_touch=all_touch,
+                to_extent=to_extent,
+                dst_nodata=dst_nodata,
+                src_nodata=src_nodata,
+                layer_to_clip=layer_to_clip,
+                overwrite=overwrite,
+                creation_options=creation_options,
+                prefix=prefix,
+                suffix=suffix,
+                verbose=verbose,
+                ram=ram,
+            )
+        )
+
+    if isinstance(raster, list):
+        return output
+
+    return output[0]
---
+++ packages/buteo/buteo-0.8.8-py3-none-any.whl/buteo/raster/convolution.py
@@ -0,0 +1,465 @@
+"""
+### Perform convolutions on arrays.  ###
+"""
+
+# External
+import numpy as np
+from numpy.lib.stride_tricks import as_strided
+from numba import jit, prange, njit
+
+
+def weight_distance(arr, method, decay, sigma, center, spherical, radius):
+    """ Weight the kernel by distance. """
+
+    if center == 0.0:
+        normed = np.linalg.norm(arr)
+    else:
+        normed = np.linalg.norm(arr - np.array([0, 0, center]))
+
+    if normed == 0.0:
+        weight = 1.0
+
+    if method is None or method == "none" or method == "":
+        weight = 1.0
+    elif method == "linear":
+        weight = np.power((1 - decay), normed)
+    elif method == "sqrt":
+        weight = np.power(np.sqrt((1 - decay)), normed)
+    elif method == "power":
+        weight = np.power(np.power((1 - decay), 2), normed)
+    elif method == "log":
+        weight = np.log(normed + 2)
+    elif method == "gaussian":
+        weight = np.exp(-(np.power(normed, 2)) / (2 * np.power(sigma, 2)))
+    else:
+        raise ValueError("Unable to parse parameters distance_calc.")
+
+    if spherical:
+        sqrt_2 = np.sqrt(2)
+        half_sqrt_2 = np.divide(sqrt_2, 2)
+
+        if normed > (radius - half_sqrt_2) and normed < (radius + half_sqrt_2):
+            adjustment = sqrt_2 / normed
+        elif normed > (radius - half_sqrt_2):
+            adjustment = 0.0
+        elif normed < (radius + half_sqrt_2):
+            adjustment = 1.0
+        else:
+            adjustment = 1.0
+
+        return weight * adjustment
+
+    return weight
+
+
+def rotate_kernel(bottom_right):
+    """ Create a whole kernel from a quadrant. """
+
+    size = ((bottom_right.shape[0] - 1) * 2) + 1
+    depth = bottom_right.shape[2]
+    kernel = np.zeros((size, size, depth), dtype="float32")
+
+    top_right = np.flipud(bottom_right)
+    lower_left = np.fliplr(bottom_right)
+    top_left = np.flipud(lower_left)
+
+    kernel[size // 2:, size // 2:, :] = bottom_right
+    kernel[:1 + -size // 2, :1 + -size // 2, :] = top_left
+    kernel[1 + size // 2:, :size // 2, :] = lower_left[1:, :-1, :]
+    kernel[:size // 2, 1 + size // 2:, :] = top_right[:-1, 1:, :]
+
+    return kernel
+
+
+def get_kernel(
+    size,
+    depth=1,
+    hole=False,
+    inverted=False,
+    normalise=True,
+    multi_dimensional=False,
+    multi_dimensional_center=0,
+    spherical=False,
+    distance_weight=None,
+    distance_decay=0.2,
+    distance_sigma=1,
+):
+    """ Get a square kernel for convolutions.
+        returns: kernel, weights, offsets.
+    """
+
+    assert size >= 3, "Kernel must have atleast size 3."
+    assert size % 2 != 0, "Kernel must be an uneven size."
+    assert isinstance(size, int), "Kernel must be an integer."
+    assert depth >= 1, "Depth must be a positive integer"
+    assert isinstance(depth, int), "Depth must be an integer."
+
+    if distance_weight is False:
+        distance_weight = None
+
+    quadrant = np.zeros((1 + size // 2, 1 + size // 2, depth), dtype="float32")
+
+    for idx_x in range(0, quadrant.shape[0]):
+        for idx_y in range(0, quadrant.shape[1]):
+            for idx_z in range(0, quadrant.shape[2]):
+
+                z_value = idx_z if multi_dimensional else 0
+
+                weighted = weight_distance(
+                    np.array([idx_x, idx_y, z_value], dtype="float32"),
+                    method=distance_weight,
+                    decay=distance_decay,
+                    sigma=distance_sigma,
+                    center=multi_dimensional_center,
+                    spherical=spherical,
+                    radius=size // 2,
+                )
+
+                quadrant[idx_x, idx_y, idx_z] = weighted
+    if hole:
+        for idx_z in range(0, quadrant.shape[2]):
+            quadrant[0, 0, idx_z] = 0
+
+    kernel = rotate_kernel(quadrant)
+
+    if distance_weight == "log":
+        kernel = kernel.max() - kernel
+
+    if inverted:
+        kernel = 1 - kernel
+
+    if normalise:
+        if multi_dimensional:
+            summed = kernel.sum()
+            if summed != 0.0:
+                kernel = kernel / summed
+        else:
+            summed = kernel.sum(axis=(0, 1))
+
+            for dim in range(0, depth):
+                kernel[:, :, dim] = kernel[:, :, dim] / summed[dim]
+
+    weights = []
+    offsets = []
+
+    for idx_x in range(0, kernel.shape[0]):
+        for idx_y in range(0, kernel.shape[1]):
+            for idx_z in range(0, kernel.shape[2]):
+                current_weight = kernel[idx_x][idx_y][idx_z]
+
+                if current_weight <= 0.0:
+                    continue
+
+                offsets.append(
+                    [
+                        idx_x - (kernel.shape[0] // 2),
+                        idx_y - (kernel.shape[1] // 2),
+                        idx_z
+                    ]
+                )
+
+                weights.append(current_weight)
+
+    return kernel, np.array(weights, dtype="float32"), np.array(offsets, dtype=int)
+
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def hood_max(values, weights):
+    """ Get the weighted maximum. """
+    idx = np.argmax(np.multiply(values, weights))
+    return values[idx]
+
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def hood_min(values, weights):
+    """ Get the weighted minimum. """
+    max_val = values.max()
+    adjusted_values = np.where(weights == 0.0, max_val, values)
+    idx = np.argmin(np.divide(adjusted_values, weights + 1e-7))
+    return values[idx]
+
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def hood_sum(values, weights):
+    """ Get the weighted sum. """
+    return np.sum(np.multiply(values, weights))
+
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def hood_mode(values, weights):
+    """ Get the weighted sum. """
+    values_ints = np.rint(values)
+    unique = np.unique(values_ints)
+
+    most_occured_value = 0
+    most_occured_weight = -9999.9
+
+    for unique_value in unique:
+        cum_weight = 0
+        for idx in range(values.shape[0]):
+            if values_ints[idx] == unique_value:
+                cum_weight += weights[idx]
+
+        if cum_weight > most_occured_weight:
+            most_occured_weight = cum_weight
+            most_occured_value = unique_value
+
+    return most_occured_value
+
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def hood_contrast(values, weights):
+    """ Get the local contrast. """
+    max_val = values.max()
+    adjusted_values = np.where(weights == 0.0, max_val, values)
+    local_min = np.min(np.divide(adjusted_values, weights + 1e-7))
+    local_max = np.max(np.multiply(values, weights))
+
+    return np.abs(local_max - local_min)
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def hood_quantile(values, weights, quant):
+    """ Get the weighted median. """
+    sort_mask = np.argsort(values)
+    sorted_data = values[sort_mask]
+    sorted_weights = weights[sort_mask]
+    cumsum = np.cumsum(sorted_weights)
+    intersect = (cumsum - 0.5 * sorted_weights) / cumsum[-1]
+    return np.interp(quant, intersect, sorted_data)
+
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def hood_median_absolute_deviation(values, weights):
+    """ Get the median absolute deviation """
+    median = hood_quantile(values, weights, 0.5)
+    absdeviation = np.abs(np.subtract(values, median))
+    return hood_quantile(absdeviation, weights, 0.5)
+
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def hood_z_score(values, center_value, weights):
+    """ Get the local z score ."""
+    std = hood_standard_deviation(values, weights)
+    mean = hood_sum(values, weights)
+
+    return (center_value - mean) / std
+
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def hood_z_score_mad(values, center_value, weights):
+    """ Get the local z score calculated around the MAD. """
+    mad_std = hood_median_absolute_deviation(values, weights) * 1.4826
+    median = hood_quantile(values, weights, 0.5)
+
+    return (center_value - median) / mad_std
+
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def hood_standard_deviation(values, weights):
+    "Get the weighted standard deviation. "
+    summed = hood_sum(values, weights)
+    variance = np.sum(np.multiply(np.power(np.subtract(values, summed), 2), weights))
+    return np.sqrt(variance)
+
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def k_to_size(size):
+    """ Preprocess Sigma Lee limits. """
+    return int(np.rint(-0.0000837834 * size ** 2 + 0.045469 * size + 0.805733))
+
+
+@jit(nopython=True, parallel=True, nogil=True, fastmath=True, inline="always")
+def hood_sigma_lee(values, weights):
+    """ Sigma lee SAR filter. """
+    std = hood_standard_deviation(values, weights)
+    selected_values = np.zeros_like(values)
+    selected_weights = np.zeros_like(weights)
+
+    sigma_mult = 1
+    passed = 0
+    attempts = 0
+    ks = k_to_size(values.size)
+
+    while passed < ks and attempts < 5:
+        for idx, _val in enumerate(values):
+            if values[idx] >= std * sigma_mult and values[idx] <= -std * sigma_mult:
+                selected_values[idx] = values[idx]
+                selected_weights[idx] = weights[idx]
+                passed += 1
+
+        sigma_mult += 1
+        attempts += 1
+
+    if passed < ks:
+        return hood_sum(values, weights)
+
+    sum_of_weights = np.sum(selected_weights)
+
+    if sum_of_weights == 0:
+        return 0
+
+    selected_weights = np.divide(selected_weights, sum_of_weights)
+
+    return hood_sum(selected_values, selected_weights)
+
+
+@jit(nopython=True, nogil=True)
+def pad_array_view(arr, pad_size=1):
+    """ Create a padded view of an array using SAME padding."""
+    # Get original array shape and strides
+    shape = arr.shape
+    strides = arr.strides
+
+    # Compute new shape and strides for the padded view
+    new_shape = (shape[0] + 2 * pad_size, shape[1] + 2 * pad_size, shape[2])
+    new_strides = (strides[0], strides[1], strides[2])
+
+    # Create the padded view using as_strided
+    padded_view = as_strided(arr, shape=new_shape, strides=new_strides)
+
+    # Pad the edges using the SAME padding
+    padded_view[:pad_size, :, :] = padded_view[pad_size:pad_size + 1, :, :]
+    padded_view[-pad_size:, :, :] = padded_view[-pad_size - 1:-pad_size, :, :]
+    padded_view[:, :pad_size, :] = padded_view[:, pad_size:pad_size + 1, :]
+    padded_view[:, -pad_size:, :] = padded_view[:, -pad_size - 1:-pad_size, :]
+
+    return padded_view
+
+
+@jit(nopython=True, nogil=True)
+def pad_array(arr, pad_size=1):
+    """ Pad an array using SAME """
+    # Core
+    arr_padded = np.zeros((arr.shape[0] + int(pad_size * 2), arr.shape[1] + int(pad_size * 2), arr.shape[2]), dtype=arr.dtype)
+    arr_padded[pad_size:-pad_size, pad_size:-pad_size, :] = arr
+
+    # Corners
+    arr_padded[0:pad_size, 0:pad_size, :] = arr[ 0,  0, :]
+    arr_padded[-pad_size:, -pad_size:, :] = arr[-1, -1, :]
+    arr_padded[0:pad_size, -pad_size:, :] = arr[ 0, -1, :]
+    arr_padded[-pad_size:, 0:pad_size, :] = arr[-1,  0, :]
+
+    # Sides
+    for idx in range(0, pad_size):
+        arr_padded[idx, pad_size:-pad_size, :] = arr[ 0,  :, :]
+        arr_padded[-(idx + 1):, pad_size:-pad_size, :] = arr[-1,  :, :]
+        arr_padded[pad_size:-pad_size, -(idx + 1), :] = arr[ :, -1, :]
+        arr_padded[pad_size:-pad_size, idx, :] = arr[ :,  0, :]
+
+    return arr_padded
+
+METHOD_ENUMS = {
+    "sum": 1,
+    "mode": 2,
+    "max": 3,
+    "min": 4,
+    "contrast": 5,
+    "median": 6,
+    "std": 7,
+    "mad": 8,
+    "z_score": 9,
+    "z_score_mad": 10,
+    "sigma_lee": 11,
+}
+
+@jit(nopython=True, parallel=True, nogil=False, fastmath=True, cache=True)
+def convolve_array(
+    arr,
+    offsets,
+    weights,
+    method=1,
+    nodata=False,
+    nodata_value=-9999.9,
+):
+    """ Convolve an image with a function. """
+    x_adj = arr.shape[0] - 1
+    y_adj = arr.shape[1] - 1
+    z_adj = (arr.shape[2] - 1) // 2
+
+    hood_size = len(offsets)
+
+    result = np.zeros((arr.shape[0], arr.shape[1], 1), dtype="float32")
+
+    for idx_x in prange(arr.shape[0]):
+        for idx_y in range(arr.shape[1]):
+
+            hood_values = np.zeros(hood_size, dtype="float32")
+
+            hood_weights = np.zeros(hood_size, dtype="float32")
+            weight_sum = np.array([0.0], dtype="float32")
+            center_value = np.float32(arr[idx_x, idx_y, 0])
+            normalise = False
+
+            for idx_n in range(hood_size):
+                offset_x = idx_x + offsets[idx_n][0]
+                offset_y = idx_y + offsets[idx_n][1]
+                offset_z = offsets[idx_n][2]
+
+                outside = False
+
+                if offset_z < -z_adj:
+                    offset_z = -z_adj
+                    outside = True
+                elif offset_z > z_adj:
+                    offset_z = z_adj
+                    outside = True
+
+                if offset_x < 0:
+                    offset_x = 0
+                    outside = True
+                elif offset_x > x_adj:
+                    offset_x = x_adj
+                    outside = True
+
+                if offset_y < 0:
+                    offset_y = 0
+                    outside = True
+                elif offset_y > y_adj:
+                    offset_y = y_adj
+                    outside = True
+
+                value = np.float32(arr[offset_x, offset_y, offset_z])
+
+                if outside is True:
+                    normalise = True
+                    hood_weights[idx_n] = 0
+                elif nodata and value == nodata_value:
+                    normalise = True
+                    hood_weights[idx_n] = 0
+                else:
+                    hood_values[idx_n] = value
+
+                    weight = weights[idx_n]
+
+                    hood_weights[idx_n] = weight
+                    weight_sum[0] += weight
+
+            if normalise and weight_sum[0] > 0.0:
+                hood_weights = np.true_divide(hood_weights, weight_sum[0])
+
+            if method == 1:
+                result[idx_x, idx_y, 0] = hood_sum(hood_values, hood_weights)
+            elif method == 2:
+                result[idx_x, idx_y, 0] = hood_mode(hood_values, hood_weights)
+            elif method == 3:
+                result[idx_x, idx_y, 0] = hood_max(hood_values, hood_weights)
+            elif method == 4:
+                result[idx_x, idx_y, 0] = hood_min(hood_values, hood_weights)
+            elif method == 5:
+                result[idx_x, idx_y, 0] = hood_contrast(hood_values, hood_weights)
+            elif method == 6:
+                result[idx_x, idx_y, 0] = hood_quantile(hood_values, hood_weights, 0.5)
+            elif method == 7:
+                result[idx_x, idx_y, 0] = hood_standard_deviation(hood_values, hood_weights)
+            elif method == 8:
+                result[idx_x, idx_y, 0] = hood_median_absolute_deviation(hood_values, hood_weights)
+            elif method == 9:
+                result[idx_x, idx_y, 0] = hood_z_score(hood_values, center_value, hood_weights)
+            elif method == 10:
+                result[idx_x, idx_y, 0] = hood_z_score_mad(hood_values, center_value, hood_weights)
+            elif method == 11:
+                result[idx_x, idx_y, 0] = hood_sigma_lee(hood_values, hood_weights)
+            else:
+                result[idx_x, idx_y, 0] = nodata_value
+
+    return result
