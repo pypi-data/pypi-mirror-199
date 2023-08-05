8e56810b9deb24e300114d83d9d10c3bb4dafdfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:47 +0000

    Add package pyqtclass/0.0.2/pyqtclass-0.0.2.tar.gz

---
+++ packages/pyqtclass/pyqtclass-0.0.2.tar.gz/pyqtclass-0.0.2/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyqtclass",
+    version="0.0.2",
+    author="innovata sambong",
+    author_email="iinnovata@gmail.com",
+    description='PyQt 기반으로 동작하는 객체들의 베이스클래스 모음 패키지',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/innovata/PyQtClasses",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
+)
