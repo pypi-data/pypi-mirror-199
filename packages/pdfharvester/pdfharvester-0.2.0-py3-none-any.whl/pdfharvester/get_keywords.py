71b02b5f49f7bee75c92e13b6d39ec0e5330c9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:46 +0000

    Add package pdfharvester/0.2.0/pdfharvester-0.2.0-py3-none-any.whl

---
+++ packages/pdfharvester/pdfharvester-0.2.0-py3-none-any.whl/pdfharvester/get_keywords.py
@@ -0,0 +1,35 @@
+import PyPDF2
+import pandas as pd
+import yake
+import textract
+
+
+def get_keywords(file_path, max_ngram_size=3, deduplication_threshold=0.9, numOfKeywords=50):
+    # open the pdf file in binary mode
+    with open(file_path, 'rb') as pdf_file:
+        # create a PDF reader object
+        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+        # get the number of pages
+        num_pages = pdf_reader.numPages
+        # initialize the text variable
+        text = ""
+        # loop through each page and extract the text
+        for page_num in range(num_pages):
+            page_obj = pdf_reader.getPage(page_num)
+            text += page_obj.extractText()
+
+        # if no text was extracted, use textract to extract text from scanned PDFs
+        if not text:
+            text = textract.process(
+                file_path, method='tesseract', language='eng')
+
+        # initialize the keyword extractor object
+        kw_extractor = yake.KeywordExtractor(
+            lan="en", n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+
+        # extract the keywords and store them in a DataFrame
+        keywords = kw_extractor.extract_keywords(text)
+        keywords_df = pd.DataFrame(keywords, columns=['keywords', 'value'])
+
+    # return the keywords DataFrame
+    return keywords_df
