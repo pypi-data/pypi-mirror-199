67042f36cb707d6812009d2937f4da86635666be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:45 +0000

    Add package gamla/142/gamla-142.tar.gz

---
+++ packages/gamla/gamla-142.tar.gz/gamla-142/gamla/higher_order_test.py
@@ -0,0 +1,29 @@
+from gamla import higher_order
+
+
+def test_prepare_and_apply():
+    def increment(x):
+        return x + 1
+
+    def decrement(x):
+        return x - 1
+
+    def conditional_transformation(x):
+        return increment if x < 10 else decrement
+
+    assert higher_order.prepare_and_apply(conditional_transformation)(15) == 14
+
+
+async def test_prepare_and_apply_async():
+    async def increment(x):
+        return x + 1
+
+    async def decrement(x):
+        return x - 1
+
+    def conditional_transformation(x):
+        return increment if x < 10 else decrement
+
+    assert (
+        await higher_order.prepare_and_apply_async(conditional_transformation)(15) == 14
+    )
