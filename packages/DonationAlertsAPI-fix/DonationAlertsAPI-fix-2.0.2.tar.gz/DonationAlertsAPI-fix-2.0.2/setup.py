9ffe3a35bf2885fc93d9c9081108ce334720f3cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:08 +0000

    Add package DonationAlertsAPI-fix/2.0.2/DonationAlertsAPI-fix-2.0.2.tar.gz

---
+++ packages/DonationAlertsAPI-fix/DonationAlertsAPI-fix-2.0.2.tar.gz/DonationAlertsAPI-fix-2.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='DonationAlertsAPI-fix',
+	version='2.0.2',
+	author='febriguns',
+	author_email='febriguns@bk.ru',
+	description='DonationAlertsAPI-2.0.2 FIX',
+	packages=['DonationAlertsAPI-fix'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
