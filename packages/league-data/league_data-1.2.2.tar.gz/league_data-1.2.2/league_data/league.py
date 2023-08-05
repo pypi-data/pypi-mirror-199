810d4bb48fbc4110f1042e2a6cd9d452ec2479fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:21 +0000

    Add package league-data/1.2.2/league_data-1.2.2.tar.gz

---
+++ packages/league-data/league_data-1.2.2.tar.gz/league_data-1.2.2/league_data/league.py
@@ -0,0 +1,72 @@
+"""Tenha acesso facilmente aos dados dos campeões e skins do League of Legends.
+
+GitHub:
+    https://github.com/controlado/league-data
+
+Discord:
+    `Balaclava#1912`
+    `854886148455399436`
+
+Licença:
+    `GNU` `Version 2`
+"""
+
+from requests import Timeout, request
+
+from league_data import Explorer
+from league_data.models import Champion, Skin
+
+
+class League:
+    """Tenha acesso facilmente aos dados dos campeões e skins do League of Legends.
+
+    Args:
+        data (dict, optional): Dados da Riot.
+
+    Attributes:
+        data (dict): Dados da Riot.
+        explorer (Explorer): Explorador especializado de dados.
+    """
+
+    @staticmethod
+    def get_data() -> dict:
+        """Busca todos os dados dos campeões do League of Legends.
+
+        Uma requisição é feita para conseguir esses dados.
+
+        Raises:
+            ConnectionError: Caso não consiga realizar a requisição.
+
+        Returns:
+            data (dict): Os dados dos campeões e suas skins.
+        """
+        url = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/skins.json"
+
+        try:
+            response = request(method="get", url=url, timeout=10)
+        except Timeout as exception:
+            message = "A request demorou muito para ser finalizada..."
+            raise ConnectionError(message) from exception
+
+        return response.json()
+
+    def __init__(self, data: dict = None) -> None:
+        """Cria o um explorador automaticamente para a instância.
+
+        Caso não receba o parâmetro data, o mesmo será requisitado automaticamente.
+        """
+        self.data = data or self.get_data()
+        self.explorer = Explorer(self.data)
+
+    def __getitem__(self, name: str) -> Champion | Skin | None:
+        """Retorna os dados do campeão ou skin, caso exista.
+
+        Args:
+            name (str): Nome do campeão ou skin.
+
+        Returns:
+            Champion (Champion): Objeto do campeão encontrado.
+            Skin (Skin): Objeto da skin encontrada.
+            None (None): Nenhum item foi encontrado.
+        """
+        return self.explorer[name]
---
+++ packages/league-data/league_data-1.2.2.tar.gz/league_data-1.2.2/league_data/models.py
@@ -0,0 +1,144 @@
+"""Modelos e objetos que contém os dados dos campeões e skins.
+
+GitHub:
+    https://github.com/controlado/league-data
+
+Discord:
+    `Balaclava#1912`
+    `854886148455399436`
+
+Licença:
+    `GNU` `Version 2`
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:  # apenas para tipagem estática
+    from league_data.explorer import Explorer
+
+
+class Champion:
+    """Objeto que contém as informações de um campeão.
+
+    Args:
+        explorer (Explorer): Explorador que buscou esse campeão.
+        data (dict): Os dados desse campeão.
+
+    Attributes:
+        explorer (Explorer): Explorador que buscou esse campeão.
+        data (dict): Os dados desse campeão.
+
+    Example:
+        ```python linenums="1"
+        from league_data import League
+
+        league = League()
+        irelia = league["irelia"]  # -> <league_data.models.Champion object at ...>
+        ```
+    """
+
+    def __init__(self, explorer: Explorer, data: dict) -> None:
+        """Instancia o objeto contendo as informações do campeão."""
+        self.explorer = explorer
+        self.data = data
+
+    def __getitem__(self, value: str) -> Any:
+        """Retorna um valor do dicionário do campeão."""
+        return self.data.get(value)
+
+    def __str__(self) -> str:
+        """Forma string da classe."""
+        return self.name
+
+    @property
+    def identity(self) -> str:
+        """ID do campeão."""
+        return self.data["id"]
+
+    @property
+    def name(self) -> str:
+        """Nome do campeão."""
+        return self.data["name"]
+
+    @property
+    def art(self) -> str:
+        """URL da arte do campeão."""
+        return self.data["art"]
+
+    @property
+    def skins(self) -> list[Skin]:
+        """Skins que o campeão possui."""
+        return [
+            Skin(
+                explorer=self.explorer,
+                champion=self.data,
+                data=self.data["skins"][skin],
+            )
+            for skin in self.data["skins"]
+        ]
+
+
+class Skin:
+    """Objeto que contém as informações de uma skin.
+
+    Args:
+        explorer (Explorer): Explorador que buscou essa skin.
+        champion (dict): Dados do campeão que possui essa skin.
+        data (dict): Os dados dessa skin.
+
+    Attributes:
+        explorer (Explorer): Explorador que buscou essa skin.
+        champion_data (dict): Dados do campeão que possui essa skin.
+        data (dict): Os dados dessa skin.
+
+    Example:
+        ```python linenums="1"
+        from league_data import League
+
+        league = League()
+        vayne = league["vayne"]
+        vayne_skins = vayne.skins  # -> [<league_data.models.Skin object>, ...]
+        vayne_skins = vayne["skins"]  # -> {"aristocrat vayne": {"id": ...}, ...}
+        ```
+    """
+
+    def __init__(self, explorer: Explorer, champion: dict, data: dict) -> None:
+        """Instancia o objeto contendo as informações da skin."""
+        self.explorer = explorer
+        self.champion_data = champion
+        self.data = data
+
+    def __getitem__(self, value: str) -> Any:
+        """Retorna um valor do dicionário da skin."""
+        return self.data.get(value)
+
+    def __str__(self) -> str:
+        """Forma string da classe."""
+        return self.name
+
+    @property
+    def identity(self) -> str:
+        """ID da skin."""
+        return self.data["id"]
+
+    @property
+    def name(self) -> str:
+        """Nome da skin."""
+        return self.data["name"]
+
+    @property
+    def art(self) -> str:
+        """URL da arte da skin."""
+        return self.data["art"]
+
+    @property
+    def rarity(self) -> str:
+        """Raridade da skin."""
+        return self.data["rarity"]
+
+    @property
+    def champion(self) -> Champion:
+        """O campeão que possui essa skin."""
+        return Champion(self.explorer, self.champion_data)
