7320d8f2add3f2f183f1243f37b3a5d86466b7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:48 +0000

    Add package personal-knowledge-library/0.9.4/personal_knowledge_library-0.9.4.tar.gz

---
+++ packages/personal-knowledge-library/personal_knowledge_library-0.9.4.tar.gz/personal_knowledge_library-0.9.4/knowledge/base/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2021 Wacom. All rights reserved.
+"""
+Base structures.
+"""
+__all__ = ['access', 'entity', 'ontology']
+
+from knowledge.base import access
+from knowledge.base import entity
+from knowledge.base import ontology
