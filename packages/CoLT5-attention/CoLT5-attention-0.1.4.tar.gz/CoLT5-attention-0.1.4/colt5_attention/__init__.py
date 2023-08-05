4fa748f75193a337085569aa0f0a86075f0edc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:26 +0000

    Add package CoLT5-attention/0.1.4/CoLT5-attention-0.1.4.tar.gz

---
+++ packages/CoLT5-attention/CoLT5-attention-0.1.4.tar.gz/CoLT5-attention-0.1.4/colt5_attention/__init__.py
@@ -0,0 +1,7 @@
+from colt5_attention.transformer_block import (
+    ConditionalRoutedFeedForward,
+    ConditionalRoutedAttention,
+    ConditionalRoutedTransformerBlock
+)
+
+from colt5_attention.coor_descent import coor_descent
