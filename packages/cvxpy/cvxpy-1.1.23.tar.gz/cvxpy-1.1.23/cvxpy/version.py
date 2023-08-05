18c39c59d4ba2f4f5f681aae1492f581f51b542b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:41 +0000

    Add package cvxpy/1.1.23/cvxpy-1.1.23.tar.gz

---
+++ packages/cvxpy/cvxpy-1.1.23.tar.gz/cvxpy-1.1.23/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.1.23'
+version = '1.1.23'
+full_version = '1.1.23'
+git_revision = 'd89870d'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
