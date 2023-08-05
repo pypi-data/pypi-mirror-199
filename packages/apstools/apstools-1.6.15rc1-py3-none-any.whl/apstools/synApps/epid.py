77e73b4ef9a6dd9e10f6ff1bee47ba61bb498b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:17 +0000

    Add package apstools/1.6.15rc1/apstools-1.6.15rc1-py3-none-any.whl

---
+++ packages/apstools/apstools-1.6.15rc1-py3-none-any.whl/apstools/synApps/epid.py
@@ -0,0 +1,76 @@
+"""
+Ophyd support for the EPICS epid record
+
+
+Public Structures
+
+.. autosummary::
+
+    ~EpidRecord
+
+:see: https://epics.anl.gov/bcda/synApps/std/epidRecord.html
+"""
+
+from ophyd import Component
+from ophyd import EpicsSignal
+from ophyd import EpicsSignalRO
+
+from ._common import EpicsRecordDeviceCommonAll
+from ._common import EpicsRecordFloatFields
+
+
+class EpidRecord(EpicsRecordFloatFields, EpicsRecordDeviceCommonAll):
+    """
+    EPICS synApps epid record support in ophyd
+
+    .. index:: Ophyd Device; synApps EpidRecord
+
+    :see: https://epics.anl.gov/bcda/synApps/std/epidRecord.html
+    """
+
+    controlled_value_link = Component(EpicsSignal, ".INP", kind="config")
+    controlled_value = Component(EpicsSignalRO, ".CVAL", kind="config")
+
+    readback_trigger_link = Component(EpicsSignal, ".TRIG", kind="config")
+    readback_trigger_link_value = Component(EpicsSignal, ".TVAL", kind="config")
+
+    setpoint_location = Component(EpicsSignal, ".STPL", kind="config")
+    setpoint_mode_select = Component(EpicsSignal, ".SMSL", kind="config")
+
+    output_location = Component(EpicsSignal, ".OUTL", kind="config")
+    feedback_on = Component(EpicsSignal, ".FBON", kind="config")
+
+    proportional_gain = Component(EpicsSignal, ".KP", kind="config")
+    integral_gain = Component(EpicsSignal, ".KI", kind="config")
+    derivative_gain = Component(EpicsSignal, ".KD", kind="config")
+
+    following_error = Component(EpicsSignalRO, ".ERR", kind="config")
+    output_value = Component(EpicsSignalRO, ".OVAL", kind="config")
+    final_value = Component(EpicsSignalRO, ".VAL", kind="normal")
+
+    calculated_P = Component(EpicsSignalRO, ".P", kind="config")
+    calculated_I = Component(EpicsSignal, ".I", kind="config")
+    calculated_D = Component(EpicsSignalRO, ".D", kind="config")
+
+    time_difference = Component(EpicsSignal, ".DT", kind="config")
+    minimum_delta_time = Component(EpicsSignal, ".MDT", kind="config")
+
+    # limits imposed by the record support:
+    #     .LOPR <= .OVAL <= .HOPR
+    #     .LOPR <= .I <= .HOPR
+    high_limit = Component(EpicsSignal, ".DRVH", kind="config")
+    low_limit = Component(EpicsSignal, ".DRVL", kind="config")
+
+    @property
+    def value(self):
+        return self.output_value.get()
+
+# -----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     jemian@anl.gov
+# :copyright: (c) 2017-2023, UChicago Argonne, LLC
+#
+# Distributed under the terms of the Argonne National Laboratory Open Source License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# -----------------------------------------------------------------------------
---
+++ packages/apstools/apstools-1.6.15rc1-py3-none-any.whl/apstools/synApps/tests/test_swait.py
@@ -0,0 +1,59 @@
+import pytest
+
+from ...tests import IOC
+from ...tests import common_attribute_quantities_test
+from ...tests import timed_pause
+from ..swait import SwaitRecord
+from ..swait import UserCalcN
+from ..swait import UserCalcsDevice
+from ..swait import setup_random_number_swait
+
+
+@pytest.mark.parametrize(
+    "device, pv, connect, attr, expected",
+    [
+        [SwaitRecord, f"{IOC}userCalc10", False, "read_attrs", 12],
+        [SwaitRecord, f"{IOC}userCalc10", False, "configuration_attrs", 72],
+        [SwaitRecord, f"{IOC}userCalc10", True, "read()", 1],
+        [SwaitRecord, f"{IOC}userCalc10", True, "summary()", 162],
+
+        [UserCalcN, f"{IOC}userCalc10", False, "read_attrs", 12],
+        [UserCalcN, f"{IOC}userCalc10", False, "configuration_attrs", 73],
+        [UserCalcN, f"{IOC}userCalc10", True, "read()", 1],
+        [UserCalcN, f"{IOC}userCalc10", True, "summary()", 164],
+
+        [UserCalcsDevice, IOC, False, "read_attrs", 130],
+        [UserCalcsDevice, IOC, False, "configuration_attrs", 741],
+        [UserCalcsDevice, IOC, True, "read()", 10],
+        [UserCalcsDevice, IOC, True, "summary()", 1496],
+    ]
+)
+def test_attribute_quantities(device, pv, connect, attr, expected):
+    """Verify the quantities of the different attributes."""
+    common_attribute_quantities_test(device, pv, connect, attr, expected)
+
+
+def test_userCalcs_reset():
+    user = UserCalcsDevice(IOC, name="user")
+    user.wait_for_connection()
+    user.enable.put("Enable")
+
+    swait = user.calc10
+    assert isinstance(swait, SwaitRecord)
+    swait.enable.put("E")  # Note: only "E"
+
+    setup_random_number_swait(swait)
+    timed_pause(0.2)
+    assert swait.description.get() == "uniform random numbers"
+    assert swait.calculation.get() == "RNDM"
+    v1 = swait.calculated_value.get()
+    timed_pause(0.2)
+    assert v1 != swait.calculated_value.get()
+
+    swait.reset()
+    timed_pause()
+    assert swait.description.get() == swait.prefix
+    assert swait.calculation.get() == "0"
+    v1 = swait.calculated_value.get()
+    timed_pause(0.2)
+    assert v1 == swait.calculated_value.get()
