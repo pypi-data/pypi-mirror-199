c6a12e3bfc4ee46e1c0aba4c53b20ec1d7b37fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:17 +0000

    Add package loadwave/0.4.5/loadwave-0.4.5.tar.gz

---
+++ packages/loadwave/loadwave-0.4.5.tar.gz/loadwave-0.4.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name="loadwave",
+	version="0.4.5",
+	description="Loader Animation",
+	author="r4isy",
+	author_email="r4isy@kenucorp.com",
+	packages=["loadwave"],
+	install_requires=["colorama"]
+	)
