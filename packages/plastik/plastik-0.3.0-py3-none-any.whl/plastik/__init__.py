97b28715acf0327c54e44d2537f6e1ea18bd11de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:02 +0000

    Add package plastik/0.3.0/plastik-0.3.0-py3-none-any.whl

---
+++ packages/plastik/plastik-0.3.0-py3-none-any.whl/plastik/__init__.py
@@ -0,0 +1,7 @@
+from importlib_metadata import version
+
+from plastik.axes import *  # noqa:F401,F403
+from plastik.legends import *  # noqa:F401,F403
+from plastik.ridge import *  # noqa:F401,F403
+
+__version__ = version(__package__)
