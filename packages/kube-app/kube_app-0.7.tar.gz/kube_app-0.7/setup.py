db33c2a851abbad64f9792e4c4f2cc97b0290455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:32 +0000

    Add package kube-app/0.7/kube_app-0.7.tar.gz

---
+++ packages/kube-app/kube_app-0.7.tar.gz/kube_app-0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='kube_app',
+    version='0.7',
+    packages=['kube_app'],
+    license='',
+    author='lalit',
+    author_email='lalit.krishna@kockpit.in',
+    description='kube app sdk'
+)
