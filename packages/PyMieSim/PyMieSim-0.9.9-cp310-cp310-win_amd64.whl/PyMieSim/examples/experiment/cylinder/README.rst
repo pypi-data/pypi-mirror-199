61e9321887de1bbb44ca092ec80f928f32424010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:50 +0000

    Add package PyMieSim/0.9.9/PyMieSim-0.9.9-cp310-cp310-win_amd64.whl

---
+++ packages/PyMieSim/PyMieSim-0.9.9-cp310-cp310-win_amd64.whl/PyMieSim/examples/experiment/cylinder/README.rst
@@ -0,0 +1,6 @@
+-----
+
+PyMieSim.Examples.Experiment.cylinder
+=====================================
+
+General examples of the `PyMieSim.Experiment` subpackage.
---
+++ packages/PyMieSim/PyMieSim-0.9.9-cp310-cp310-win_amd64.whl/PyMieSim/examples/validation/plot_pymiescatt_2.py
@@ -0,0 +1,58 @@
+"""
+=========================
+PyMieSim vs PyMieScatt: 2
+=========================
+
+"""
+
+import numpy
+import matplotlib.pyplot as plt
+
+from PyMieSim.tools.directories import validation_data_path
+from PyMieSim.experiment import SourceSet, CoreShellSet, Setup
+from PyMieSim import measure
+
+
+theoretical = numpy.genfromtxt(f"{validation_data_path}/PyMieScattQscaCoreShell.csv", delimiter=',')
+
+diameter = numpy.geomspace(10e-9, 500e-9, 400)
+
+scatterer_set = CoreShellSet(
+    core_diameter=diameter,
+    shell_diameter=600e-9,
+    core_index=1.4,
+    shell_index=1.5,
+    n_medium=1.0
+)
+
+source_set = SourceSet(
+    wavelength=600e-9,
+    polarization=[0],
+    amplitude=1
+)
+
+experiment = Setup(
+    scatterer_set=scatterer_set,
+    source_set=source_set,
+    detector_set=None
+)
+
+data = experiment.Get(measures=measure.Qsca)
+data = data._data.squeeze()
+
+plt.figure(figsize=(8, 4))
+plt.plot(diameter, data, 'C1-', linewidth=3, label='PyMieSim')
+
+plt.plot(diameter, theoretical, 'k--', linewidth=1, label='PyMieScatt')
+
+plt.xlabel(r'diameter [$\mu$m]')
+plt.ylabel('Scattering efficiency [CoreShell]')
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+assert numpy.all(numpy.isclose(data, theoretical, 1e-9)), 'Error: mismatch on PyMieScatt calculation occuring'
+
+
+# -
