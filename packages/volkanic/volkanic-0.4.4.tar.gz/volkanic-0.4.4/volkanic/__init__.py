25ef545a285afc6138685a615d964aa55c31f53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:06 +0000

    Add package volkanic/0.4.4/volkanic-0.4.4.tar.gz

---
+++ packages/volkanic/volkanic-0.4.4.tar.gz/volkanic-0.4.4/volkanic/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+__version__ = '0.4.4'
+
+if __name__ == '__main__':
+    print(__version__)
+
+from volkanic.cmdline import CommandRegistry
+from volkanic.environ import GlobalInterface
