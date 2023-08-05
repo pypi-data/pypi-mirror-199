56ef0e8b5f068bf000b8b02383971ea21bb8465f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:17 +0000

    Add package ewoksjob/0.2.1/ewoksjob-0.2.1.tar.gz

---
+++ packages/ewoksjob/ewoksjob-0.2.1.tar.gz/ewoksjob-0.2.1/src/ewoksjob/tests/test_execute_and_upload.py
@@ -0,0 +1,33 @@
+import time
+import pytest
+from ewokscore.tests.examples.graphs import get_graph
+from ..client import celery
+from ..client import local
+from .utils import get_result
+
+
+def test_submit(ewoks_worker):
+    assert_submit(celery)
+
+
+def test_submit_local(local_ewoks_worker):
+    assert_submit(local)
+
+
+def assert_submit(mod):
+    graph, expected = get_graph("acyclic1")
+    expected = expected["task6"]
+    kwargs = {
+        "upload_parameters": {
+            "metadata_urls": list(),
+            "beamline": "id00",
+            "proposal": f"id00{time.strftime('%y%m')}",
+            "dataset": "testdataset",
+            "path": "/path/to/localed/dataset",
+            "metadata": {"Sample_name": "test"},
+            "raw": "/path/to/raw/dataset",
+        }
+    }
+    future1 = mod.submit(args=(graph,), kwargs=kwargs)
+    with pytest.raises(RuntimeError, match="requires pyicat-plus"):
+        get_result(future1, timeout=10)
