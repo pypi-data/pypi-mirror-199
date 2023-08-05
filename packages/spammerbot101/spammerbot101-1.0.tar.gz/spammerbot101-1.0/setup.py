6915f06813b7160ac824f601da8ac195a468dbbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:14 +0000

    Add package spammerbot101/1.0/spammerbot101-1.0.tar.gz

---
+++ packages/spammerbot101/spammerbot101-1.0.tar.gz/spammerbot101-1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spammerbot101',
+    version='1.0',
+    description='Spam messages easily',
+    author='Sushil Dhakal',
+    author_email='sushilldhakal25@gmail.com',
+    url='https://github.com/sushilld/Loan-acceptance-streamlit',
+    packages=find_packages(),
+    install_requires=[
+        "pyautogui",
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
