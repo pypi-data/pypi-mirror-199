87fb62048650b8babada4502f55d91121586665c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:33 +0000

    Add package kube-app/0.8/kube_app-0.8.tar.gz

---
+++ packages/kube-app/kube_app-0.8.tar.gz/kube_app-0.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='kube_app',
+    version='0.8',
+    packages=['kube_app'],
+    license='MIT',
+    author='lalit',
+    author_email='lalit.krishna@kockpit.in'
+)
