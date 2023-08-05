979f65b7687bed0b42ceab2c7ada32129138cb29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:22 +0000

    Add package sparksnake/0.1.8/sparksnake-0.1.8-py3-none-any.whl

---
+++ packages/sparksnake/sparksnake-0.1.8-py3-none-any.whl/tests/conftest.py
@@ -0,0 +1,31 @@
+"""Confest file for managing pytest fixtures and other components.
+
+This file will handle essential components and elements to be used on test
+scripts along the project, like features and other things.
+
+___
+"""
+
+# Importing libraries
+import sys
+import pytest
+
+from sparksnake.glue import GlueJobManager
+
+from tests.helpers.user_inputs import FAKE_ARGV_LIST, FAKE_DATA_DICT
+
+
+# A GlueJobManager objects
+@pytest.fixture()
+def job_manager() -> GlueJobManager:
+    # Adding system args
+    for fake_arg in FAKE_ARGV_LIST:
+        sys.argv.append(f"--{fake_arg}=a-fake-arg-value")
+
+    # Initializing a class object
+    job_manager = GlueJobManager(
+        argv_list=FAKE_ARGV_LIST,
+        data_dict=FAKE_DATA_DICT
+    )
+
+    return job_manager
