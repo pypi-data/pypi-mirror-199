175dbfb3d30078ab3cf4a77fbc2467efde1d88a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:39 +0000

    Add package netbox-storage/0.0.0.0.931/netbox_storage-0.0.0.0.931-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.931-py3-none-any.whl/netbox_storage/tables/drive.py
@@ -0,0 +1,38 @@
+import django_tables2 as tables
+
+from netbox.tables import (
+    NetBoxTable,
+    ToggleColumn,
+)
+
+from netbox_storage.models import Drive
+
+
+class DriveTable(NetBoxTable):
+    """Table for displaying Drives objects."""
+
+    pk = ToggleColumn()
+    identifier = tables.Column(
+        linkify=True
+    )
+    size = tables.Column(
+        linkify=True
+    )
+    cluster = tables.Column(
+        linkify=True
+    )
+
+    class Meta(NetBoxTable.Meta):
+        model = Drive
+        fields = (
+            "pk",
+            "size",
+            "cluster",
+            "identifier",
+            "description",
+        )
+        default_columns = (
+            "identifier",
+            "size",
+            "cluster",
+        )
