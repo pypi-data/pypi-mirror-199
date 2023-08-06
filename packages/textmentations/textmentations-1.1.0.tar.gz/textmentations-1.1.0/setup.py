07914982dff1491d4e4d58b3ba4f024fd56ab63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:00 +0000

    Add package textmentations/1.1.0/textmentations-1.1.0.tar.gz

---
+++ packages/textmentations/textmentations-1.1.0.tar.gz/textmentations-1.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="textmentations",
+    version="1.1.0",
+    description="A Python library for augmenting Korean text.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Jaesu Han",
+    author_email="gkswotn9753@gmail.com",
+    url="https://github.com/Jaesu26/textmentations",
+    packages=find_packages(exclude=["tests"]),
+    license="MIT",
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+    extras_require={"tests": ["pytest"]},
+    python_requires=">=3.7",
+)
