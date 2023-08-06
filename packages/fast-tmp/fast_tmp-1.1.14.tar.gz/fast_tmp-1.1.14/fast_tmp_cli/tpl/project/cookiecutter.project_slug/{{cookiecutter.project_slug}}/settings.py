bdf94d973681b4ec482538fa2a9e00ca6eb94113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:30 +0000

    Add package fast-tmp/1.1.14/fast_tmp-1.1.14.tar.gz

---
+++ packages/fast-tmp/fast_tmp-1.1.14.tar.gz/fast_tmp-1.1.14/fast_tmp_cli/tpl/project/cookiecutter.project_slug/{{cookiecutter.project_slug}}/settings.py
@@ -0,0 +1,19 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = os.getenv("SECRET_KEY") # todo 环境变量默认配置该参数
+
+DEBUG = True
+
+TORTOISE_ORM = {
+    "connections": {
+        "default": "sqlite://src.sqlite3",
+    },
+    'apps': {
+        'fast_tmp': {
+            'models': ['fast_tmp.models','{{cookiecutter.project_slug}}.models' ],  # 注册app.models
+            'default_connection': 'default',
+        }
+    }
+}
+EXTRA_SCRIPT = []  # 自定义执行脚本
