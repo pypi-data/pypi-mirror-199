3aaad011cf1c08836adc87e7dce029e6106ced9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:48 +0000

    Add package XAISuite/1.0.8/XAISuite-1.0.8-py3-none-any.whl

---
+++ packages/XAISuite/XAISuite-1.0.8-py3-none-any.whl/xaisuite/__init__.py
@@ -0,0 +1,15 @@
+#For creating package
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    dist = get_distribution("XAISuite")
+except DistributionNotFound:
+    __version__ = "Please install XAISuite with setup.py"
+else:
+    __version__ = dist.version
+
+#Package Modules
+from .dataLoader import load_data_CSV, load_data_sklearn, generate_data
+from .hub import train_and_explainModel
+from .analyzer import compare_explanations, compare_explanationssinglef, maxImportanceScoreGenerator
+from models import*
