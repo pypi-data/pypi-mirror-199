63f1664960cecc0185ed7af5549a1b108c0e3da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:10 +0000

    Add package chatairunner/1.0.5/chatairunner-1.0.5.tar.gz

---
+++ packages/chatairunner/chatairunner-1.0.5.tar.gz/chatairunner-1.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chatairunner',
+    version='1.0.5',
+    author='Capsize LLC',
+    description='Chat AI: A chatbot framework',
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="",
+    keywords="ai, chatbot, chat, ai",
+    license="AGPL-3.0",
+    author_email="contact@capsize.gg",
+    url="https://github.com/w4ffl35/chat-ai",
+    package_dir={"": "chatai"},
+    packages=find_packages("chatai"),
+    include_package_data=True,
+    python_requires=">=3.10.0",
+    install_requires=[
+        "aihandler==1.8.11",
+    ]
+)
