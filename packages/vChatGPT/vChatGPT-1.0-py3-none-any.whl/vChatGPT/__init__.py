cb57fbc3e6d3bbed267a8e086f7c3c43c4c4d29e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:12 +0000

    Add package vChatGPT/1.0/vChatGPT-1.0-py3-none-any.whl

---
+++ packages/vChatGPT/vChatGPT-1.0-py3-none-any.whl/vChatGPT/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: "2023-03-26 14:44:26 (ywatanabe)"
+
+__copyright__ = "Copyright (C) 2023 Yusuke Watanabe"
+__version__ = "1.0"
+__license__ = "GPL3.0"
+__author__ = "ywatanabe1989"
+__author_email__ = "ywata1989@gmail.com"
+__url__ = "https://github.com/ywatanabe1989/vChatGPT"
+
