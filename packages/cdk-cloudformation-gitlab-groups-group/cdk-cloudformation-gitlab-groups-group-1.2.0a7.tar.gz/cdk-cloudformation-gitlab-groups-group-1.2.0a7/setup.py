0bb8e276c2bef1879af8cc4343cbc789f9f236b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:28 +0000

    Add package cdk-cloudformation-gitlab-groups-group/1.2.0a7/cdk-cloudformation-gitlab-groups-group-1.2.0a7.tar.gz

---
+++ packages/cdk-cloudformation-gitlab-groups-group/cdk-cloudformation-gitlab-groups-group-1.2.0a7.tar.gz/cdk-cloudformation-gitlab-groups-group-1.2.0a7/setup.py
@@ -0,0 +1,65 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cdk-cloudformation-gitlab-groups-group",
+    "version": "1.2.0.a7",
+    "description": "Creates a group in GitLab",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws-ia/cloudformation-gitlab-resource-providers",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/cdklabs/cdk-cloudformation.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cdk_cloudformation_gitlab_groups_group",
+        "cdk_cloudformation_gitlab_groups_group._jsii"
+    ],
+    "package_data": {
+        "cdk_cloudformation_gitlab_groups_group._jsii": [
+            "gitlab-groups-group@1.2.0-alpha.7.jsii.tgz"
+        ],
+        "cdk_cloudformation_gitlab_groups_group": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.7",
+    "install_requires": [
+        "aws-cdk-lib>=2.70.0, <3.0.0",
+        "constructs>=10.1.292, <11.0.0",
+        "jsii>=1.79.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard~=2.13.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
