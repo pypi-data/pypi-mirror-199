ae42992be90bf5a66d195c021ab6895b7e213a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:43 +0000

    Add package senstream/1.4.8/senstream-1.4.8.tar.gz

---
+++ packages/senstream/senstream-1.4.8.tar.gz/senstream-1.4.8/senstream/report_manager.py
@@ -0,0 +1,5 @@
+# 1. Handles report generation tasks
+# 2. Used to define a scheduler for sending email/text notifications (every day, week, month, year, etc.)
+# 3. Used to configure the content of reports
+# 4. Handles the plotting and data visualization tasks using matplotlib.pyplot
+# 5. Stores recipient information and report naming (e.g. name a report based on site)
