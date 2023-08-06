3dac2719178f6aa328a63f8947c733eff8fc36c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:55 +0000

    Add package httpdbg/0.8.1/httpdbg-0.8.1.tar.gz

---
+++ packages/httpdbg/httpdbg-0.8.1.tar.gz/httpdbg-0.8.1/httpdbg/webapp/app.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+import json
+from http.server import BaseHTTPRequestHandler
+import mimetypes
+import os
+import re
+from urllib.parse import urlparse, parse_qs
+
+from httpdbg.webapp.api import RequestListPayload, RequestPayload
+
+
+class HttpbgHTTPRequestHandler(BaseHTTPRequestHandler):
+    def __init__(self, records, *args, **kwargs):
+        self.records = records
+        super().__init__(*args, **kwargs)
+
+    def do_GET(self):
+        url = urlparse(self.path)
+
+        # we try different method to serve the URL until the good one is done
+        for serve in [
+            self.serve_static,
+            self.serve_requests,
+            self.serve_request,
+            self.serve_request_content_up,
+            self.serve_request_content_down,
+            self.serve_not_found,
+        ]:
+            if serve(url):
+                break
+
+    def serve_static(self, url):
+        if not (
+            (url.path.lower() in ["/", "index.htm", "index.html"])
+            or url.path.startswith("/static/")
+        ):
+            return False
+
+        if url.path.lower() in ["/", "index.htm", "index.html"]:
+            self.path = "/static/index.htm"
+
+        fullpath = os.path.realpath(
+            os.path.join(os.path.dirname(os.path.realpath(__file__)), self.path[1:])
+        )
+
+        if not fullpath.startswith(os.path.dirname(os.path.realpath(__file__))):
+            # if the file is not in the static directory, we don't serve it
+            return self.serve_not_found()
+
+        if not os.path.exists(fullpath):
+            return self.serve_not_found()
+        else:
+            self.send_response(200)
+            self.send_header(
+                "content-type", mimetypes.types_map[os.path.splitext(fullpath)[1]]
+            )
+            self.send_header_no_cache()
+            self.end_headers()
+            with open(fullpath, "rb") as f:
+                self.wfile.write(f.read())
+
+        return True
+
+    def serve_requests(self, url):
+        if not (url.path.lower() == "/requests"):
+            return False
+
+        query = parse_qs(url.query)
+
+        if query.get("id", [""])[0] == self.records.id:
+            self.records.requests_already_loaded = int(
+                query.get("requests_already_loaded", [0])[0]
+            )
+        else:
+            self.records.requests_already_loaded = 0
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header_no_cache()
+        self.end_headers()
+        self.wfile.write(
+            json.dumps(self.records, cls=RequestListPayload).encode("utf-8")
+        )
+
+        return True
+
+    def serve_request(self, url):
+        regexp = r"/request/([\w\-]+)"
+
+        if re.fullmatch(regexp, url.path) is None:
+            return False
+
+        req_id = re.findall(regexp, url.path)[0]
+
+        if req_id not in self.records.requests:
+            return self.serve_not_found()
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header_no_cache()
+        self.end_headers()
+        self.wfile.write(
+            json.dumps(self.records.requests[req_id], cls=RequestPayload).encode(
+                "utf-8"
+            )
+        )
+
+        return True
+
+    def serve_request_content_up(self, url):
+        regexp = r"/request/([\w\-]+)/up"
+
+        if re.fullmatch(regexp, url.path) is None:
+            return False
+
+        req_id = re.findall(regexp, url.path)[0]
+
+        if req_id not in self.records.requests:
+            return self.serve_not_found()
+
+        req = self.records.requests[req_id]
+
+        self.send_response(200)
+        self.send_header("Content-type", "application/octet-stream")
+        self.send_header_no_cache()
+        self.end_headers()
+        self.wfile.write(
+            req.request.content.encode("utf-8")
+            if isinstance(req.request.content, str)
+            else req.request.content
+        )
+
+        return True
+
+    def serve_request_content_down(self, url):
+        regexp = r"/request/([\w\-]+)/down"
+
+        if re.fullmatch(regexp, url.path) is None:
+            return False
+
+        req_id = re.findall(regexp, url.path)[0]
+
+        if req_id not in self.records.requests:
+            return self.serve_not_found()
+
+        req = self.records.requests[req_id]
+
+        self.send_response(200)
+        self.send_header("Content-type", req.response.get_header("Content-Type"))
+        self.send_header_no_cache()
+        self.end_headers()
+        self.wfile.write(req.response.content)
+
+        return True
+
+    def serve_not_found(self, *kwargs):
+        self.send_response(404)
+        self.send_header_no_cache()
+        self.end_headers()
+        self.wfile.write(b"404 Not found")
+
+        return True
+
+    def log_message(self, format, *args):
+        pass
+
+    def send_header_no_cache(self):
+        self.send_header("Cache-Control", "max-age=0, no-cache, no-store, private")
---
+++ packages/httpdbg/httpdbg-0.8.1.tar.gz/httpdbg-0.8.1/httpdbg/webapp/static/render.js
@@ -0,0 +1,189 @@
+"use strict";
+
+var k7_id = null;
+
+async function refresh_resquests() {
+    var table = document.getElementById("requests-list");
+    var template_request = document.getElementById("template_request").innerHTML;
+    var template_initiator = document.getElementById("template_initiator").innerHTML;
+
+    if (global.k7 != k7_id) {
+        clean();
+    };
+    k7_id = global.k7;
+
+    for (const [request_id, request] of Object.entries(global.requests)) {
+        if (request.to_refresh) {
+            var elt = document.getElementById("request-" + request.id);
+            var rendered = Mustache.render(template_request, request);
+            if (!elt) {
+                var elt_initiator = document.getElementById("initiator-" + request.initiator.id);
+                if (!elt_initiator) {
+                    var rendered_initiator = Mustache.render(template_initiator, request);
+                    table.insertAdjacentHTML("beforeend", rendered_initiator);
+                    elt_initiator = document.getElementById("initiator-" + request.initiator.id);
+                };
+                elt_initiator.insertAdjacentHTML("beforeend", rendered);
+            } else {
+                elt.innerHTML = rendered;
+            };
+            request.to_refresh = false;
+        }
+    };
+}
+
+
+function update_with_template(template_id, target_id, data) {
+    var template = document.getElementById(template_id).innerHTML;
+    var rendered = Mustache.render(template, data);
+    document.getElementById(target_id).innerHTML = rendered;
+}
+
+
+function show_request(request_id) {
+
+    var active_rows = document.getElementsByClassName("active-row");
+    [].forEach.call(active_rows, function (el) {
+        el.classList.remove("active-row");
+    });
+
+    document.getElementById("request-" + request_id).classList.add("active-row");
+
+    var data = global.requests[request_id].data;
+
+    update_with_template("template_headers", "headers", data);
+
+    update_with_template("template_cookies", "cookies", data);
+
+    var request = data.request ? data.request : {
+        "body": null
+    };
+
+    if (request.body && request.body.text) {
+        request.body.raw_text = "global.requests['" + request_id + "'].data.request.body.text";
+        if (request.body.parsed) {
+            request.body.parsed_text = "global.requests['" + request_id + "'].data.request.body.parsed";
+        }
+    };
+
+    update_with_template("template_body", "body_sent", request);
+
+    var response = data.response ? data.response : {
+        "body": null
+    };
+
+    if (response.body && response.body.text) {
+        response.body.raw_text = "global.requests['" + request_id + "'].data.response.body.text";
+        if (response.body.parsed) {
+            response.body.parsed_text = "global.requests['" + request_id + "'].data.response.body.parsed";
+        }
+    };
+
+    update_with_template("template_body", "body_received", response);
+
+    update_with_template("template_exception", "exception", data);
+
+    update_with_template("template_stack", "stack", data);
+}
+
+
+function opentab(btn, tabname) {
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabname).style.display = "block";
+    btn.className += " active";
+}
+
+async function disable_link_if_server_disconnected() {
+    var sheet = document.getElementById("serverstatuscss").sheet;
+
+    while (sheet.cssRules.length > 0) {
+        sheet.deleteRule(0);
+    }
+
+    if (global.connected) {
+        sheet.insertRule(".need-server {}");
+        sheet.insertRule(".need-server-info {display: none;}");
+    } else {
+        sheet.insertRule(".need-server {\
+            color: var(--link-server-disconnected);\
+            pointer-events: none;\
+            opacity: 0.5;\
+            text-decoration: none;\
+        }");
+        sheet.insertRule(".need-server-info {\
+             display: inline;\
+             color: var(--link-server-disconnected);\
+             opacity: 0.5;\
+         }");
+    }
+}
+
+async function enable_refresh() {
+
+    while (true) {
+        await Promise.all([
+            disable_link_if_server_disconnected(),
+            refresh_resquests(),
+            wait_for(500),
+        ]);
+    }
+}
+
+function show_wrap_lines(elt) {
+    elt.classList.toggle("prewrap");
+}
+
+function show_raw_data(elt, show_raw_text, raw_text, parsed_text) {
+    var preview;
+
+    if (show_raw_text) {
+        preview = raw_text;
+    } else {
+        preview = parsed_text;
+    }
+
+    elt.textContent = preview;
+}
+
+function clean(force_clean = false) {
+    var onlynew = document.getElementById("onlynew");
+    if (onlynew.checked || force_clean) {
+
+        var initiators = document.getElementsByName("initiator");
+        while (initiators.length > 0) {
+            initiators[0].remove();
+        }
+        document.getElementById("headers").innerHTML = 'select a request to view details';
+        document.getElementById("cookies").innerHTML = 'select a request to view details';
+        document.getElementById("body_sent").innerHTML = 'select a request to view details';
+        document.getElementById("body_received").innerHTML = 'select a request to view details';
+        document.getElementById("exception").innerHTML = 'select a request to view details';
+        document.getElementById("stack").innerHTML = 'select a request to view details';
+
+        var tmprequests = {};
+
+        for (const [request_id, request] of Object.entries(global.requests)) {
+            if (request.pin == "checked") {
+                tmprequests[request_id] = request
+            }
+        };
+
+        global.requests = {};
+
+        for (const [request_id, request] of Object.entries(tmprequests)) {
+            save_request(request_id, request);
+        };
+    }
+}
