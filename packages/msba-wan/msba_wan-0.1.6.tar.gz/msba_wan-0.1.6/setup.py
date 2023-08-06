1f0fc6f07e842815151200603af270b1b7aafbc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:33 +0000

    Add package msba-wan/0.1.6/msba_wan-0.1.6.tar.gz

---
+++ packages/msba-wan/msba_wan-0.1.6.tar.gz/msba_wan-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='msba_wan',
+    version='0.1.6',
+    description='For fun',
+    author='Xuhu Wan',
+    author_email='xuhu.wan@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        "TextBlob",
+        "GoogleNews",
+        "tweepy",
+        "pyLDAvis",
+        "newspaper3k",
+        "wordcloud",
+        "stop_words",
+        "gensim",
+        "nltk"
+    ],
+)
