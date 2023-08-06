c5bb57ebadb802ca02a7969c286236f56520af29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:21 +0000

    Add package PyAlchemista/0.0.2/PyAlchemista-0.0.2.tar.gz

---
+++ packages/PyAlchemista/PyAlchemista-0.0.2.tar.gz/PyAlchemista-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='PyAlchemista',
+    version='0.0.2',
+    author='Siddhu Pendyala',
+    author_email='elcientifico.pendyala@gmail.com',
+    description='A Python library that deals with different fields of science.',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/PyndyalaCoder/PyAlchemista',
+    project_urls = {
+        "Bug Tracker": "https://github.com/PyndyalaCoder/PyAlchemista/issues"
+    },
+    license='MIT',
+    packages=['PyAlchemista', 'Tempore', 'Lunastra'],
+    install_requires=['requests', 'matplotlib', 'numpy'],
+)
