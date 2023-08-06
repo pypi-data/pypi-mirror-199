51a56743f97b38795aa1350685bd847369ff8b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:37 +0000

    Add package logyca/0.0.2/logyca-0.0.2-py3-none-any.whl

---
+++ packages/logyca/logyca-0.0.2-py3-none-any.whl/logyca/__init__.py
@@ -0,0 +1,5 @@
+"""Logyca libraries, apiresult, health check dto"""
+
+__version__ = "0.0.2"
+
+from logyca.data.enums.healthenum import HealthEnum as HealthEnum
---
+++ packages/logyca/logyca-0.0.2-py3-none-any.whl/logyca/data/enums/__init__.py
@@ -0,0 +1,1 @@
+from .healthenum import HealthEnum as HealthEnum
