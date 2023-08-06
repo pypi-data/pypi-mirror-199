5e46db991d83fe0ac39c8cb511f2c1a8b8ee93e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:54 +0000

    Add package ppdb-auth-lib/0.2.0/ppdb_auth_lib-0.2.0.tar.gz

---
+++ packages/ppdb-auth-lib/ppdb_auth_lib-0.2.0.tar.gz/ppdb_auth_lib-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ppdb_auth_lib',
+    packages=find_packages(include=['ppdb_auth_lib']),
+    version='0.2.0',
+    description='Auth Library PPDBV2',
+    author='mrizkyff',
+    license='MIT',
+    install_requires=[
+        'pyjwt',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='test'
+)
