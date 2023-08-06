5eead1a806529a5537bd1cad943af00b71fe96c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:30 +0000

    Add package users-db/0.0.8/users_db-0.0.8.tar.gz

---
+++ packages/users-db/users_db-0.0.8.tar.gz/users_db-0.0.8/users_db/schema.py
@@ -0,0 +1,30 @@
+import enum
+from sqlalchemy import Table, Enum, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Role(enum.Enum):
+    SUPER_ADMIN = 1
+    ADMIN = 2
+    USER = 3
+
+
+role_permission = Table(
+    "role_permissions",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("role", Enum(Role), nullable=False),
+    Column("permission", String(100), nullable=False)
+)
+
+users = Table(
+    "users",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("first_name", String(64), nullable=False),
+    Column("middle_name", String(64), nullable=True),
+    Column("last_name", String(64), nullable=False),
+    Column("role", Enum(Role), nullable=False),
+)
