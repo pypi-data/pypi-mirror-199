a6d31a852dc147fe0adefce0c427cc5697dd537f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:17 +0000

    Add package AIpictures/1.0.2/AIpictures-1.0.2.tar.gz

---
+++ packages/AIpictures/AIpictures-1.0.2.tar.gz/AIpictures-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AIpictures',
+    version='1.0.2',
+    description='My awesome package',
+    author='Justin Alter',
+    author_email='jane@example.com',
+    install_requires=[
+        'openai',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'AIpictures=pictures:get_response',
+        ],
+    },
+)
