bc89163d26ec719d9d686b462909a753512349d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:51 +0000

    Add package EuclidSearchPackage/0.3.2/EuclidSearchPackage-0.3.2-py3-none-any.whl

---
+++ packages/EuclidSearchPackage/EuclidSearchPackage-0.3.2-py3-none-any.whl/Test/Test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/28 11:43
+# @Author  : Euclid-Jie
+# @File    : Test.py
+from src.EuclidSearchPackage import *
+
+res = Get_user_info('1202150843')
+print(res)
