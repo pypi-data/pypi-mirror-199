bf334f40b88c95f1741fc97b3fd9f001fc8781d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:00 +0000

    Add package pg3d/0.0.4/pg3d-0.0.4-py3-none-any.whl

---
+++ packages/pg3d/pg3d-0.0.4-py3-none-any.whl/pg3d/__init__.py
@@ -0,0 +1,6 @@
+from pg3d.point import Point
+from pg3d.app import App
+from pg3d.triangle import Triangle
+from pg3d.shape import Shape
+from pg3d.model import Model
+from pg3d.MatrixMath import matrix as mm
---
+++ packages/pg3d/pg3d-0.0.4-py3-none-any.whl/pg3d/triangle.py
@@ -0,0 +1,35 @@
+from pg3d.point import Point
+import pygame as pg
+
+
+class Triangle:
+    def __init__(self, app, vertices):
+        self.points = [
+            Point(app, vertices[0], False),
+            Point(app, vertices[1], False),
+            Point(app, vertices[2], False),
+        ]
+        self.projected_points = []
+        self.app = app
+        self.app._add_triangle(self)
+
+    def _project(self):
+        self.projected_points = []
+        for point in self.points:
+            projected = point._project(
+                self.app.projection_matrix, self.app.camera._cam_mat()
+            )
+            if projected != None:
+                self.projected_points.append(projected)
+
+        self._draw_triangle()
+
+    def _draw_triangle(self):
+        if len(self.projected_points) == 3:
+            a, b, c = self.projected_points
+            pg.draw.polygon(
+                self.app.screen, self.app.LINE_COLOR, (a[:-1], b[:-1], c[:-1]), 1
+            )
+
+    def __getitem__(self, index):
+        return self.points[index]
