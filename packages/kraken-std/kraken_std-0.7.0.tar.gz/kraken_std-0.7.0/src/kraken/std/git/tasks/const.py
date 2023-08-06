6b638eba599b96dd89f6ae103517f36298e5f424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:39 +0000

    Add package kraken-std/0.7.0/kraken_std-0.7.0.tar.gz

---
+++ packages/kraken-std/kraken_std-0.7.0.tar.gz/kraken_std-0.7.0/src/kraken/std/git/tasks/const.py
@@ -0,0 +1,26 @@
+GITIGNORE_TASK_NAME = "gitignore"
+
+DEFAULT_KRAKEN_GITIGNORE_PATHS = [
+    "## Kraken",
+    "/build",
+]
+
+DEFAULT_GITIGNORE_TOKENS = [
+    # Platforms
+    "macos",
+    "linux",
+    "windows",
+    # IDEs
+    "visualstudiocode",
+    "vim",
+    "emacs",
+    "clion",
+    "intellij",
+    "pycharm",
+    "jupyternotebooks",
+    # Tooling
+    "git",
+    "gcov",
+    "node",
+    "yarn",
+]
