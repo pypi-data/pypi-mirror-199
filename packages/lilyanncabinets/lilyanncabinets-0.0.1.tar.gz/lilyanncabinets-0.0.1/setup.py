7d492552716cf2cf47365494674174c66a78b705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:04 +0000

    Add package lilyanncabinets/0.0.1/lilyanncabinets-0.0.1.tar.gz

---
+++ packages/lilyanncabinets/lilyanncabinets-0.0.1.tar.gz/lilyanncabinets-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lilyanncabinets',
+    version='0.0.1',
+    packages=find_packages(),
+    description='Python API for Lily Ann Cabinets',
+    author='Joseph Caligiuri',
+    author_email='josephcaligiuri1@gmail.com',
+)
