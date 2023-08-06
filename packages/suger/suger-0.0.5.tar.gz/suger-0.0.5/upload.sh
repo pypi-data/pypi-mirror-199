79d4cc4d46fc8cf45071be3aa550ea70eb9e8c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:49 +0000

    Add package suger/0.0.5/suger-0.0.5.tar.gz

---
+++ packages/suger/suger-0.0.5.tar.gz/suger-0.0.5/upload.sh
@@ -0,0 +1,11 @@
+rmdir dist/
+
+py -m pip install --upgrade pip
+
+py -m pip install --upgrade build
+
+ py -m build
+
+py -m pip install --upgrade twine
+
+ py -m twine upload --repository pypi dist/*
