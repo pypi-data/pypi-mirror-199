752aa3d732e6c81c62eca84924685fdb3a093821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:40 +0000

    Add package ocacaptcha/0.0.6/ocacaptcha-0.0.6.tar.gz

---
+++ packages/ocacaptcha/ocacaptcha-0.0.6.tar.gz/ocacaptcha-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='ocacaptcha',
+  version='0.0.6',
+  description='Solving capctha hcapctha, recaptcha, funcaptcha, TikTok capctha)',
+  url='',  
+  author='Nazarii',
+  author_email='nazar.muxaulyk961@mail.ru',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='tiktokcaptcha', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
