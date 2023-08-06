0d63144fafe86641cdb65823cb43d5ae29d3b412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:23 +0000

    Add package trengx/0.0.2/trengx-0.0.2.tar.gz

---
+++ packages/trengx/trengx-0.0.2.tar.gz/trengx-0.0.2/setup.py
@@ -0,0 +1,19 @@
+# python setup.py sdist bdist_wheel
+# pip install twine
+# twine upload dist/*
+# username: jun-trengx
+# password: B******9
+
+
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='trengx',
+    version='0.0.2',
+    author='Yong-Jun Shin',
+    packages=find_packages(),
+    install_requires=[
+    ]
+)
