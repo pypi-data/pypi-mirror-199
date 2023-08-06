21d8605e00f19084e5d65741caf21d441e07feab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:58 +0000

    Add package kicad-netlist-reader/7.0.1/kicad_netlist_reader-7.0.1.tar.gz

---
+++ packages/kicad-netlist-reader/kicad_netlist_reader-7.0.1.tar.gz/kicad_netlist_reader-7.0.1/setup.py
@@ -0,0 +1,8 @@
+"""KiCad python module for interpreting generic netlists which can be used to generate bills of materials."""
+# This file is generated automatically from metadata
+# File edits may be overwritten!
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup()
