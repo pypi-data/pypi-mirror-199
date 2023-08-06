87a9f7f8459ce7992e7d3afd6f2bc0cfd7eab7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:41 +0000

    Add package poros/0.0.70/poros-0.0.70-py3-none-any.whl

---
+++ packages/poros/poros-0.0.70-py3-none-any.whl/poros/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+comments
+author: diqiuzhuanzhuan
+email: diqiuzhuanzhuan@gmail.com
+
+"""
+
+name = "poros"
+from poros import poros_chars
+from poros import poros_dataset
+from poros import poros_loss
+from poros import poros_train
+from poros import poros_metrics
+from poros import poros_layers
+from poros import ner
+from poros import cf
