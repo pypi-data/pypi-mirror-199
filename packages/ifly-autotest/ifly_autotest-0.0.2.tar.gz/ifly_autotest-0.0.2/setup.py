a694327b30e356122db8c5e9dc96203ab6ce4844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:57 +0000

    Add package ifly-autotest/0.0.2/ifly_autotest-0.0.2.tar.gz

---
+++ packages/ifly-autotest/ifly_autotest-0.0.2.tar.gz/ifly_autotest-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ifly_autotest",
+    version="0.0.2",
+    author="",
+    author_email="",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
