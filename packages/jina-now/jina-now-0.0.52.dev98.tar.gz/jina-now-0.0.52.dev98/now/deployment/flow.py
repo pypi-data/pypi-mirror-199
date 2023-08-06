bd0710c9d63cbd8f5b86b74a916e043ebe5c5c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:23 +0000

    Add package jina-now/0.0.52.dev98/jina-now-0.0.52.dev98.tar.gz

---
+++ packages/jina-now/jina-now-0.0.52.dev98.tar.gz/jina-now-0.0.52.dev98/now/deployment/flow.py
@@ -0,0 +1,35 @@
+import os.path
+import tempfile
+
+from jina.clients import Client
+
+from now.deployment.deployment import deploy_wolf
+from now.log import time_profiler
+from now.utils.jcloud.helpers import write_flow_file
+
+
+@time_profiler
+def deploy_flow(
+    flow_yaml: str,
+):
+    """Deploy a Flow on JCloud, Kubernetes, or using Jina Orchestration"""
+    # TODO create tmpdir top level and pass it down
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # hack we don't know if the flow yaml is a path or a string
+        if type(flow_yaml) == dict:
+            flow_file = os.path.join(tmpdir, 'flow.yml')
+            write_flow_file(flow_yaml, flow_file)
+            flow_yaml = flow_file
+
+        flow = deploy_wolf(path=flow_yaml)
+        host = flow.endpoints['gateway (grpc)']
+        client = Client(host=host)
+
+        # host & port
+        gateway_port = None
+        gateway_host_internal = host
+    return (
+        client,
+        gateway_port,
+        gateway_host_internal,
+    )
