b7956992b7dc11df6e025250c7c455ec0eda3c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:40 +0000

    Add package judoscale/1.2.0/judoscale-1.2.0.tar.gz

---
+++ packages/judoscale/judoscale-1.2.0.tar.gz/judoscale-1.2.0/judoscale/celery/__init__.py
@@ -0,0 +1,31 @@
+import time
+from typing import Mapping
+
+from celery import Celery
+from celery import __version__ as celery_version
+from celery.signals import before_task_publish
+
+from judoscale.celery.collector import CeleryMetricsCollector
+from judoscale.core.adapter import Adapter, AdapterInfo
+from judoscale.core.config import config as judoconfig
+from judoscale.core.reporter import reporter
+
+
+@before_task_publish.connect
+def before_publish(*args, properties={}, **kwargs):
+    properties["published_at"] = time.time()
+
+
+def judoscale_celery(celery: Celery, extra_config: Mapping = {}) -> None:
+    celery.conf.task_send_sent_event = True
+
+    judoconfig.update(extra_config)
+    collector = CeleryMetricsCollector(config=judoconfig, broker=celery)
+    adapter = Adapter(
+        identifier="judoscale-celery",
+        adapter_info=AdapterInfo(platform_version=celery_version),
+        metrics_collector=collector,
+    )
+
+    reporter.add_adapter(adapter)
+    reporter.ensure_running()
