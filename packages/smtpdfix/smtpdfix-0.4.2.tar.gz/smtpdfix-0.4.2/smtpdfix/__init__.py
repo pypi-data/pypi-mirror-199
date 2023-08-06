4de1bfcc97f2e1d465ff0b9c4b9fc1c8a189c5b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:08 +0000

    Add package smtpdfix/0.4.2/smtpdfix-0.4.2.tar.gz

---
+++ packages/smtpdfix/smtpdfix-0.4.2.tar.gz/smtpdfix-0.4.2/smtpdfix/__init__.py
@@ -0,0 +1,17 @@
+__all__ = (
+    "AuthController",
+    "Authenticator",
+    "AuthMessage",
+    "Config",
+    "smtpd",
+    "SMTPDFix",
+)
+__version__ = "0.4.2"
+
+from .authenticator import Authenticator
+from .configuration import Config
+from .controller import AuthController
+from .fixture import SMTPDFix, smtpd
+from .handlers import AuthMessage
+
+config = Config()
