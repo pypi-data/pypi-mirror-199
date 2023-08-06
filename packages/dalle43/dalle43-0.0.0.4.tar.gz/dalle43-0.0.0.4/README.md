4892d8de07a1374d32521d47f1aaccf9fc59b7fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:00 +0000

    Add package dalle43/0.0.0.4/dalle43-0.0.0.4.tar.gz

---
+++ packages/dalle43/dalle43-0.0.0.4.tar.gz/dalle43-0.0.0.4/README.md
@@ -0,0 +1,25 @@
+# DALL-E FOR FREE!
+
+Use OpenAI's DALL-E for free
+ 
+With dalle43 you can use  OpenAi's DALL-E for free
+
+**UPDATE: dalle43 is now available for both Windows and Unix.**
+
+# HOW TO USE
+
+1. INSTALL DALLE43: `pip install -U dalle43`
+
+
+2. GENERATE AN IMAGE
+
+`import dalle43.dalle43 as DALL_E`
+
+`DALL_E.show_img('the milky way')`
+
+
+
+3. Enjoy! :heart:
+
+
+![Screenshot from 2023-03-20 02-32-03](https://user-images.githubusercontent.com/114559605/226227544-66d2e956-8bd6-43d5-8397-41e720674b7a.png)
---
+++ packages/dalle43/dalle43-0.0.0.4.tar.gz/dalle43-0.0.0.4/dalle43/dalle43.py
@@ -0,0 +1,48 @@
+import webbrowser
+import os
+import sys
+from random import randint
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+if os.name=='posix':
+   os.system("clear")
+if os.name=='nt':
+   os.system("cls")
+global USER;USER=os.getlogin()
+global DIR_PATH
+if os.name=='nt':
+   DIR_PATH="C:/Users/"+USER+"/Pictures/generated_images"
+if os.name=='posix':
+   DIR_PATH="/home/"+USER+"/Pictures/generated_images"
+if os.path.isdir(DIR_PATH)==False:
+        os.mkdir(DIR_PATH)
+global URL;URL='https://open.ai/images?prompt='
+
+
+
+
+
+def show_img(prompt):
+    prompt=prompt.replace(" ","%20")
+    REQURL=URL+prompt
+    driver.get(REQURL)
+    time.sleep(5)
+    myImg = driver.find_element(By.TAG_NAME, 'img')
+    url= myImg.get_attribute("src")
+    r=requests.get(url)
+    newpath=DIR_PATH+"/"+str(randint(100000000,999999999))+".jpg"
+    f=open(newpath,"wb")
+    f.write(r.content)
+    f.close()
+    webbrowser.open_new_tab("file:///"+newpath)
+
