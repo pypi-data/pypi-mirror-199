b4a9402bfbbfc9e75db4428ec3611e5adf170fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:51 +0000

    Add package Melodie/0.7.0/Melodie-0.7.0-cp39-cp39-win_amd64.whl

---
+++ packages/Melodie/Melodie-0.7.0-cp39-cp39-win_amd64.whl/MelodieInfra/lowcode/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# @Time: 2022/12/13 21:49
+# @Author: Zhanyi Hou
+# @Email: 1295752786@qq.com
+# @File: __init__.py.py
+
+from .params import ArrayParam, FloatParam, IntParam, StringParam, SelectionParam, ParamsManager, Param, BoolParam
