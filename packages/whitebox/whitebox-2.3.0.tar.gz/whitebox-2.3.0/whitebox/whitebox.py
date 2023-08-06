1516fa28591ef272a04e18eacac8deb9f08bdd62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:51 +0000

    Add package whitebox/2.3.0/whitebox-2.3.0.tar.gz

---
+++ packages/whitebox/whitebox-2.3.0.tar.gz/whitebox-2.3.0/whitebox/whitebox.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+
+from .whitebox_tools import WhiteboxTools
+
+
+def Runner(clear_app_state=False, callback=None):
+    wbt = WhiteboxTools()
+    wbt.launch_wb_runner(clear_app_state, callback)
