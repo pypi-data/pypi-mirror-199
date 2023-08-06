ad6f352ad640005b551fcc263ca5dcb6a82e5dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:14 +0000

    Add package EuclidSearchPackage/0.3.4/EuclidSearchPackage-0.3.4.tar.gz

---
+++ packages/EuclidSearchPackage/EuclidSearchPackage-0.3.4.tar.gz/EuclidSearchPackage-0.3.4/src/Test/Test.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/28 11:43
+# @Author  : Euclid-Jie
+# @File    : Test.py
+from src.EuclidSearchPackage import *
+# Set_cookie('cookie.txt')
+res = Get_single_weibo_data(mblogid='MrOtA75Fd')
+print(res)
+
