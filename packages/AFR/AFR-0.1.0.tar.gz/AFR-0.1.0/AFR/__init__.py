381b9c95863ec0a64193a98dc014d3ea48c2e1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:20 +0000

    Add package AFR/0.1.0/AFR-0.1.0.tar.gz

---
+++ packages/AFR/AFR-0.1.0.tar.gz/AFR-0.1.0/AFR/__init__.py
@@ -0,0 +1,20 @@
+"""
+Utilities useful during the build.
+"""
+
+__version__ = "0.1.0"
+
+from .adjr2_score import adjr2_score
+from .aic_score import aic_score
+from .bic_score import bic_score
+from .check_betas import check_betas
+from .checkdata import checkdata
+from .corsel import corsel
+from .dec_plot import dec_plot
+from .opt_size import opt_size
+from .pt_multi import pt_multi
+from .pt_one import pt_one
+from .reg_test import reg_test
+from .regsel_f import regsel_f
+from .sbic_score import sbic_score
+from .vif_reg import vif_reg
