b75f02185ce3569ac01e117eb21fa180e6fca566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:17 +0000

    Add package ert/4.4.4/ert-4.4.4-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-4.4.4-cp38-cp38-macosx_10_15_x86_64.whl/ert/gui/simulation/single_test_run_panel.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+
+from qtpy.QtWidgets import QFormLayout, QLabel
+
+from ert.gui.ertwidgets import addHelpToWidget
+from ert.gui.ertwidgets.caseselector import CaseSelector
+from ert.gui.ertwidgets.models.activerealizationsmodel import ActiveRealizationsModel
+from ert.libres_facade import LibresFacade
+from ert.shared.models import SingleTestRun
+
+from .simulation_config_panel import SimulationConfigPanel, escape_string
+
+
+@dataclass
+class Arguments:
+    mode: str
+
+
+class SingleTestRunPanel(SimulationConfigPanel):
+    def __init__(self, ert, notifier):
+        self.ert = ert
+        facade = LibresFacade(ert)
+        SimulationConfigPanel.__init__(self, SingleTestRun)
+        self.setObjectName("Single_test_run_panel")
+        layout = QFormLayout()
+
+        case_selector = CaseSelector(facade, notifier)
+        layout.addRow("Current case:", case_selector)
+
+        run_path_label = QLabel(f"<b>{escape_string(facade.run_path)}</b>")
+        addHelpToWidget(run_path_label, "config/simulation/runpath")
+        layout.addRow("Runpath:", run_path_label)
+
+        self._active_realizations_model = ActiveRealizationsModel(facade)
+
+        self.setLayout(layout)
+
+    def getSimulationArguments(self):
+        return Arguments("test_run")
