b69642172103cad0d15bffddb10c0918f6297d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:34 +0000

    Add package telegram-botty/0.2.5/telegram_botty-0.2.5-py3-none-any.whl

---
+++ packages/telegram-botty/telegram_botty-0.2.5-py3-none-any.whl/botty/__init__.py
@@ -0,0 +1,33 @@
+from .buttons import CallbackButton, UrlButton
+from .handlers import (
+    CommandHandler,
+    CompositeHandler,
+    InlineMenuHandler,
+    MessageHandler,
+    QueryHandler,
+    StartHandler,
+    TextHandler,
+    UpdateHandler,
+    StartGroupHandler,
+)
+from .keyboards import InlineButtons, InlineKeyboard
+from .loader import app
+from .texts import Texts
+
+__all__ = [
+    "app",
+    "CompositeHandler",
+    "UpdateHandler",
+    "MessageHandler",
+    "QueryHandler",
+    "TextHandler",
+    "CommandHandler",
+    "StartHandler",
+    "InlineMenuHandler",
+    "CallbackButton",
+    "UrlButton",
+    "InlineKeyboard",
+    "InlineButtons",
+    "Texts",
+    "StartGroupHandler",
+]
---
+++ packages/telegram-botty/telegram_botty-0.2.5-py3-none-any.whl/botty/handlers/__init__.py
@@ -0,0 +1,25 @@
+from .classes import (
+    CommandHandler,
+    InlineMenuHandler,
+    MessageHandler,
+    QueryHandler,
+    StartHandler,
+    TextHandler,
+    UpdateHandler,
+    StartGroupHandler,
+)
+from .composite import CompositeHandler
+from .handler import Handler
+
+__all__ = [
+    "Handler",
+    "UpdateHandler",
+    "MessageHandler",
+    "QueryHandler",
+    "TextHandler",
+    "CommandHandler",
+    "StartHandler",
+    "CompositeHandler",
+    "InlineMenuHandler",
+    "StartGroupHandler",
+]
