4dd21af2260d0d71ed4da96e853d1b396a9afbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:18 +0000

    Add package NotionLab/1.0.3/NotionLab-1.0.3-py3-none-any.whl

---
+++ packages/NotionLab/NotionLab-1.0.3-py3-none-any.whl/notion_lab/converter/util/html/elements/Image.py
@@ -0,0 +1,18 @@
+from typing import Dict
+
+
+class Image:
+    b_ctx: Dict
+
+    def __init__(self, b_ctx: Dict):
+        self.b_ctx = b_ctx
+
+    def export(self):
+        if self.b_ctx["type"] == "file":
+            return f"""
+                <img src="{self.b_ctx["file"]["url"]}" />
+            """
+        elif self.b_ctx["type"] == "external":
+            return f"""
+                <img src="{self.b_ctx["external"]["url"]}" />
+            """
