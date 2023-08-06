e275a7f0a7b64731a974b3aeb70eaeebda28ee76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:44 +0000

    Add package furry-tool/0.0.6/furry_tool-0.0.6.tar.gz

---
+++ packages/furry-tool/furry_tool-0.0.6.tar.gz/furry_tool-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="furry_tool",
+    version="0.0.6",
+    author="AaronDoge",
+    author_email="pangyuyu@email.cn",
+    description="个人工具集",
+    license="Apache License 2.0",
+    keywords=['python', 'furry_tool', 'toolkit'],
+    install_requires=['docxtpl','pdf2docx'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xsxz01/furry_tool.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
