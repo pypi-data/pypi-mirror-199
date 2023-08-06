a006cdc937be1a4fce7b995fc73d9f9f836fac20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:10 +0000

    Add package pyrocko/2023.3.27/pyrocko-2023.3.27-cp310-cp310-win_amd64.whl

---
+++ packages/pyrocko/pyrocko-2023.3.27-cp310-cp310-win_amd64.whl/pyrocko/squirrel/tool/commands/summon.py
@@ -0,0 +1,53 @@
+# http://pyrocko.org - GPLv3
+#
+# The Pyrocko Developers, 21st Century
+# ---|P------/S----------~Lg----------
+
+import math
+import logging
+
+from pyrocko.squirrel.error import SquirrelError
+from pyrocko.progress import progress
+
+logger = logging.getLogger('psq.cli.summon')
+
+headline = 'Fill local cache.'
+
+
+def make_subparser(subparsers):
+    return subparsers.add_parser(
+        'summon',
+        help=headline,
+        description=headline)
+
+
+def setup(parser):
+    parser.add_squirrel_selection_arguments()
+    parser.add_squirrel_query_arguments()
+
+
+def run(parser, args):
+    d = args.squirrel_query
+    squirrel = args.make_squirrel()
+
+    if 'tmin' not in d or 'tmax' not in d:
+        raise SquirrelError('Time span required.')
+
+    tinc = 3600.
+
+    with progress.view():
+        nwindows = int(math.ceil((d['tmax'] - d['tmin']) / tinc))
+        task = progress.task('Summoning', nwindows)
+        iwindow = 0
+        for trs in squirrel.chopper_waveforms(
+                tinc=tinc, load_data=False, **d):
+
+            iwindow += 1
+            task.update(iwindow)
+
+        task.done()
+
+    stats = str(squirrel)
+    stats = '\n'.join('  ' + s for s in stats.splitlines())
+
+    logger.info('Squirrel stats:\n%s' % stats)
