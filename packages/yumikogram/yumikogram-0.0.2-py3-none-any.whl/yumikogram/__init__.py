06ce5cb0497702167474fd713fc8eed166ff8538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:16 +0000

    Add package yumikogram/0.0.2/yumikogram-0.0.2-py3-none-any.whl

---
+++ packages/yumikogram/yumikogram-0.0.2-py3-none-any.whl/yumikogram/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.2"
+__license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
+__copyright__ = "Copyright (C) 2023-present yumiko-api <https://github.com/yumiko-api>"
+
+def yumiko_api():
+  print(
+     f'Yumikogram Successfully installed Current version of Yumikogram {__version__}' + "\n"
+     f'Under {__license__}'
+  )
+
