930f00e16ed36d9d7f2da4cbaa8b32d657bde271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:26 +0000

    Add package WLED-light-controller/0.3.9/WLED_light_controller-0.3.9-py3-none-any.whl

---
+++ packages/WLED-light-controller/WLED_light_controller-0.3.9-py3-none-any.whl/wled/Exceptions.py
@@ -0,0 +1,10 @@
+class InvalidFX(Exception):
+    pass
+class InvalidPalette(Exception):
+    pass
+class InvalidSegment(Exception):
+    pass
+class InvalidHost(Exception):
+    pass
+class InvalidRange(Exception):
+    pass
