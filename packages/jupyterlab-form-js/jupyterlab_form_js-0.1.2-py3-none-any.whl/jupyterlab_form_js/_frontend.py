4f22a65db46ee01b0a4643724a5d7107f54a7497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:49 +0000

    Add package jupyterlab-form-js/0.1.2/jupyterlab_form_js-0.1.2-py3-none-any.whl

---
+++ packages/jupyterlab-form-js/jupyterlab_form_js-0.1.2-py3-none-any.whl/jupyterlab_form_js/_frontend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Asko Soukka.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "jupyterlab-form-js"
+module_version = "^0.1.0"
