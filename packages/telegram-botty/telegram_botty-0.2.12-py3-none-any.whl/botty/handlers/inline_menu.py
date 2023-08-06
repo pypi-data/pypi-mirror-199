60a5d1669c13e28243574423bd89a856fd6c2793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:41 +0000

    Add package telegram-botty/0.2.12/telegram_botty-0.2.12-py3-none-any.whl

---
+++ packages/telegram-botty/telegram_botty-0.2.12-py3-none-any.whl/botty/handlers/inline_menu.py
@@ -0,0 +1,18 @@
+from collections.abc import Mapping
+
+from botty.buttons import CallbackButton
+
+from .query import QueryHandler
+
+
+class InlineMenuHandler(QueryHandler):
+    def __init__(self, buttons: Mapping[CallbackButton, QueryHandler]) -> None:
+        self.buttons = buttons
+        on_button = [b.text for b in buttons]
+        super().__init__(on_button)
+
+    async def callback(self) -> None:
+        for button, handler in self.buttons.items():
+            if button.callback_data == self.query.data:
+                await handler.handle(self.update.raw, self.context)
+                break
