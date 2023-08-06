63b146f6b7a3afb697496bda891132ac473e4af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:22 +0000

    Add package trengx/0.0.1/trengx-0.0.1.tar.gz

---
+++ packages/trengx/trengx-0.0.1.tar.gz/trengx-0.0.1/setup.py
@@ -0,0 +1,14 @@
+# python setup.py sdist bdist_wheel
+
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='trengx',
+    version='0.0.1',
+    author='Yong-Jun Shin',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
