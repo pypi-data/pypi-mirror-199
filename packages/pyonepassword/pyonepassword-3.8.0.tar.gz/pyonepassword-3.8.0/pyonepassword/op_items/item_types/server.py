3eeda2375a4be427c1d886beced0933c7860bfa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:53 +0000

    Add package pyonepassword/3.8.0/pyonepassword-3.8.0.tar.gz

---
+++ packages/pyonepassword/pyonepassword-3.8.0.tar.gz/pyonepassword-3.8.0/pyonepassword/op_items/item_types/server.py
@@ -0,0 +1,63 @@
+from typing import Union
+
+from .._item_descriptor_registry import op_register_item_descriptor_type
+from .._item_type_registry import op_register_item_type
+from ._item_base import OPAbstractItem, OPFieldNotFoundException
+from ._item_descriptor_base import OPAbstractItemDescriptor
+
+
+@op_register_item_descriptor_type
+class OPServerItemDescriptor(OPAbstractItemDescriptor):
+    CATEGORY = "SERVER"
+
+    def __init__(self, item_dict):
+        super().__init__(item_dict)
+
+
+@op_register_item_type
+class OPServerItem(OPAbstractItem):
+    CATEGORY = "SERVER"
+
+    def __init__(self, item_dict):
+        super().__init__(item_dict)
+
+    @property
+    def password(self):
+        password = self.field_value_by_id("password")
+        return password
+
+    @property
+    def username(self):
+        username = self.field_value_by_id("username")
+        return username
+
+    @property
+    def url(self) -> Union[str, None]:
+        try:
+            url = self.field_value_by_id("url")
+        except OPFieldNotFoundException:
+            url = None
+        return url
+
+    @property
+    def admin_console_password(self):
+        password = self.field_value_by_section_title(
+            "Admin Console", "console password")
+        return password
+
+    @property
+    def admin_console_username(self):
+        username = self.field_value_by_section_title(
+            "Admin Console", "admin console username")
+        return username
+
+    @property
+    def admin_console_url(self):
+        url = self.field_value_by_section_title(
+            "Admin Console", "admin console URL")
+        return url
+
+
+@op_register_item_type
+class OPServerItemRelaxedValidation(OPServerItem):
+    _relaxed_validation = True
