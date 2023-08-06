aefdcc8fb12b18ec3441e4fe12a29ba64499c873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:25 +0000

    Add package pipStudy001/0.0.3/pipStudy001-0.0.3.tar.gz

---
+++ packages/pipStudy001/pipStudy001-0.0.3.tar.gz/pipStudy001-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='pipStudy001',
+      version='0.0.3',
+      description='pip test functions',
+      author='fdd',
+      author_email='1316017498@qq.com',
+      requires=['json', 'requests'],  # 定义依赖哪些模块
+      # packages=find_packages(),  # 系统自动从当前目录开始找包
+      # 如果有的文件不用打包，则只能指定需要打包的文件
+      packages=['pipStudy'],  # 指定目录中需要打包的py文件，注意不要.py后缀
+      license="apache 3.0"
+      )
