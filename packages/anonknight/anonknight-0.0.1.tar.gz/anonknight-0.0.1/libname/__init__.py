a2d914a0ea7a121811bdbd6c59078ba109474418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:40 +0000

    Add package anonknight/0.0.1/anonknight-0.0.1.tar.gz

---
+++ packages/anonknight/anonknight-0.0.1.tar.gz/anonknight-0.0.1/libname/__init__.py
@@ -0,0 +1,1 @@
+from anonknight import sayhello
---
+++ packages/anonknight/anonknight-0.0.1.tar.gz/anonknight-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Test Package anonknight'
+
+# Setting up
+setup(
+    name="anonknight",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pyautogui'],
+    keywords=['python', 'video', 'stream',
+              'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
