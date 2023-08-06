982af66ed7177a7c54ea31f656c9bdf321531711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:54 +0000

    Add package devgoldyutils/2.3.8/devgoldyutils-2.3.8.tar.gz

---
+++ packages/devgoldyutils/devgoldyutils-2.3.8.tar.gz/devgoldyutils-2.3.8/demo.py
@@ -0,0 +1,5 @@
+from dataclasses import dataclass, field
+from logging import getLogger
+from devgoldyutils import DictDataclass, Colours
+
+print(Colours.RED + "ERROR: YOU CAN'T DO THIS!!!!" + Colours.RESET_COLOUR)
