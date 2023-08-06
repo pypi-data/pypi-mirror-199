7d0dc1704accd9c6d30b9fe6eed81b943e33364c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:21 +0000

    Add package aligned/0.0.6/aligned-0.0.6-py3-none-any.whl

---
+++ packages/aligned/aligned-0.0.6-py3-none-any.whl/aligned/schemas/model.py
@@ -0,0 +1,57 @@
+import logging
+from dataclasses import dataclass, field
+
+from aligned.data_source.batch_data_source import BatchDataSource
+from aligned.data_source.stream_data_source import StreamDataSource
+from aligned.schemas.codable import Codable
+from aligned.schemas.derivied_feature import DerivedFeature
+from aligned.schemas.event_trigger import EventTrigger
+from aligned.schemas.feature import EventTimestamp, Feature, FeatureReferance
+from aligned.schemas.literal_value import LiteralValue
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Target(Codable):
+    estimating: FeatureReferance
+    feature: Feature
+
+    on_ground_truth_event: StreamDataSource | None = field(default=None)
+
+    # This is a limitation of the current setup.
+    # Optimaly will this be on the features feature view, but this is not possible at the moment
+    event_trigger: EventTrigger | None = field(default=None)
+
+    def __hash__(self) -> int:
+        return self.feature.name.__hash__()
+
+
+@dataclass
+class TargetProbability(Codable):
+    outcome: LiteralValue
+    feature: Feature
+
+
+@dataclass
+class PredictionsView(Codable):
+    target: set[Target]
+    entities: set[Feature]
+    features: set[Feature]
+    derived_features: set[DerivedFeature]
+    event_timestamp: EventTimestamp | None = field(default=None)
+    source: BatchDataSource | None = field(default=None)
+    probabilities: set[TargetProbability] | None = field(default=None)
+
+
+@dataclass
+class Model(Codable):
+    name: str
+    features: set[FeatureReferance]
+    predictions_view: PredictionsView
+    description: str | None = field(default=None)
+    contacts: list[str] | None = field(default=None)
+    tags: dict[str, str] | None = field(default=None)
+
+    def __hash__(self) -> int:
+        return self.name.__hash__()
