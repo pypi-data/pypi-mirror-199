be6fd32ff81a27b68706a77a9d089bd8441614b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:06 +0000

    Add package safe-ds/0.6.0/safe_ds-0.6.0-py3-none-any.whl

---
+++ packages/safe-ds/safe_ds-0.6.0-py3-none-any.whl/safeds/ml/classification/_random_forest.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from safeds.data.tabular.containers import Table, TaggedTable
+from safeds.ml._util_sklearn import fit, predict
+from sklearn.ensemble import RandomForestClassifier as sk_RandomForestClassifier
+
+from ._classifier import Classifier
+
+
+class RandomForest(Classifier):
+    """
+    This class implements the Random Forest classification. It can only be trained on a tagged table.
+    """
+
+    def __init__(self) -> None:
+        self._wrapped_classifier: Optional[sk_RandomForestClassifier] = None
+        self._target_name: Optional[str] = None
+
+    def fit(self, training_set: TaggedTable) -> RandomForest:
+        """
+        Create a new classifier based on this one and fit it with the given training data. This classifier is not
+        modified.
+
+        Parameters
+        ----------
+        training_set : TaggedTable
+            The training data containing the feature and target vectors.
+
+        Returns
+        -------
+        fitted_classifier : RandomForest
+            The fitted classifier.
+
+        Raises
+        ------
+        LearningError
+            If the training data contains invalid values or if the training failed.
+        """
+
+        wrapped_classifier = sk_RandomForestClassifier(n_jobs=-1)
+        fit(wrapped_classifier, training_set)
+
+        result = RandomForest()
+        result._wrapped_classifier = wrapped_classifier
+        result._target_name = training_set.target.name
+
+        return result
+
+    def predict(self, dataset: Table) -> TaggedTable:
+        """
+        Predict a target vector using a dataset containing feature vectors. The model has to be trained first.
+
+        Parameters
+        ----------
+        dataset : Table
+            The dataset containing the feature vectors.
+
+        Returns
+        -------
+        table : TaggedTable
+            A dataset containing the given feature vectors and the predicted target vector.
+
+        Raises
+        ------
+        PredictionError
+            If prediction with the given dataset failed.
+        """
+        return predict(self._wrapped_classifier, dataset, self._target_name)
