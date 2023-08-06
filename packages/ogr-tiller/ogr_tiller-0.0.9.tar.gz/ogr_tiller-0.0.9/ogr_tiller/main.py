ac934b24ee2ee7a5d5450bf2b6b9dd8c1226d717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:06 +0000

    Add package ogr-tiller/0.0.9/ogr_tiller-0.0.9.tar.gz

---
+++ packages/ogr-tiller/ogr_tiller-0.0.9.tar.gz/ogr_tiller-0.0.9/ogr_tiller/main.py
@@ -0,0 +1,34 @@
+import sys
+import argparse
+
+from ogr_tiller.entities.job_param import JobParam
+from ogr_tiller.ogr_tiller import start_api
+
+
+def execute(job_param: JobParam):
+    print('started...')
+    print('data_folder:', job_param.data_folder)
+    print('cache_folder:', job_param.cache_folder)
+    print('port:', job_param.port)
+
+    print('Web UI started')
+    start_api(job_param)
+    print('Web UI stopped')
+    print('completed')
+
+
+def get_arg(param):
+    source_index = sys.argv.index(param)
+    val = sys.argv[source_index + 1]
+    return val
+
+
+def cli():
+    parser = argparse.ArgumentParser(prog='ogr_tiller')
+    parser.add_argument('--data_folder', help='data folder', required=True)
+    parser.add_argument('--cache_folder', help='cache folder', required=True)
+    parser.add_argument('--port', help='port', default='8080')
+
+    args = parser.parse_args()
+    param = JobParam(args.data_folder, args.cache_folder, args.port)
+    execute(param)
