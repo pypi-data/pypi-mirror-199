f6c1b04a93ffb834ca597a91ce020a0635d8ac7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:40 +0000

    Add package epics-containers-cli/0.4.1/epics_containers_cli-0.4.1-py3-none-any.whl

---
+++ packages/epics-containers-cli/epics_containers_cli-0.4.1-py3-none-any.whl/epics_containers_cli/context.py
@@ -0,0 +1,13 @@
+"""
+Define a context for passing root parameters to sub-commands
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Context:
+    domain: str = ""
+    helm_registry: str = ""
+    image_registry: str = ""
+    show_cmd: bool = False
