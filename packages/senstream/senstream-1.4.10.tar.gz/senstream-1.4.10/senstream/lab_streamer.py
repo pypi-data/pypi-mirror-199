60d7bec64359ea1e855fabcf7c678d9b809ec561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:26 +0000

    Add package senstream/1.4.10/senstream-1.4.10.tar.gz

---
+++ packages/senstream/senstream-1.4.10.tar.gz/senstream-1.4.10/senstream/lab_streamer.py
@@ -0,0 +1,5 @@
+# 1. Encodes high-functionality python SDK for streaming data from senspots into .txt, .csv, .xml, etc.
+# 2. Streams data from USB sink serial port to a local sqlite db
+# 3. ETL functions to push locally streamed data to MySQL db on resensys.net
+# 4. Option to open a streaming window to view all present streaming devices
+
