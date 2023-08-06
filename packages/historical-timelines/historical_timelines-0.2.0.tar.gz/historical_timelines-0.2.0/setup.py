06dda3bf2ae62477ad0c7523b44327bf860b523d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:52 +0000

    Add package historical-timelines/0.2.0/historical_timelines-0.2.0.tar.gz

---
+++ packages/historical-timelines/historical_timelines-0.2.0.tar.gz/historical_timelines-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='historical_timelines',
+    version='0.2.0',
+    description='A Python package for making historical timelines',
+    author='darthbeep',
+    author_email='darthbeep@gmail.com',
+    packages=['historical_timelines'],
+    install_requires=[
+        'bokeh',
+    ],
+)
