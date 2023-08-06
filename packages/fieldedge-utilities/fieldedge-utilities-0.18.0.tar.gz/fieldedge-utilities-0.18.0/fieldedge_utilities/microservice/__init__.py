d196df2f608c85697ce170d929bc7c25c75d0876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:10 +0000

    Add package fieldedge-utilities/0.18.0/fieldedge-utilities-0.18.0.tar.gz

---
+++ packages/fieldedge-utilities/fieldedge-utilities-0.18.0.tar.gz/fieldedge-utilities-0.18.0/fieldedge_utilities/microservice/__init__.py
@@ -0,0 +1,5 @@
+from .interservice import IscTask, IscTaskQueue
+from .propertycache import CachedProperty, PropertyCache
+from .microservice import Microservice, MicroserviceProxy
+
+__all__ = [CachedProperty, PropertyCache, IscTask, IscTaskQueue]
