0a859964a950ae9d2e68b74217c5eb45c386bc8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:00 +0000

    Add package sapx/0.1.5/sapx-0.1.5-py3-none-any.whl

---
+++ packages/sapx/sapx-0.1.5-py3-none-any.whl/sap/fastapi/__init__.py
@@ -0,0 +1,22 @@
+"""
+FastAPI.
+
+This package regroups all common helpers exclusively for FastAPI.
+Learn more about FastAPI: https://github.com/tiangolo/fastapi
+"""
+
+from .exceptions import Object404Error, Validation422Error
+from .serializers import ObjectSerializer, WriteObjectSerializer
+from .utils import Flash, FlashLevel, pydantic_format_errors
+
+__all__ = [
+    "Flash",
+    "FlashLevel",
+    "pydantic_format_errors",
+    # Serializers
+    "ObjectSerializer",
+    "WriteObjectSerializer",
+    # Exceptions
+    "Validation422Error",
+    "Object404Error",
+]
