152647bf42865c7a3e9e053de58fce4932a2a292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:52 +0000

    Add package coded/1.10/coded-1.10.tar.gz

---
+++ packages/coded/coded-1.10.tar.gz/coded-1.10/coded/x.py
@@ -0,0 +1,47 @@
+import os
+import random
+
+def write(*text, seperator="", end="", flush=True):
+	"""Prints the given text to the console"""
+	text = list(text)
+	for a in range(len(text)):
+		text[a] = str(text[a])
+	print(seperator.join(text), end=end, sep=seperator, flush=flush)
+
+def writeline(*text, seperator="", flush=True):
+	"""Prints the given text to the console, appends a \\n character"""
+	write(*text, seperator=seperator, end="\n", flush=flush)
+
+def prompt(input_type):
+	"""Prompts the user for console input, then casts to input_type"""
+	try:
+		return input_type(input())
+	except Exception as e:
+		return e
+
+def clear():
+	"""Clear the console window"""
+	if os.name == "nt":
+		os.system("cls")
+	else:
+		os.system("clear")
+
+def rand(minimum, maximum=None):
+	"""Generate a random number between minimum (inclusive) and maximum (exclusive)"""
+	if maximum == None:
+		return random.randrange(minimum)
+	return random.randrange(minimum, maximum)
+
+def readf(path):
+	with open(path) as f:
+		return f.read()
+
+def writef(path, content):
+	with open(path, "w") as f:
+		return f.write(content)
+
+def rm(path):
+	os.remove(path)
+
+def rmdir(path):
+	os.rmdir(path)
