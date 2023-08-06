2aff2f38a29628de3f8035c53f347da22e59d23a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:03 +0000

    Add package image-decoding-algorithms/0.0.6/image-decoding-algorithms-0.0.6.tar.gz

---
+++ packages/image-decoding-algorithms/image-decoding-algorithms-0.0.6.tar.gz/image-decoding-algorithms-0.0.6/IDA/IDA_main.py
@@ -0,0 +1,35 @@
+from sys import path, version_info
+from pathlib import Path
+path.append(str(Path(__file__).parent.resolve()))
+from JFIFDecoder import decode as JFIF_decoder
+from Image_reader import Image_reading_class as Image_reader
+
+from numpy import asarray, ndarray
+from skimage.io import imread
+
+JFIF_FILE_TEST_SIZE = 11
+def read_image(path: str) -> ndarray:
+    py_ver = True
+    if not (version_info.major == 3 and version_info.minor > 6):
+        py_ver = False
+    read_not_failed = True
+    raw_img = list
+    img = ndarray
+
+    image = Image_reader(path)
+    file_test = image.read_str(JFIF_FILE_TEST_SIZE)
+    if file_test.startswith(b'\xff\xd8\xff\xe0') and file_test.endswith(b'JFIF\x00'): #signature of JFIF format
+        image.read(file_test[5] - JFIF_FILE_TEST_SIZE)
+        try:
+            raw_img = JFIF_decoder(image)
+        except:
+            read_not_failed = False
+    else:
+        read_not_failed = False
+
+    if read_not_failed and py_ver:
+        img = asarray(raw_img)
+    else:
+        img = imread(path)
+
+    return img
---
+++ packages/image-decoding-algorithms/image-decoding-algorithms-0.0.6.tar.gz/image-decoding-algorithms-0.0.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+setup(
+    name = "image-decoding-algorithms",
+    version = "0.0.6",
+    author = "Victor Andrievskiy",
+    author_email = "superavb222@gmail.com",
+    description= "Scripts for decoding an image",
+    long_description= "Scripts for decoding an image. I have myself have written a decoding algorithm for JFIF, baseline encoding mode, used skimage for everything else.",
+    packages=find_packages(),
+    install_requires = ['scikit-image', 'numpy'],
+    include_package_data=True,
+    url = "https://github.com/gejirz/image-decoding-algorithms",
+    keywords=['python', 'image decoding', 'JFIF', 'jpeg', 'decoding', 'baseline encoding'],
+    license= "MIT License"
+)
