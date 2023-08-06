7f990ed2c0847d90d7db46f7958fbb989b59b46d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:31 +0000

    Add package i18nice/0.6.2/i18nice-0.6.2.tar.gz

---
+++ packages/i18nice/i18nice-0.6.2.tar.gz/i18nice-0.6.2/i18n/tests/run_tests.py
@@ -0,0 +1,85 @@
+import sys
+import unittest
+from os.path import dirname
+from importlib.machinery import PathFinder
+
+
+sys.path.insert(
+    0, dirname(dirname(dirname(__file__)))
+)
+
+
+class ModuleDisabler(PathFinder):
+    _disabled_modules = []
+
+    @property
+    def disabled_modules(self):
+        return self._disabled_modules
+
+    @disabled_modules.setter
+    def disabled_modules(self, modules):
+        for k in modules:
+            if k in sys.modules:
+                del sys.modules[k]
+        self._disabled_modules = modules
+
+    def find_spec(self, name, *_):
+        if name in self.disabled_modules:
+            raise ImportError(
+                "module {0} is disabled".format(
+                    name
+                )
+            )
+        return None
+
+
+def suite():
+    from i18n.tests.loader_tests import (
+        TestFileLoader,
+    )
+    from i18n.tests.translation_tests import (
+        TestTranslationFormat,
+    )
+
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+
+    suite.addTest(
+        loader.loadTestsFromTestCase(
+            TestFileLoader
+        )
+    )
+    suite.addTest(
+        loader.loadTestsFromTestCase(
+            TestTranslationFormat
+        )
+    )
+
+    return suite
+
+
+def test_without(modules):
+    disabler = ModuleDisabler()
+    disabler.disabled_modules = modules
+    sys.meta_path.insert(0, disabler)
+    for k in [
+        name
+        for name in sys.modules
+        if name.startswith("i18n")
+    ]:
+        del sys.modules[k]
+    runner = unittest.TextTestRunner()
+    result = runner.run(suite())
+    del sys.meta_path[0]
+    if not result.wasSuccessful():
+        sys.exit(1)
+
+
+def main():
+    test_without(["json"])
+    test_without(["yaml"])
+    test_without([])
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/i18nice/i18nice-0.6.2.tar.gz/i18nice-0.6.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='i18nice',
+    version='0.6.2',
+    description='Translation library for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Daniel Perez',
+    author_email='tuvistavie@gmail.com',
+    url='https://github.com/Krutyi-4el/i18nice',
+    download_url='https://github.com/Krutyi-4el/i18nice/archive/master.zip',
+    license='MIT',
+    packages=['i18n', 'i18n.loaders', 'i18n.tests'],
+    include_package_data=True,
+    zip_safe=True,
+    test_suite='i18n.tests',
+    extras_require={
+        'YAML': ["pyyaml>=3.10"],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
