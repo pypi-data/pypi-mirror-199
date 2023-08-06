a39fa3fdc669cec83ef433421ff819af9c7d93ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:30 +0000

    Add package not1mm/23.3.24/not1mm-23.3.24.tar.gz

---
+++ packages/not1mm/not1mm-23.3.24.tar.gz/not1mm-23.3.24/not1mm/plugins/arrl_field_day.py
@@ -0,0 +1,56 @@
+from PyQt5 import QtWidgets
+
+name = "ARRL Field Day"
+mode = "BOTH"  # CW SSB BOTH RTTY
+
+# 1 once per contest, 2 work each band, 3 each band/mode, 4 no dupe checking
+dupe_type = 3
+
+
+def init_contest(self):
+    set_tab_next(self)
+    set_tab_prev(self)
+    interface(self)
+
+
+def interface(self):
+    self.field1.hide()
+    self.field2.hide()
+    self.field3.show()
+    self.field4.show()
+    label = self.field3.findChild(QtWidgets.QLabel)
+    label.setText("Class")
+    label = self.field4.findChild(QtWidgets.QLabel)
+    label.setText("Section")
+
+
+def set_tab_next(self):
+    self.tab_next = {
+        self.callsign: self.field1.findChild(QtWidgets.QLineEdit),
+        self.field1.findChild(QtWidgets.QLineEdit): self.field2.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field2.findChild(QtWidgets.QLineEdit): self.field3.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field3.findChild(QtWidgets.QLineEdit): self.field4.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field4.findChild(QtWidgets.QLineEdit): self.callsign,
+    }
+
+
+def set_tab_prev(self):
+    self.tab_prev = {
+        self.callsign: self.field4.findChild(QtWidgets.QLineEdit),
+        self.field1.findChild(QtWidgets.QLineEdit): self.callsign,
+        self.field2.findChild(QtWidgets.QLineEdit): self.field1.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field3.findChild(QtWidgets.QLineEdit): self.field2.findChild(
+            QtWidgets.QLineEdit
+        ),
+        self.field4.findChild(QtWidgets.QLineEdit): self.field3.findChild(
+            QtWidgets.QLineEdit
+        ),
+    }
