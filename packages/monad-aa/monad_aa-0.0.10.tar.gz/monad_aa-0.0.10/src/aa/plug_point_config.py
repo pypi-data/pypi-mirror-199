366a3b889b2f9a417610c7d15c6ae89f0e17112a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:32 +0000

    Add package monad-aa/0.0.10/monad_aa-0.0.10.tar.gz

---
+++ packages/monad-aa/monad_aa-0.0.10.tar.gz/monad_aa-0.0.10/src/aa/plug_point_config.py
@@ -0,0 +1,28 @@
+monad_aa_plug_point_configuration = {
+    "report_status_plug_point": {
+        "plugin": "aa.plugs.report_status_plug_in",
+        "description": "provides a core persistence for report status object",
+        "pluggable": "report_status"  # python module that uses the plug point
+    },
+    "initiate_report_generation_plug_point": {
+        "plugin": "aa.plugs.initiate_report_generation_plug_in",
+        "description": "provides core messaging implementation of initiate report generation."
+                       "it uses queues allowing report generation to be processed asynchronously",
+        "pluggable": "report_generator"  # python module that uses the plug point
+    },
+    "source_aa_data_plug_point": {
+        "plugin": "aa.plugs.source_aa_data_plug_in",
+        "description": "provides a core persistence for the source data used for the report",
+        "pluggable": "report_generator"  # python module that uses the plug point
+    },
+    "generated_aa_report_plug_point": {
+        "plugin": "aa.plugs.generated_aa_report_plug_in",
+        "description": "provides a core persistence for generated aa report object",
+        "pluggable": "report_generator"  # python module that uses the plug point
+    }
+    # "aa_report_persistence_plug_point": {
+    #     "plugin": "aa.plugs.aa_report_persistence_plug_in",
+    #     "description": "provides a core persistence for generated report object",
+    #     "pluggable": "report_generator"  # python module that uses the plug point
+    # }
+}
---
+++ packages/monad-aa/monad_aa-0.0.10.tar.gz/monad_aa-0.0.10/src/aa/plugs/initiate_report_generation_plug_in.py
@@ -0,0 +1,43 @@
+import os
+import queue
+import sys
+import threading
+import traceback
+
+from aa.business_logic.report_generator import generate
+
+from aa.plugs.initiate_report_generation_plug_point import InitiateReportGenerationPlugPoint
+
+qq = queue.Queue()
+
+
+class InitiateReportGenerationPlugIn(InitiateReportGenerationPlugPoint):
+    def __init__(self):
+        super().__init__()
+        # self.q = queue.Queue()
+        self.q = qq
+
+    def consume_msg_report_generate(self):
+        try:
+            while True:
+                report_id, account_number, date = self.q.get()
+                print(f'{os.getpid()}:{threading.get_ident()}: consume_msg_report_generate: working on {report_id}')
+                generate(report_id, account_number, date)
+                self.q.task_done()
+                print(f'{os.getpid()}:{threading.get_ident()}: consume_msg_report_generate: finished {report_id}')
+        except Exception as excep:
+            print(f'{os.getpid()}:{threading.get_ident()}: consume_msg_report_generate: caught exception {excep=}')
+            print(f'{sys.exc_info()[2]}')
+            print(f'{traceback.format_exc()}')
+            # exit(1)
+            sys.exit(1)
+
+    def produce_msg_report_generate(self, report_id, account_number, date):
+        threading.Thread(target=self.consume_msg_report_generate, daemon=True).start()
+        self.q.put((report_id, account_number, date))
+        print(f'{os.getpid()}:{threading.get_ident()}: initiate_report_generate: {report_id=}')
+
+
+# This method is needed for plugin framework.
+def get_implementation():
+    return InitiateReportGenerationPlugIn()
