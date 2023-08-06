62feb8bc33e9d97f9a6b6bdbed30f5e62089a1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:07 +0000

    Add package hyt-gpt/0.2.3/hyt-gpt-0.2.3.tar.gz

---
+++ packages/hyt-gpt/hyt-gpt-0.2.3.tar.gz/hyt-gpt-0.2.3/hyt_gpt/gpt.py
@@ -0,0 +1,41 @@
+import requests
+from typing import List
+import logging
+
+
+def chat_gpt(key, prompt, text):
+    API_KEY = key
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {API_KEY}',
+    }
+    json_data = {
+        'model': 'gpt-3.5-turbo',
+        'messages': [
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": text},
+        ],
+    }
+    response = requests.post(
+        'https://api.openai.com/v1/chat/completions', headers=headers, json=json_data)
+    # Return Error message if response is not 200
+    if response.status_code != 200:
+        logging.info('response:', response.content)
+        return str(response.json()['error'])
+
+    return response.json()['choices'][0]['message']['content']
+
+
+def seg_transcript(transcript: List[str]):
+    transcript = [{"text": item["text"], "index": index,
+                   "timestamp": item["start"]} for index, item in enumerate(transcript)]
+    text = " ".join([x["text"]
+                    for x in sorted(transcript, key=lambda x: x["index"])])
+    length = len(text)
+    seg_length = 3500
+    n = length // seg_length + 1
+    division = len(transcript) // n
+    new_l = [transcript[i * division: (i + 1) * division] for i in range(n)]
+    segedTranscipt = [" ".join([x["text"] for x in sorted(
+        j, key=lambda x: x["index"])]) for j in new_l]
+    return segedTranscipt
