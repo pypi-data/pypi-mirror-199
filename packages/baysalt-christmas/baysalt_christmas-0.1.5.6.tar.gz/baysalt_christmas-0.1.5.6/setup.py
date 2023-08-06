af59c156625f1cedc8fc0d1debb737252565dcfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:15 +0000

    Add package baysalt-christmas/0.1.5.6/baysalt_christmas-0.1.5.6.tar.gz

---
+++ packages/baysalt-christmas/baysalt_christmas-0.1.5.6.tar.gz/baysalt_christmas-0.1.5.6/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="baysalt_christmas",
+    version="0.1.5.6",
+    author="Christmas",
+    author_email="273519355@qq.com",
+    description="Somde simple tools for Christmas",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy',
+        'netCDF4',
+        'xarray',
+        # 'mpi4py',
+    ],
+)
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
