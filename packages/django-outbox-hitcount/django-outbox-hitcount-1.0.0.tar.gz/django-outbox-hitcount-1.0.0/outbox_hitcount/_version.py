9e91852955d6abb8c4533911f13dc6691ea48c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:20 +0000

    Add package django-outbox-hitcount/1.0.0/django-outbox-hitcount-1.0.0.tar.gz

---
+++ packages/django-outbox-hitcount/django-outbox-hitcount-1.0.0.tar.gz/django-outbox-hitcount-1.0.0/outbox_hitcount/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides menu version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update menu` to change this file.
+
+from incremental import Version
+
+__version__ = Version("outbox-hitcount", 1, 0, 0)
+__all__ = ["__version__"]
