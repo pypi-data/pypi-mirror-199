90adc64efd014dc32f9d649af3ba2d78a0f1e2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:39 +0000

    Add package alchql/3.2.16.19173731/alchql-3.2.16.19173731.tar.gz

---
+++ packages/alchql/alchql-3.2.16.19173731.tar.gz/alchql-3.2.16.19173731/alchql/__init__.py
@@ -0,0 +1,25 @@
+import pkg_resources
+
+from .fields import SQLAlchemyConnectionField
+from .types import SQLAlchemyObjectType
+from .utils import get_query
+from .sql_mutation import (
+    SQLAlchemyUpdateMutation,
+    SQLAlchemyCreateMutation,
+    SQLAlchemyDeleteMutation,
+)
+
+__version__ = "3.2.16"
+
+__all__ = [
+    "__version__",
+    "SQLAlchemyObjectType",
+    "SQLAlchemyConnectionField",
+    "SQLAlchemyUpdateMutation",
+    "SQLAlchemyCreateMutation",
+    "SQLAlchemyDeleteMutation",
+    "get_query",
+]
+
+if pkg_resources.get_distribution("SQLAlchemy").parsed_version.release < (1, 4):
+    raise Exception("Use SQLAlchemy version > 1.4")
