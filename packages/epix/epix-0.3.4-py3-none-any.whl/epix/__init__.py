c69935bdb769970789581747e90f046a264b6e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:57 +0000

    Add package epix/0.3.4/epix-0.3.4-py3-none-any.whl

---
+++ packages/epix/epix-0.3.4-py3-none-any.whl/epix/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.3.4'
+
+from .common import *
+from .io import *
+from .clustering import *
+from .quanta_generation import *
+from .detector_volumes import *
+from .electric_field_handler import *
+from .event_separation import *
+from .detectors import *
+from .run_epix import *
+from .quantgen_wrapper import BBF_quanta_generator
