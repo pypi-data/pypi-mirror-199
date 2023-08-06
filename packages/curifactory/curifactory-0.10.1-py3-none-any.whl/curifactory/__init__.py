925e04fe0994e47d9224e7ff333b1ac13a0c800c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:50 +0000

    Add package curifactory/0.10.1/curifactory-0.10.1-py3-none-any.whl

---
+++ packages/curifactory/curifactory-0.10.1-py3-none-any.whl/curifactory/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+from curifactory.args import ExperimentArgs
+from curifactory.caching import Lazy
+from curifactory.hashing import set_hash_functions
+from curifactory.manager import ArtifactManager
+from curifactory.procedure import Procedure
+from curifactory.record import Record
+from curifactory.staging import (
+    CachersMismatchError,
+    EmptyCachersError,
+    InputSignatureError,
+    OutputSignatureError,
+    aggregate,
+    stage,
+)
+
+__version__ = "0.10.1"
