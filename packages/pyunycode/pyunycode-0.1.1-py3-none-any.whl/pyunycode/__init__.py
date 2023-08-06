71147b843f7f2b262f4b6ec8037da4304adc84ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:08 +0000

    Add package pyunycode/0.1.1/pyunycode-0.1.1-py3-none-any.whl

---
+++ packages/pyunycode/pyunycode-0.1.1-py3-none-any.whl/pyunycode/__init__.py
@@ -0,0 +1,7 @@
+from pyunycode.pyunycode import *
+
+__copyright__    = 'Copyright (C) 2023 Yutsuro'
+__license__      = 'MIT'
+__author__       = 'Yutsuro'
+__author_email__ = 'Yuki@utsu.ro'
+__url__          = 'https://github.com/Yutsuro/pyunycode'
