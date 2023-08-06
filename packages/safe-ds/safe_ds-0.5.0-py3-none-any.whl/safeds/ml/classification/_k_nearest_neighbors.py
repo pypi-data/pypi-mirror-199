6658a815ab5c0070e6717339fe8fd41f886b103c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:13 +0000

    Add package safe-ds/0.5.0/safe_ds-0.5.0-py3-none-any.whl

---
+++ packages/safe-ds/safe_ds-0.5.0-py3-none-any.whl/safeds/ml/classification/_k_nearest_neighbors.py
@@ -0,0 +1,64 @@
+from safeds.data.tabular.containers import Table, TaggedTable
+from safeds.ml._util_sklearn import fit, predict
+from sklearn.neighbors import KNeighborsClassifier as sk_KNeighborsClassifier
+
+from ._classifier import Classifier
+
+
+class KNearestNeighbors(Classifier):
+    """
+    This class implements K-nearest-neighbors classifier. It can only be trained on a tagged table.
+
+    Parameters
+    ----------
+    n_neighbors : int
+        The number of neighbors to be interpolated with. Has to be less than or equal to the sample size.
+    """
+
+    def __init__(self, n_neighbors: int) -> None:
+        self._wrapped_classifier = sk_KNeighborsClassifier(
+            n_jobs=-1, n_neighbors=n_neighbors
+        )
+        self._target_name = ""
+
+    def fit(self, training_set: TaggedTable) -> None:
+        """
+        Fit this model given a tagged table.
+
+        Parameters
+        ----------
+        training_set : TaggedTable
+            The tagged table containing the feature and target vectors.
+
+        Raises
+        ------
+        LearningError
+            If the tagged table contains invalid values or if the training failed.
+        """
+        fit(self._wrapped_classifier, training_set)
+        self._target_name = training_set.target.name
+
+    def predict(self, dataset: Table) -> TaggedTable:
+        """
+        Predict a target vector using a dataset containing feature vectors. The model has to be trained first
+
+        Parameters
+        ----------
+        dataset : Table
+            The dataset containing the feature vectors.
+
+        Returns
+        -------
+        table : TaggedTable
+            A dataset containing the given feature vectors and the predicted target vector.
+
+        Raises
+        ------
+        PredictionError
+            If prediction with the given dataset failed.
+        """
+        return predict(
+            self._wrapped_classifier,
+            dataset,
+            self._target_name,
+        )
