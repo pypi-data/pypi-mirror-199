299898e3ee0913bacf561feda291311b82e53a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:51 +0000

    Add package Bubbelz-Logger/0.0.1/Bubbelz%20Logger-0.0.1.tar.gz

---
+++ packages/Bubbelz-Logger/Bubbelz%20Logger-0.0.1.tar.gz/Bubbelz Logger-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+     name='Bubbelz Logger',  
+     version='0.0.1',
+     author="Bubbelz",
+     author_email="info@bubbelz.life",
+     description="PyPI Package for Bubbelz Logger functions",
+     long_description="This is a package for sharing common Logger function used in different repositories",
+     long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+     ],
+ )
---
+++ packages/Bubbelz-Logger/Bubbelz%20Logger-0.0.1.tar.gz/Bubbelz Logger-0.0.1/src/functions.py
@@ -0,0 +1,10 @@
+import mysql
+import os
+
+def connect_to_database():
+    mydb = mysql.connector.connect(
+    host=os.getenv("RDS_HOSTNAME"),
+    user=os.getenv("RDS_USERNAME"),
+    password=os.getenv("RDS_PASSWORD")
+    )
+    return mydb
