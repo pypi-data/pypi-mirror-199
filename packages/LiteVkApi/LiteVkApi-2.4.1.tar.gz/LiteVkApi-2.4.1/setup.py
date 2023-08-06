f6e4bd4dfd22a2be6d360ff02d3c64b7198ea159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:02 +0000

    Add package LiteVkApi/2.4.1/LiteVkApi-2.4.1.tar.gz

---
+++ packages/LiteVkApi/LiteVkApi-2.4.1.tar.gz/LiteVkApi-2.4.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+long_description = """
+# LiteVkApi
+Бот в Вк? Легко!
+
+Из-за некорректного отображения документации в PyPI, она была полностью перемещена на гитхаб - https://github.com/Ma-Mush/LiteVkApi/
+"""
+
+
+setup(
+    name="LiteVkApi",
+    version="2.4.1",
+    description="Библиотека для легкого написания ботов ВК!",
+    packages=["LiteVkApi"],
+    author_email="ma_mush@mail.ru",
+    zip_safe=False,
+    python_requires=">=3.8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires= "vk-api==11.9.8"
+)
