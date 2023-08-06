3049c1aaf66be65d3680fb0c47d7727bcb9339e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:54 +0000

    Add package e3nn-jax/0.17.2/e3nn_jax-0.17.2.tar.gz

---
+++ packages/e3nn-jax/e3nn_jax-0.17.2.tar.gz/e3nn_jax-0.17.2/e3nn_jax/_src/activation.py
@@ -0,0 +1,98 @@
+from typing import Callable, List, Optional
+
+import jax
+import jax.numpy as jnp
+from e3nn_jax import Irreps, IrrepsArray
+from e3nn_jax._src.util.decorators import overload_for_irreps_without_array
+
+
+def normalize_function(phi):
+    with jax.ensure_compile_time_eval():
+        k = jax.random.PRNGKey(0)
+        x = jax.random.normal(k, (1_000_000,), dtype=jnp.float64)
+        c = jnp.mean(phi(x) ** 2) ** 0.5
+        c = c.item()
+
+        if jnp.allclose(c, 1.0):
+            return phi
+        else:
+
+            def rho(x):
+                return phi(x) / c
+
+            return rho
+
+
+def parity_function(phi):
+    with jax.ensure_compile_time_eval():
+        x = jnp.linspace(0.0, 10.0, 256)
+
+        a1, a2 = phi(x), phi(-x)
+        if jnp.max(jnp.abs(a1 - a2)) < 1e-5:
+            return 1
+        elif jnp.max(jnp.abs(a1 + a2)) < 1e-5:
+            return -1
+        else:
+            return 0
+
+
+def is_zero_in_zero(phi):
+    with jax.ensure_compile_time_eval():
+        return jnp.allclose(phi(jnp.array(0.0)), 0.0)
+
+
+@overload_for_irreps_without_array(irrepsarray_argnums=[0])
+def scalar_activation(input: IrrepsArray, acts: List[Optional[Callable[[float], float]]]) -> IrrepsArray:
+    assert isinstance(input, IrrepsArray)
+
+    assert len(input.irreps) == len(acts), (input.irreps, acts)
+
+    list = []
+
+    irreps_out = []
+    for (mul, (l_in, p_in)), x, act in zip(input.irreps, input.list, acts):
+        if act is not None:
+            if l_in != 0:
+                raise ValueError(
+                    f"Activation: cannot apply an activation function to a non-scalar input. {input.irreps} {acts}"
+                )
+
+            act = normalize_function(act)
+
+            p_out = parity_function(act) if p_in == -1 else p_in
+            if p_out == 0:
+                raise ValueError(
+                    "Activation: the parity is violated! The input scalar is odd but the activation is neither even nor odd."
+                )
+
+            irreps_out.append((mul, (0, p_out)))
+            if x is None:
+                if is_zero_in_zero(act):
+                    list.append(None)
+                else:
+                    list.append(act(jnp.ones(input.shape[:-1] + (mul, 1), input.dtype)))
+            else:
+                list.append(act(x))
+        else:
+            irreps_out.append((mul, (l_in, p_in)))
+            list.append(x)
+
+    irreps_out = Irreps(irreps_out)
+
+    if acts and acts.count(acts[0]) == len(acts):
+        # for performance, if all the activation functions are the same, we can apply it to the contiguous array as well
+        array = input.array if acts[0] is None else normalize_function(acts[0])(input.array)
+        return IrrepsArray(irreps=irreps_out, array=array, list=list)
+
+    return IrrepsArray.from_list(irreps_out, list, input.shape[:-1], input.dtype)
+
+
+def key_value_activation(phi, key, value):
+    assert key.ndim == 1
+    assert value.ndim == 1
+
+    d = value.shape[0]
+    key = key / jnp.sqrt(1 / 16 + jnp.sum(key**2))  # 1/16 is arbitrary small... but not too small...
+    scalar = jnp.sum(key * value)
+    scalar = normalize_function(phi)(scalar)
+    return d**0.5 * scalar * key  # component normalized
---
+++ packages/e3nn-jax/e3nn_jax-0.17.2.tar.gz/e3nn_jax-0.17.2/e3nn_jax/_src/gate.py
@@ -0,0 +1,90 @@
+from functools import partial
+from typing import Callable, Optional
+
+import e3nn_jax as e3nn
+import jax
+import jax.numpy as jnp
+from e3nn_jax import IrrepsArray, scalar_activation
+from e3nn_jax._src.util.decorators import overload_for_irreps_without_array
+
+
+@partial(jax.jit, static_argnums=(1, 2, 3, 4))
+def _gate(input: IrrepsArray, even_act, odd_act, even_gate_act, odd_gate_act) -> IrrepsArray:
+    act = {1: even_act, -1: odd_act}
+    gate_act = {1: even_gate_act, -1: odd_gate_act}
+
+    scalars = input.filtered(keep=["0e", "0o"])
+    vectors = input.filtered(drop=["0e", "0o"])
+    del input
+
+    if vectors.shape[-1] == 0:
+        return scalar_activation(scalars, [act[ir.p] for _, ir in scalars.irreps])
+
+    if scalars.irreps.dim < vectors.irreps.num_irreps:
+        raise ValueError("The input must have at least as many scalars as the number of non-scalar irreps")
+
+    scalars_extra = scalars.slice_by_mul[: scalars.irreps.dim - vectors.irreps.num_irreps]
+    scalars_gates = scalars.slice_by_mul[scalars.irreps.dim - vectors.irreps.num_irreps :]
+    del scalars
+
+    scalars_extra = scalar_activation(scalars_extra, [act[ir.p] for _, ir in scalars_extra.irreps])
+    scalars_gates = scalar_activation(scalars_gates, [gate_act[ir.p] for _, ir in scalars_gates.irreps])
+
+    return e3nn.concatenate([scalars_extra, scalars_gates * vectors], axis=-1)
+
+
+@overload_for_irreps_without_array((0,))
+def gate(
+    input: IrrepsArray,
+    even_act: Optional[Callable[[float], float]] = None,
+    odd_act: Optional[Callable[[float], float]] = None,
+    even_gate_act: Optional[Callable[[float], float]] = None,
+    odd_gate_act: Optional[Callable[[float], float]] = None,
+) -> IrrepsArray:
+    r"""Gate activation function.
+
+    The input is split into scalars that are activated separately, scalars that are used as gates, and non-scalars that are
+    multiplied by the gates.
+
+    List of assumptions:
+
+    - The gate scalars are on the right side of the scalars.
+
+    Args:
+        input (IrrepsArray): Input data.
+        even_act (Callable[[float], float], optional): Activation function for even scalars.
+        odd_act (Callable[[float], float], optional): Activation function for odd scalars.
+        even_gate_act (Callable[[float], float], optional): Activation function for even gate scalars.
+        odd_gate_act (Callable[[float], float], optional): Activation function for odd gate scalars.
+
+    Returns:
+        IrrepsArray: Output data.
+
+    Examples:
+        The 3 last scalars are used as gates.
+
+        >>> gate("15x0e + 2x1e + 1x2e")
+        12x0e+2x1e+1x2e
+
+        Odd scalars used as gates change the parity of the gated quantities:
+
+        >>> gate("12x0e + 3x0o + 2x1e + 1x2e")
+        12x0e+2x1o+1x2o
+
+        Without anything to gate, all the scalars are activated:
+
+        >>> gate("12x0e + 3x0o")
+        12x0e+3x0o
+    """
+    assert isinstance(input, IrrepsArray)
+
+    if even_act is None:
+        even_act = jax.nn.gelu
+    if odd_act is None:
+        odd_act = lambda x: (1 - jnp.exp(-(x**2))) * x
+    if even_gate_act is None:
+        even_gate_act = jax.nn.sigmoid
+    if odd_gate_act is None:
+        odd_gate_act = jax.nn.tanh
+
+    return _gate(input, even_act, odd_act, even_gate_act, odd_gate_act)
