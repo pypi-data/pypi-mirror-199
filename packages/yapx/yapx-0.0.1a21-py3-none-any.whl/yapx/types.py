6e118c8b0730a3be7b3ac76c6feb65d31e040f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:37 +0000

    Add package yapx/0.0.1a21/yapx-0.0.1a21-py3-none-any.whl

---
+++ packages/yapx/yapx-0.0.1a21-py3-none-any.whl/yapx/types.py
@@ -0,0 +1,51 @@
+import sys
+from typing import Any, Dict, Optional, Sequence, Union
+
+__all__ = [
+    "Dataclass",
+    "ArgumentParser",
+    "NoneType",
+    "ArgValueType",
+    "Literal",
+    "Annotated",
+    "Protocol",
+]
+
+if sys.version_info >= (3, 8):
+    from typing import Literal  # pylint: disable=unused-import # noqa: F401
+else:
+    from typing_extensions import Literal  # pylint: disable=unused-import # noqa: F401
+
+if sys.version_info >= (3, 9):
+    from typing import Annotated  # pylint: disable=unused-import # noqa: F401
+else:
+    from typing_extensions import (  # pylint: disable=unused-import # noqa: F401
+        Annotated,
+    )
+
+if sys.version_info >= (3, 8):
+    from typing import Protocol
+else:
+    from typing_extensions import Protocol
+
+NoneType = type(None)
+
+ArgValueType = Union[None, str, Sequence[str]]
+
+
+class Dataclass(Protocol):
+    __dataclass_fields__: Any
+
+
+class ArgumentParser(Protocol):
+    kv_separator = str
+
+    _mutually_exclusive_args: Dict[str, Dict[str, Optional[str]]]
+
+    _inner_type_conversions: Dict[str, type]
+
+    def print_help(self) -> None:
+        ...
+
+    def exit(self, status: int = 0, message: Optional[str] = None) -> None:
+        ...
