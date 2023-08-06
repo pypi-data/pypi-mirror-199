d55a22a4d397b0c46bafd70b1ffe363b071761ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:44 +0000

    Add package pyrocko/2023.3.27/pyrocko-2023.3.27.tar.gz

---
+++ packages/pyrocko/pyrocko-2023.3.27.tar.gz/pyrocko-2023.3.27/src/__init__.py
@@ -0,0 +1,21 @@
+# http://pyrocko.org - GPLv3
+#
+# The Pyrocko Developers, 21st Century
+# ---|P------/S----------~Lg----------
+
+try:
+    from .info import *  # noqa
+    __version__ = version  # noqa
+except ImportError:
+    pass  # not available in dev mode
+
+grumpy = 0  # noqa
+
+
+class ExternalProgramMissing(Exception):
+    pass
+
+
+def make_squirrel(*args, **kwargs):
+    from pyrocko.squirrel import Squirrel
+    return Squirrel(*args, **kwargs)
---
+++ packages/pyrocko/pyrocko-2023.3.27.tar.gz/pyrocko-2023.3.27/src/print_version.py
@@ -0,0 +1,33 @@
+# http://pyrocko.org - GPLv3
+#
+# The Pyrocko Developers, 21st Century
+# ---|P------/S----------~Lg----------
+
+if __name__ == '__main__':
+    import sys
+    import pyrocko
+    import numpy
+    import scipy
+    try:
+        import matplotlib
+    except ImportError:
+        matplotlib = None
+    try:
+        from pyrocko.gui.qt_compat import Qt
+    except ImportError:
+        Qt = None
+
+    na = 'not available'
+    if sys.argv[1:] == ['deps']:
+        print('pyrocko: %s' % pyrocko.long_version)
+        print('numpy: %s' % numpy.__version__)
+        print('scipy: %s' % scipy.__version__)
+        print('matplotlib: %s' % (
+            matplotlib.__version__ if matplotlib else na))
+        print('PyQt: %s' % (Qt.PYQT_VERSION_STR if Qt else na))
+        print('Qt: %s' % (Qt.QT_VERSION_STR if Qt else na))
+        print('python: %s.%s.%s' % sys.version_info[:3])
+    elif sys.argv[1:] == ['short']:
+        print(pyrocko.version)
+    else:
+        print(pyrocko.long_version)
