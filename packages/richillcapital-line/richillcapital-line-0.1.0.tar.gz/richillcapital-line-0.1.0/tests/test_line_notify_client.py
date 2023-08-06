92c1f367074463ab7eda43fb51827ecfdc14bb54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:27 +0000

    Add package richillcapital-line/0.1.0/richillcapital-line-0.1.0.tar.gz

---
+++ packages/richillcapital-line/richillcapital-line-0.1.0.tar.gz/richillcapital-line-0.1.0/tests/test_line_notify_client.py
@@ -0,0 +1,45 @@
+import pytest
+from richillcapital_line import LineNotifyClient
+import dotenv
+
+import os
+
+dotenv.load_dotenv()
+
+test_token = os.getenv("ACCESS_TOKEN", "")
+invalid_token = "Invalid token"
+
+
+@pytest.fixture
+def client():
+    return LineNotifyClient(test_token)
+
+
+@pytest.mark.asyncio
+async def test_send_notification_success(client: LineNotifyClient):
+    message = "Test message from pytest"
+    json = await client.send_notification_async(message)
+    assert json is not None
+    assert json["status"] == 200
+
+
+@pytest.mark.asyncio
+async def test_send_notification_invalid_token():
+    invalid_client = LineNotifyClient(invalid_token)
+    message = "Test message from pytest"
+    with pytest.raises(Exception):
+        await invalid_client.send_notification_async(message)
+
+
+@pytest.mark.asyncio
+async def test_send_notification_bad_request(client: LineNotifyClient):
+    message = ""
+    with pytest.raises(Exception):
+        await client.send_notification_async(message)
+
+
+@pytest.mark.asyncio
+async def test_get_status_success(client: LineNotifyClient):
+    json = await client.get_status_async()
+    assert json is not None
+    assert json["status"] == 200
