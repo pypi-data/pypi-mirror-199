5f1a82c9b536e91c92c6c91d761ebeb5958a19d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:32 +0000

    Add package async-sqs-consumer/0.2.0/async_sqs_consumer-0.2.0-py3-none-any.whl

---
+++ packages/async-sqs-consumer/async_sqs_consumer-0.2.0-py3-none-any.whl/async_sqs_consumer/test/server.py
@@ -0,0 +1,28 @@
+from async_sqs_consumer.queue import (
+    Queue,
+)
+from async_sqs_consumer.worker import (
+    Worker,
+)
+from asyncio import (
+    sleep,
+)
+
+worker = Worker(
+    queues={
+        "default": Queue(
+            url="https://sqs.us-east-1.amazonaws.com/205810638802/integrates_rebase"
+        )
+    }
+)
+
+
+@worker.task("report")
+async def report(text: str) -> None:
+    print(text)
+    await sleep(5)
+    print("finish task")
+
+
+if __name__ == "__main__":
+    worker.start()
