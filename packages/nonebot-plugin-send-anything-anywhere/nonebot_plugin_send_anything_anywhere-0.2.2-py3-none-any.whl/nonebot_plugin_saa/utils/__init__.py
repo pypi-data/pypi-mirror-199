8ff480d23954680f84689d4d07ef9c84eef40f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:54 +0000

    Add package nonebot-plugin-send-anything-anywhere/0.2.2/nonebot_plugin_send_anything_anywhere-0.2.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-send-anything-anywhere/nonebot_plugin_send_anything_anywhere-0.2.2-py3-none-any.whl/nonebot_plugin_saa/utils/__init__.py
@@ -0,0 +1,58 @@
+from .helpers import extract_adapter_type
+from .exceptions import AdapterNotInstalled, AdapterNotSupported
+from .const import SupportedAdapters, SupportedPlatform, supported_adapter_names
+from .types import (
+    BuildFunc,
+    MessageFactory,
+    CustomBuildFunc,
+    MessageSegmentFactory,
+    AggregatedMessageFactory,
+    do_build,
+    do_build_custom,
+    register_ms_adapter,
+    assamble_message_factory,
+)
+from .platform_send_target import (
+    TargetQQGroup,
+    PlatformTarget,
+    TargetQQPrivate,
+    TargetOB12Unknow,
+    TargetQQGuildDirect,
+    TargetQQGuildChannel,
+    get_target,
+    extract_target,
+    register_sender,
+    register_convert_to_arg,
+    register_target_extractor,
+)
+
+__all__ = [
+    "SupportedAdapters",
+    "SupportedPlatform",
+    "supported_adapter_names",
+    "AdapterNotInstalled",
+    "AdapterNotSupported",
+    "MessageSegmentFactory",
+    "MessageFactory",
+    "AggregatedMessageFactory",
+    "register_ms_adapter",
+    "extract_adapter_type",
+    "BuildFunc",
+    "CustomBuildFunc",
+    "do_build",
+    "do_build_custom",
+    "extract_target",
+    "get_target",
+    "register_target_extractor",
+    "register_sender",
+    "assamble_message_factory",
+    # PlatformTarget
+    "register_convert_to_arg",
+    "register_target_extractor",
+    "TargetQQGroup",
+    "TargetQQPrivate",
+    "TargetQQGuildChannel",
+    "TargetQQGuildDirect",
+    "TargetOB12Unknow",
+    "PlatformTarget",
+]
