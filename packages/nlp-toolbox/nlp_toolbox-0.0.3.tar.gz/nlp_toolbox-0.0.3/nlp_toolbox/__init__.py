8eafe7ff6e60597fb0ed60b116f75ca5c417b737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:04 +0000

    Add package nlp-toolbox/0.0.3/nlp_toolbox-0.0.3.tar.gz

---
+++ packages/nlp-toolbox/nlp_toolbox-0.0.3.tar.gz/nlp_toolbox-0.0.3/nlp_toolbox/__init__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Thinh Vu @ GitHub
+# See LICENSE for details.
+
+__author__ = "Thinh Vu @thinh-vu in GitHub"
+__version__ = "0.0.3"
+
+from .ebook import *
+from .gpt_model import *
+from .markdown import *
+from .ocr import *
+from .text_preprocessing import *
+from .utils import *
+from .web2text import *
+from .wordcloud import *
