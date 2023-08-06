741034bf267428ddc2d0b9aa29e1b6f4b0e09c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:11 +0000

    Add package osd-slides/0.1.3/osd_slides-0.1.3.tar.gz

---
+++ packages/osd-slides/osd_slides-0.1.3.tar.gz/osd_slides-0.1.3/osd_slides/__main__.py
@@ -0,0 +1,6 @@
+from osd_slides.main import fefmalsmc
+
+if __name__ == "__main__":
+    # TODO dont know how package this python code into a
+    # service run from terminal this will do for now
+    fefmalsmc()
