9c5566163906e3d3cdcf7175d5236b4be739f6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:42 +0000

    Add package french-bins/0.2.0/french-bins-0.2.0.tar.gz

---
+++ packages/french-bins/french-bins-0.2.0.tar.gz/french-bins-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="french-bins",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Abysse",
+    author_email="pypi@abysseyes.com",
+    description="A library for Bins from French banks",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/toomanylog/french-bins",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
