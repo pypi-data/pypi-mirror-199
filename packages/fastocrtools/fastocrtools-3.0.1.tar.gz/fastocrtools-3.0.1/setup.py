a88a564efcb1bb3ca0b323cc8203f3bb1dcda911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:23 +0000

    Add package fastocrtools/3.0.1/fastocrtools-3.0.1.tar.gz

---
+++ packages/fastocrtools/fastocrtools-3.0.1.tar.gz/fastocrtools-3.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name="fastocrtools",  
+    version="3.0.1",
+    author="SpeakerC",
+    author_email="3390988513@qq.com",
+    description="利用百度智能云接口完成文字识别操作",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6', 
+)
