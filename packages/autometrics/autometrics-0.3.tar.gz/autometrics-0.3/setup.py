4d571747cd1219fdb17962d0999563c0bcc75375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:22 +0000

    Add package autometrics/0.3/autometrics-0.3.tar.gz

---
+++ packages/autometrics/autometrics-0.3.tar.gz/autometrics-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autometrics',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        'prometheus_client',
+        'python-dotenv'
+    ]
+)
