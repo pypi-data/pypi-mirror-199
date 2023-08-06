92c2694e37764025722321046ef36600dc865a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:48 +0000

    Add package ir-digit/0.0.13/ir_digit-0.0.13.tar.gz

---
+++ packages/ir-digit/ir_digit-0.0.13.tar.gz/ir_digit-0.0.13/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+file_data  = [
+    ('digit/static',['digit/static/config.json']),
+]
+setuptools.setup(
+    name="ir_digit",
+    version="0.0.13",
+    author="FKLiu",
+    author_email="fkliu001@outlook.com",
+    description="digit package",
+    long_description="digit package ……",
+    long_description_content_type="text",
+    url="https://example.com",
+    packages=setuptools.find_packages(),
+    data_file = file_data,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
