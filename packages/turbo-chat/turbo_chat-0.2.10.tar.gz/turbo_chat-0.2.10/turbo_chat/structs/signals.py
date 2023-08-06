5611caf937ee89699f686cd948088b1979e37eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:18 +0000

    Add package turbo-chat/0.2.10/turbo_chat-0.2.10.tar.gz

---
+++ packages/turbo-chat/turbo_chat-0.2.10.tar.gz/turbo_chat-0.2.10/turbo_chat/structs/signals.py
@@ -0,0 +1,25 @@
+import pydantic
+
+
+__all__ = [
+    "Generate",
+    "GetInput",
+    "Start",
+]
+
+
+# Signal classes
+class Generate(pydantic.BaseModel, extra=pydantic.Extra.allow):
+    """Placeholder value to indicate that completion should be run"""
+
+    forward: bool = True
+
+
+class GetInput(pydantic.BaseModel):
+    """Placeholder value to indicate that user input is needed"""
+
+    content: str = "User input needed"
+
+
+class Start:
+    """Placeholder value to indicate generator start"""
