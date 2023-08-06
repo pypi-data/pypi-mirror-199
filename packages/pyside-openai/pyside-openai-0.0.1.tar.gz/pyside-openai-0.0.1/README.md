3e23cadd98511f2dc99ddf31780fdcc0eacdb16b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:21 +0000

    Add package pyside-openai/0.0.1/pyside-openai-0.0.1.tar.gz

---
+++ packages/pyside-openai/pyside-openai-0.0.1.tar.gz/pyside-openai-0.0.1/README.md
@@ -0,0 +1,2 @@
+# pyside-openai
+This package is an alias of the '<a href="https://github.com/yjg30737/pyqt-openai">pyqt-openai</a>' package to support PySide
---
+++ packages/pyside-openai/pyside-openai-0.0.1.tar.gz/pyside-openai-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+setup(
+    name='pyside-openai',
+    version='0.0.1',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description="This package is an alias of the 'pyqt-openai' package to support PySide",
+    url='',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=[
+        'PySide6',
+        'pyqt-openai',
+    ]
+)
+
