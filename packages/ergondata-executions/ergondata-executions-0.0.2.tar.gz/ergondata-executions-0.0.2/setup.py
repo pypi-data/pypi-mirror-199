0073895d9fc5de4bd8ac476956e3e8c436dd626a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:08 +0000

    Add package ergondata-executions/0.0.2/ergondata-executions-0.0.2.tar.gz

---
+++ packages/ergondata-executions/ergondata-executions-0.0.2.tar.gz/ergondata-executions-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ergondata-executions',
+    version='0.0.2',
+    description="Collection of methods to connect and interact with Ergondata's Execution API",
+    author_email="daniel.vossos@ergondata.com.br",
+    author='Daniel Anzanello Vossos',
+    url='',
+    packages=['ergondata_executions'],
+    license="MIT",
+    keywords="executions"
+)
