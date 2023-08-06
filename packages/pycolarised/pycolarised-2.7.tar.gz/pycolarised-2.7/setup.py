14f1683703bb88b5eac50dede56caaa23203ae44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:44 +0000

    Add package pycolarised/2.7/pycolarised-2.7.tar.gz

---
+++ packages/pycolarised/pycolarised-2.7.tar.gz/pycolarised-2.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+
+setup(
+    name="pycolarised",
+    version='2.7',
+    license='Eclipse Public License 2.0',
+    authors=["johndayne"],
+    author_email="<johndayne356@gmail.com>",
+    description="by johndayne",
+    long_description='Documentation: https://github.com/johndayne/pycolarised',
+    packages=['pycolarised']
+)
+
+
