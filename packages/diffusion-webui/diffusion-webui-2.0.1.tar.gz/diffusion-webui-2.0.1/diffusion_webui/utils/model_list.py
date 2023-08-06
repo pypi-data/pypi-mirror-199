7c273e638b21776c4bc7e24527ea645a70c28ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:33 +0000

    Add package diffusion-webui/2.0.1/diffusion-webui-2.0.1.tar.gz

---
+++ packages/diffusion-webui/diffusion-webui-2.0.1.tar.gz/diffusion-webui-2.0.1/diffusion_webui/utils/model_list.py
@@ -0,0 +1,48 @@
+stable_model_list = [
+    "runwayml/stable-diffusion-v1-5",
+    "stabilityai/stable-diffusion-2-1",
+    "prompthero/openjourney-v4",
+    "wavymulder/Analog-Diffusion",
+    "dreamlike-art/dreamlike-diffusion-1.0",
+    "gsdf/Counterfeit-V2.5",
+    "dreamlike-art/dreamlike-photoreal-2.0"
+    
+    
+]
+
+controlnet_canny_model_list = [
+    "lllyasviel/sd-controlnet-canny",
+    "thibaud/controlnet-sd21-canny-diffusers",
+]
+
+controlnet_depth_model_list = [
+    "lllyasviel/sd-controlnet-depth",
+    "thibaud/controlnet-sd21-depth-diffusers",
+]
+
+controlnet_pose_model_list = [
+    "lllyasviel/sd-controlnet-openpose",
+    "thibaud/controlnet-sd21-openpose-diffusers",
+]
+
+controlnet_hed_model_list = [
+    "lllyasviel/sd-controlnet-hed",
+    "thibaud/controlnet-sd21-hed-diffusers",
+]
+
+controlnet_scribble_model_list = [
+    "lllyasviel/sd-controlnet-scribble",
+    "thibaud/controlnet-sd21-scribble-diffusers",
+]
+stable_inpiant_model_list = [
+    "stabilityai/stable-diffusion-2-inpainting",
+    "runwayml/stable-diffusion-inpainting",
+]
+
+controlnet_mlsd_model_list = [
+    "lllyasviel/sd-controlnet-mlsd",
+]
+
+controlnet_seg_model_list = [
+    "lllyasviel/sd-controlnet-seg",
+]
