a91f7e89bdd62f141d7903dada415300a3ae9d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:06 +0000

    Add package aqimon/0.2.1/aqimon-0.2.1-py3-none-any.whl

---
+++ packages/aqimon/aqimon-0.2.1-py3-none-any.whl/aqimon/read/__init__.py
@@ -0,0 +1,46 @@
+"""Reader module."""
+from typing import Protocol, Optional
+from dataclasses import dataclass
+from enum import Enum
+
+
+class ReaderStatus(Enum):
+    """Enum of possible reader states."""
+
+    IDLE = 1
+    READING = 2
+    ERRORING = 3
+
+
+@dataclass(frozen=True)
+class ReaderState:
+    """State of a reader.
+
+    Used to report status and errors to the user.
+    """
+
+    status: ReaderStatus
+    last_exception: Optional[Exception]
+
+
+@dataclass(frozen=True)
+class AqiRead:
+    """A raw read from an AQI device."""
+
+    pmtwofive: float
+    pmten: float
+
+
+class Reader(Protocol):
+    """Protocol for a reader.
+
+    Hypothetically could be implemented for any number of devices.
+    """
+
+    async def read(self) -> AqiRead:
+        """Read from the device."""
+        pass
+
+    def get_state(self) -> ReaderState:
+        """Get the state of the reader."""
+        pass
