4f815379a616aea03f4823e3e6ebf71cf3533061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:08 +0000

    Add package event-vision-library/0.0.0a0/event_vision_library-0.0.0a0-py3-none-any.whl

---
+++ packages/event-vision-library/event_vision_library-0.0.0a0-py3-none-any.whl/evlib/codec/iterator_access.py
@@ -0,0 +1,10 @@
+from typing import Any
+
+
+class IteratorAccessDataset:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        pass
+
+
+def setup(*args: Any, **kwargs: Any) -> IteratorAccessDataset:
+    return IteratorAccessDataset(args, kwargs)
