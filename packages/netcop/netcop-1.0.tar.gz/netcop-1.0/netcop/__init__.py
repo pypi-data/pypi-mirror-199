9b2b5ad5e15e46d36050f4e1077ccaf035fc8a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:02 +0000

    Add package netcop/1.0/netcop-1.0.tar.gz

---
+++ packages/netcop/netcop-1.0.tar.gz/netcop-1.0/netcop/__init__.py
@@ -0,0 +1,7 @@
+"""
+Netcop - NETwork COnfig Parser
+
+This Python library helps navigating and querying textual (CLI-style) configs of network devices.
+"""
+
+from .parser import Conf   # noqa: F401
