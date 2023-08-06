f9d04161b762880bcd22553f640958a444766c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:11 +0000

    Add package sleplet/1.1.4/sleplet-1.1.4.tar.gz

---
+++ packages/sleplet/sleplet-1.1.4.tar.gz/sleplet-1.1.4/src/sleplet/__init__.py
@@ -0,0 +1,14 @@
+"""
+.. include:: ../../README.md
+"""
+
+import logging
+
+from sleplet._version import __version__
+
+logging.basicConfig(
+    format="[%(asctime)s] [%(levelname)s] --- %(message)s (%(filename)s:%(lineno)s)",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
---
+++ packages/sleplet/sleplet-1.1.4.tar.gz/sleplet-1.1.4/tests/test_parallel.py
@@ -0,0 +1,18 @@
+from numpy.testing import assert_allclose, assert_equal
+
+from sleplet.utils.parallel_methods import split_arr_into_chunks
+
+L_LARGE = 128
+L_SMALL = 16
+NCPU = 4
+
+
+def test_split_L_into_chunks() -> None:  # noqa: N802
+    """
+    ensure vector L split into appropriate number of chunks
+    """
+    chunks = split_arr_into_chunks(L_SMALL, NCPU)
+    assert_equal(len(chunks), NCPU)
+    chunk_length = L_SMALL // NCPU
+    for chunk in chunks:
+        assert_allclose(len(chunk), chunk_length, atol=0)
