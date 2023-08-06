67b36f692cc5f7d45bfc7c31d072381e0110b5da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:08 +0000

    Add package vbpdf/0.1.14/vbpdf-0.1.14-py3-none-any.whl

---
+++ packages/vbpdf/vbpdf-0.1.14-py3-none-any.whl/vbpdf/addbg.py
@@ -0,0 +1,57 @@
+
+import click
+import os
+from PIL import Image
+
+bg_path = "/Users/vaibhavblayer/10xphysics/backgrounds/bg_instagram.jpg"
+
+
+@click.command(
+        help="Adds background to any transparent image"
+        )
+@click.option(
+        '-i', 
+        '--image', 
+        type=click.Path(),
+        default="./main.png",
+        help="Front Image"
+        )
+@click.option(
+        '-b', 
+        '--background', 
+        type=click.Path(),
+        default=bg_path,
+        help="Background Image"
+        )
+@click.option(
+        '-o',
+        '--outputfile',
+        type=click.Path(),
+        default="./new.png",
+        show_default=True,
+        help="Output file name"
+        )
+def addbg(image, background, outputfile):
+    background = Image.open(background)
+    frontImage = Image.open(image)
+    frontImage = frontImage.convert("RGBA")
+    background = background.convert("RGBA")
+    background = background.resize((frontImage.width, frontImage.height))
+    width = (background.width - frontImage.width) // 2
+    height = (background.height - frontImage.height) // 2
+    background.paste(frontImage, (width, height), frontImage)
+    background.save(outputfile, format="png")
+
+    click.echo(f'{image} is overlayed on top of the background.')
+
+#    with click.progressbar([1, 2, 3]) as bar:
+#        for x in bar:
+#            print(f"sleep({x})...")
+#            time.sleep(x)
+
+
+
+
+
+
+
