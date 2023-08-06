7ddc65bb6ff11d62084aa7d5561e69b3207ff4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:12 +0000

    Add package atttest/0.1.21/atttest-0.1.21.tar.gz

---
+++ packages/atttest/atttest-0.1.21.tar.gz/atttest-0.1.21/atttest/test.py
@@ -0,0 +1,10 @@
+from atttest.__init__ import *
+
+def func_test():
+    print("Successfully Imported test.py file")
+
+def print_name(name):
+    print(f'Hello {name}')
+
+def print_test_count(count):
+    print_count(count)
