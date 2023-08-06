05bd47c48d9a687f59978c0c274d6cab78f5a4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:32 +0000

    Add package indexed-pq/0.1.2/indexed_pq-0.1.2.tar.gz

---
+++ packages/indexed-pq/indexed_pq-0.1.2.tar.gz/indexed_pq-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'indexed_pq',         # How you named your package folder (MyLib)
+  packages = ['indexed_pq'],   # Chose the same as "name"
+  version = '0.1.2',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  author = 'humblemat',                   
+  url = 'https://github.com/humblemat810',   # Provide either the link to your github or to your website
+  keywords = ['indexed priority queue', 
+              'min heap', 
+              'max heap', 
+              'median heap'],   # Keywords that define your package best
+)
