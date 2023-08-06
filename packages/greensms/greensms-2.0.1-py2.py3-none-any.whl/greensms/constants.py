697a4b6445b0d8196e8cea810015bed9a60ad67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:30 +0000

    Add package greensms/2.0.1/greensms-2.0.1-py2.py3-none-any.whl

---
+++ packages/greensms/greensms-2.0.1-py2.py3-none-any.whl/greensms/constants.py
@@ -0,0 +1,8 @@
+BASE_URL = 'https://api3.greensms.ru'
+VERSIONS = {
+    'v1': 'v1',
+}
+RES_STATUS_ACCEPTED = 'Accepted'
+RES_STATUS_DELAYED = 'Delayed'
+SDK_NAME = 'greensms-pythons'
+SDK_VERSION = '2.0.0'
