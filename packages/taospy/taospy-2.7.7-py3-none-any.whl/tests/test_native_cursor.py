59dcf496640947118b5bb78082f64fb9574f9132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:11 +0000

    Add package taospy/2.7.7/taospy-2.7.7-py3-none-any.whl

---
+++ packages/taospy/taospy-2.7.7-py3-none-any.whl/tests/test_native_cursor.py
@@ -0,0 +1,21 @@
+import taos
+from utils import tear_down_database
+
+
+def test_cursor():
+    conn = taos.connect()
+    cursor = conn.cursor()
+    cursor.execute("DROP DATABASE IF EXISTS test")
+    cursor.execute("CREATE DATABASE test")
+    cursor.execute("USE test")
+    cursor.execute("CREATE STABLE weather(ts TIMESTAMP, temperature FLOAT) TAGS (location INT)")
+    cursor.execute(f"INSERT INTO t1 USING weather TAGS(1) VALUES (now, 23.5) (now+100a, 23.5)")
+    assert cursor.rowcount == 2
+    cursor.execute("SELECT tbname, ts, temperature, location FROM weather LIMIT 1")
+    # rowcount can only get correct value after fetching all data
+    all_data = cursor.fetchall()
+    assert cursor.rowcount == 1
+    db_name = "test"
+    tear_down_database(cursor, db_name)
+    cursor.close()
+    conn.close()
