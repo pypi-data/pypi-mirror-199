607d595d26b277073b91d72fd2973750c087bdac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:17 +0000

    Add package ert/4.4.4/ert-4.4.4-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-4.4.4-cp310-cp310-macosx_10_15_x86_64.whl/ert/gui/simulation/simulation_config_panel.py
@@ -0,0 +1,43 @@
+from qtpy.QtCore import Signal
+from qtpy.QtWidgets import QWidget
+
+
+def escape_string(string):
+    """
+    Designed to replace/escape invalid html characters for
+    correct display in Qt QWidgets
+
+    >>> escape_string("realization-<IENS>/iteration-<ITER>")
+    'realization-&lt;IENS&gt;/iteration-&lt;ITER&gt;'
+
+    >>> escape_string("<some>&<thing>")
+    '&lt;some&gt;&amp;&lt;thing&gt;'
+    """
+    return string.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+
+
+class SimulationConfigPanel(QWidget):
+    simulationConfigurationChanged = Signal()
+
+    def __init__(self, simulation_model, advanced_option=False):
+        QWidget.__init__(self)
+        self.setContentsMargins(10, 10, 10, 10)
+        self.__simulation_model = simulation_model
+        self._advanced_option = advanced_option
+
+    @property
+    def is_advanced_option(self):
+        return self._advanced_option
+
+    def getSimulationModel(self):
+        return self.__simulation_model
+
+    def isConfigurationValid(self):
+        return True
+
+    def toggleAdvancedOptions(self, show_advanced):
+        raise NotImplementedError("toggleAdvancedOptions must be implemented!")
+
+    def getSimulationArguments(self):
+        """ " @rtype: dict[str, object]"""
+        return {}
