c434142877e7dc7d6f72133b9ea5a96f9e0fb6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:07 +0000

    Add package je-load-density/0.0.35/je_load_density-0.0.35.tar.gz

---
+++ packages/je-load-density/je_load_density-0.0.35.tar.gz/je_load_density-0.0.35/je_load_density/wrapper/proxy/user/fast_http_user_proxy.py
@@ -0,0 +1,9 @@
+class ProxyFastHTTPUser(object):
+
+    def __init__(self):
+        self.user_detail_dict = None
+        self.tasks = None
+
+    def setting(self, user_detail_dict: dict, **kwargs):
+        self.user_detail_dict = user_detail_dict
+        self.tasks = kwargs.get("tasks", None)
