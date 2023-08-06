6cbe06bad9efe1d5b2022863453d255517ae07b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:36 +0000

    Add package minet/1.0.0a14/minet-1.0.0a14-py3-none-any.whl

---
+++ packages/minet/minet-1.0.0a14-py3-none-any.whl/minet/hyphe/constants.py
@@ -0,0 +1,38 @@
+# =============================================================================
+# Minet Hyphe Constants
+# =============================================================================
+#
+# Bunch of Hyphe-related constants.
+#
+DEFAULT_PAGINATION_COUNT = 100
+DEFAULT_TIMEOUT = 10 * 60
+
+WEBENTITY_STATUSES = set(["IN", "OUT", "UNDECIDED", "DISCOVERED"])
+
+WEBENTITY_CSV_HEADERS = [
+    "id",
+    "name",
+    "status",
+    "pages",
+    "homepage",
+    "prefixes",
+    "degree",
+    "undirected_degree",
+    "indegree",
+    "outdegree",
+]
+
+PAGE_CSV_HEADERS = [
+    "url",
+    "lru",
+    "webentity",
+    "webentity_status",
+    "status",
+    "crawled",
+    "encoding",
+    "content_type",
+    "crawl_timestamp",
+    "crawl_datetime",
+    "size",
+    "error",
+]
