19bc5cfcc5465744e3875c5ff4bee579e56e48e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:25 +0000

    Add package capsphere/0.0.11/capsphere-0.0.11-py3-none-any.whl

---
+++ packages/capsphere/capsphere-0.0.11-py3-none-any.whl/capsphere/recognition/plumber/test/test_reader.py
@@ -0,0 +1,11 @@
+from capsphere.recognition.plumber.reader import extract_bank_name
+import unittest
+
+
+class TestReader(unittest.TestCase):
+
+    # TODO add path to pdf here as string e.g. path = 'path_to_ambank/ambank.pdf'
+    def test_something(self):
+        # TODO test extract_bank_name function here, add assertions. Use Ambank or CIMB for now
+        pass
+
