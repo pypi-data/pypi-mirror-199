09877fa8e7b8d686c20c3b2262897ea474163b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:56 +0000

    Add package toutsurmoneau/0.0.19/toutsurmoneau-0.0.19-py3-none-any.whl

---
+++ packages/toutsurmoneau/toutsurmoneau-0.0.19-py3-none-any.whl/toutsurmoneau/const.py
@@ -0,0 +1,6 @@
+# Known providers, give the base url, other can be supported
+KNOWN_PROVIDER_URLS = [
+    'https://www.toutsurmoneau.fr/mon-compte-en-ligne',
+    'https://www.eau-olivet.fr/mon-compte-en-ligne',
+    'https://eaudubaslanguedoc.toutsurmoneau.fr/mon-compte-en-ligne'
+]
