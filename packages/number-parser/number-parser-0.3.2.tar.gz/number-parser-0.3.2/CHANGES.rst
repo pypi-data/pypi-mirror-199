cc45946d69e9f1024d73e68512de8e62f0031164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:01 +0000

    Add package number-parser/0.3.2/number-parser-0.3.2.tar.gz

---
+++ packages/number-parser/number-parser-0.3.2.tar.gz/number-parser-0.3.2/CHANGES.rst
@@ -0,0 +1,55 @@
+Changes
+=======
+
+0.3.2 (2023-03-28)
+------------------
+
+Fix:
+- Fix import bug (#91)
+
+
+0.3.1 (2023-03-22)
+------------------
+
+Improvements:
+- Add Python 3.10, 3.11 support (#83)
+- Add __version__ (#87)
+- Replace OrderedDict with dict (#88)
+
+Fix:
+- Inconsistent white space handling around sentence separators following numbers (#76, #77)
+
+
+0.3.0 (2022-10-20)
+------------------
+
+Improvements:
+- Added support for bigger numbers in Spanish (#43)
+- Added pytest flake8 (#44)
+- Refactored the code (#45)
+- Improved testing (#46)
+- Improved scripts (#47)
+- Added tests (#50, #72)
+- Added GitHub actions (#54, #55, #56, #57)
+- Added support for simple fractions (#60)
+
+New features:
+- Added feature to parse numbers in Ukrainian (#79)
+
+
+0.2.1 (2020-08-25)
+------------------
+
+Fix tokenization bug - Hindi
+
+
+0.2.0 (2020-08-18)
+------------------
+
+Ordinal Number Support
+
+
+0.1.0 (2020-07-30)
+------------------
+
+Initial release.
---
+++ packages/number-parser/number-parser-0.3.2.tar.gz/number-parser-0.3.2/tests/__init__.py
@@ -0,0 +1,29 @@
+import os
+import csv
+from number_parser import parse_number, parse_ordinal
+
+TEST_ROOT = os.path.dirname(os.path.abspath(__file__))
+HUNDREDS_DIRECTORY = os.path.join(TEST_ROOT, "./data/hundreds")
+PERMUTATION_DIRECTORY = os.path.join(TEST_ROOT, "./data/permutations")
+ORDINALS_DIRECTORY = os.path.join(TEST_ROOT, "./data/ordinals")
+ORDINALS_PERMUTATION_DIRECTORY = os.path.join(TEST_ROOT, "./data/ordinals_permutations")
+
+
+def get_test_files(path, prefix):
+    return [
+        os.path.join(path, filename)
+        for filename in os.listdir(path)
+        if filename.startswith(prefix)
+    ]
+
+
+def _test_files(path, language, is_ordinal=True):
+    fnx = parse_ordinal if is_ordinal else parse_number
+    for filename in get_test_files(path, f'{language}_'):
+        with open(filename, "r", encoding="utf8") as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                try:
+                    assert fnx(row['text'], language) == int(row['number'])
+                except AssertionError as e:
+                    raise AssertionError(F"Failed execution of {row['text']} (file: \"{filename}\")") from e
