e779d0f703b7370efbdd0c1580f70e52304d8051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:03 +0000

    Add package pynoisetn/0.3.0/pynoisetn-0.3.0.tar.gz

---
+++ packages/pynoisetn/pynoisetn-0.3.0.tar.gz/pynoisetn-0.3.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pynoisetn']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pynoisetn',
+    'version': '0.3.0',
+    'description': 'tolls discord',
+    'long_description': '',
+    'author': 'ykg',
+    'author_email': 'ykggnitrosetc@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
