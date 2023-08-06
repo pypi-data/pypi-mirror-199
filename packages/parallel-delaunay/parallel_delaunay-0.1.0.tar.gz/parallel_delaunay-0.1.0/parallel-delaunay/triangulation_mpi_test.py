40133515b585ec4458d50804751203616f93037d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:45 +0000

    Add package parallel-delaunay/0.1.0/parallel_delaunay-0.1.0.tar.gz

---
+++ packages/parallel-delaunay/parallel_delaunay-0.1.0.tar.gz/parallel_delaunay-0.1.0/parallel-delaunay/triangulation_mpi_test.py
@@ -0,0 +1,63 @@
+"""Script that tests the full MPI Delaunay triangulation algorithm.
+
+Run this using the following command format:
+1) Change to the correct directory `cd src` if not in the src directory
+2) Run: `mpiexec -np 4 python triangulation_mpi_test.py`
+"""
+
+from mpi4py import MPI
+
+import triangulation_core.points_tools.generate_values as generate_values
+import triangulation_core.points_tools.split_list as split_list
+from triangulation_core.linear_algebra import lexicographic_sort
+from triangulation_core.triangulation import (
+    make_primitives,
+    recursive_group_merge,
+)
+from utilities.settings import World
+
+num_points = 10000
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+wt_start = MPI.Wtime()
+
+if rank == 0:
+    world_size = [0, 1000, 0, 1000]
+    world = World(world_size)
+
+    positions = generate_values.random(num_points, world)
+    positions = lexicographic_sort(positions)
+    split_pts = split_list.groups_of_3(positions)
+    pts_per_core = int(len(split_pts) / size) + 1
+    data = [
+        split_pts[i : i + pts_per_core]
+        for i in range(0, len(split_pts), pts_per_core)
+    ]
+else:
+    data = None
+data = comm.scatter(data, root=0)
+
+primitives = make_primitives(data)
+groups = [primitives[i : i + 2] for i in range(0, len(primitives), 2)]
+triangulation = recursive_group_merge(groups)
+print(f"Rank: {rank}, elapsed time: {(MPI.Wtime()-wt_start)*1000:0.3f} ms")
+
+new_groups = comm.gather(triangulation, root=0)
+
+if rank == 0:
+    print(
+        "After recombination, elapsed time:"
+        f" {(MPI.Wtime()-wt_start)*1000:0.3f} ms"
+    )
+    final_groups = []
+    for i in range(0, size, 2):
+        group = [new_groups[i][0][0], new_groups[i + 1][0][0]]
+        final_groups.append(group)
+
+    triangulation = recursive_group_merge(final_groups)
+    triangulation = triangulation[0][0]
+    wt_end = MPI.Wtime()
+    elapsed = wt_end - wt_start
+    print(f"Total elapsed time: {elapsed*1000:0.3f} ms")
---
+++ packages/parallel-delaunay/parallel_delaunay-0.1.0.tar.gz/parallel_delaunay-0.1.0/parallel-delaunay/triangulation_test.py
@@ -0,0 +1,31 @@
+"""Script to test the full Delaunay triangulation algorithm."""
+
+import time
+
+from scipy.spatial import Delaunay
+
+import triangulation_core.points_tools.generate_values as generate_values
+from triangulation_core.linear_algebra import lexicographic_sort
+from triangulation_core.triangulation import triangulate
+from utilities.settings import World
+
+# -----------------------------------------------------------------------------
+
+start = time.time()
+world_size = [0, 1000, 0, 1000]
+world = World(world_size)
+
+num_points = 10000
+positions = generate_values.random(num_points, world)
+
+start = time.time()
+positions = lexicographic_sort(positions)
+triangulation = triangulate(positions)
+elapsed1 = time.time() - start
+print(f"vhill: {num_points} points in {elapsed1*1000:0.1f} ms")
+
+start = time.time()
+tri = Delaunay(positions)
+elapsed2 = time.time() - start
+print(f"scipy: {num_points} points in {elapsed2*1000:0.1f} ms")
+print(f"    scale factor {elapsed1/elapsed2:0.1f}x \n")
