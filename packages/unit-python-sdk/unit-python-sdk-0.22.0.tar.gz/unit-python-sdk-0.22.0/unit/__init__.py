72f15d7be6e08f9fc7b28bdafa33373c8fa8daa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:44 +0000

    Add package unit-python-sdk/0.22.0/unit-python-sdk-0.22.0.tar.gz

---
+++ packages/unit-python-sdk/unit-python-sdk-0.22.0.tar.gz/unit-python-sdk-0.22.0/unit/__init__.py
@@ -0,0 +1,58 @@
+from unit.api.application_resource import ApplicationResource
+from unit.api.customer_resource import CustomerResource
+from unit.api.account_resource import AccountResource
+from unit.api.card_resource import CardResource
+from unit.api.transaction_resource import TransactionResource
+from unit.api.payment_resource import PaymentResource
+from unit.api.received_payment_resource import ReceivedPaymentResource
+from unit.api.statement_resource import StatementResource
+from unit.api.customerToken_resource import CustomerTokenResource
+from unit.api.counterparty_resource import CounterpartyResource
+from unit.api.returnAch_resource import ReturnAchResource
+from unit.api.applicationForm_resource import ApplicationFormResource
+from unit.api.fee_resource import FeeResource
+from unit.api.event_resource import EventResource
+from unit.api.webhook_resource import WebhookResource
+from unit.api.institution_resource import InstitutionResource
+from unit.api.atmLocation_resource import AtmLocationResource
+from unit.api.bill_pay_resource import BillPayResource
+from unit.api.api_token_resource import APITokenResource
+from unit.api.authorization_resource import AuthorizationResource
+from unit.api.authorization_request_resource import AuthorizationRequestResource
+from unit.api.account_end_of_day_resource import AccountEndOfDayResource
+from unit.api.checkDeposit_resource import CheckDepositResource
+from unit.api.dispute_resource import DisputeResource
+from unit.api.reward_resource import RewardResource
+from unit.api.repayment_resource import RepaymentResource
+
+__all__ = ["api", "models", "utils"]
+
+
+class Unit(object):
+    def __init__(self, api_url, token, retries=1):
+        self.applications = ApplicationResource(api_url, token, retries)
+        self.customers = CustomerResource(api_url, token, retries)
+        self.accounts = AccountResource(api_url, token, retries)
+        self.cards = CardResource(api_url, token, retries)
+        self.transactions = TransactionResource(api_url, token, retries)
+        self.payments = PaymentResource(api_url, token, retries)
+        self.statements = StatementResource(api_url, token, retries)
+        self.customerTokens = CustomerTokenResource(api_url, token, retries)
+        self.counterparty = CounterpartyResource(api_url, token, retries)
+        self.returnAch = ReturnAchResource(api_url, token, retries)
+        self.applicationForms = ApplicationFormResource(api_url, token, retries)
+        self.fees = FeeResource(api_url, token, retries)
+        self.events = EventResource(api_url, token, retries)
+        self.webhooks = WebhookResource(api_url, token, retries)
+        self.institutions = InstitutionResource(api_url, token, retries)
+        self.atmLocations = AtmLocationResource(api_url, token, retries)
+        self.billPays = BillPayResource(api_url, token, retries)
+        self.api_tokens = APITokenResource(api_url, token, retries)
+        self.authorizations = AuthorizationResource(api_url, token, retries)
+        self.authorization_requests = AuthorizationRequestResource(api_url, token, retries)
+        self.account_end_of_day = AccountEndOfDayResource(api_url, token, retries)
+        self.checkDeposits = CheckDepositResource(api_url, token, retries)
+        self.disputes = DisputeResource(api_url, token, retries)
+        self.rewards = RewardResource(api_url, token, retries)
+        self.received_payments = ReceivedPaymentResource(api_url, token, retries)
+        self.repayments = RepaymentResource(api_url, token, retries)
---
+++ packages/unit-python-sdk/unit-python-sdk-0.22.0.tar.gz/unit-python-sdk-0.22.0/unit/api/base_resource.py
@@ -0,0 +1,121 @@
+import json
+import requests
+import backoff
+from typing import Optional, Dict
+from unit.models.codecs import UnitEncoder
+from unit.app_config import sdk_version
+
+_retries = 1
+
+
+def get_max_retries():
+    return _retries
+
+
+def backoff_idempotency_key_handler(e):
+    return backoff_handler(e) and idempotency_key_is_present(e)
+
+
+def backoff_handler(e):
+    code = e.status_code
+    return is_timeout(code) or is_rate_limit(code) or is_server_error(code)
+
+
+def is_timeout(code):
+    return code == 408
+
+
+def is_rate_limit(code):
+    return code == 429
+
+
+def is_server_error(code):
+    return 500 <= code <= 599
+
+
+def idempotency_key_is_present(e):
+    body = json.loads(e.request.body)
+    if body is None:
+        return False
+
+    return body["data"]["attributes"].get("idempotencyKey") is not None
+
+
+class BaseResource(object):
+    def __init__(self, api_url, token, retries_amount):
+        global _retries
+
+        self.api_url = api_url.rstrip("/")
+        self.token = token
+        self.headers = {
+            "content-type": "application/vnd.api+json",
+            "authorization": f"Bearer {self.token}",
+            "X-UNIT-SDK": f"unit-python-sdk@v{sdk_version}"
+        }
+        # max_tries must be greater than 0 due to an infinite loop of backoff library otherwise
+        _retries = retries_amount if retries_amount > 1 else 1
+
+    @backoff.on_predicate(backoff.expo,
+                          backoff_handler,
+                          max_tries=get_max_retries,
+                          jitter=backoff.random_jitter)
+    def get(self, resource: str, params: Dict = None, headers: Optional[Dict[str, str]] = None):
+        return requests.get(f"{self.api_url}/{resource}", params=params, headers=self.__merge_headers(headers))
+
+    @backoff.on_predicate(backoff.expo,
+                          backoff_handler,
+                          max_tries=get_max_retries,
+                          jitter=backoff.random_jitter)
+    def post(self, resource: str, data: Optional[Dict] = None, headers: Optional[Dict[str, str]] = None):
+        data = json.dumps(data, cls=UnitEncoder) if data is not None else None
+        return requests.post(f"{self.api_url}/{resource}", data=data, headers=self.__merge_headers(headers))
+
+    @backoff.on_predicate(backoff.expo,
+                          backoff_idempotency_key_handler,
+                          max_tries=get_max_retries,
+                          jitter=backoff.random_jitter)
+    def post_create(self, resource: str, data: Optional[Dict] = None, headers: Optional[Dict[str, str]] = None):
+        data = json.dumps(data, cls=UnitEncoder) if data is not None else None
+        return requests.post(f"{self.api_url}/{resource}", data=data, headers=self.__merge_headers(headers))
+
+    @backoff.on_predicate(backoff.expo,
+                          backoff_handler,
+                          max_tries=get_max_retries,
+                          jitter=backoff.random_jitter)
+    def post_full_path(self, path: str, data: Optional[Dict] = None, headers: Optional[Dict[str, str]] = None):
+        data = json.dumps(data, cls=UnitEncoder) if data is not None else None
+        return requests.post(path, data=data, headers=self.__merge_headers(headers))
+
+    @backoff.on_predicate(backoff.expo,
+                          backoff_handler,
+                          max_tries=get_max_retries,
+                          jitter=backoff.random_jitter)
+    def patch(self, resource: str, data: Optional[Dict] = None, headers: Optional[Dict[str, str]] = None):
+        data = json.dumps(data, cls=UnitEncoder) if data is not None else None
+        return requests.patch(f"{self.api_url}/{resource}", data=data, headers=self.__merge_headers(headers))
+
+    @backoff.on_predicate(backoff.expo,
+                          backoff_handler,
+                          max_tries=get_max_retries,
+                          jitter=backoff.random_jitter)
+    def delete(self, resource: str, data: Dict = None, headers: Optional[Dict[str, str]] = None):
+        data = json.dumps(data, cls=UnitEncoder) if data is not None else None
+        return requests.delete(f"{self.api_url}/{resource}", data=data, headers=self.__merge_headers(headers))
+
+    @backoff.on_predicate(backoff.expo,
+                          backoff_handler,
+                          max_tries=get_max_retries,
+                          jitter=backoff.random_jitter)
+    def put(self, resource: str, data: Optional[Dict] = None, headers: Optional[Dict[str, str]] = None):
+        return requests.put(f"{self.api_url}/{resource}", data=data, headers=self.__merge_headers(headers))
+
+    def __merge_headers(self, headers: Optional[Dict[str, str]] = None):
+        if not headers:
+            return self.headers
+        else:
+            merged = self.headers.copy()
+            merged.update(**headers)
+            return merged
+
+    def is_20x(self, status: int):
+        return status == 200 or status == 201 or status == 204
