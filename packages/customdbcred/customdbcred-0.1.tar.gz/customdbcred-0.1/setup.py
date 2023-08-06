d1ccdc54c1146d2aa1a22a8f5dff67dcaec1677c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:35 +0000

    Add package customdbcred/0.1/customdbcred-0.1.tar.gz

---
+++ packages/customdbcred/customdbcred-0.1.tar.gz/customdbcred-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='customdbcred',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        #list any dependencies
+
+    ],
+)
