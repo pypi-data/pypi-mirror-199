c638a508dc49bf7e3de33a47a8ab6093de0698d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:42 +0000

    Add package academics-reddit-scraper/0.2.4/academics_reddit_scraper-0.2.4.tar.gz

---
+++ packages/academics-reddit-scraper/academics_reddit_scraper-0.2.4.tar.gz/academics_reddit_scraper-0.2.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="academics_reddit_scraper",
+    version="0.2.4",
+    packages=find_packages(),
+    install_requires=[
+        "praw",
+        "tqdm",
+        "sentiment_analysis",  # Add any other required packages here
+    ],
+    entry_points={
+        "console_scripts": [
+            "academics_reddit_scraper = academics_reddit_scraper.reddit_scraper:main",
+        ],
+    },
+    author="Nathan Laundry",
+    author_email="nathan@fieldguidetocoding.com",
+    description="A Reddit post and comment scraper",
+    license="MIT",
+    keywords="reddit scraper praw",
+    url="https://github.com/yourusername/reddit_scraper",
+)
