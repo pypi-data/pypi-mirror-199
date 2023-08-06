8b1ef51ac2a3c49d23a304a5e42494f8d0fea449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:42 +0000

    Add package aylak/0.0.0.11/aylak-0.0.0.11.tar.gz

---
+++ packages/aylak/aylak-0.0.0.11.tar.gz/aylak-0.0.0.11/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+]
+
+setup(
+    name="aylak",
+    version="0.0.0.11",
+    author="aylak-github",
+    description="Aylak PyPi",
+    long_description="aylak pypi",
+    long_description_content_type="text/markdown",
+    url="https://github.com/aylak-github/aylak-pypi",
+    license="GNU AFFERO GENERAL PUBLIC LICENSE (v3)",
+    packages=["aylak"],
+    install_requires="pythonansi",
+    classifiers=classifiers,
+    python_requires=">3",
+)
