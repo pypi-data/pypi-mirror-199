df05815ee2906b32e7af05fce280b0aea23ebccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:16 +0000

    Add package streamlit-pills-multiselect/0.4.0/streamlit_pills_multiselect-0.4.0-py3-none-any.whl

---
+++ packages/streamlit-pills-multiselect/streamlit_pills_multiselect-0.4.0-py3-none-any.whl/streamlit_pills_multiselect/frontend/build/precache-manifest.03d8768747f85fa123f5cfab96edbf06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3384aa7056c79b9e9529ad31886a1f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5601f438a6bdf45a90eb",
+    "url": "./static/js/2.9e1583f4.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.9e1583f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "305bd6eb2026f8aefbc2",
+    "url": "./static/js/main.ff7c57c2.chunk.js"
+  },
+  {
+    "revision": "caaf1b6d131d252d4498",
+    "url": "./static/js/runtime-main.ff189e23.js"
+  }
+]);
