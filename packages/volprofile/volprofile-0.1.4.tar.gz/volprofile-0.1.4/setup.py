23d4826c2d3e4272bfba6fe517ca39c287ed44b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:23 +0000

    Add package volprofile/0.1.4/volprofile-0.1.4.tar.gz

---
+++ packages/volprofile/volprofile-0.1.4.tar.gz/volprofile-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['volprofile']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.23.4,<2.0.0', 'pandas>=1.5.1,<2.0.0', 'plotly>=5.11.0,<6.0.0']
+
+setup_kwargs = {
+    'name': 'volprofile',
+    'version': '0.1.4',
+    'description': 'calculate the volume profile in a flexible manner!',
+    'long_description': '`pip install volprofile`\n',
+    'author': 'maghrebi',
+    'author_email': 'sajad.faghfoor@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
