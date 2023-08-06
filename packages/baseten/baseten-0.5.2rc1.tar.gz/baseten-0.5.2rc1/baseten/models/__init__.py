5272785dfdfd5d1294aee53cc447e8a214994b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:52 +0000

    Add package baseten/0.5.2rc1/baseten-0.5.2rc1.tar.gz

---
+++ packages/baseten/baseten-0.5.2rc1.tar.gz/baseten-0.5.2rc1/baseten/models/__init__.py
@@ -0,0 +1,5 @@
+from baseten.models import flan_t5, stable_diffusion, whisper
+
+StableDiffusionPipeline = stable_diffusion.StableDiffusionPipeline
+FlanT5 = flan_t5.FlanT5
+Whisper = whisper.Whisper
