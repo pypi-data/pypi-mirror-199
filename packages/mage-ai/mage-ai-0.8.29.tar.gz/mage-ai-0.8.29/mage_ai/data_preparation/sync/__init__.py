4f726313a1415ba0422ff98c57ae8787713b6e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:09 +0000

    Add package mage-ai/0.8.29/mage-ai-0.8.29.tar.gz

---
+++ packages/mage-ai/mage-ai-0.8.29.tar.gz/mage-ai-0.8.29/mage_ai/data_preparation/sync/__init__.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from mage_ai.shared.config import BaseConfig
+import os
+
+
+@dataclass
+class GitConfig(BaseConfig):
+    remote_repo_link: str
+    repo_path: str = os.getcwd()
+    branch: str = 'main'
+    sync_on_pipeline_run: bool = False
+    # This is not necessary anymore, but leaving it for backwards compatibility
+    type: str = 'git'
