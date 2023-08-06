4506d38df6aae346b95e148167ca250b3c717caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:45:41 +0000

    Add package coherent/0.0.1/coherent-0.0.1.tar.gz

---
+++ packages/coherent/coherent-0.0.1.tar.gz/coherent-0.0.1/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name="coherent", version="0.0.1", py_modules=["coherent"], author="me", author_email="me@me.com", description="This is a placeholder package created by registerit.", url="http://www.github.com")
