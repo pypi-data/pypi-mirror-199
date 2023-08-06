70996f736bba9a2ee50e2e522f79600c9dcb9374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:44 +0000

    Add package pyrfc/2.7.2/pyrfc-2.7.2.tar.gz

---
+++ packages/pyrfc/pyrfc-2.7.2.tar.gz/pyrfc-2.7.2/ci/utils/build.sh
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2013 SAP SE Srdjan Boskovic <srdjan.boskovic@sap.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+python_versions="3.7.16 3.8.16 3.9.16 3.10.9 3.11.2"
+
+rm -rf build
+
+for version in $( echo "$python_versions")
+do
+    rm -rf tests/__pycache__
+    rm -rf tests/stfc-mrfc/__pycache__
+    echo py$version
+    pyenv activate py$version
+    PYRFC_BUILD_CYTHON=yes python setup.py bdist_wheel
+    pip install --upgrade --force --find-links=dist pyrfc
+    [[ $1 != skip ]] && [[ $version == 3.11.2 ]] && pytest -vvx
+done
+[[ $1 == sdist ]] && python setup.py sdist
