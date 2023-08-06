66152afd2be6000e30ae4b6381f487262724b905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:41 +0000

    Add package syngen/0.0.80/syngen-0.0.80.tar.gz

---
+++ packages/syngen/syngen-0.0.80.tar.gz/syngen-0.0.80/src/syngen/ml/data_loaders/__init__.py
@@ -0,0 +1,6 @@
+from syngen.ml.data_loaders.data_loaders import CSVLoader
+from syngen.ml.data_loaders.data_loaders import AvroLoader
+from syngen.ml.data_loaders.data_loaders import DataLoader
+from syngen.ml.data_loaders.data_loaders import MetadataLoader
+from syngen.ml.data_loaders.data_loaders import YAMLLoader
+from syngen.ml.data_loaders.data_loaders import BaseDataLoader
