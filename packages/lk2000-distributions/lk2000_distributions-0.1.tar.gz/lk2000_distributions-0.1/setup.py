0567131fc181909ed287bc952d260e0ea9f4831b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:07 +0000

    Add package lk2000-distributions/0.1/lk2000_distributions-0.1.tar.gz

---
+++ packages/lk2000-distributions/lk2000_distributions-0.1.tar.gz/lk2000_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lk2000_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['lk2000_distributions'],
+      author= 'Thien Le',
+      author_email = 'thienlk2000@gmail.com',
+      zip_safe=False)
