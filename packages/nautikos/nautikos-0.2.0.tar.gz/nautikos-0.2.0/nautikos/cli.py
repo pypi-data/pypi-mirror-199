b21bcd5c4dcdd4fdc75ac3328736c060f894ebb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:28 +0000

    Add package nautikos/0.2.0/nautikos-0.2.0.tar.gz

---
+++ packages/nautikos/nautikos-0.2.0.tar.gz/nautikos-0.2.0/nautikos/cli.py
@@ -0,0 +1,23 @@
+import typer
+
+from .nautikos import Nautikos
+
+app = typer.Typer()
+
+nautikos = Nautikos()
+
+
+@app.command()
+def main(
+    repository: str,
+    tag: str,
+    env: str = typer.Option(None),
+    labels: str = typer.Option(None),
+    config: str = typer.Option("nautikos.yaml"),
+    dry_run: bool = typer.Option(False, "--dry-run"),
+):
+    nautikos.set_dry_run(dry_run)
+    nautikos.load_config(config)
+    nautikos.update_manifests(
+        repository, tag, environment=env, labels=labels.split(",")
+    )
