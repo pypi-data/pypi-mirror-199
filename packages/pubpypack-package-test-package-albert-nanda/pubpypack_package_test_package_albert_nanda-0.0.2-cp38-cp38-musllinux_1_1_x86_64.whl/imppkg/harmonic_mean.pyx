6c2d3f508be4a8859d2e31fc292a2a40f28f4050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:34 +0000

    Add package pubpypack-package-test-package-albert-nanda/0.0.2/pubpypack_package_test_package_albert_nanda-0.0.2-cp38-cp38-musllinux_1_1_x86_64.whl

---
+++ packages/pubpypack-package-test-package-albert-nanda/pubpypack_package_test_package_albert_nanda-0.0.2-cp38-cp38-musllinux_1_1_x86_64.whl/imppkg/harmonic_mean.pyx
@@ -0,0 +1,9 @@
+def harmonic_mean(n):
+    result = 0
+    for i in range(n):
+        result += 1/n
+    harmonic_mean = n/result
+    return harmonic_mean
+
+def arithmetic_mean(x):
+    return 10
