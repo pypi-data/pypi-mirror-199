25481fadba6081d6e03c5d8460137299e11cb2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:47 +0000

    Add package binsync/3.4.1/binsync-3.4.1-py3-none-manylinux2014_x86_64.whl

---
+++ packages/binsync/binsync-3.4.1-py3-none-manylinux2014_x86_64.whl/binsync/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "3.4.1"
+
+#
+# logging
+#
+
+import logging
+logging.getLogger("binsync").addHandler(logging.NullHandler())
+from binsync.loggercfg import Loggers
+loggers = Loggers()
+del Loggers
+del logging
+
+import binsync.data
