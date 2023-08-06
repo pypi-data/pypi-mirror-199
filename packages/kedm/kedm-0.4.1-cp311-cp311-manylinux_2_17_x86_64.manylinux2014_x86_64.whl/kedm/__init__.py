110b48726d695302f366eff988cb1e2855222d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:36 +0000

    Add package kedm/0.4.1/kedm-0.4.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/kedm/kedm-0.4.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/kedm/__init__.py
@@ -0,0 +1,22 @@
+"""
+Python bindings for kEDM
+------------------------
+
+.. currentmodule:: kedm
+
+.. autosummary::
+   :toctree: _generate
+
+   edim
+   simplex
+   eval_simplex
+   smap
+   eval_smap
+   xmap
+   get_kokkos_config
+"""
+
+from ._kedm import *
+
+from . import _version
+__version__ = _version.get_versions()['version']
