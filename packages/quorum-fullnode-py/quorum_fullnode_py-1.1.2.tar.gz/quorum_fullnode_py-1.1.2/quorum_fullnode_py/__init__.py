1e42add616a64dddd2f08b9cbcfb844820824ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:13 +0000

    Add package quorum-fullnode-py/1.1.2/quorum_fullnode_py-1.1.2.tar.gz

---
+++ packages/quorum-fullnode-py/quorum_fullnode_py-1.1.2.tar.gz/quorum_fullnode_py-1.1.2/quorum_fullnode_py/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+from quorum_fullnode_py.client import FullNode, HttpRequest
+
+__version__ = "1.1.2"
+__author__ = "liujuanjuan1984"
+
+# Set default logging handler to avoid "No handler found" warnings.
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
