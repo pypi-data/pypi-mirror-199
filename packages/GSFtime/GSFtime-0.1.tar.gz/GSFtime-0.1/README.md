d278eba9cd6a26e45de38dde9ce9011685f9bd38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:28 +0000

    Add package GSFtime/0.1/GSFtime-0.1.tar.gz

---
+++ packages/GSFtime/GSFtime-0.1.tar.gz/GSFtime-0.1/README.md
@@ -0,0 +1,7 @@
+# GSFTime
+
+GSFTime - Get Seconds From Time
+
+This library help you get seconds from entered time
+Example:
+    print(getSeconds(12, 0))
---
+++ packages/GSFtime/GSFtime-0.1.tar.gz/GSFtime-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GSFtime',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='Get Seconds From Time',
+    long_description=" print(getSeconds(12, 0))",
+    install_requires=[
+        # список зависимостей, необходимых для вашей библиотеки
+    ],
+    url='https://github.com/Anready/',
+    author='Anready',
+    author_email='anready939@gmailu.com'
+)
