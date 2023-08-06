6098fdf1f0e7319f5439eeab2355cd9dc80331d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:42 +0000

    Add package ErCore/0.7/ErCore-0.7.tar.gz

---
+++ packages/ErCore/ErCore-0.7.tar.gz/ErCore-0.7/README.md
@@ -0,0 +1,6 @@
+# ErCore for my package(or your)
+
+### author: Eragod
+### version: 0.5
+
+#### [Github](https://github.com/Eragod/ErCore)
---
+++ packages/ErCore/ErCore-0.7.tar.gz/ErCore-0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
+with open('ErCore/VERSION', 'r') as version_file:
+    version = version_file.read().strip()
+
+setup(
+    name='ErCore',
+    version=version,
+    packages=find_packages(),
+    author='Eragod',
+    description='This is a core for my package',
+    url="https://github.com/Eragod/ErCore",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={'console_scripts': ['ErCore = ErCore.cli:make_project']},
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent'],
+)
