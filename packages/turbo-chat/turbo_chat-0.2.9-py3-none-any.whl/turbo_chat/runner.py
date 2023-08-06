bbc523b210757f42de0c17d311215eb051ed1ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:20 +0000

    Add package turbo-chat/0.2.9/turbo_chat-0.2.9-py3-none-any.whl

---
+++ packages/turbo-chat/turbo_chat-0.2.9-py3-none-any.whl/turbo_chat/runner.py
@@ -0,0 +1,44 @@
+from typing import cast, Union
+
+from .structs import Result, Start
+
+from .errors import GeneratorAlreadyExhausted, GeneratorAlreadyExhaustedError
+from .types import TurboGenWrapper
+
+__all__ = [
+    "run",
+]
+
+
+async def run(
+    gen: TurboGenWrapper,
+    input: Union[str, dict],
+) -> Result:
+    """Run a turbo app"""
+
+    # Set placeholder values
+    done = False
+    output = GeneratorAlreadyExhausted()
+
+    # Run generator
+    try:
+        while output := await gen.asend(input):
+            if isinstance(output, Start):
+                continue
+
+            if output.needs_input:
+                break
+
+    # Generator exhausted, mark done
+    except StopAsyncIteration:
+        done = True
+
+    # Output is still placeholder? Raise error
+    if isinstance(output, GeneratorAlreadyExhausted):
+        raise GeneratorAlreadyExhaustedError()
+
+    # Set result values
+    result = cast(Result, output)
+    result.done = done
+
+    return result
