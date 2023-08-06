cf5690c420533c54b9eb395f2391fc20ff4fb37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:28 +0000

    Add package misp-galaxy-parser/0.1.5/misp_galaxy_parser-0.1.5-py3-none-any.whl

---
+++ packages/misp-galaxy-parser/misp_galaxy_parser-0.1.5-py3-none-any.whl/galaxy_parser/__init__.py
@@ -0,0 +1,48 @@
+# Copyright 2022 VMware, Inc.
+# SPDX-License-Identifier: BSD-2
+from galaxy_parser import discerner
+from galaxy_parser import exceptions
+from galaxy_parser import galaxy
+from typing import Any
+from typing import Dict
+from typing import Iterable
+from typing import List
+from typing import Optional
+
+
+def get_discernments(
+    discerners: Iterable[discerner.BaseDiscernerSubType],
+    string: str,
+    include_partial_matches: bool = True,
+    hint: Optional[str] = None,
+) -> List[discerner.Discernment]:
+    discernments = []
+    for d in discerners:
+        try:
+            discernment = d.discern(string, include_partial_matches, hint)
+            discernments.append(discernment)
+        except exceptions.FailedDiscernment:
+            continue
+    return discernments
+
+
+def get_discerned_tags(
+    discerners: Iterable[discerner.BaseDiscernerSubType],
+    string: str,
+    include_partial_matches: bool = True,
+    hint: Optional[str] = None,
+    resolve_local: bool = True,
+) -> List[str]:
+    """Get a list of tags from the loaded discerners."""
+    if not string:
+        return []
+    discernments = get_discernments(discerners, string, include_partial_matches, hint)
+    return [x.get_tag(resolve_local) for x in discernments]
+
+
+def get_mitre_technique_mapping(mitre_attack_galaxy_cluster: Dict[str, Any]) -> Dict[str, str]:
+    """Get an updated mapping between technique IDs and technique names."""
+    return {
+        x["value"].split("-")[1].strip(): x["tag_name"]
+        for x in mitre_attack_galaxy_cluster["values"]
+    }
---
+++ packages/misp-galaxy-parser/misp_galaxy_parser-0.1.5-py3-none-any.whl/misp_galaxy_parser-0.1.5.data/scripts/query_galaxy.py
@@ -0,0 +1,88 @@
+#!python
+# Copyright 2022 VMware, Inc.
+# SPDX-License-Identifier: BSD-2
+import argparse
+import sys
+import tempfile
+
+from galaxy_parser import exceptions
+from galaxy_parser import galaxy
+
+
+def main():
+    """Script to query a MISP galaxy and resolve synonyms."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "query",
+        help="the value to query",
+    )
+    parser.add_argument(
+        "-g",
+        "--galaxy-list",
+        dest="galaxy_list",
+        nargs="+",
+        default=["mitre-intrusion-set", "mitre-malware", "mitre-tool"],
+        help="list of galaxy clusters to query",
+    )
+    parser.add_argument(
+        "-f",
+        "--force-download",
+        dest="force_download",
+        default=False,
+        action="store_true",
+        help="whether to force download",
+    )
+    parser.add_argument(
+        "-m",
+        "--include_partial_matches",
+        dest="include_partial_matches",
+        default=False,
+        action="store_true",
+        help="whether the query should allow partial matches",
+    )
+    parser.add_argument(
+        "-i",
+        "--hint",
+        dest="hint",
+        default=None,
+        help="specify a hint",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        default=False,
+        action="store_true",
+        help="whether to be verbose",
+    )
+    args = parser.parse_args()
+
+    # Create galaxy manager
+    galaxy_manager = galaxy.GalaxyManagerOnDemand(
+        cache_directory=tempfile.gettempdir(),
+        galaxy_names=args.galaxy_list,
+        verbose=True,
+        force=args.force_download,
+    )
+
+    # Process
+    labels = []
+    for d in galaxy_manager.create_discerners():
+        try:
+            discernment = d.discern(
+                args.query,
+                include_partial_matches=args.include_partial_matches,
+                hint=args.hint,
+            )
+            labels.append(discernment.get_tag())
+        except exceptions.FailedDiscernment:
+            pass
+
+    # Print output
+    print(f"Mapping '{args.query}' to: ", labels)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
