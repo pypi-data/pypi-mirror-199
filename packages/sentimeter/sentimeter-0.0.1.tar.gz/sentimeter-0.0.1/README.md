94c0147d91b707126f357d156e4883cb17e757cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:24 +0000

    Add package sentimeter/0.0.1/sentimeter-0.0.1.tar.gz

---
+++ packages/sentimeter/sentimeter-0.0.1.tar.gz/sentimeter-0.0.1/README.md
@@ -0,0 +1,41 @@
+# Sentimeter
+![Alt text](logo.png?raw=true  "Logo")
+ 
+Sentimeter is an app written in python to understand the emotions in audio / text. Currently it has the below modules
+
+### Audio Module
+Process the Emotions in a given audio file. Currently it supports only wav file . In future we will add more formats
+### Live Speech
+Process the Emotions in a live speech
+### Telegram Mode
+Sentimeter has a Telegram Bot, it can process the emotions in a Telegram chat
+### Text File
+Process a text file and identifies the emotions
+### Youtube Video
+Under Development
+
+## Usage
+    python main.py --live to use live speech as source
+    python main.py --audio_file <path to the audio file> to use as source
+
+## Todo
+
+ - Documentation
+ - Google Hangout Connector 
+ - Convert the Observer Pattern through Decorator
+
+## Setup
+Create a Virtual env
+
+    py -m venv virtual-env
+    .\virtual-env\Scripts\activate
+
+Install All Dependencies
+
+    pip3 install -r requirements.txt
+
+#### Developer Notes
+generate docs
+pyreverse -o png -p sentimeter .
+Look at the unwanted imports
+pyflakes core
---
+++ packages/sentimeter/sentimeter-0.0.1.tar.gz/sentimeter-0.0.1/src/sentimeter/speech_to_text.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Speech to Text Module, Currently uses Google STT, Future we will add more offline Engines
+"""
+import logging
+import speech_recognition as sr
+
+
+class STTEngine:
+    """
+    Speech to Text Engine, Currently using Google Service, Options to be added for Sphinix
+    """
+
+    def __init__(self, recognizer) -> None:
+        """
+        constructor
+        """
+        self.recognizer = recognizer
+        self.lang = "en-IN"
+        pass
+
+    def speech_to_text(self, audiodata):
+        """
+        function to get the text for inputed audio data
+        """
+        result=""
+        try:
+            actual_result = self.recognizer.recognize_google(audiodata, language=self.lang, show_all=True)
+        except sr.UnknownValueError:
+            logging.fatal("Google Speech Recognition could not understand audio")
+            exit(-1)
+        except sr.RequestError as e:
+            logging.fatal("Error service; {0}".format(e))
+            exit(-1)
+        if len(actual_result) == 0:
+            logging.debug("Unable to get the Speech to Text,Ignoring")
+            return ""
+
+        # Lets get the best text
+        if "confidence" in actual_result["alternative"]:
+            # return alternative with highest confidence score
+            best_hypothesis = max(
+                actual_result["alternative"],
+                key=lambda alternative: alternative["confidence"],
+            )
+        else:
+            # when there is no confidence available, we arbitrarily choose the first hypothesis.
+            best_hypothesis = actual_result["alternative"][0]
+        result = best_hypothesis["transcript"]
+        return result
