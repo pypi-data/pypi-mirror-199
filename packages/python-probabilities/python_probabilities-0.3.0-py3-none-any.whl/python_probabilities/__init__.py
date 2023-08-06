c43826fcc2ab57d921f3678ba71aaedaff296e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:47 +0000

    Add package python-probabilities/0.3.0/python_probabilities-0.3.0-py3-none-any.whl

---
+++ packages/python-probabilities/python_probabilities-0.3.0-py3-none-any.whl/python_probabilities/__init__.py
@@ -0,0 +1,11 @@
+from .distributions_binomial import BinomialPD as Bpd
+from .distributions_binomial import BinomialCD as Bcd
+from .distributions_binomial import InvBinomialCD as InvB
+
+from .distributions_poisson import PoissonPD as Ppd
+from .distributions_poisson import PoissonCD as Pcd
+from .distributions_poisson import InvPoissonCD as InvP
+
+from .distributions_normal import NormalPD as Npd
+from .distributions_normal import NormalCD as Ncd
+from .distributions_normal import InvNormalCD as InvN
