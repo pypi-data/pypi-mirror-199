d48bdaa02c9451690ffb4c93308abf3d2aa78a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:58 +0000

    Add package ir-digit/0.0.1/ir_digit-0.0.1-py3-none-any.whl

---
+++ packages/ir-digit/ir_digit-0.0.1-py3-none-any.whl/src/digit/data/download.py
@@ -0,0 +1,28 @@
+import os
+
+import git
+
+
+def download_repo_git(url, path):
+    print('Download start...')
+    if not os.path.exists(path):  # 不存在该路径，创建该路径
+        os.mkdir(path)
+        # 下载git仓库
+        git.Repo.clone_from(url, path)
+    else:
+        # 存在该路径则清空该路径下的所有文件
+        for file in os.listdir(path):
+            if not file:  # 无旧数据
+                # 下载git仓库
+                git.Repo.clone_from(url, path)
+            else:
+                # 如果有旧数据，则拉取git仓库最新更新
+                git.Repo(path).remotes.origin.pull()
+
+    print('Successfully download')
+    print(os.listdir(path))
+
+
+def download_repo(url, path='./download_repo/', url_type='git'):
+    if url_type == 'git':
+        download_repo_git(url, path)
