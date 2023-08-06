193b16742638f3a51d10a460512c357ed455e7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:00 +0000

    Add package ipfabric-diagrams/6.2.0b1/ipfabric_diagrams-6.2.0b1-py3-none-any.whl

---
+++ packages/ipfabric-diagrams/ipfabric_diagrams-6.2.0b1-py3-none-any.whl/ipfabric_diagrams/__init__.py
@@ -0,0 +1,37 @@
+"""
+For backwards compatability until ipfabric_diagrams is merged into ipfabric in v7.0.0
+"""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+import logging
+
+from ipfabric.diagrams import *
+
+logger = logging.getLogger("ipfabric_diagrams")
+
+logger.warning("ipfabric_diagrams will be merged into ipfabric in v7.0.0 with backwards compatability until then.\n"
+               "Please update imports from `ipfabric_diagrams` to `ipfabric.diagrams`")
+
+__all__ = [
+    "IPFDiagram",
+    "Unicast",
+    "Multicast",
+    "Host2GW",
+    "Network",
+    "OtherOptions",
+    "Algorithm",
+    "Overlay",
+    "icmp",
+    "NetworkSettings",
+    "PathLookupSettings",
+    "EntryPoint",
+    "VALID_NET_PROTOCOLS",
+    "VALID_PATH_PROTOCOLS",
+    "Layout",
+]
+
+__version__ = importlib_metadata.version(__name__)
