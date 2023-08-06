6e3f22cb59b6e1fc17c864623353e224a834f6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:37 +0000

    Add package web-aaron-alphabet/0.0.6/web_aaron_alphabet-0.0.6-py3-none-any.whl

---
+++ packages/web-aaron-alphabet/web_aaron_alphabet-0.0.6-py3-none-any.whl/web_aaron_alphabet/converting/pdfkit.py
@@ -0,0 +1,4 @@
+import pdfkit
+
+def main(url, pdf):
+	pdfkit.from_url(url, pdf) 
---
+++ packages/web-aaron-alphabet/web_aaron_alphabet-0.0.6-py3-none-any.whl/web_aaron_alphabet/merging/pypdf.py
@@ -0,0 +1,8 @@
+from pypdf import PdfMerger
+
+def main(infiles, outfile):
+	merger = PdfMerger()
+	for pdf in infiles:
+	    merger.append(pdf)
+	merger.write(outfile)
+	merger.close() 
