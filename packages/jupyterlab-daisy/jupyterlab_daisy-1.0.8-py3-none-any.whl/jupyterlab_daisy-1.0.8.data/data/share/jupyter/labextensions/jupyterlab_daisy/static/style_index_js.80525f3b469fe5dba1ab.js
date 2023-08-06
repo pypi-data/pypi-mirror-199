d4608e579dd30f3f772fe5fb59081c2c6e3ea32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:34 +0000

    Add package jupyterlab-daisy/1.0.8/jupyterlab_daisy-1.0.8-py3-none-any.whl

---
+++ packages/jupyterlab-daisy/jupyterlab_daisy-1.0.8-py3-none-any.whl/jupyterlab_daisy-1.0.8.data/data/share/jupyter/labextensions/jupyterlab_daisy/static/style_index_js.80525f3b469fe5dba1ab.js
@@ -0,0 +1,73 @@
+"use strict";
+(self["webpackChunkjupyterlab_daisy"] = self["webpackChunkjupyterlab_daisy"] || []).push([["style_index_js"],{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!**************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \**************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "./node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "/*\n    See the JupyterLab Developer Guide for useful CSS Patterns:\n\n    https://jupyterlab.readthedocs.io/en/stable/developer/css.html\n*/\n\n.my-panel {\n    background: var(--jp-layout-color1);\n    padding: 8px;\n    height: 100%;  \n    display: flex;\n    flex-direction: column;\n} \n\n\n.my-list {\n    list-style: none;\n    padding-left: 0px;\n    background: var(--jp-layout-color2);\n    overflow-y: scroll;\n    flex-grow: 1;\n}\n\n.my-column-table-div {\n\n    transition: height 0.25s, opacity 0.5s;\n\n    flex-basis: 100%;\n    pointer-events: visible;\n    opacity: 1;\n    width: 100%;\n    background: var(--jp-layout-color2);\n}\n\n.my-column-table-div-collapsed {\n\n    transition: height 0.25s, opacity 0.1s;\n    pointer-events: none;\n\n    opacity: 0;\n    width: 100%;\n}\n\n\n.my-highlighted-item {\n    font-family: var(--jp-code-font-family);\n    padding: 12px;\n    border-top: 1px solid var(--jp-shadow-ambient-color);\n    background: var(--jp-layout-color2);\n    width: 100%;\n    box-sizing: border-box;\n} \n\n.my-button {\n    width: 16px;\n    height: 16px;\n    padding: 0px;\n    line-height: 16px;\n    margin: 4px;\n    background: var(--jp-layout-color2);\n    border-style: solid;\n    border-radius: 0px;\n    border-color:  var(--jp-shadow-ambient-color);\n    outline:none;  \n    font-family: var(--jp-code-font-family);\n    min-width: 16px;\n    min-height: 16px;\n}\n\n.my-query-button {\n    padding: 4px;\n    line-height: 16px;\n    margin: 8px;\n    background: var(--jp-layout-color2);\n    border-style: solid;\n    border-radius: 0px;\n    border-color:  var(--jp-shadow-ambient-color);\n    outline:none;  \n    font-family: var(--jp-code-font-family);\n}\n\n.alnright {\n    text-align: right;\n}\n\n.my-button-toggled {\n    width: 16px;\n    height: 16px;\n    padding: 0px;\n    line-height: 16px;\n    margin: 4px;\n    background: var(--jp-shadow-ambient-color);\n    border-style: solid;\n    border-radius: 0px;\n    border-color:  var(--jp-shadow-ambient-color);  \n    outline:none;\n    font-family: var(--jp-code-font-family);\n    min-width: 16px;\n    min-height: 16px;\n}\n\n\n.my-list-item {\n    font-family: var(--jp-code-font-family);\n    padding: 12px;\n    border-top: 1px solid var(--jp-shadow-ambient-color);\n    text-overflow: ellipsis;\n\n} \n\n.my-input-span {\n    display: flex;\n    align-items: center;\n}\n\n.my-list-item-div {\n    display: flex;\n    align-items: center;\n}\n\n.my-list-item:hover {\n    background: var(--jp-editor-selected-background);\n} \n\n.my-list-item-text {\n    overflow: hidden;\n    text-overflow: ellipsis;\n    white-space: nowrap;\n} \n\n.my-list-item-text:hover {\n    color: var(--jp-layout-color1);\n} \n\n\n/* Solid border */\nhr.solid {\n  border-top: 3px solid #bbb;\n}\n", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAAA;;;;CAIC;;AAED;IACI,mCAAmC;IACnC,YAAY;IACZ,YAAY;IACZ,aAAa;IACb,sBAAsB;AAC1B;;;AAGA;IACI,gBAAgB;IAChB,iBAAiB;IACjB,mCAAmC;IACnC,kBAAkB;IAClB,YAAY;AAChB;;AAEA;;IAEI,sCAAsC;;IAEtC,gBAAgB;IAChB,uBAAuB;IACvB,UAAU;IACV,WAAW;IACX,mCAAmC;AACvC;;AAEA;;IAEI,sCAAsC;IACtC,oBAAoB;;IAEpB,UAAU;IACV,WAAW;AACf;;;AAGA;IACI,uCAAuC;IACvC,aAAa;IACb,oDAAoD;IACpD,mCAAmC;IACnC,WAAW;IACX,sBAAsB;AAC1B;;AAEA;IACI,WAAW;IACX,YAAY;IACZ,YAAY;IACZ,iBAAiB;IACjB,WAAW;IACX,mCAAmC;IACnC,mBAAmB;IACnB,kBAAkB;IAClB,6CAA6C;IAC7C,YAAY;IACZ,uCAAuC;IACvC,eAAe;IACf,gBAAgB;AACpB;;AAEA;IACI,YAAY;IACZ,iBAAiB;IACjB,WAAW;IACX,mCAAmC;IACnC,mBAAmB;IACnB,kBAAkB;IAClB,6CAA6C;IAC7C,YAAY;IACZ,uCAAuC;AAC3C;;AAEA;IACI,iBAAiB;AACrB;;AAEA;IACI,WAAW;IACX,YAAY;IACZ,YAAY;IACZ,iBAAiB;IACjB,WAAW;IACX,0CAA0C;IAC1C,mBAAmB;IACnB,kBAAkB;IAClB,6CAA6C;IAC7C,YAAY;IACZ,uCAAuC;IACvC,eAAe;IACf,gBAAgB;AACpB;;;AAGA;IACI,uCAAuC;IACvC,aAAa;IACb,oDAAoD;IACpD,uBAAuB;;AAE3B;;AAEA;IACI,aAAa;IACb,mBAAmB;AACvB;;AAEA;IACI,aAAa;IACb,mBAAmB;AACvB;;AAEA;IACI,gDAAgD;AACpD;;AAEA;IACI,gBAAgB;IAChB,uBAAuB;IACvB,mBAAmB;AACvB;;AAEA;IACI,8BAA8B;AAClC;;;AAGA,iBAAiB;AACjB;EACE,0BAA0B;AAC5B","sourcesContent":["/*\n    See the JupyterLab Developer Guide for useful CSS Patterns:\n\n    https://jupyterlab.readthedocs.io/en/stable/developer/css.html\n*/\n\n.my-panel {\n    background: var(--jp-layout-color1);\n    padding: 8px;\n    height: 100%;  \n    display: flex;\n    flex-direction: column;\n} \n\n\n.my-list {\n    list-style: none;\n    padding-left: 0px;\n    background: var(--jp-layout-color2);\n    overflow-y: scroll;\n    flex-grow: 1;\n}\n\n.my-column-table-div {\n\n    transition: height 0.25s, opacity 0.5s;\n\n    flex-basis: 100%;\n    pointer-events: visible;\n    opacity: 1;\n    width: 100%;\n    background: var(--jp-layout-color2);\n}\n\n.my-column-table-div-collapsed {\n\n    transition: height 0.25s, opacity 0.1s;\n    pointer-events: none;\n\n    opacity: 0;\n    width: 100%;\n}\n\n\n.my-highlighted-item {\n    font-family: var(--jp-code-font-family);\n    padding: 12px;\n    border-top: 1px solid var(--jp-shadow-ambient-color);\n    background: var(--jp-layout-color2);\n    width: 100%;\n    box-sizing: border-box;\n} \n\n.my-button {\n    width: 16px;\n    height: 16px;\n    padding: 0px;\n    line-height: 16px;\n    margin: 4px;\n    background: var(--jp-layout-color2);\n    border-style: solid;\n    border-radius: 0px;\n    border-color:  var(--jp-shadow-ambient-color);\n    outline:none;  \n    font-family: var(--jp-code-font-family);\n    min-width: 16px;\n    min-height: 16px;\n}\n\n.my-query-button {\n    padding: 4px;\n    line-height: 16px;\n    margin: 8px;\n    background: var(--jp-layout-color2);\n    border-style: solid;\n    border-radius: 0px;\n    border-color:  var(--jp-shadow-ambient-color);\n    outline:none;  \n    font-family: var(--jp-code-font-family);\n}\n\n.alnright {\n    text-align: right;\n}\n\n.my-button-toggled {\n    width: 16px;\n    height: 16px;\n    padding: 0px;\n    line-height: 16px;\n    margin: 4px;\n    background: var(--jp-shadow-ambient-color);\n    border-style: solid;\n    border-radius: 0px;\n    border-color:  var(--jp-shadow-ambient-color);  \n    outline:none;\n    font-family: var(--jp-code-font-family);\n    min-width: 16px;\n    min-height: 16px;\n}\n\n\n.my-list-item {\n    font-family: var(--jp-code-font-family);\n    padding: 12px;\n    border-top: 1px solid var(--jp-shadow-ambient-color);\n    text-overflow: ellipsis;\n\n} \n\n.my-input-span {\n    display: flex;\n    align-items: center;\n}\n\n.my-list-item-div {\n    display: flex;\n    align-items: center;\n}\n\n.my-list-item:hover {\n    background: var(--jp-editor-selected-background);\n} \n\n.my-list-item-text {\n    overflow: hidden;\n    text-overflow: ellipsis;\n    white-space: nowrap;\n} \n\n.my-list-item-text:hover {\n    color: var(--jp-layout-color1);\n} \n\n\n/* Solid border */\nhr.solid {\n  border-top: 3px solid #bbb;\n}\n"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _base_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.css */ "./style/base.css");
+
+
+
+/***/ }),
+
+/***/ "./style/base.css":
+/*!************************!*\
+  !*** ./style/base.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./base.css */ "./node_modules/css-loader/dist/cjs.js!./style/base.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"], options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"].locals || {});
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.80525f3b469fe5dba1ab.js.map
