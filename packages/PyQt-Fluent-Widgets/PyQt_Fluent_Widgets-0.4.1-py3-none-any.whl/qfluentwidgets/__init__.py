b633944f7b3c153bdbd443baf994727efa2f6067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:44 +0000

    Add package PyQt-Fluent-Widgets/0.4.1/PyQt_Fluent_Widgets-0.4.1-py3-none-any.whl

---
+++ packages/PyQt-Fluent-Widgets/PyQt_Fluent_Widgets-0.4.1-py3-none-any.whl/qfluentwidgets/__init__.py
@@ -0,0 +1,19 @@
+"""
+PyQt-Fluent-Widgets
+===================
+A fluent design widgets library based on PyQt5.
+
+Documentation is available in the docstrings and
+online at https://pyqt-fluent-widgets.readthedocs.io.
+
+Examples are available at https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/master/examples.
+
+:copyright: (c) 2023 by zhiyiYo.
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "0.4.1"
+
+from .components import *
+from .common import *
+from ._rc import resource
