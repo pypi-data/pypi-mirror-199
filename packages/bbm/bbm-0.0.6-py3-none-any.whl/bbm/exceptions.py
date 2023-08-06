8213989c37d8d117634a1c79ddc27b98411d2421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:37 +0000

    Add package bbm/0.0.6/bbm-0.0.6-py3-none-any.whl

---
+++ packages/bbm/bbm-0.0.6-py3-none-any.whl/bbm/exceptions.py
@@ -0,0 +1,14 @@
+class BBMNotInitialized(Exception):
+    pass
+
+
+class NoChannelException(Exception):
+    pass
+
+
+class NoJoinChannelException(Exception):
+    pass
+
+
+class ReporterNotInitialized(Exception):
+    pass
