9937c111fefbb93068232b7cfed4ca8669d90c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:07 +0000

    Add package diator/0.0.4/diator-0.0.4-py3-none-any.whl

---
+++ packages/diator/diator-0.0.4-py3-none-any.whl/diator/events/message_brokers/redis.py
@@ -0,0 +1,33 @@
+import logging
+
+import orjson
+
+try:
+    from redis.asyncio import Redis  # type: ignore
+except ImportError:
+
+    class Redis:  # type: ignore
+        def __init__(self) -> None:
+            raise ImportError("Redis is required to use this module")
+
+
+from diator.events.message_brokers.protocol import Message
+
+logger = logging.getLogger(__name__)
+
+
+class RedisMessageBroker:
+    def __init__(self, client: Redis, *, channel_prefix: str | None = None) -> None:
+        self._client = client
+        self._channel_prefix = channel_prefix or "python_diator_channel"
+
+    async def send_message(self, message: Message) -> None:
+        async with self._client.pubsub() as pubsub:
+            channel = (
+                f"{self._channel_prefix}:{message.message_type}:{message.message_id}"
+            )
+
+            await pubsub.subscribe(channel)
+
+            logger.info("Sending message to Redis Pub/Sub %s.", message.message_id)
+            await self._client.publish(channel, orjson.dumps(message))
---
+++ packages/diator/diator-0.0.4-py3-none-any.whl/diator/middlewares/__init__.py
@@ -0,0 +1,4 @@
+from diator.middlewares.base import Middleware, MiddlewareChain
+
+
+__all__ = ("Middleware", "MiddlewareChain")
