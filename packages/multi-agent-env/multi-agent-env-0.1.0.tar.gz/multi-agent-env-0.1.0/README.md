aa51cf3f292c28119884de4ab690c1d25f48ec08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:25 +0000

    Add package multi-agent-env/0.1.0/multi-agent-env-0.1.0.tar.gz

---
+++ packages/multi-agent-env/multi-agent-env-0.1.0.tar.gz/multi-agent-env-0.1.0/README.md
@@ -0,0 +1,2 @@
+# Multi-agent-Env
+Abstract baseclasses for multi-agent enviroments
---
+++ packages/multi-agent-env/multi-agent-env-0.1.0.tar.gz/multi-agent-env-0.1.0/setup.py
@@ -0,0 +1,24 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='multi-agent-env',
+    packages=find_packages(include=['multi-agent-env']),
+    version='0.1.0',
+    description='Multi-agent environment for reinforcement learning',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Ted',
+    license='GPL-3.0',
+    install_requires=["numpy", "pygame"],
+)
