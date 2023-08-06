6a17c99d1a88cec3dbc1b521201a92e74c3e3b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:02 +0000

    Add package OctoPrint-Nanny/0.16.0/OctoPrint_Nanny-0.16.0-py3-none-any.whl

---
+++ packages/OctoPrint-Nanny/OctoPrint_Nanny-0.16.0-py3-none-any.whl/octoprint_nanny/clients/rest.py
@@ -0,0 +1,173 @@
+import aiohttp
+import logging
+import urllib.parse
+import backoff
+import os
+from typing import Optional
+
+import printnanny_api_client
+from printnanny_api_client import ApiClient as AsyncApiClient
+from printnanny_api_client.models import PatchedOctoPrintServerRequest
+from printnanny_api_client.api.accounts_api import AccountsApi
+from printnanny_api_client.api.octoprint_api import OctoprintApi
+
+from octoprint_nanny.env import MAX_BACKOFF_TIME
+
+logger = logging.getLogger("octoprint.plugins.octoprint_nanny.clients.rest")
+
+API_CLIENT_EXCEPTIONS = (
+    printnanny_api_client.exceptions.ApiException,
+    aiohttp.client_exceptions.ClientError,
+)
+
+
+def fatal_code(e) -> bool:
+    """
+    Returns True if error code is fatal and should not be retried
+    """
+    if isinstance(e, aiohttp.ClientConnectionError):
+        return False
+    return 400 <= e.response.status_code < 500
+
+
+def backoff_hdlr(details):
+    logger.warning(
+        "Backing off {wait:0.1f} seconds afters {tries} tries "
+        "calling function {target} with args {args} and kwargs "
+        "{kwargs}".format(**details)
+    )
+
+
+def giveup_hdlr(details):
+    logger.error(
+        "Giving up {elapsed:0.1f} seconds afters {tries} tries "
+        "calling function {target} with args {args} and kwargs "
+        "{kwargs}".format(**details)
+    )
+
+
+class PrintNannyCloudAPIClient:
+    """
+    webapp rest API calls and retry behavior
+    """
+
+    def __init__(self, base_path: str, bearer_access_token: Optional[str] = None):
+        self.base_path = base_path
+        self.bearer_access_token = bearer_access_token
+
+    @property
+    def _api_config(self):
+        parsed_uri = urllib.parse.urlparse(self.base_path)
+        host = f"{parsed_uri.scheme}://{parsed_uri.netloc}"
+        config = printnanny_api_client.Configuration(host=host)
+
+        config.access_token = self.bearer_access_token
+        return config
+
+    @backoff.on_exception(
+        backoff.expo,
+        aiohttp.ClientConnectionError,
+        logger=logger,
+        max_time=MAX_BACKOFF_TIME,
+        jitter=backoff.random_jitter,
+        giveup=fatal_code,
+        on_backoff=backoff_hdlr,
+        on_giveup=giveup_hdlr,
+    )
+    async def get_user(self):
+        async with AsyncApiClient(self._api_config) as api_client:
+            api_instance = AccountsApi(api_client=api_client)
+            user = await api_instance.accounts_user_retrieve()
+            return user
+
+    # @backoff.on_exception(
+    #     backoff.expo,
+    #     aiohttp.ClientConnectionError,
+    #     logger=logger,
+    #     max_time=MAX_BACKOFF_TIME,
+    #     jitter=backoff.random_jitter,
+    #     giveup=fatal_code,
+    #     on_backoff=backoff_hdlr,
+    #     on_giveup=giveup_hdlr,
+    # )
+    async def update_octoprint_server_api_key(self, octoprint_server_id, api_key):
+        async with AsyncApiClient(self._api_config) as api_client:
+            api_instance = OctoprintApi(api_client=api_client)
+            request = PatchedOctoPrintServerRequest(api_key=api_key)
+            result = await api_instance.octoprint_partial_update(
+                octoprint_server_id,
+                patched_octo_print_server_request=request,
+            )
+            return result
+
+    # @backoff.on_exception(
+    #     backoff.expo,
+    #     aiohttp.ClientConnectionError,
+    #     logger=logger,
+    #     max_time=MAX_BACKOFF_TIME,
+    #     jitter=backoff.random_jitter,
+    #     giveup=fatal_code,
+    #     on_backoff=backoff_hdlr,
+    #     on_giveup=giveup_hdlr,
+    # )
+    # async def update_or_create_printer_profile(
+    #     self, printer_profile, octoprint_device_id
+    # ):
+    #     """
+    #     https://github.com/OctoPrint/OctoPrint/blob/f67c15a9a47794a68be9aed4f2d5a12a87e70179/src/octoprint/printer/profile.py#L46
+    #     """
+
+    #     async with AsyncApiClient(self._api_config) as api_client:
+    #         # printer profile
+    #         # TODO re-enable with OctoPrintApi module
+    #         # api_instance = RemoteControlApi(api_client=api_client)
+
+    #         # cooerce duck-typed fields
+    #         if type(printer_profile["volume"]["custom_box"]) is bool:
+    #             volume_custom_box = {}
+    #         else:
+    #             volume_custom_box = printer_profile["volume"]["custom_box"]
+
+    #         request = PrinterProfileRequest(
+    #             octoprint_device=octoprint_device_id,
+    #             octoprint_key=printer_profile["id"],
+    #             axes_e_inverted=printer_profile["axes"]["e"]["inverted"],
+    #             axes_x_inverted=printer_profile["axes"]["x"]["inverted"],
+    #             axes_y_inverted=printer_profile["axes"]["y"]["inverted"],
+    #             axes_z_inverted=printer_profile["axes"]["z"]["inverted"],
+    #             axes_e_speed=printer_profile["axes"]["e"]["speed"],
+    #             axes_x_speed=printer_profile["axes"]["x"]["speed"],
+    #             axes_y_speed=printer_profile["axes"]["y"]["speed"],
+    #             axes_z_speed=printer_profile["axes"]["z"]["speed"],
+    #             extruder_count=printer_profile["extruder"]["count"],
+    #             extruder_nozzle_diameter=printer_profile["extruder"]["nozzleDiameter"],
+    #             extruder_shared_nozzle=printer_profile["extruder"]["sharedNozzle"],
+    #             name=printer_profile["name"],
+    #             model=printer_profile["model"],
+    #             heated_bed=printer_profile["heatedBed"],
+    #             heated_chamber=printer_profile["heatedChamber"],
+    #             volume_custom_box=volume_custom_box,
+    #             volume_depth=printer_profile["volume"]["depth"],
+    #             volume_formfactor=printer_profile["volume"]["formFactor"],
+    #             volume_height=printer_profile["volume"]["height"],
+    #             volume_origin=printer_profile["volume"]["origin"],
+    #             volume_width=printer_profile["volume"]["width"],
+    #         )
+    #         printer_profile = await api_instance.printer_profiles_update_or_create(
+    #             request
+    #         )
+    #         return printer_profile
+
+    async def create_backup(
+        self, hostname: str, name: str, octoprint_version: str, file: str
+    ):
+        async with AsyncApiClient(self._api_config) as api_client:
+            api_instance = printnanny_api_client.OctoprintApi(api_client=api_client)
+
+            backup = await api_instance.octoprint_backups_create(
+                hostname,
+                name,
+                octoprint_version,
+                file,
+            )
+            return backup
---
+++ packages/OctoPrint-Nanny/OctoPrint_Nanny-0.16.0-py3-none-any.whl/octoprint_nanny/utils/printnanny_os.py
@@ -0,0 +1,215 @@
+import os
+from typing import Optional, Any, Dict, List, TypedDict
+import logging
+import json
+import subprocess
+
+import printnanny_api_client
+from printnanny_api_client.models import Pi
+
+logger = logging.getLogger("octoprint.plugins.octoprint_nanny.utils")
+
+PRINTNANNY_BIN = os.environ.get("PRINTNANNY_BIN", "/usr/bin/printnanny")
+PRINTNANNY_DEBUG = os.environ.get("PRINTNANNY_DEBUG", False)
+PRINTNANNY_DEBUG = PRINTNANNY_DEBUG in ["True", "true", "1", "yes"]
+
+
+PRINTNANNY_CLOUD_PI: Optional[Pi] = None
+PRINTNANNY_CLOUD_NATS_CREDS: Optional[str] = None
+
+
+class PrintNannyApiConfig(TypedDict):
+    base_path: str
+    bearer_access_token: Optional[str]
+
+
+PRINTNANNY_CLOUD_API: Optional[PrintNannyApiConfig] = None
+
+
+class PrintNannyConfig(TypedDict):
+    cmd: List[str]
+    stdout: str
+    stderr: str
+    returncode: Optional[int]
+
+    config: Optional[Dict[str, Any]]
+
+
+async def deserialize_pi(pi_dict) -> Pi:
+    async with printnanny_api_client.api_client.ApiClient() as client:
+        return client._ApiClient__deserialize(pi_dict, Pi)  # type: ignore
+
+
+async def load_pi_model(pi_dict: Dict[str, Any]) -> Pi:
+    result = await deserialize_pi(pi_dict)
+    global PRINTNANNY_CLOUD_PI
+    PRINTNANNY_CLOUD_PI = result
+    return PRINTNANNY_CLOUD_PI
+
+
+def load_api_config(api_config_dict: Dict[str, str]) -> PrintNannyApiConfig:
+    global PRINTNANNY_CLOUD_API
+
+    PRINTNANNY_CLOUD_API = PrintNannyApiConfig(
+        base_path=api_config_dict.get("api_base_path", "https://printnanny.ai/"),
+        bearer_access_token=api_config_dict.get("api_bearer_access_token"),
+    )
+    return PRINTNANNY_CLOUD_API
+
+
+def sync_printnanny_cloud_data():
+    logger.info("Attempting to sync PrintNanny Cloud data...")
+    cmd = [PRINTNANNY_BIN, "cloud", "sync-models"]
+    try:
+        p = subprocess.run(cmd, capture_output=True)
+        stdout = p.stdout.decode("utf-8")
+        stderr = p.stderr.decode("utf-8")
+        if p.returncode != 0:
+            logger.error(
+                f"Failed to get printnanny settings cmd={cmd} returncode={p.returncode} stdout={stdout} stderr={stderr}"
+            )
+            return
+    except Exception as e:
+        logger.error("Error running cmd %s %s", cmd, e)
+
+
+async def load_printnanny_cloud_data():
+    cmd = [PRINTNANNY_BIN, "cloud", "show"]
+    # run /usr/bin/printnanny cloud show --format json
+    try:
+        p = subprocess.run(cmd, capture_output=True)
+        stdout = p.stdout.decode("utf-8")
+        stderr = p.stderr.decode("utf-8")
+        if p.returncode != 0:
+            logger.error(
+                f"Failed to get printnanny settings cmd={cmd} returncode={p.returncode} stdout={stdout} stderr={stderr}"
+            )
+            return
+
+        cloud_data = json.loads(stdout)
+        # try setting global PRINTNANNY_CLOUD_PI var
+        result = await load_pi_model(cloud_data)
+        logger.debug("Loaded PrintNanny Cloud pi data %s", result)
+        return result
+    except Exception as e:
+        logger.error("Error running cmd %s %s", cmd, e)
+
+
+def load_printnanny_settings() -> PrintNannyConfig:
+    cmd = [PRINTNANNY_BIN, "settings", "show", "--format", "json"]
+    returncode = None
+    config = None
+
+    # run /usr/bin/printnanny settings show -F json
+    try:
+        p = subprocess.run(cmd, capture_output=True)
+        stdout = p.stdout.decode("utf-8")
+        stderr = p.stderr.decode("utf-8")
+        returncode = p.returncode
+        if p.returncode != 0:
+            logger.error(
+                f"Failed to get printnanny settings cmd={cmd} returncode={p.returncode} stdout={stdout} stderr={stderr}"
+            )
+            return PrintNannyConfig(
+                cmd=cmd,
+                stdout=stdout,
+                stderr=stderr,
+                returncode=returncode,
+                config=config,
+            )
+    # FileNotFoundError thrown when PRINTNANNY_BIN is not found
+    except FileNotFoundError as e:
+        logger.warning("%s is not installed", PRINTNANNY_BIN)
+        return PrintNannyConfig(
+            cmd=cmd,
+            stdout="",
+            stderr="",
+            returncode=1,
+            config=config,
+        )
+    try:
+        # parse JSON
+        config = json.loads(stdout)
+        logger.debug("Parsed PrintNanny conf.d, loaded keys: %s", config.keys())
+
+        api_config = config.get("cloud")
+
+        # try setting PRINTNANNY_CLOUD_API var
+        if api_config is not None:
+            load_api_config(api_config)
+
+        nats_creds = config.get("paths", {}).get("state_dir")
+        if nats_creds is not None:
+            nats_creds = os.path.join(
+                config.get("paths", {}).get("state_dir"),
+                "creds/printnanny-cloud-nats.creds",
+            )
+            global PRINTNANNY_CLOUD_NATS_CREDS
+            PRINTNANNY_CLOUD_NATS_CREDS = nats_creds
+
+    except json.JSONDecodeError as e:
+        logger.warning(f"Failed to decode printnanny config: %", e)
+    return PrintNannyConfig(
+        cmd=cmd,
+        stdout=stdout,
+        stderr=stderr,
+        config=config,
+        returncode=returncode,
+    )
+
+
+def issue_txt() -> str:
+    """
+    Captured the contents of /etc/issue as plain text
+    """
+    try:
+        result = open("/etc/issue", "r").read().strip()
+    except Exception as e:
+        logger.error("Failed to read /etc/issue %s", e)
+        result = "Failed to read /etc/issue"
+    return result
+
+
+def etc_os_release() -> Dict[str, str]:
+    """
+    Captures the contents of /etc/os-release as a dictionary
+    """
+    config = load_printnanny_settings()
+    os_release_path = "/etc/os-release"
+    if config["config"] is not None:
+        os_release_path = (
+            config["config"].get("paths", {}).get("os_release", os_release_path)
+        )
+
+    f = open(os_release_path, "r").read()
+    result = dict(ID="unknown")
+    try:
+        lines = f.strip().split("\n")
+        for line in lines:
+            k, v = line.split("=")
+            result[k] = v
+    except Exception as e:
+        logger.error("Error parsing contents of %s %s", os_release_path, e)
+    return result
+
+
+def is_printnanny_os() -> bool:
+    osrelease = etc_os_release()
+    return osrelease.get("ID") == "printnanny" or PRINTNANNY_DEBUG is True
+
+
+def set_octoprint_api_key(api_key: str):
+    cmd = [PRINTNANNY_BIN, "cloud", "set", "pi.octoprint_server.api_key", api_key]
+    # run /usr/bin/printnanny settings show -F json
+    try:
+        p = subprocess.run(cmd, capture_output=True)
+        stdout = p.stdout.decode("utf-8")
+        stderr = p.stderr.decode("utf-8")
+        returncode = p.returncode
+        if p.returncode != 0:
+            logger.error(
+                f"Failed to run cmd={cmd} returncode={p.returncode} stdout={stdout} stderr={stderr}"
+            )
+
+    except Exception as e:
+        logger.error(f"Failed to run cmd={cmd} with error={e}")
