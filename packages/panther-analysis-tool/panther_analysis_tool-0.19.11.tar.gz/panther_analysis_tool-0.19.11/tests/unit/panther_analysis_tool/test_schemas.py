174e1819348f9edc4e56ff8c2a7d297666e41077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:18 +0000

    Add package panther-analysis-tool/0.19.11/panther_analysis_tool-0.19.11.tar.gz

---
+++ packages/panther-analysis-tool/panther_analysis_tool-0.19.11.tar.gz/panther_analysis_tool-0.19.11/tests/unit/panther_analysis_tool/test_schemas.py
@@ -0,0 +1,21 @@
+import unittest
+
+from schema import SchemaError
+
+from panther_analysis_tool.schemas import LOG_TYPE_REGEX, MOCK_SCHEMA
+
+
+class TestPATSchemas(unittest.TestCase):
+    def test_logtypes_regex_amazon_eks(self):
+        LOG_TYPE_REGEX.validate("Amazon.EKS.Audit")
+        LOG_TYPE_REGEX.validate("Amazon.EKS.Authenticator")
+
+        with self.assertRaises(SchemaError):
+            LOG_TYPE_REGEX.validate("Amazon.EKS.Foo")
+
+    def test_mocks_are_str(self):
+        MOCK_SCHEMA.validate({"objectName": "hello", "returnValue": "Testing a string"})
+        MOCK_SCHEMA.validate({"objectName": "hello", "returnValue": "False"})
+
+        with self.assertRaises(SchemaError):
+            LOG_TYPE_REGEX.validate({"objectName": "hello", "returnValue": ["Testing a non-string"]})
