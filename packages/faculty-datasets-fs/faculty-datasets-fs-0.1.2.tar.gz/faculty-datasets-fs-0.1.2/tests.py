e2ba9561582e22dd90f354fc6dac25baa11e0733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:44 +0000

    Add package faculty-datasets-fs/0.1.2/faculty-datasets-fs-0.1.2.tar.gz

---
+++ packages/faculty-datasets-fs/faculty-datasets-fs-0.1.2.tar.gz/faculty-datasets-fs-0.1.2/tests.py
@@ -0,0 +1,43 @@
+import os
+
+import fsspec
+import pytest
+
+FOLDER_CONTENTS = {"/folder"} | {f"/folder/data{i}.txt" for i in range(5)}
+
+
+@pytest.mark.parametrize(
+    "prefix",
+    ["", "/", "faculty-datasets://", "faculty-datasets:///"],
+)
+@pytest.mark.parametrize(
+    "path, expected_result",
+    [
+        ("", {"/", "/folder", "/data.txt", "/upload.txt"}),
+        ("data.txt", {"/data.txt"}),
+        ("folder", FOLDER_CONTENTS),
+        ("folder/", FOLDER_CONTENTS),
+        ("folder/data0.txt", {"/folder/data0.txt"}),
+    ],
+)
+def test_list(prefix, path, expected_result):
+    fs = fsspec.filesystem("faculty-datasets")
+    assert set(fs.ls(prefix + path, detail=False)) == expected_result
+
+
+def test_read():
+    with fsspec.open("faculty-datasets://data.txt") as fp:
+        assert fp.read() == b"This is some test data"
+
+
+def test_read_tail():
+    with fsspec.open("faculty-datasets://data.txt") as fp:
+        fp.seek(-4, os.SEEK_END)
+        assert fp.read() == b"data"
+
+
+def test_write():
+    with fsspec.open("faculty-datasets://upload.txt", "wb") as fp:
+        fp.write(b"Test written data")
+    with fsspec.open("faculty-datasets://upload.txt") as fp:
+        assert fp.read() == b"Test written data"
