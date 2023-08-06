bcc72977c1cc8a75612f7fb897bed9943e2a87dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:35 +0000

    Add package axanexa-moncli/2.0.15/axanexa-moncli-2.0.15.tar.gz

---
+++ packages/axanexa-moncli/axanexa-moncli-2.0.15.tar.gz/axanexa-moncli-2.0.15/setup.py
@@ -0,0 +1,35 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+config = {
+    'description': 'Moncli, a pythonic/DDD client for Monday.com',
+    'author': 'Andrew Shatz, Trix Solutions',
+    'url': r'https://github.com/trix-solutions/moncli',
+    'download_url': r'https://github.com/trix-solutions/moncli',
+    'author_email': 'andrew.shatz@trix.solutions',
+    'version': '2.0.15',
+    'license': 'BSD 3',
+    'install_requires': [
+        'requests>=2.24.0',
+        'pytz>=2020.1',
+        'pycountry>=20.7.3',
+        'deprecated>=1.2.10',
+        'schematics>=2.1.0'
+    ],
+    'tests_require': [
+        'nose>=1.3.7'
+    ],
+    'packages': [
+        'moncli',
+        'moncli.api_v1',
+        'moncli.api_v2',
+        'moncli.entities',
+        'moncli.column_value'
+    ],
+    'scripts': [],
+    'name': 'axanexa-moncli'
+}
+
+setup(**config)
