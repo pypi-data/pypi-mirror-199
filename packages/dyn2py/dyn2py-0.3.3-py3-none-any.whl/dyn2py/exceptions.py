7ff21b6c2b336aae36b1facb6d6acdcc84bfbcec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:27 +0000

    Add package dyn2py/0.3.3/dyn2py-0.3.3-py3-none-any.whl

---
+++ packages/dyn2py/dyn2py-0.3.3-py3-none-any.whl/dyn2py/exceptions.py
@@ -0,0 +1,16 @@
+class DynamoFileException(Exception):
+    """Something wrong in this DynamoFile"""
+    pass
+
+
+class PythonNodeNotFoundException(Exception):
+    """PythonNode not found"""
+    pass
+
+
+class PythonNodeException(Exception):
+    pass
+
+
+class PythonFileException(Exception):
+    pass
