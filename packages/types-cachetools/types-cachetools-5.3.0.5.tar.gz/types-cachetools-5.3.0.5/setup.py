98009f5e7814b72876ff32d46764c53920e08427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:03 +0000

    Add package types-cachetools/5.3.0.5/types-cachetools-5.3.0.5.tar.gz

---
+++ packages/types-cachetools/types-cachetools-5.3.0.5.tar.gz/types-cachetools-5.3.0.5/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+name = "types-cachetools"
+description = "Typing stubs for cachetools"
+long_description = '''
+## Typing stubs for cachetools
+
+This is a PEP 561 type stub package for the `cachetools` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`cachetools`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/cachetools. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `6fd7e36e80e0448d9199d62d582c659c147be149`.
+'''.lstrip()
+
+setup(name=name,
+      version="5.3.0.5",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/cachetools.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['cachetools-stubs'],
+      package_data={'cachetools-stubs': ['__init__.pyi', 'func.pyi', 'keys.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
