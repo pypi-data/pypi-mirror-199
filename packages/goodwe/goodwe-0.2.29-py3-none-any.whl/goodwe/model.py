619640ef8fbdac357790595a033cb5901d40e9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:07 +0000

    Add package goodwe/0.2.29/goodwe-0.2.29-py3-none-any.whl

---
+++ packages/goodwe/goodwe-0.2.29-py3-none-any.whl/goodwe/model.py
@@ -0,0 +1,22 @@
+from .inverter import Inverter
+
+# Serial number tags to identify inverter type
+ET_MODEL_TAGS = ["ETU", "ETL", "ETR", "ETC", "EHU", "EHR", "EHB", "BTU", "BTN", "BTC", "BHU", "AES", "ABP", "HHI",
+                 "HSB", "HUA", "CUA"]
+ES_MODEL_TAGS = ["ESU", "EMU", "ESA", "BPS", "BPU", "EMJ", "IJL"]
+DT_MODEL_TAGS = ["DTU", "DTS", "MSU", "MST", "DSN", "DTN", "DST", "NSU", "SSN", "SST", "SSX", "SSY", "PSB", "PSC"]
+
+SINGLE_PHASE_MODELS = ["DSN", "DST", "NSU", "SSN", "SST", "SSX", "SSY",
+                       "MSU", "MST", "PSB", "PSC", "EHU", "EHR", "HSB"]
+
+
+def is_single_phase(inverter: Inverter) -> bool:
+    return any(model in inverter.serial_number for model in SINGLE_PHASE_MODELS)
+
+
+def is_3_mptt(inverter: Inverter) -> bool:
+    return any(model in inverter.serial_number for model in ["MSU", "MST", "PSC"])
+
+
+def is_4_mptt(inverter: Inverter) -> bool:
+    return any(model in inverter.serial_number for model in ["HSB"])
