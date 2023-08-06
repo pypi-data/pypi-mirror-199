b5765046fe3f727287d14df308617cf1f2f53a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:13 +0000

    Add package mfNRcatpy/0.0.12/mfNRcatpy-0.0.12.tar.gz

---
+++ packages/mfNRcatpy/mfNRcatpy-0.0.12.tar.gz/mfNRcatpy-0.0.12/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+ld = (this_directory / "README.md").read_text()
+print(ld)
+
+find_packages(where='mfNRcatpy')
+
+setup(
+    name='mfNRcatpy',
+    version='0.0.12',
+    long_description=ld,
+    long_description_content_type='text/markdown',
+    packages=find_packages() + find_packages(where='mfNRcatpy'),
+    install_requires=['numpy',
+                      'scipy',
+                      'core-watpy',
+                      'h5py',
+                      'matplotlib',
+                      'lalsuite',
+                      'numba'],
+    author = 'Sebastian Gomez Lopez',
+    author_email= 'segomezlo@unal.edu.co',
+    url = 'https://github.com/sebastiangomezlopez/mfNRcatpy'
+
+    )
