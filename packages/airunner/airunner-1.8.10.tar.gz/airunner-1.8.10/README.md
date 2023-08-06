94d127161abaccba72c5791611ca8008f171a74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:04 +0000

    Add package airunner/1.8.10/airunner-1.8.10.tar.gz

---
+++ packages/airunner/airunner-1.8.10.tar.gz/airunner-1.8.10/README.md
@@ -0,0 +1,13 @@
+[![Banner](banner.png)](https://capsizegames.itch.io/ai-runner)
+
+AI Runner official repository which houses the templates and primary client code for AI Runner.
+
+---
+
+## Installation
+
+`pip install aihandler`
+
+---
+
+## Development setup
---
+++ packages/airunner/airunner-1.8.10.tar.gz/airunner-1.8.10/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='airunner',
+    version='1.8.10',
+    author="Capsize LLC",
+    description="A Stable Diffusion GUI",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="",
+    keywords="ai, chatbot, chat, ai",
+    license="AGPL-3.0",
+    author_email="contact@capsize.gg",
+    url="https://github.com/w4ffl35/airunner",
+    package_dir={"": "sdrunner"},
+    packages=find_packages("sdrunner"),
+    python_requires=">=3.10.0",
+    install_requires=[
+        "aihandler==1.8.11",
+    ]
+)
