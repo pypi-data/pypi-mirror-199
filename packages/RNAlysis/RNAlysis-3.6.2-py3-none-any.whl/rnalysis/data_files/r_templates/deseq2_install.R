fe70f8fd7ec628ad5da99d62926d8df65e910ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:03 +0000

    Add package RNAlysis/3.6.2/RNAlysis-3.6.2-py3-none-any.whl

---
+++ packages/RNAlysis/RNAlysis-3.6.2-py3-none-any.whl/rnalysis/data_files/r_templates/deseq2_install.R
@@ -0,0 +1,26 @@
+if (("DESeq2" %in% rownames(installed.packages()) == FALSE) || (!require("DESeq2", quietly = TRUE))) {
+    install.packages("png")
+
+    tryCatch(
+    {install.packages("XML")
+    require("XML")},
+    warning = function(e) {
+    install.packages("XML", type = "binary")},
+    error = function(e) {
+    install.packages("XML", type = "binary")})
+
+    tryCatch(
+    {install.packages("vctrs")
+    require("vctrs")},
+    warning = function(e) {
+    install.packages("vctrs", type = "binary")},
+    error = function(e) {
+    install.packages("vctrs", type = "binary")})
+
+    if (!require("BiocManager", quietly = TRUE)) {
+        install.packages("BiocManager")
+        }
+    BiocManager::install("DelayedArray",update=TRUE, ask=FALSE, force=TRUE)
+    BiocManager::install("RSQLite",update=TRUE, ask=FALSE, force=TRUE)
+    BiocManager::install("DESeq2",update=TRUE, ask=FALSE, force=TRUE)
+}
