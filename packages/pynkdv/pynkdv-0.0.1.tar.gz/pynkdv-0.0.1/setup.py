1cdcc4ca0b8890a3765e010104d16b77dc52c2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:08 +0000

    Add package pynkdv/0.0.1/pynkdv-0.0.1.tar.gz

---
+++ packages/pynkdv/pynkdv-0.0.1.tar.gz/pynkdv-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='pynkdv',
+    version='0.0.1',
+    author='rui',
+    author_email='19251017@life.hkbu.edu.hk',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description=u'吃枣药丸',
+    packages=['pynkdv'],
+    install_requires=[],
+    entry_points={
+    }
+)
