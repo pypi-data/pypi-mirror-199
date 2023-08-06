e85c02282784ffe0ebd4e1a92d151b6227ba2db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:33 +0000

    Add package jax-moseq/0.0.1/jax-moseq-0.0.1.tar.gz

---
+++ packages/jax-moseq/jax-moseq-0.0.1.tar.gz/jax-moseq-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='jax-moseq',
+    version='0.0.1',
+    author='Caleb Weinreb',
+    author_email='calebsw@gmail.com',
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'numba',
+        'dynamax',
+        'chex==0.1.6', 
+        'tqdm',
+    ], 
+    url='https://github.com/dattalab/jax-moseq/tree/0.0.0',
+    download_url='https://github.com/dattalab/jax-moseq/archive/refs/tags/0.0.0.tar.gz'
+)
