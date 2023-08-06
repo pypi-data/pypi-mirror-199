f3ded6eb64866717f604d938a8f3ba2feaf0e575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:05 +0000

    Add package terra-ai-datasets/0.0.1/terra_ai_datasets-0.0.1.tar.gz

---
+++ packages/terra-ai-datasets/terra_ai_datasets-0.0.1.tar.gz/terra_ai_datasets-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='terra_ai_datasets',
+    version='0.0.1',
+    packages=['terra_ai_datasets'],
+    url='',
+    license='',
+    author='AI University',
+    author_email='nejronki@gmail.com'    
+)
