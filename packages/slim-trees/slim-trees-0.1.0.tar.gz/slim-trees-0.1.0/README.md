d244644253627fcdca457bb987cddd930b13f440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:02 +0000

    Add package slim-trees/0.1.0/slim-trees-0.1.0.tar.gz

---
+++ packages/slim-trees/slim-trees-0.1.0.tar.gz/slim-trees-0.1.0/README.md
@@ -0,0 +1,25 @@
+# Slim Trees
+
+[![CI](https://github.com/pavelzw/slim-trees/actions/workflows/ci.yml/badge.svg)](https://github.com/pavelzw/slim-trees/actions/workflows/ci.yml)
+[![conda-forge](https://img.shields.io/conda/vn/conda-forge/slim-trees?logoColor=white&logo=conda-forge)](https://anaconda.org/conda-forge/slim-trees)
+[![pypi-version](https://img.shields.io/pypi/v/slim-trees.svg?logo=pypi&logoColor=white)](https://pypi.org/project/slim-trees)
+[![python-version](https://img.shields.io/pypi/pyversions/slim-trees?logoColor=white&logo=python)](https://pypi.org/project/slim-trees)
+
+A python package for efficient pickling.
+
+## Installation
+
+You can install the package in development mode using:
+
+```bash
+git clone git@github.com:pavelzw/slim-trees.git
+cd slim-trees
+
+# create and activate a fresh environment named slim_trees
+# see environment.yml for details
+mamba env create
+conda activate slim_trees
+
+pre-commit install
+pip install --no-build-isolation -e .
+```
---
+++ packages/slim-trees/slim-trees-0.1.0.tar.gz/slim-trees-0.1.0/slim_trees/__init__.py
@@ -0,0 +1,50 @@
+"""
+This module changes the default pickle behavior. Instead of using pickle.dump to pickle an object,
+you can create a custom Pickler and change the default pickling behavior for certain classes by
+changing the dispatch_table of the pickler.
+TODO: Update this docstring
+In this module, a custom pickling behavior for the sklearn Tree class is specified that compresses
+the internal values from int64 to int16 and from float64 to float32 (in the value field) and from
+float64 to a mixture between float64 and int16 (in the threshold field).
+You can pickle a model with custom picklers by specifying dump_function in dump_compressed in the
+pickling module.
+"""
+
+from pathlib import Path
+from typing import Any, Union
+
+import pkg_resources
+
+from slim_trees.pickling import dump_compressed
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except Exception:
+    __version__ = "unknown"
+
+
+def dump_sklearn_compressed(
+    model: Any, path: Union[str, Path], compression: Union[str, dict] = "lzma"
+):
+    """
+    Pickles a model and saves a compressed version to the disk.
+
+    Saves the parameters of the model as int16 and float32 instead of int64 and float64.
+    :param model: the model to save
+    :param path: where to save the model
+    :param compression: the compression method used. Either a string or a dict with key 'method' set
+                        to the compression method and other key-value pairs are forwarded to `open`
+                        of the compression library.
+                        Inspired by the pandas.to_csv interface.
+    """
+    from slim_trees.sklearn_tree import dump_sklearn
+
+    dump_compressed(model, path, compression, dump_sklearn)
+
+
+def dump_lgbm_compressed(
+    model: Any, path: Union[str, Path], compression: Union[str, dict] = "lzma"
+):
+    from slim_trees.lgbm_booster import dump_lgbm
+
+    dump_compressed(model, path, compression, dump_lgbm)
