7284f0784b9d0fb4c9744219f05bcbc28857b461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:42 +0000

    Add package shutil-extra/0.0.4/shutil_extra-0.0.4.tar.gz

---
+++ packages/shutil-extra/shutil_extra-0.0.4.tar.gz/shutil_extra-0.0.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "shutil_extra",
+    version = "0.0.4",
+    keywords = ("pip", "shutil extra", "dir tree"),
+    description = "A successful sign for python setup",
+    long_description_content_type = 'text/markdown',
+    long_description = open('README.md', encoding='UTF8').read(),
+    license = "MIT Licence",
+    url = "https://github.com/novawei/shutil_extra",
+    author = "novawei",
+    author_email = "913252732@qq.com",
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = [],
+)
