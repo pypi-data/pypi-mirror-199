5a8b23b7531827858570a87ffcf460faea714aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:05 +0000

    Add package je-load-density/0.0.35/je_load_density-0.0.35-py3-none-any.whl

---
+++ packages/je-load-density/je_load_density-0.0.35-py3-none-any.whl/test/unit_test/user_test/fast_http_user_test.py
@@ -0,0 +1,12 @@
+from je_load_density import start_test
+
+start_test(
+    {
+        "user": "fast_http_user",
+    },
+    50, 10, 5,
+    tasks={
+        "get": {"request_url": "http://httpbin.org/get"},
+        "post": {"request_url": "http://httpbin.org/post"}
+    }
+)
