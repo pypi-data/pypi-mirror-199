e39e1996f10b75d6d69183610d090343bad6595d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:11 +0000

    Add package rythmnet/0.0.1/rythmnet-0.0.1.tar.gz

---
+++ packages/rythmnet/rythmnet-0.0.1.tar.gz/rythmnet-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rythmnet', # Replace with your package name
+    version='0.0.1', # Replace with your package version
+    description='Rythmnet_implementation',
+    author='ErnisMeshi',
+    author_email='ernis.meshi@student.uni-pr.edu',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies that your package requires here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+    ],
+)
