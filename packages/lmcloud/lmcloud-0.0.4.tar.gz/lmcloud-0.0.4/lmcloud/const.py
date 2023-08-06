2323de231bf7811b1fbf9171d76ea1c602f27826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:45 +0000

    Add package lmcloud/0.0.4/lmcloud-0.0.4.tar.gz

---
+++ packages/lmcloud/lmcloud-0.0.4.tar.gz/lmcloud-0.0.4/lmcloud/const.py
@@ -0,0 +1,25 @@
+# URLs
+TOKEN_URL = "https://cms.lamarzocco.io/oauth/v2/token"
+CUSTOMER_URL = "https://cms.lamarzocco.io/api/customer"
+GW_MACHINE_BASE_URL = "https://gw.lamarzocco.io/v1/home/machines"
+
+POLLING_DELAY_S = 10
+
+MODEL_LMU = "Linea Micra"
+
+# Key Names for Dictionaries
+KEY = "key"
+SERIAL_NUMBER = "serial_number"
+MACHINE_NAME = "machine_name"
+MODEL_NAME = "model_name"
+
+# Dict keys are camel case for local API, all upper for remote API
+MACHINE_MODE="machineMode"
+STEAM_BOILER_NAME = "SteamBoiler"
+COFFEE_BOILER_NAME = "CoffeeBoiler1"
+BOILER_TARGET_TEMP = "boilerTargetTemperature"
+BOILERS = "boilers"
+PLUMBED_IN = "isPlumbedIn"
+PRE_INFUSION_SETTINGS = "preinfusionSettings"
+WEEKLY_SCHEDULING_CONFIG = "weeklySchedulingConfig"
+
