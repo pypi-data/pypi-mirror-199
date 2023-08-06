5a2a52095a8eb896bd98f12bb10738f452ab00aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:50 +0000

    Add package bot-engine-client/0.1.1/bot-engine-client-0.1.1.tar.gz

---
+++ packages/bot-engine-client/bot-engine-client-0.1.1.tar.gz/bot-engine-client-0.1.1/bot_engine_client/models.py
@@ -0,0 +1,92 @@
+from typing import Dict
+from bot_engine_client.enums import BotType, TradeType, TradingPlatform
+from datetime import datetime as DateTime
+
+
+class Bot:
+    def __init__(
+        self, id: str, description: str, type: BotType, platform: TradingPlatform
+    ) -> None:
+        self.__id = id
+        self.__description = description
+        self.__type = type
+        self.__platform = platform
+
+    @property
+    def id(self) -> str:
+        return self.__id
+
+    @property
+    def description(self) -> str:
+        return self.__description
+
+    @property
+    def type(self) -> BotType:
+        return self.__type
+
+    @property
+    def platform(self) -> TradingPlatform:
+        return self.__platform
+
+    @classmethod
+    def from_json(cls, json: Dict) -> "Bot":
+        return cls(
+            json.get("id", ""),
+            json.get("description", ""),
+            json.get("type", ""),
+            json.get("platform", ""),
+        )
+
+
+class Execution:
+    def __init__(
+        self,
+        datetime: DateTime,
+        trade_type: TradeType,
+        symbol: str,
+        quantity: float,
+        price: float,
+        bot_id: str,
+    ) -> None:
+        self.__datetime = datetime
+        self.__trade_type = trade_type
+        self.__symbol = symbol
+        self.__quantity = quantity
+        self.__price = price
+        self.__bot_id = bot_id
+
+    @property
+    def datetime(self) -> DateTime:
+        return self.__datetime
+
+    @property
+    def trade_type(self) -> TradeType:
+        return self.__trade_type
+
+    @property
+    def symbol(self) -> str:
+        return self.__symbol
+
+    @property
+    def quantity(self) -> float:
+        return self.__quantity
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @property
+    def bot_id(self) -> str:
+        return self.__bot_id
+
+    @classmethod
+    def from_json(cls, json: Dict) -> "Execution":
+        """"""
+        return cls(
+            DateTime.fromtimestamp(json.get("timestamp", 0)),
+            TradeType(json.get("tradeType", "")),
+            json.get("symbol", ""),
+            json.get("quantity", 0.0),
+            json.get("price", 0.0),
+            json.get("botId", ""),
+        )
