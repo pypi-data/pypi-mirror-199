663fec60bc2cd818b407a17ac997fcdcd640934d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:11 +0000

    Add package Profile-Py/0.0.0/Profile-Py-0.0.0.tar.gz

---
+++ packages/Profile-Py/Profile-Py-0.0.0.tar.gz/Profile-Py-0.0.0/README.md
@@ -0,0 +1,13 @@
+# Profile-Py
+A Python package that fetches key information about users from various social platforms.
+This provides both ways to utilize the official APIs of each platform, as well as unofficial approaches.
+
+Platforms to be supported
+- [ ] YouTube
+- [ ] Twitch
+- [ ] Twitter
+- [ ] Instagram
+- [ ] TikTok
+- [ ] Facebook
+- [ ] AfreecaTV
+- [ ] Mastodon
---
+++ packages/Profile-Py/Profile-Py-0.0.0.tar.gz/Profile-Py-0.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Profile-Py',
+    version='0.0.0',
+    description='A Python package that fetches key information about users from various social platforms.',
+    author='hellosunghyun',
+    author_email='hello@sunghyun.me',
+    url='https://github.com/hellosunghyun/Profile-Py',
+    install_requires=[],
+    packages=find_packages(exclude=[]),
+    keywords=[],
+    python_requires='>=3.9',
+    package_data={},
+    zip_safe=False,
+    classifiers=[],
+)
