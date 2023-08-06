10e50aad6ad22f2a03f601f686820b5fe313604e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:28 +0000

    Add package lnschema_wetlab/0.15rc1/lnschema_wetlab-0.15rc1-py2.py3-none-any.whl

---
+++ packages/lnschema_wetlab/lnschema_wetlab-0.15rc1-py2.py3-none-any.whl/lnschema_wetlab/link.py
@@ -0,0 +1,19 @@
+"""Link tables.
+
+.. autosummary::
+   :toctree: .
+
+   BiosampleTechsample
+   ProjectExperiment
+   FileBiosample
+   FileExperiment
+   FileReadout
+
+"""
+from ._link import (  # noqa
+    BiosampleTechsample,
+    FileBiosample,
+    FileExperiment,
+    FileReadout,
+    ProjectExperiment,
+)
