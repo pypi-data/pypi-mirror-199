7e0a4a12709d59b6ee766d4b8f27b0ad0b90d47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:28 +0000

    Add package pystarpix/1.0.0/pystarpix-1.0.0.tar.gz

---
+++ packages/pystarpix/pystarpix-1.0.0.tar.gz/pystarpix-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name ='pystarpix',
+    version = '1.0.0',
+    description = 'only star pix',
+    author = 'crandi',
+    author_email = None,
+    url = None,
+    install_requires = [],
+    packages=find_packages(),
+)
