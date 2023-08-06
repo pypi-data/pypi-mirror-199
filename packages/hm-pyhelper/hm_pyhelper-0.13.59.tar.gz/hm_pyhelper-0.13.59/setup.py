7aeb1c92c818ff8c479f4002ce4787ebdb8ac05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:29 +0000

    Add package hm-pyhelper/0.13.59/hm_pyhelper-0.13.59.tar.gz

---
+++ packages/hm-pyhelper/hm_pyhelper-0.13.59.tar.gz/hm_pyhelper-0.13.59/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='hm_pyhelper',
+    version='0.13.59',
+    author="Nebra Ltd",
+    author_email="support@nebra.com",
+    description="Helium Python Helper",
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/NebraLtd/hm-pyhelper",
+    install_requires=[
+        'requests>=2.28.1',
+        'retry==0.9.2',
+        'base58==2.1.1',
+        'protobuf==4.21.12',
+        'packaging>=22.0'
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/NebraLtd/hm-pyhelper/issues",
+    },
+    packages=find_packages(),
+    include_package_data=True
+)
