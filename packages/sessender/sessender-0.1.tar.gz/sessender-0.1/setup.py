45513d8df0424b3baba870d2b171531257ba597b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:17 +0000

    Add package sessender/0.1/sessender-0.1.tar.gz

---
+++ packages/sessender/sessender-0.1.tar.gz/sessender-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='sessender',
+    version='0.1',
+    author='Amit Dalal',
+    description='A package for sending emails using Amazon SES',
+    py_modules=['sessender'],
+)
