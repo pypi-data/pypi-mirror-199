91812e0fef777dd0a1d1f58c5af3654a9ce64a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:56 +0000

    Add package mandown/1.3.3/mandown-1.3.3.tar.gz

---
+++ packages/mandown/mandown-1.3.3.tar.gz/mandown-1.3.3/mandown/sources/__init__.py
@@ -0,0 +1,46 @@
+# pylint: disable=invalid-name
+import sys
+import types
+
+from . import (
+    source_mangadex,
+    source_mangakakalot,
+    source_manganato,
+    source_mangasee,
+    source_readcomiconline,
+    source_webtoons,
+)
+from .base_source import BaseSource
+
+__class_list: list[type[BaseSource]] = []
+
+
+def _get_all_source_modules() -> list[str]:
+    out = []
+    for _, val in globals().items():
+        if isinstance(val, types.ModuleType) and val.__name__.startswith(
+            "mandown.sources.source_"
+        ):
+            out.append(val.__name__)
+    return out
+
+
+for i in _get_all_source_modules():
+    __class_list.append(sys.modules[i].get_class())
+
+
+def get_class_for(url: str) -> type[BaseSource]:
+    """
+    Return a source that matches the URL.
+    """
+    for c in __class_list:
+        if c.check_url(url):
+            return c
+    raise ValueError("No sources found matched the URL query.")
+
+
+def get_all_classes() -> list[type[BaseSource]]:
+    """
+    Return all imported source module classes. Best used for manual poking.
+    """
+    return __class_list
