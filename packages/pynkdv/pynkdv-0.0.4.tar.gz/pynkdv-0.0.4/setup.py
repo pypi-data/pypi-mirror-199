85b13a34d2440cfdedcc388bf7fbd60fb053b283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:04 +0000

    Add package pynkdv/0.0.4/pynkdv-0.0.4.tar.gz

---
+++ packages/pynkdv/pynkdv-0.0.4.tar.gz/pynkdv-0.0.4/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='pynkdv',
+    version='0.0.4',
+    author='rui',
+    author_email='19251017@life.hkbu.edu.hk',
+    url='https://github.com/ZangRui-666/pynkdv',
+    description='for nkdv in python',
+    packages=['pynkdv'],
+    install_requires=['nkdv', 'osmnx', 'pandas', 'shapely'],
+    entry_points={
+    }
+)
