8ebdf8e0f6dea0f650a34b907df0a75607150bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:29 +0000

    Add package crunch-cli/0.5.0/crunch_cli-0.5.0-py3-none-any.whl

---
+++ packages/crunch-cli/crunch_cli-0.5.0-py3-none-any.whl/crunch_cli/constants.py
@@ -0,0 +1,11 @@
+DOT_CRUNCHDAO_DIRECTORY = ".crunchdao"
+DOT_DATA_DIRECTORY = ".data"
+TOKEN_FILE = "token"
+PROJECT_FILE = "project"
+DOT_GITIGNORE_FILE = ".gitignore"
+
+IGNORED_FILES = [
+    ".git/",
+    f"{DOT_CRUNCHDAO_DIRECTORY}/",
+    f"{DOT_DATA_DIRECTORY}/",
+]
