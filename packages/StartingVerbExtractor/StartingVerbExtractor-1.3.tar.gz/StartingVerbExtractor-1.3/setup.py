1e2684292d31109d20b03fe7ff089a3048bd83e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:44 +0000

    Add package StartingVerbExtractor/1.3/StartingVerbExtractor-1.3.tar.gz

---
+++ packages/StartingVerbExtractor/StartingVerbExtractor-1.3.tar.gz/StartingVerbExtractor-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='StartingVerbExtractor', # e o nome da pasta do pacote
+      version='1.3',
+      description='StartingVerbExtractor package',
+      packages=['StartingVerbExtractor'], #mesmo nome utilizado em name
+      author='Felipe Rainel',
+      author_email= 'raineldias88@gmail.com',
+      zip_safe=False) #não pode ser exeutado diretamente de um arquivo 
