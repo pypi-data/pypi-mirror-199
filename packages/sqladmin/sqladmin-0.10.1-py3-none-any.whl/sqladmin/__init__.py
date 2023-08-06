f00c025d1ac9b6a3d166501074341fc7672a7fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:38 +0000

    Add package sqladmin/0.10.1/sqladmin-0.10.1-py3-none-any.whl

---
+++ packages/sqladmin/sqladmin-0.10.1-py3-none-any.whl/sqladmin/__init__.py
@@ -0,0 +1,12 @@
+from sqladmin.application import Admin, expose
+from sqladmin.models import BaseView, ModelAdmin, ModelView
+
+__version__ = "0.10.1"
+
+__all__ = [
+    "Admin",
+    "expose",
+    "BaseView",
+    "ModelAdmin",
+    "ModelView",
+]
