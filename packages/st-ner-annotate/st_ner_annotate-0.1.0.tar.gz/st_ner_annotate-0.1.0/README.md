a607d4ed84382171dca9de088e61c0f39334f0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:46 +0000

    Add package st-ner-annotate/0.1.0/st_ner_annotate-0.1.0.tar.gz

---
+++ packages/st-ner-annotate/st_ner_annotate-0.1.0.tar.gz/st_ner_annotate-0.1.0/README.md
@@ -0,0 +1,9 @@
+# Streamlit Named Entity Recognition (NER) Annotation component
+
+This is a component to assist with the annotation of *named entities* in an unstructured piece of text. The concept for this is borrowed from what is offered by [Prodi.gy](https://prodi.gy/). This code is derived from the work done by [andfalino](https://github.com/andfanilo) in his excellent [streamlit-named-entity-svelte](https://github.com/andfanilo/streamlit-named-entity-svelte) repository.
+
+The functionality has been expanded to allow for selecting of new entities and uses the [streamlit-component-svelte-template](https://github.com/93degree/streamlit-component-svelte-template).
+
+I am also using this component as a testbed to get familiar with streamlit custom components and svelte.
+
+![Streamlit NER Annotate Demo GIF](https://github.com/Hironsan/st_ner_annotate/blob/master/docs/st_ner_annotate_demo.gif)
---
+++ packages/st-ner-annotate/st_ner_annotate-0.1.0.tar.gz/st_ner_annotate-0.1.0/st_ner_annotate/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+import streamlit.components.v1 as components
+
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend/public")
+_component_func = components.declare_component("st_ner_annotate", path=build_dir)
+
+
+def st_ner_annotate(label, text, ents, key=None):
+    """st_edit_named_entities.
+
+    Parameters
+    ----------
+    text: str
+        Text to render
+    ents: object
+        Entities found in text
+    key: str or None
+        An optional key that uniquely identifies this component. If this is
+        None, and the component's arguments are changed, the component will
+        be re-mounted in the Streamlit frontend and lose its current state.
+
+    Returns
+    -------
+    object
+        Entities that have been selected
+    """
+    component_value = _component_func(label=label, text=text, ents=ents, key=key, default=ents)
+
+    return component_value
