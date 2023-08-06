557b4dd4e1294c890529f00e738392bbc5b04aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:29 +0000

    Add package kivywine/0.0.0/kivywine-0.0.0.tar.gz

---
+++ packages/kivywine/kivywine-0.0.0.tar.gz/kivywine-0.0.0/setup.py
@@ -0,0 +1,15 @@
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration('kivywine', parent_package, top_path)
+    config.add_subpackage('mod1')
+    config.add_subpackage('mod2')
+    return config
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+
+    config = configuration(top_path='').todict()
+    setup(**config)
