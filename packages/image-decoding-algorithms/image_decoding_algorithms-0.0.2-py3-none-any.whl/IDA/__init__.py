859fa36a10319f52864b080a4ebda2934a14335e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:07 +0000

    Add package image-decoding-algorithms/0.0.2/image_decoding_algorithms-0.0.2-py3-none-any.whl

---
+++ packages/image-decoding-algorithms/image_decoding_algorithms-0.0.2-py3-none-any.whl/IDA/__init__.py
@@ -0,0 +1,13 @@
+"""
+IDA - image decoding algorithms
+https://github.com/gejirz/image-decoding-algorithms - you can visit my repository on github
+Made by Victor Andrievskiy, e-mail: superavb222@gmail.com
+"""
+
+from sys import path
+from pathlib import Path
+path.append(str(Path(__file__).parent.resolve()))
+
+from IDA_main import read_image
+
+__version__ = "0.0.2"
