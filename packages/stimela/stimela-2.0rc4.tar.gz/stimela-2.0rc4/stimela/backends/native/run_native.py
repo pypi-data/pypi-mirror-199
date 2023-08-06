a66269f4494ec1735f21e5b037d2779e0a0f4acb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:30 +0000

    Add package stimela/2.0rc4/stimela-2.0rc4.tar.gz

---
+++ packages/stimela/stimela-2.0rc4.tar.gz/stimela-2.0rc4/stimela/backends/native/run_native.py
@@ -0,0 +1,91 @@
+import logging, datetime, resource
+
+from typing import Dict, Optional, Any
+
+import stimela
+from stimela.kitchen.cab import Cab
+from stimela.utils.xrun_asyncio import xrun
+from stimela.schedulers.slurm import SlurmBatch
+from stimela.schedulers import SlurmBatch
+from stimela.exceptions import StimelaProcessRuntimeError
+
+
+def update_rlimits(rlimits: Dict[str, Any], log: logging.Logger):
+    for name, limit in rlimits.items():
+        rname = f"RLIMIT_{name}"
+        if not hasattr(resource, rname):
+            raise StimelaProcessRuntimeError(f"unknown resource limit 'opts.rlimits.{name}'")
+        rconst = getattr(resource, rname)
+        # get current limits
+        soft, hard = resource.getrlimit(rconst)
+        # check for unlimited
+        if limit is None:
+            limit = resource.RLIM_INFINITY
+            if hard != resource.RLIM_INFINITY:
+                raise StimelaProcessRuntimeError(f"can't set opts.rlimits.{name}=unlimited: hard limit is {hard}")
+        else:
+            if limit > hard:
+                raise StimelaProcessRuntimeError(f"can't set opts.rlimits.{name}={limit}: hard limit is {hard}")
+        resource.setrlimit(rconst, (limit, hard))
+        log.debug(f"setting soft limit {name}={limit} (hard limit is {hard})")
+
+
+def run(cab: Cab, params: Dict[str, Any], runtime: Dict[str, Any], fqname: str,
+        log: logging.Logger, subst: Optional[Dict[str, Any]] = None, batch=None):
+    """Runs cab contents
+
+    Args:
+        cab (Cab): cab object
+        log (logger): logger to use
+        subst (Optional[Dict[str, Any]]): Substitution dict for commands etc., if any.
+
+    Returns:
+        Any: return value (e.g. exit code) of content
+    """
+    update_rlimits(stimela.CONFIG.opts.rlimits, log)
+
+    args = cab.flavour.get_arguments(cab, params, subst)
+
+    log.debug(f"command line is {args}")
+
+    cabstat = cab.reset_status()
+
+    command_name = cab.flavour.command_name
+
+    if batch:
+        batch = SlurmBatch(**batch)
+        batch.__init_cab__(cabstat.cab, params, subst, log)
+        runcmd = "/bin/bash -c" + " ".join(args)
+        jobfile = "foo-bar.job"
+        batch.name = "foo-bar"
+        batch.submit(jobfile=jobfile, runcmd=runcmd)
+
+        return
+
+    #-------------------------------------------------------
+    # run command
+    start_time = datetime.datetime.now()
+    def elapsed(since=None):
+        """Returns string representing elapsed time"""
+        return str(datetime.datetime.now() - (since or start_time)).split('.', 1)[0]
+
+    # log.info(f"argument lengths are {[len(a) for a in args]}")
+
+    retcode = xrun(args[0], args[1:], shell=False, log=log,
+                output_wrangler=cabstat.apply_wranglers,
+                return_errcode=True, command_name=command_name, 
+                gentle_ctrl_c=True,
+                log_command=True if cab.flavour.log_full_command else command_name, 
+                log_result=False)
+
+    # check if output marked it as a fail
+    if cabstat.success is False:
+        log.error(f"declaring '{command_name}' as failed based on its output")
+
+    # if retcode != 0 and not explicitly marked as success, mark as failed
+    if retcode and cabstat.success is not True:
+        cabstat.declare_failure(f"{command_name} returns error code {retcode} after {elapsed()}")
+    else:
+        log.info(f"{command_name} returns exit code {retcode} after {elapsed()}")
+
+    return cabstat
---
+++ packages/stimela/stimela-2.0rc4.tar.gz/stimela-2.0rc4/stimela/main.py
@@ -0,0 +1,128 @@
+import os
+import logging
+import glob
+import sys
+import click
+import datetime
+from dataclasses import dataclass
+from omegaconf import OmegaConf
+import stimela
+from stimela import config, stimelogging
+
+UID = stimela.UID
+GID = stimela.GID
+LOG_HOME = stimela.LOG_HOME
+LOG_FILE = stimela.LOG_FILE
+
+log = None
+
+_command_aliases = dict(exec="run", help="doc")
+
+class RunExecGroup(click.Group):
+    """ Makes the run and exec commands point to the same thing
+
+    Args:
+        click (_type_): _description_
+    """
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+
+        if cmd_name in _command_aliases:  
+            return click.Group.get_command(self, ctx, _command_aliases[cmd_name])
+        ctx.fail("Uknown command or alias")
+
+    def resolve_command(self, ctx, args):
+        # always return the full command name
+        _, cmd, args = super().resolve_command(ctx, args)
+        return cmd.name, cmd, args
+
+
+@click.group(cls=RunExecGroup)
+@click.option('--backend', '-b', type=click.Choice(config.Backend._member_names_), 
+                help="Backend to use (for containerization).")
+@click.option('--config', '-c', 'config_files', metavar='FILE', multiple=True,
+                help="Extra user-defined config file(s) to load.")
+@click.option('--set', '-s', 'config_dotlist', metavar='SECTION.VAR=VALUE', multiple=True,
+                help="Extra user-defined config settings to apply.")
+@click.option('--no-sys-config', is_flag=True, 
+                help="Do not load standard config files.")
+@click.option('-I', '--include', metavar="DIR", multiple=True, 
+                help="Add directory to _include paths. Can be given multiple times.")
+@click.option('--clear-cache', '-C', is_flag=True, 
+                help="Reset the configuration cache. First thing to try in case of strange configuration errors.")
+@click.option('--verbose', '-v', is_flag=True, help='Be extra verbose in output.')
+@click.version_option(str(stimela.__version__))
+def cli(backend, config_files=[], config_dotlist=[], include=[], verbose=False, no_sys_config=False, clear_cache=False):
+    global log
+    log = stimela.logger(loglevel=logging.DEBUG if verbose else logging.INFO)
+    log.info(f"starting")        # remove this eventually, but it's handy for timing things right now
+
+    stimela.VERBOSE = verbose
+    if verbose:
+        log.debug("verbose output enabled")
+
+    # use this logger for exceptions
+    import scabha.exceptions
+    scabha.exceptions.set_logger(log)
+    if verbose:
+        scabha.exceptions.ALWAYS_REPORT_TRACEBACK = True
+
+    import scabha.configuratt.cache
+    scabha.configuratt.cache.set_cache_dir(os.path.expanduser("~/.cache/stimela-configs"))
+    # clear cache if requested
+    if clear_cache:
+        scabha.configuratt.cache.clear_cache(log)
+
+    # load config files
+    stimela.CONFIG = config.load_config(extra_configs=config_files, extra_dotlist=config_dotlist, include_paths=include,
+                                        verbose=verbose, use_sys_config=not no_sys_config)
+    if stimela.CONFIG is None:
+        log.error("failed to load configuration, exiting")
+        sys.exit(1)
+
+    if config.CONFIG_LOADED:
+        log.info(f"loaded config from {config.CONFIG_LOADED}") 
+
+    # enable logfiles and such
+    if stimela.CONFIG.opts.log.enable:
+        if verbose:
+            stimela.CONFIG.opts.log.level = "DEBUG"
+        # setup file logging
+        subst = OmegaConf.create(dict(
+                    info=OmegaConf.create(dict(fqname='stimela')), 
+                    config=stimela.CONFIG))
+        stimelogging.update_file_logger(log, stimela.CONFIG.opts.log, nesting=-1, subst=subst)
+
+
+    # print backends
+    log.info(f"available backends: {' '.join(config.AVAILABLE_BACKENDS)}")
+
+    # get default backend module
+    global BACKEND 
+    if backend:
+        stimela.CONFIG.opts.backend = backend
+
+    if stimela.CONFIG.opts.backend.name not in config.AVAILABLE_BACKENDS:
+        status = config.get_backend_status(stimela.CONFIG.opts.backend.name)
+        log.error(f"the backend '{stimela.CONFIG.opts.backend.name}' is not available: {status}")
+        sys.exit(1)
+
+    log.info(f"default backend is {stimela.CONFIG.opts.backend.name}")
+
+    # report dependencies
+    for filename, attrs in config.CONFIG_DEPS.get_description().items():
+        log.debug(f"config dependency {', '.join([filename] + attrs)}")
+
+    # dump dependencies
+    filename = os.path.join(stimelogging.get_logfile_dir(log) or '.', "stimela.config.deps")
+    log.info(f"saving config dependencies to {filename}")
+    config.CONFIG_DEPS.save(filename)
+
+
+# import commands
+from stimela.commands import doc, run, images, build, push, save_config
+
+## the ones not listed above haven't been converted to click yet. They are:
+# cabs, clean, containers, kill, ps, pull
