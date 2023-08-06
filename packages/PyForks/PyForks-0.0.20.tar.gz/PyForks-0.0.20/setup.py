1683079ed96b08f840991c7cf5a4376e0933a21d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:47 +0000

    Add package PyForks/0.0.20/PyForks-0.0.20.tar.gz

---
+++ packages/PyForks/PyForks-0.0.20.tar.gz/PyForks-0.0.20/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name="PyForks",
+    version="0.0.20",
+    author="Trailforks Python Library",
+    author_email="josh@mn-mtb.com",
+    packages=["PyForks", "PyForks._test", "PyForks.data"],
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/cribdragg3r/PyForks/issues",
+        "Funding": "https://donate.pypi.org",
+        "Source": "https://github.com/cribdragg3r/PyForks",
+        "Documentation": "https://pyforks.mn-mtb.com",
+    },
+    url="http://pypi.python.org/pypi/PyForks/",
+    license="LICENSE.txt",
+    description="A package to interface with Trailforks.com",
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    package_data={
+        'PyForks': ['PyForks/data/region_data.parquet']
+        },
+    install_requires=[
+        "pytest",
+        "tqdm",
+        "requests",
+        "pandas",
+        "pyarrow"
+    ],
+)
