c16142339523fa7125c324f5dd184d0513a887c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:17 +0000

    Add package Azule-Hair-Transplant/0.0.1/Azule_Hair_Transplant-0.0.1.tar.gz

---
+++ packages/Azule-Hair-Transplant/Azule_Hair_Transplant-0.0.1.tar.gz/Azule_Hair_Transplant-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 11',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2'
+]
+
+setup(
+    name='Azule_Hair_Transplant',
+    version='0.0.1',
+    description='Hair Transplant',
+    author='arin.adil',
+    author_email='azule107@gmail.com',
+    classifiers=classifiers,
+    keywords='',
+    packages=find_packages(),
+    install_requires=['']
+)
