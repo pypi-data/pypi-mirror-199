3e12a28ee14c8fb3604288fa908f196418cc18c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:24 +0000

    Add package opens/1.0.0/opens-1.0.0-py3-none-any.whl

---
+++ packages/opens/opens-1.0.0-py3-none-any.whl/opens/opens.py
@@ -0,0 +1,20 @@
+_ = {}
+
+def r(response):
+	print(f"\u001b[42m {response} \u001b[0m\u001b[32m\uE0B0\u001b[0m")
+
+def put(variable, value):
+	_[variable] = value
+	return value
+
+def get(variable):
+	res = _[variable]
+	r(res)
+	return res
+
+def echo(text, colour=1):
+	print(f"\u001b[{colour}m{text}\u001b[0m")
+
+def q(code=0):
+	r(f"Exiting... code: {code}")
+	quit(code)
