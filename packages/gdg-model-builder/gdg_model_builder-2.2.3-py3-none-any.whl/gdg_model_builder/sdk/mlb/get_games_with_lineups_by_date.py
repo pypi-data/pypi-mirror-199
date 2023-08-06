90f07cf2c2fcbe3e0791063be6a379d335c3d9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:11 +0000

    Add package gdg-model-builder/2.2.3/gdg_model_builder-2.2.3-py3-none-any.whl

---
+++ packages/gdg-model-builder/gdg_model_builder-2.2.3-py3-none-any.whl/gdg_model_builder/sdk/mlb/get_games_with_lineups_by_date.py
@@ -0,0 +1,56 @@
+from typing import Any, List, Optional
+import requests
+from dotenv import load_dotenv
+from pydantic import BaseModel
+import os
+from ...util.lru.lru import lru_cache_time
+from datetime import datetime
+from .get_games_by_date import GameByDate, get_games_by_date
+from .lineups_by_date import LineupsByDate, get_lineups
+load_dotenv()
+
+
+class GameWithLineupByDate(BaseModel):
+    game : GameByDate
+    lineups : LineupsByDate
+
+
+def get_games_with_lineups_by_date(date : datetime) -> List[GameWithLineupByDate]:
+    """Gets games by date directly from sportsdataio
+
+    Args:
+        date (datetime): is the date in question.
+
+    Returns:
+        List[GameByDatelike]: are the games by date.
+    """
+    games = get_games_by_date(date)
+    lineups = get_lineups(date)
+    
+    games_map = {}
+    for game in games:
+        games_map[game.GameID] = game
+       
+    games_with_lineups = [] 
+    for lineup in lineups:
+        if lineup.GameID in games_map:
+            games_with_lineups.append(GameWithLineupByDate(
+                game=games_map[lineup.GameID],
+                lineups=lineup
+            ))
+    return games_with_lineups
+
+def get_game_with_lineups_by_date(*, date : datetime, game_id : int) -> Optional[GameWithLineupByDate]:
+    """Gets games by date directly from sportsdataio
+
+    Args:
+        date (datetime): is the date in question.
+
+    Returns:
+        List[GameByDatelike]: are the games by date.
+    """
+    games_with_lineups = get_games_with_lineups_by_date(date)
+    for game in games_with_lineups:
+        if game.game.GameID == game_id:
+            return game
+    return None
