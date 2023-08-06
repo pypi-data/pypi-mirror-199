c157aa4c699ace46cc8b0016f109da12d6b49e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:19 +0000

    Add package pympipool/0.3.0/pympipool-0.3.0.tar.gz

---
+++ packages/pympipool/pympipool-0.3.0.tar.gz/pympipool-0.3.0/pympipool/__main__.py
@@ -0,0 +1,85 @@
+import pickle
+import cloudpickle
+from mpi4py import MPI
+
+MPI.pickle.__init__(
+    cloudpickle.dumps,
+    cloudpickle.loads,
+    pickle.HIGHEST_PROTOCOL,
+)
+
+from mpi4py.futures import MPIPoolExecutor
+from tqdm import tqdm
+import sys
+import zmq
+
+
+def wrap(funct, number_of_cores_per_communicator):
+    def functwrapped(input_parameter):
+        MPI.COMM_WORLD.Barrier()
+        rank = MPI.COMM_WORLD.Get_rank()
+        comm_new = MPI.COMM_WORLD.Split(
+            rank // number_of_cores_per_communicator,
+            rank % number_of_cores_per_communicator,
+        )
+        comm_new.Barrier()
+        return funct(input_parameter, comm=comm_new)
+
+    return functwrapped
+
+
+def exec_funct(executor, funct, lst, cores_per_task):
+    if cores_per_task == 1:
+        results = executor.map(funct, lst)
+        return list(tqdm(results, desc="Tasks", total=len(lst)))
+    else:
+        lst_parallel = []
+        for input_parameter in lst:
+            for _ in range(cores_per_task):
+                lst_parallel.append(input_parameter)
+        results = executor.map(
+            wrap(funct=funct, number_of_cores_per_communicator=cores_per_task),
+            lst_parallel,
+        )
+        return list(tqdm(results, desc="Tasks", total=len(lst_parallel)))[
+            ::cores_per_task
+        ]
+
+
+def main():
+    argument_lst = sys.argv
+    total_cores = int(argument_lst[argument_lst.index("--cores-total") + 1])
+    with MPIPoolExecutor(total_cores) as executor:
+        if executor is not None:
+            context = zmq.Context()
+            socket = context.socket(zmq.PAIR)
+            port_selected = argument_lst[argument_lst.index("--zmqport") + 1]
+            cores_per_task = int(
+                argument_lst[argument_lst.index("--cores-per-task") + 1]
+            )
+            socket.connect("tcp://localhost:" + port_selected)
+        while True:
+            if executor is not None:
+                input_dict = cloudpickle.loads(socket.recv())
+                if "c" in input_dict.keys() and input_dict["c"] == "close":
+                    socket.close()
+                    context.term()
+                    break
+                elif "f" in input_dict.keys() and "l" in input_dict.keys():
+                    try:
+                        output = exec_funct(
+                            executor=executor,
+                            funct=input_dict["f"],
+                            lst=input_dict["l"],
+                            cores_per_task=cores_per_task,
+                        )
+                    except Exception as error:
+                        socket.send(
+                            cloudpickle.dumps({"e": error, "et": str(type(error))})
+                        )
+                    else:
+                        socket.send(cloudpickle.dumps({"r": output}))
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pympipool/pympipool-0.3.0.tar.gz/pympipool-0.3.0/setup.py
@@ -0,0 +1,43 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+from pathlib import Path
+import versioneer
+
+
+setup(
+    name='pympipool',
+    version=versioneer.get_version(),
+    description='pympipool - scale functions over multiple compute nodes using mpi4py',
+    long_description=Path("README.md").read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jan-janssen/pympipool',
+    author_email='jan.janssen@outlook.com',
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
+    packages=find_packages(exclude=["*tests*", "*.ci_support*"]),
+    install_requires=[
+        'cloudpickle>=2.2.1',
+        'mpi4py>=3.1.4',
+        'tqdm>=4.65.0',
+        'pyzmq>=25.0.2'
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+
+    entry_points={
+            "console_scripts": [
+                'pympipool=pympipool.__main__:main'
+            ]
+    }
+)
