3fccc540e7ce275346642f2cd2c3509917aa6821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:43 +0000

    Add package onlinekx/0.1/onlinekx-0.1.tar.gz

---
+++ packages/onlinekx/onlinekx-0.1.tar.gz/onlinekx-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='onlinekx',
+      version='0.1',
+      description='The onlinekx joke in the world',
+      url='http://www.baidu.com',
+      author='ben',
+      author_email='ben@example.com',
+      license='MIT',
+      packages=['onlinekx'],
+      zip_safe=False)
