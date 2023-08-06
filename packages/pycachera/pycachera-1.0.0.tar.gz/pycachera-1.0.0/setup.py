876010f16d4f724fcd1fa03441d22c66c6a3e5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:34 +0000

    Add package pycachera/1.0.0/pycachera-1.0.0.tar.gz

---
+++ packages/pycachera/pycachera-1.0.0.tar.gz/pycachera-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+files = ["pycachera/*"]
+setup(
+        name = 'pycachera',
+        packages = ['pycachera'],
+        package_data = {'pycachera' : files },
+        version = '1.0.0',
+        install_requires = ['numpy','pandas'],
+        description = 'A powerfull python caching tool',
+        author = 'Anto Idicherian Lonappan',
+        author_email = 'mail@antolonappan.me',
+        url = 'https://github.com/antolonappan/pycacher'
+        )
