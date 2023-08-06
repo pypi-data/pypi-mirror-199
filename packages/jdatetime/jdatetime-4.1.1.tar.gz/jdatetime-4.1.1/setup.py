049adf440515b2f0b5a683a13be062171874e0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:45 +0000

    Add package jdatetime/4.1.1/jdatetime-4.1.1.tar.gz

---
+++ packages/jdatetime/jdatetime-4.1.1.tar.gz/jdatetime-4.1.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+import setuptools
+
+setup(
+    name='jdatetime',
+    version='4.1.1',
+    packages=['jdatetime', ],
+    license='Python Software Foundation License',
+    keywords='Jalali implementation of Python datetime',
+    platforms='any',
+    author='Milad Rastian',
+    author_email='eslashmili@gmail.com',
+    description=("Jalali datetime binding for python"),
+    url="https://github.com/slashmili/python-jalali",
+    long_description=open('README').read(),
+    python_requires=">=3.7",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development",
+    ],
+)
