b15c6e49e50e2c8954e350a9935e2487acc34654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:39 +0000

    Add package TrimMCStruct/0.0.4/TrimMCStruct-0.0.4.tar.gz

---
+++ packages/TrimMCStruct/TrimMCStruct-0.0.4.tar.gz/TrimMCStruct-0.0.4/TrimMCStruct/__init__.py
@@ -0,0 +1,8 @@
+"""
+Read and write Minecraft .mcstructure files.
+"""
+
+__version__ = "0.0.4"
+__author__ = (("金羿", "Eilles Wan"), ("诸葛亮与八卦阵", "bgArray"), ("phoenixR", "phoenixR"))
+
+from .main import *
