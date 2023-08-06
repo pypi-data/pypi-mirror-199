7456fbb1c548ebb3a8402c9d9776abf8f55ff6ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:41 +0000

    Add package tupa123/1.0.1/tupa123-1.0.1.tar.gz

---
+++ packages/tupa123/tupa123-1.0.1.tar.gz/tupa123-1.0.1/README.md
@@ -0,0 +1,9 @@
+# tupa123
+
+tupa123 is a Python library for four-layer fully connected neural networks.
+
+# Installation
+
+To install the library, you can use pip:
+
+pip install tupa123
---
+++ packages/tupa123/tupa123-1.0.1.tar.gz/tupa123-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='tupa123',
+    version='1.0.1',
+    description= 'fully connected neural network with four layers',
+    author='Leandro Schemmer',
+    packages=['tupa123'],
+    install_requires=['numpy','matplotlib','pandas']
+)
