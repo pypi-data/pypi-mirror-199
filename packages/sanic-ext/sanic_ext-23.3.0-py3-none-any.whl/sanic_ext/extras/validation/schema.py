f6e847161016a291eab0cc01cf122f92afdb7853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:41 +0000

    Add package sanic-ext/23.3.0/sanic_ext-23.3.0-py3-none-any.whl

---
+++ packages/sanic-ext/sanic_ext-23.3.0-py3-none-any.whl/sanic_ext/extras/validation/schema.py
@@ -0,0 +1,123 @@
+import types
+from dataclasses import MISSING, Field, is_dataclass
+from inspect import isclass, signature
+from typing import (
+    Any,
+    Dict,
+    Literal,
+    Optional,
+    Tuple,
+    Union,
+    get_args,
+    get_origin,
+    get_type_hints,
+)
+
+from sanic_ext.utils.typing import is_attrs, is_generic
+
+from .check import Hint
+
+try:
+    UnionType = types.UnionType  # type: ignore
+except AttributeError:
+    UnionType = type("UnionType", (), {})
+
+try:
+    from attr import NOTHING, Attribute
+except ModuleNotFoundError:
+    NOTHING = object()  # type: ignore
+    Attribute = type("Attribute", (), {})  # type: ignore
+
+
+def make_schema(agg, item):
+    if type(item) in (bool, str, int, float):
+        return agg
+
+    if is_generic(item) and (args := get_args(item)):
+        for arg in args:
+            make_schema(agg, arg)
+    elif item.__name__ not in agg and (is_dataclass(item) or is_attrs(item)):
+        fields = (
+            item.__dataclass_fields__
+            if is_dataclass(item)
+            else {attr.name: attr for attr in item.__attrs_attrs__}
+        )
+        sig = signature(item)
+        hints = parse_hints(get_type_hints(item), fields)
+
+        agg[item.__name__] = {
+            "sig": sig,
+            "hints": hints,
+        }
+
+        for hint in hints.values():
+            make_schema(agg, hint.hint)
+
+    return agg
+
+
+def parse_hints(
+    hints, fields: Dict[str, Union[Field, Attribute]]
+) -> Dict[str, Hint]:
+    output: Dict[str, Hint] = {
+        name: parse_hint(hint, fields.get(name))
+        for name, hint in hints.items()
+    }
+    return output
+
+
+def parse_hint(hint, field: Optional[Union[Field, Attribute]] = None):
+    origin = None
+    literal = not isclass(hint)
+    nullable = False
+    typed = False
+    model = False
+    allowed: Tuple[Any, ...] = tuple()
+    allow_missing = False
+
+    if field and (
+        (
+            isinstance(field, Field)
+            and field.default_factory is not MISSING  # type: ignore
+        )
+        or (isinstance(field, Attribute) and field.default is not NOTHING)
+    ):
+        allow_missing = True
+
+    if is_dataclass(hint) or is_attrs(hint):
+        model = True
+    elif is_generic(hint):
+        typed = True
+        literal = False
+        origin = get_origin(hint)
+        args = get_args(hint)
+        nullable = origin in (Union, UnionType) and type(None) in args
+
+        if nullable:
+            allowed = tuple(
+                [
+                    arg
+                    for arg in args
+                    if is_generic(arg) or not isinstance(None, arg)
+                ]
+            )
+        elif origin is dict:
+            allowed = (args[1],)
+        elif (
+            origin is list
+            or origin is Literal
+            or origin is Union
+            or origin is UnionType
+        ):
+            allowed = args
+
+    return Hint(
+        hint,
+        model,
+        literal,
+        typed,
+        nullable,
+        origin,
+        tuple([parse_hint(item, None) for item in allowed]),
+        allow_missing,
+    )
---
+++ packages/sanic-ext/sanic_ext-23.3.0-py3-none-any.whl/sanic_ext/utils/typing.py
@@ -0,0 +1,68 @@
+import types
+import typing
+from inspect import isclass
+
+try:
+    UnionType = types.UnionType  # type: ignore
+except AttributeError:
+    UnionType = type("UnionType", (), {})  # type: ignore
+
+try:
+    from pydantic import BaseModel
+
+    PYDANTIC = True
+except ImportError:
+    PYDANTIC = False
+
+try:
+    import attrs  # noqa
+
+    ATTRS = True
+except ImportError:
+    ATTRS = False
+
+
+def is_generic(item):
+    return (
+        isinstance(item, typing._GenericAlias)
+        or isinstance(item, UnionType)
+        or hasattr(item, "__origin__")
+    )
+
+
+def is_optional(item):
+    if is_generic(item):
+        args = typing.get_args(item)
+        return len(args) == 2 and type(None) in args
+    return False
+
+
+def is_pydantic(model):
+    return PYDANTIC and (
+        issubclass(model, BaseModel) or hasattr(model, "__pydantic_model__")
+    )
+
+
+def is_attrs(model):
+    return ATTRS and (hasattr(model, "__attrs_attrs__"))
+
+
+def flat_values(
+    item: typing.Union[
+        typing.Dict[str, typing.Any], typing.Iterable[typing.Any]
+    ]
+) -> typing.Set[typing.Any]:
+    values = set()
+    if isinstance(item, dict):
+        item = item.values()
+    for value in item:
+        if isinstance(value, dict) or isinstance(value, list):
+            values.update(flat_values(value))
+        else:
+            values.add(value)
+    return values
+
+
+def contains_annotations(d: typing.Dict[str, typing.Any]) -> bool:
+    values = flat_values(d)
+    return any(isclass(q) or is_generic(q) for q in values)
