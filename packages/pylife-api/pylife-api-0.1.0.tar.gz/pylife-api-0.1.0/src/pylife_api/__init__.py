fd3c9fcbd1f677b8014e6dacb355dc82bae4e191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:28 +0000

    Add package pylife-api/0.1.0/pylife-api-0.1.0.tar.gz

---
+++ packages/pylife-api/pylife-api-0.1.0.tar.gz/pylife-api-0.1.0/src/pylife_api/__init__.py
@@ -0,0 +1,14 @@
+from .client import PylifeAPIClient
+from .models import Achievement, Fine, House, Member, Membership, Organization, Player
+
+__version__ = "0.1.0"
+__all__ = [
+    "PylifeAPIClient",
+    "Achievement",
+    "Fine",
+    "House",
+    "Member",
+    "Membership",
+    "Organization",
+    "Player",
+]
