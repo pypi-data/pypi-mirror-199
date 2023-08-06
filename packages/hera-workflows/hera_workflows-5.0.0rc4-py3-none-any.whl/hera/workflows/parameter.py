8c919067e5aadd54abd1eebc32882e135ed78a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:14 +0000

    Add package hera-workflows/5.0.0rc4/hera_workflows-5.0.0rc4-py3-none-any.whl

---
+++ packages/hera-workflows/hera_workflows-5.0.0rc4-py3-none-any.whl/hera/workflows/parameter.py
@@ -0,0 +1,85 @@
+"""The parameter module provides the Parameter class.
+
+See https://argoproj.github.io/argo-workflows/walk-through/parameters/
+for a tutorial on Parameters.
+"""
+from typing import Any, Optional
+
+from pydantic import root_validator
+
+from hera.shared.serialization import serialize
+from hera.workflows.models import Parameter as _ModelParameter
+
+
+class Parameter(_ModelParameter):
+    """A `Parameter` is used to pass values in and out of templates.
+
+    They are to declare input and output parameters in the case of templates, and are used
+    for Steps and Tasks to assign values.
+    """
+
+    value: Optional[Any]
+
+    @root_validator(pre=True)
+    def _check_values(cls, values):
+        if values.get("value") is not None and values.get("value_from") is not None:
+            raise ValueError("Cannot specify both `value` and `value_from` when instantiating `Parameter`")
+
+        if values.get("value") is not None and not isinstance(values.get("value"), str):
+            values["value"] = serialize(values.get("value"))
+
+        if values.get("default") is not None and not isinstance(values.get("value"), str):
+            values["default"] = serialize(values.get("default"))
+
+        return values
+
+    def __str__(self):
+        """Represent the parameter as a string by pointing to its value.
+
+        This is useful in situations where we want to concatenate string values such as
+        Task.args=["echo", wf.get_parameter("message")].
+        """
+        if self.value is None:
+            raise ValueError("Cannot represent `Parameter` as string as `value` is not set")
+        return self.value
+
+    def as_input(self) -> _ModelParameter:
+        """Assembles the parameter for use as an input of a template"""
+        return _ModelParameter(
+            name=self.name,
+            description=self.description,
+            default=self.default,
+            enum=self.enum,
+            value=self.value,
+            value_from=self.value_from,
+        )
+
+    def as_argument(self) -> _ModelParameter:
+        """Assembles the parameter for use as an argument of a step or a task"""
+        # Setting a default value when used as an argument is a no-op so we exclude it as it would get overwritten by
+        # `value` or `value_from` (one of which is required)
+        # Overwrite ref: https://github.com/argoproj/argo-workflows/blob/781675ddcf6f1138d697cb9c71dae484daa0548b/workflow/common/util.go#L126-L139
+        # One of value/value_from required ref: https://github.com/argoproj/argo-workflows/blob/ab178bb0b36a5ce34b4c1302cf4855879a0e8cf5/workflow/validate/validate.go#L794-L798
+        return _ModelParameter(
+            name=self.name,
+            global_name=self.global_name,
+            description=self.description,
+            enum=self.enum,
+            value=self.value,
+            value_from=self.value_from,
+        )
+
+    def as_output(self) -> _ModelParameter:
+        """Assembles the parameter for use as an output of a template"""
+        # Only `value` and `value_from` are valid here
+        # see https://github.com/argoproj/argo-workflows/blob/e3254eca115c9dd358e55d16c6a3d41403c29cae/workflow/validate/validate.go#L1067
+        return _ModelParameter(
+            name=self.name,
+            global_name=self.global_name,
+            description=self.description,
+            value=self.value,
+            value_from=self.value_from,
+        )
+
+
+__all__ = ["Parameter"]
