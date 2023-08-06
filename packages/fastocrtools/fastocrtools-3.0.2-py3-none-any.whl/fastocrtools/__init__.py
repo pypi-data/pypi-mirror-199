3f40c0a26c4b821990bab99c66332ecd1dd04895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:24 +0000

    Add package fastocrtools/3.0.2/fastocrtools-3.0.2-py3-none-any.whl

---
+++ packages/fastocrtools/fastocrtools-3.0.2-py3-none-any.whl/fastocrtools/__init__.py
@@ -0,0 +1,13 @@
+__title__ = 'fastocrtools'
+__author__ = 'SpeakerC'
+__license__ = 'MIT'
+__version__ = '3.0.2'
+from .tools import*
+
+
+__all__ = [
+    'ocr',
+    'high_accuracy_ocr',
+    'Handwritten_ocr',
+    'ocr_with_location'
+    ]
