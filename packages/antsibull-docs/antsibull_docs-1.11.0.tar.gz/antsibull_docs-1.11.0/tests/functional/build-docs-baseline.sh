6f623f7d8239f37e5c0592582aa1bcee5abce398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:07 +0000

    Add package antsibull-docs/1.11.0/antsibull_docs-1.11.0.tar.gz

---
+++ packages/antsibull-docs/antsibull_docs-1.11.0.tar.gz/antsibull_docs-1.11.0/tests/functional/build-docs-baseline.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright (c) Ansible Project
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+set -e
+
+
+make_baseline() {
+    DEST="$1"
+    shift
+
+    echo "Building baseline ${DEST}..."
+    rm -rf "${DEST}"
+    mkdir -p "${DEST}"
+    ANSIBLE_COLLECTIONS_PATHS= ANSIBLE_COLLECTIONS_PATH=collections/ antsibull-docs collection --dest-dir "${DEST}" --use-current "$@" 2>&1 | (
+        set +e
+        grep -v "ERROR:antsibull:func=create_plugin_rst:mod=antsibull_docs.write_docs.plugins:nonfatal_errors="
+        set -e
+    )
+
+    rstcheck --report-level warning --ignore-roles ansible-option-default,ansible-rv-sample-value,ansopt,ansval,ansretval -r "${DEST}" 2>&1 | (
+        set +e
+        grep -v "CRITICAL:rstcheck_core.checker:An \`AttributeError\` error occured."
+        set -e
+    )
+}
+
+
+make_baseline baseline-default ns.col1 ns.col2 ns2.col
+make_baseline baseline-no-breadcrumbs ns.col1 ns.col2 ns2.col --no-breadcrumbs
+make_baseline baseline-no-indexes ns.col1 ns2.col --fail-on-error --no-indexes
+make_baseline baseline-use-html-blobs ns2.col --fail-on-error --use-html-blobs
+make_baseline baseline-squash-hierarchy ns2.col --fail-on-error --squash-hierarchy
