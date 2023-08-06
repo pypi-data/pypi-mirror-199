7c80af7dd346333be25ea3d07f3c9e261ede503c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:35 +0000

    Add package bgmiget/1.0.2/bgmiget-1.0.2.tar.gz

---
+++ packages/bgmiget/bgmiget-1.0.2.tar.gz/bgmiget-1.0.2/README.md
@@ -0,0 +1,27 @@
+# BgmiGet
+
+## How to Install
+
+```
+pip install bgmiget
+```
+
+## Basic Usage
+
+Search anime by "search" commnad.
+
+```
+bgmiget search '因为太怕痛' 7 "sc" "桜都字幕组"
+>0  -> 【因为太怕痛...】【07】【桜都字幕组】
+```
+
+Then, download anime by index.
+
+```
+bgmiget download 0
+```
+
+Subscribe anime by "subscribe" command, and list them by "show_subscribed" command.
+
+At last, check for updates on anime by "upgrade" command.
+
---
+++ packages/bgmiget/bgmiget-1.0.2.tar.gz/bgmiget-1.0.2/bgmiget/sources/mikanproject.py
@@ -0,0 +1,26 @@
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+
+from ..result import Result
+
+BASE_URL = "https://mikanani.me/"
+
+
+class MiKanProject:
+
+    def search(self, query: str) -> List[Result]:
+
+        URL = BASE_URL + "Home/Search?searchstr=" + query
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        results = []
+        for i in soup.find_all("tr", class_="js-search-results-row"):
+            title = i.find("a", class_="magnet-link-wrap").text
+            url = i.find(
+                "a", class_="js-magnet magnet-link")["data-clipboard-text"]
+            result = Result(title, url)
+            results.append(result)
+        return results
