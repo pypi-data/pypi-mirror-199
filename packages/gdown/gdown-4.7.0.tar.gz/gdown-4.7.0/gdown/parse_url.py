16bcba2c5fe87945931c21c01c67996ac730aba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:16 +0000

    Add package gdown/4.7.0/gdown-4.7.0.tar.gz

---
+++ packages/gdown/gdown-4.7.0.tar.gz/gdown-4.7.0/gdown/parse_url.py
@@ -0,0 +1,48 @@
+import re
+import warnings
+
+from six.moves import urllib_parse
+
+
+def parse_url(url, warning=True):
+    """Parse URLs especially for Google Drive links.
+
+    file_id: ID of file on Google Drive.
+    is_download_link: Flag if it is download link of Google Drive.
+    """
+    parsed = urllib_parse.urlparse(url)
+    query = urllib_parse.parse_qs(parsed.query)
+    is_gdrive = parsed.hostname in ["drive.google.com", "docs.google.com"]
+    is_download_link = parsed.path.endswith("/uc")
+
+    if not is_gdrive:
+        return is_gdrive, is_download_link
+
+    file_id = None
+    if "id" in query:
+        file_ids = query["id"]
+        if len(file_ids) == 1:
+            file_id = file_ids[0]
+    else:
+        patterns = [
+            r"^/file/d/(.*?)/view$",
+            r"^/document/d/(.*?)/(view|edit)$",
+            r"^/presentation/d/(.*?)/(view|edit)$",
+            r"^/spreadsheets/u/0/d/(.*?)/(htmlview|edit)$",
+        ]
+        for pattern in patterns:
+            match = re.match(pattern, parsed.path)
+            if match:
+                file_id = match.groups()[0]
+                break
+
+    if warning and not is_download_link:
+        warnings.warn(
+            "You specified a Google Drive link that is not the correct link "
+            "to download a file. You might want to try `--fuzzy` option "
+            "or the following url: {url}".format(
+                url="https://drive.google.com/uc?id={}".format(file_id)
+            )
+        )
+
+    return file_id, is_download_link
