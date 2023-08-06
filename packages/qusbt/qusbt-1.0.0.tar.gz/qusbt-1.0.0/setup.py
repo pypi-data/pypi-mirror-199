d6b05b124cd1d99a4cab32237c9fcec392b676b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:55 +0000

    Add package qusbt/1.0.0/qusbt-1.0.0.tar.gz

---
+++ packages/qusbt/qusbt-1.0.0.tar.gz/qusbt-1.0.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="qusbt",
+    version="1.0.0",
+    author="Xinyi Wang",
+    author_email="xinyi@simula.no",
+    description="Search-based testing of quantum programs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url="https://github.com/Simula-COMPLEX/qusbt-tool",
+    classifiers= ["Programming Language :: Python :: 3",  # 编程语言
+        "License :: OSI Approved :: MIT License",  # license
+        "Operating System :: OS Independent"],  # 操作系统
+    install_requires=[
+        "qiskit",
+        "jmetalpy",
+        "openpyxl"
+    ],
+    package_data={"pipmodule": ["*.png", ]},
+    python_requires=">=3.7",
+    entry_points={
+        'console_scripts': [
+            'qusbt = qusbt:help'
+ ]
+    }
+)
