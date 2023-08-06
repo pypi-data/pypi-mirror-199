4ed4b742fb999723d72a6792238d1a4f0b0ab66c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:55 +0000

    Add package onnx-extended/0.1.0/onnx_extended-0.1.0-cp310-cp310-win_amd64.whl

---
+++ packages/onnx-extended/onnx_extended-0.1.0-cp310-cp310-win_amd64.whl/onnx_extended/reference/c_reference_evaluator.py
@@ -0,0 +1,43 @@
+from typing import Any, Dict, List, Optional, Union
+
+from onnx import FunctionProto
+from onnx.reference import ReferenceEvaluator
+from onnx.reference.op_run import OpRun
+from onnx_extended.reference.c_ops.c_op_conv import Conv
+
+
+class CReferenceEvaluator(ReferenceEvaluator):
+    """
+    This class replaces the python implementation by C implementation
+    for a short list of operators quite slow in python (such as `Conv`).
+    The class automatically replaces a python implementation
+    by a C implementation if available. See example :ref:`l-example-conv`.
+
+    ::
+
+        from onnx.reference import ReferenceEvaluator
+        from from onnx.reference.c_ops import Conv
+        ref = ReferenceEvaluator(..., new_ops=[Conv])
+    """
+
+    def __init__(
+        self,
+        proto: Any,
+        opsets: Optional[Dict[str, int]] = None,
+        functions: Optional[List[Union[ReferenceEvaluator, FunctionProto]]] = None,
+        verbose: int = 0,
+        new_ops: Optional[List[OpRun]] = None,
+    ):
+        if new_ops is None:
+            new_ops = [Conv]
+        else:
+            new_ops = new_ops.copy()
+            new_ops.append(Conv)
+        ReferenceEvaluator.__init__(
+            self,
+            proto,
+            opsets=opsets,
+            functions=functions,
+            verbose=verbose,
+            new_ops=new_ops,
+        )
