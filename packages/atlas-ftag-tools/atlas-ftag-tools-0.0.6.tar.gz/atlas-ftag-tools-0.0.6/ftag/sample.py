2611d6e0c6887aa3a00e3c8eb1e4337263511ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:39 +0000

    Add package atlas-ftag-tools/0.0.6/atlas-ftag-tools-0.0.6.tar.gz

---
+++ packages/atlas-ftag-tools/atlas-ftag-tools-0.0.6.tar.gz/atlas-ftag-tools-0.0.6/ftag/sample.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class Sample:
+    name: str
+    ntuple_dir: Path
+    pattern: str | tuple[str]
+
+    @property
+    def path(self) -> Path | tuple[Path]:
+        if isinstance(self.pattern, str):
+            return self.ntuple_dir / self.pattern
+        return tuple(self.ntuple_dir / p for p in self.pattern)  # type: ignore
+
+    def __str__(self):
+        return self.name
+
+    def __lt__(self, other):
+        return self.name < other.name
+
+    def __eq__(self, other):
+        return self.name == other.name
