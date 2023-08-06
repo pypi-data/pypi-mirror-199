e50f6be42b6bc828a90bc8aa1812f6eda494cdfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:43 +0000

    Add package rad-tools/0.5.16/rad_tools-0.5.16-py3-none-any.whl

---
+++ packages/rad-tools/rad_tools-0.5.16-py3-none-any.whl/rad_tools-0.5.16.data/scripts/rad-dos-plotter.py
@@ -0,0 +1,8 @@
+#!python
+
+from rad_tools.score.rad_dos_plotter_core import get_parser, manager
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    manager(**vars(args))
---
+++ packages/rad-tools/rad_tools-0.5.16-py3-none-any.whl/rad_tools-0.5.16.data/scripts/tb2j-extractor.py
@@ -0,0 +1,8 @@
+#!python
+
+from rad_tools.score.tb2j_extractor_core import get_parser, manager
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    manager(**vars(args))
