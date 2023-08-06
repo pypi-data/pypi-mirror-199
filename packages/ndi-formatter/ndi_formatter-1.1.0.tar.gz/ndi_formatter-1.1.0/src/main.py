65d6f9d887c212cfc6d4ee297d71e11871f15979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:27 +0000

    Add package ndi-formatter/1.1.0/ndi_formatter-1.1.0.tar.gz

---
+++ packages/ndi-formatter/ndi_formatter-1.1.0.tar.gz/ndi_formatter-1.1.0/src/main.py
@@ -0,0 +1,9 @@
+from ndi_formatter.format import main
+
+
+def format_ndi():
+    main()
+
+
+if __name__ == '__main__':
+    format_ndi()
---
+++ packages/ndi-formatter/ndi_formatter-1.1.0.tar.gz/ndi_formatter-1.1.0/src/ndi_formatter/__init__.py
@@ -0,0 +1,2 @@
+"""Format data for National Death Index (NDI) requests."""
+__version__ = '1.1.0'
