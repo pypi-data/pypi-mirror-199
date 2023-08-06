35892843123b674e8900ed785c7e1706ba4cadea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:29 +0000

    Add package bird-dev/1.0.0a4/bird_dev-1.0.0a4-py3-none-any.whl

---
+++ packages/bird-dev/bird_dev-1.0.0a4-py3-none-any.whl/bird/__init__.py
@@ -0,0 +1,10 @@
+from bird.object import BObject
+
+try:
+    from bird_form import *
+    from bird_telerik import *
+    from bird_devexpress import *
+    from bird_win32 import *
+    from bird_tk import *
+except:
+    pass
