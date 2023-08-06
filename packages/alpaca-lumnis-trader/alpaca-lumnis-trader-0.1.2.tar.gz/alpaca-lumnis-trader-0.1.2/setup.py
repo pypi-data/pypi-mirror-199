7e2c4d0bc0eaf6293e69e828a374cd4719ba5128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:11 +0000

    Add package alpaca-lumnis-trader/0.1.2/alpaca-lumnis-trader-0.1.2.tar.gz

---
+++ packages/alpaca-lumnis-trader/alpaca-lumnis-trader-0.1.2.tar.gz/alpaca-lumnis-trader-0.1.2/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="alpaca-lumnis-trader",
+    packages = ['AlpacaLumnisTrader'],
+    version="0.1.2",
+    author="Abubakarr Jaye",
+    author_email="contact@lumnis.io",
+    description="Lumnis Alternative Data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lumnisfactors/AlpacaLumnisTrader/archive/refs/tags/0.1.2.tar.gz",
+    py_modules = ["alpaca-lumnis"],
+    install_requires=[
+        'grequests',
+        'pandas',
+        'scipy',
+        'numpy',
+        'scikit-learn',
+        'seaborn',
+        'matplotlib',
+        'alpaca-trade-api',
+        'alpaca-py',
+        'lumnisfactors',
+        'tqdm',
+        'arcticdb'
+],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
