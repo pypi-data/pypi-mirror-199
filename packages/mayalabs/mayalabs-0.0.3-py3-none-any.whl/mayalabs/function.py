112bd4fa7bbca3d1449cd14a4bb513ec5d56a51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:44 +0000

    Add package mayalabs/0.0.3/mayalabs-0.0.3-py3-none-any.whl

---
+++ packages/mayalabs/mayalabs-0.0.3-py3-none-any.whl/mayalabs/function.py
@@ -0,0 +1,110 @@
+from mayalabs import Session
+from mayalabs import Worker, WorkerClient, SessionClient
+from typing import Any, Dict
+from .mayalabs import authenticate
+import asyncio
+
+class Function:
+    # @authenticate
+    def __init__(self, name, script = None, init = True):
+        self.name : str = name
+        self.script : str = script
+        self.worker : Worker = None
+        self.session : Session = None
+        if init:
+            self.init()
+
+    @staticmethod
+    def create(name, script):
+        func = Function(
+            name=name,
+            script=script,
+            init=False
+        )
+
+        worker = None
+        try:
+            worker = Worker.create(name=name, alias=name)
+        except:
+            raise Exception('Failed to create a worker for the function')
+
+        session = None
+        try:
+            session = Session.new(script=script)
+            worker.attach_session(session_id=session.id)
+        except:
+            raise Exception('Failed to create a new session for the worker')
+        
+        func.worker = worker
+        func.session = session
+
+        return func
+
+    def init(self, api_key=None):
+        """
+        Initializes the function.
+        Checks if the worker exists. If not, creates a new worker.
+        Pass an api_key to override default and use a different profile.
+        """
+        if self.name is None:
+            raise Exception("No name provided in the argument `name`")
+
+        try:
+            # check if the worker exists    
+            if api_key is None:
+                self.worker = WorkerClient().get_worker_by_alias(self.name)
+            else:
+                self.worker = WorkerClient().get_worker_by_alias(self.name, api_key=api_key)
+
+            if self.worker:
+                if self.worker.session_id:
+                    try:
+                        self.session = Session.get(session_id=self.worker.session_id)
+                        self.script = self.session.script
+                    except Exception as e:
+                        print("Session not found. Error:", e)
+                        raise e
+        except Exception as err:
+            print("Worker not found.")
+            raise err
+
+    def deploy(self):
+        """
+        Deploys the function serverlessly.
+        Creates a new session from script and deploys session to a worker.
+        """
+        if self.script is None:
+            raise Exception("No script provided")
+
+        if self.session is None:
+            self.session = Session.new(script=self.script)
+
+        if self.worker is None:
+            self.worker = Worker.new(name=self.name, alias=self.name)
+
+        self.session.deploy(worker_id=self.worker.id)
+
+    def call(self, **kwargs : Dict) -> Dict:
+        """
+        Call the deployed function. 
+        Arguments passed here are passed to the `msg` object in the script.
+        A value like `msg['key']` can be accessed in the script by using {{key}}
+        """
+        return self.worker.call(msg = kwargs)
+    
+    def __call__(self, **kwds: Dict) -> Dict:
+        """
+        Call the deployed function. 
+        Arguments passed here are passed to the `msg` object in the script.
+        A value like `msg['key']` can be accessed in the script by using {{key}}
+        """
+        return self.call(**kwds)
+    
+    def clear(self):
+        """
+        Deletes the function by deleteing the associated session and the worker.
+        """
+        if self.session is not None:
+            self.session.delete()
+        if self.worker is not None:
+            self.worker.delete()
---
+++ packages/mayalabs/mayalabs-0.0.3-py3-none-any.whl/mayalabs/utils/pac_engine.py
@@ -0,0 +1,104 @@
+import uuid
+import asyncio
+import websockets
+import json
+from typing import Dict, Any
+import os
+from ..mayalabs import authenticate
+from ..consts import api_base_url, api_ws_url
+from colorama import Fore, Style
+
+
+def get_message(pac_message):
+    pass
+    try:
+        if pac_message['status'] and pac_message['status'] == 'complete':
+            return {
+                'status': 'success',
+                'message': 'Generation successful'
+            }
+        elif 'error' in pac_message and pac_message['error'] == True:
+            msg = pac_message['msg']
+            if not msg:
+                msg = 'Something went wrong'
+            return {
+                'status': 'error',
+                'message': msg
+            }
+        else:
+            return {
+                'status': 'progress',
+                'message': 'Step generated'
+            }
+    except:
+        return {
+            'status': 'error',
+            'message': 'Something went wrong'
+        }
+
+
+class PacTask:
+    @authenticate
+    def __init__(self, type: str, opts: Dict[str, Any], api_key=None):
+        self.type = type
+        self.opts = opts
+        self.api_key = api_key
+        self.done_future = asyncio.Future()
+
+    async def execute(self):
+        connection_id = uuid.uuid4()
+        url = f"{api_ws_url}?connId={connection_id}&apiKey={self.api_key}"
+        
+        async with websockets.connect(url) as socket:
+            task_id = uuid.uuid4()
+
+            message = {
+                "task_id": str(task_id),
+                "type": self.type,
+                "data": self.opts,
+            }
+            await socket.send(json.dumps(message))
+
+            async for message in socket:
+                msg_object = json.loads(json.loads(message))
+                if msg_object["task_id"] == str(task_id):
+                    data = msg_object["data"]
+                    if isinstance(data, str):
+                        data = json.loads(data)
+
+                    msg = get_message(data)
+
+                    if msg['status'] == 'error':
+                        print('[Maya]', Fore.RED + 'There was an error during program generation: ' + msg['message'] + Style.RESET_ALL)
+                        raise Exception('Error occured during generation: ' + msg['message'])
+                    elif msg['status'] == 'success':
+                        break
+                    else:
+                        print('[Maya]', Fore.CYAN + 'Step generated' + Style.RESET_ALL)
+                        # print(data)
+                    
+                    # self.done_future.set_result(data)
+
+    async def done(self):
+        await self.done_future
+
+
+class GenerateTask(PacTask):
+    def __init__(self, session_id: str):
+        super().__init__(
+            type="GENERATE",
+            opts={"session_id": session_id},
+        )
+
+
+class InstructTask(PacTask):
+    def __init__(self, session_id: str, instruction: str, from_scratch: bool):
+        super().__init__(
+            type="INSTRUCT",
+            opts={
+                "session_id": session_id,
+                "instruction": instruction,
+                "from_scratch": from_scratch,
+            },
+        )
+
