834f6f4ec78a300e3922b1059d4c227f94f04387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:21 +0000

    Add package safe-ds/0.4.0/safe_ds-0.4.0-py3-none-any.whl

---
+++ packages/safe-ds/safe_ds-0.4.0-py3-none-any.whl/safeds/ml/regression/_decision_tree.py
@@ -0,0 +1,56 @@
+# noinspection PyProtectedMember
+from safeds.data.tabular.containers import Table, TaggedTable
+from safeds.ml._util_sklearn import fit, predict
+from sklearn.tree import DecisionTreeRegressor as sk_DecisionTreeRegressor
+
+from ._regressor import Regressor
+
+
+# noinspection PyProtectedMember
+class DecisionTree(Regressor):
+    """
+    This class implements Decision Tree regression. It is used as a regression model.
+    It can only be trained on a tagged table.
+    """
+
+    def __init__(self) -> None:
+        self._wrapped_regressor = sk_DecisionTreeRegressor()
+        self._target_name = ""
+
+    def fit(self, training_set: TaggedTable) -> None:
+        """
+        Fit this model given a tagged table.
+
+        Parameters
+        ----------
+        training_set : TaggedTable
+            The tagged table containing the feature and target vectors.
+
+        Raises
+        ------
+        LearningError
+            If the tagged table contains invalid values or if the training failed.
+        """
+        fit(self._wrapped_regressor, training_set)
+        self._target_name = training_set.target.name
+
+    def predict(self, dataset: Table) -> TaggedTable:
+        """
+        Predict a target vector using a dataset containing feature vectors. The model has to be trained first.
+
+        Parameters
+        ----------
+        dataset : Table
+            The dataset containing the feature vectors.
+
+        Returns
+        -------
+        table : TaggedTable
+            A dataset containing the given feature vectors and the predicted target vector.
+
+        Raises
+        ------
+        PredictionError
+            If prediction with the given dataset failed.
+        """
+        return predict(self._wrapped_regressor, dataset, self._target_name)
---
+++ packages/safe-ds/safe_ds-0.4.0-py3-none-any.whl/safeds/ml/regression/_k_nearest_neighbors.py
@@ -0,0 +1,60 @@
+# noinspection PyProtectedMember
+from safeds.data.tabular.containers import Table, TaggedTable
+from safeds.ml._util_sklearn import fit, predict
+from sklearn.neighbors import KNeighborsRegressor as sk_KNeighborsRegressor
+
+from ._regressor import Regressor
+
+
+# noinspection PyProtectedMember
+class KNearestNeighbors(Regressor):
+    """
+    This class implements K-nearest-neighbors regressor. It can only be trained on a tagged table.
+
+    Parameters
+    ----------
+    n_neighbors : int
+        The number of neighbors to be interpolated with. Has to be less than or equal than the sample size.
+    """
+
+    def __init__(self, n_neighbors: int) -> None:
+        self._wrapped_regressor = sk_KNeighborsRegressor(n_neighbors)
+        self._target_name = ""
+
+    def fit(self, training_set: TaggedTable) -> None:
+        """
+        Fit this model given a tagged table.
+
+        Parameters
+        ----------
+        training_set : TaggedTable
+            The tagged table containing the feature and target vectors.
+
+        Raises
+        ------
+        LearningError
+            If the tagged table contains invalid values or if the training failed.
+        """
+        fit(self._wrapped_regressor, training_set)
+        self._target_name = training_set.target.name
+
+    def predict(self, dataset: Table) -> TaggedTable:
+        """
+        Predict a target vector using a dataset containing feature vectors. The model has to be trained first.
+
+        Parameters
+        ----------
+        dataset : Table
+            The dataset containing the feature vectors.
+
+        Returns
+        -------
+        table : TaggedTable
+            A dataset containing the given feature vectors and the predicted target vector.
+
+        Raises
+        ------
+        PredictionError
+            If prediction with the given dataset failed.
+        """
+        return predict(self._wrapped_regressor, dataset, self._target_name)
