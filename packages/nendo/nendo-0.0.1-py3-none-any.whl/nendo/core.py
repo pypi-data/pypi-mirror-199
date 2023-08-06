c4f0d914a311b017b444eeb3dc158cddb6495995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:03 +0000

    Add package nendo/0.0.1/nendo-0.0.1-py3-none-any.whl

---
+++ packages/nendo/nendo-0.0.1-py3-none-any.whl/nendo/core.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''Main class of the nendo framework.'''
+
+class Nendo:
+    def __init__(self,
+                 database = "localhost:2323"):
+        self.database = database
+
+    def load_plugin():
+        pass
+    
+    def set_backend():
+        pass
