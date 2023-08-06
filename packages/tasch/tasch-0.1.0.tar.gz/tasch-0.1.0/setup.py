dc5b73176776f5f5fedba5a24ed6feb8a2e35266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:32 +0000

    Add package tasch/0.1.0/tasch-0.1.0.tar.gz

---
+++ packages/tasch/tasch-0.1.0.tar.gz/tasch-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tasch']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'tasch',
+    'version': '0.1.0',
+    'description': 'An advanced calculator',
+    'long_description': None,
+    'author': 'jscz',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
