3a3e828d448454b7623699cfb61da2ed0d5b5d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:05 +0000

    Add package splyne/0.0.1/splyne-0.0.1.tar.gz

---
+++ packages/splyne/splyne-0.0.1.tar.gz/splyne-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='splyne',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/80sVectorz/splyne',
+    author='80sVectorz',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
