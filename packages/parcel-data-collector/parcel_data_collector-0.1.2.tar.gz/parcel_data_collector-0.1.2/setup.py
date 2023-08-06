7066ed43d01ff53720472a674600f1738d865391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:50 +0000

    Add package parcel-data-collector/0.1.2/parcel_data_collector-0.1.2.tar.gz

---
+++ packages/parcel-data-collector/parcel_data_collector-0.1.2.tar.gz/parcel_data_collector-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="parcel_data_collector",
+    version="0.1.2",
+    author="Philip Diegel",
+    author_email="philipdiegel@gmail.com",
+    description="A package for collecting and analyzing data",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pdiegel/Parcel-Data-Collector",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.11",
+    install_requires=open("requirements.txt").readlines(),
+)
