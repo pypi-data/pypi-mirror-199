e29872b284b16e3422cffac480f56d53d9d1c18f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:31 +0000

    Add package evalml/0.72.0/evalml-0.72.0-py3-none-any.whl

---
+++ packages/evalml/evalml-0.72.0-py3-none-any.whl/evalml/pipelines/components/transformers/imputers/simple_imputer.py
@@ -0,0 +1,170 @@
+"""Component that imputes missing data according to a specified imputation strategy."""
+import pandas as pd
+import woodwork
+from sklearn.impute import SimpleImputer as SkImputer
+from woodwork.logical_types import Double
+
+from evalml.pipelines.components.transformers import Transformer
+from evalml.pipelines.components.utils import (
+    drop_natural_language_columns,
+    set_boolean_columns_to_integer,
+)
+from evalml.utils import infer_feature_types
+from evalml.utils.gen_utils import is_categorical_actually_boolean
+
+
+class SimpleImputer(Transformer):
+    """Imputes missing data according to a specified imputation strategy.  Natural language columns are ignored.
+
+    Args:
+        impute_strategy (string): Impute strategy to use. Valid values include "mean", "median", "most_frequent", "constant" for
+           numerical data, and "most_frequent", "constant" for object data types.
+        fill_value (string): When impute_strategy == "constant", fill_value is used to replace missing data.
+           Defaults to 0 when imputing numerical data and "missing_value" for strings or object data types.
+        random_seed (int): Seed for the random number generator. Defaults to 0.
+
+    """
+
+    name = "Simple Imputer"
+    hyperparameter_ranges = {"impute_strategy": ["mean", "median", "most_frequent"]}
+    """{
+        "impute_strategy": ["mean", "median", "most_frequent"]
+    }"""
+
+    def __init__(
+        self, impute_strategy="most_frequent", fill_value=None, random_seed=0, **kwargs
+    ):
+        parameters = {"impute_strategy": impute_strategy, "fill_value": fill_value}
+        parameters.update(kwargs)
+        self.impute_strategy = impute_strategy
+        imputer = SkImputer(
+            strategy=impute_strategy,
+            fill_value=fill_value,
+            missing_values=pd.NA,
+            **kwargs,
+        )
+        self._all_null_cols = None
+        super().__init__(
+            parameters=parameters,
+            component_obj=imputer,
+            random_seed=random_seed,
+        )
+
+    def fit(self, X, y=None):
+        """Fits imputer to data. 'None' values are converted to np.nan before imputation and are treated as the same.
+
+        Args:
+            X (pd.DataFrame or np.ndarray): the input training data of shape [n_samples, n_features]
+            y (pd.Series, optional): the target training data of length [n_samples]
+
+        Returns:
+            self
+
+        Raises:
+            ValueError: if the SimpleImputer receives a dataframe with both Boolean and Categorical data.
+
+        """
+        X = infer_feature_types(X)
+
+        if set([lt.type_string for lt in X.ww.logical_types.values()]) == {
+            "boolean",
+            "categorical",
+        } and not all(
+            [
+                is_categorical_actually_boolean(X, col)
+                for col in X.ww.select("Categorical")
+            ],
+        ):
+            raise ValueError(
+                "SimpleImputer cannot handle dataframes with both boolean and categorical features.  Use Imputer instead.",
+            )
+
+        nan_ratio = X.isna().sum() / X.shape[0]
+        self._all_null_cols = nan_ratio[nan_ratio == 1].index.tolist()
+
+        X, _ = drop_natural_language_columns(X)
+
+        # Convert any boolean columns to IntegerNullable, but keep track of the columns so they can be converted back
+        self._boolean_cols = list(
+            X.ww.select(
+                include=["Boolean", "BooleanNullable"],
+                return_schema=True,
+            ).columns,
+        )
+        # Make sure we're tracking Categorical columns that should be boolean as well
+        self._boolean_cols.extend(
+            [
+                col
+                for col in X.ww.select("Categorical")
+                if is_categorical_actually_boolean(X, col)
+            ],
+        )
+        X = set_boolean_columns_to_integer(X)
+
+        # If the Dataframe only had natural language columns, do nothing.
+        if X.shape[1] == 0:
+            return self
+        self._component_obj.fit(X, y)
+        return self
+
+    def transform(self, X, y=None):
+        """Transforms input by imputing missing values. 'None' and np.nan values are treated as the same.
+
+        Args:
+            X (pd.DataFrame): Data to transform.
+            y (pd.Series, optional): Ignored.
+
+        Returns:
+            pd.DataFrame: Transformed X
+        """
+        X = infer_feature_types(X)
+        original_schema = X.ww.schema
+        X = set_boolean_columns_to_integer(X)
+
+        not_all_null_cols = [col for col in X.columns if col not in self._all_null_cols]
+        original_index = X.index
+
+        # Drop natural language columns and transform the other columns
+        X_t, natural_language_cols = drop_natural_language_columns(X)
+        if X_t.shape[1] == 0:
+            return X
+        not_all_null_or_natural_language_cols = [
+            col for col in not_all_null_cols if col not in natural_language_cols
+        ]
+
+        X_t = self._component_obj.transform(X_t)
+        X_t = pd.DataFrame(X_t, columns=not_all_null_or_natural_language_cols)
+
+        new_schema = original_schema.get_subset_schema(X_t.columns)
+
+        # Iterate through previously saved boolean columns and convert them back to boolean
+        for col in self._boolean_cols:
+            X_t[col] = X_t[col].astype(bool)
+
+        # Convert Nullable Integers to Doubles for the "mean" and "median" strategies
+        if self.impute_strategy in ["mean", "median"]:
+            nullable_int_cols = X.ww.select(["IntegerNullable"], return_schema=True)
+            nullable_int_cols = [x for x in nullable_int_cols.columns.keys()]
+            for col in nullable_int_cols:
+                new_schema.set_types({col: Double})
+        X_t.ww.init(schema=new_schema)
+
+        # Add back in natural language columns, unchanged
+        if len(natural_language_cols) > 0:
+            X_t = woodwork.concat_columns([X_t, X[natural_language_cols]])
+
+        if not_all_null_or_natural_language_cols:
+            X_t.index = original_index
+        return X_t
+
+    def fit_transform(self, X, y=None):
+        """Fits on X and transforms X.
+
+        Args:
+            X (pd.DataFrame): Data to fit and transform
+            y (pd.Series, optional): Target data.
+
+        Returns:
+            pd.DataFrame: Transformed X
+        """
+        return self.fit(X, y).transform(X, y)
---
+++ packages/evalml/evalml-0.72.0-py3-none-any.whl/evalml/pipelines/components/transformers/imputers/time_series_imputer.py
@@ -0,0 +1,225 @@
+"""Component that imputes missing data according to a specified timeseries-specific imputation strategy."""
+import pandas as pd
+import woodwork as ww
+from woodwork.logical_types import BooleanNullable, Double
+
+from evalml.pipelines.components.transformers import Transformer
+from evalml.utils import infer_feature_types
+
+
+class TimeSeriesImputer(Transformer):
+    """Imputes missing data according to a specified timeseries-specific imputation strategy.
+
+    This Transformer should be used after the `TimeSeriesRegularizer` in order to impute the missing values that were
+    added to X and y (if passed).
+
+    Args:
+        categorical_impute_strategy (string): Impute strategy to use for string, object, boolean, categorical dtypes.
+            Valid values include "backwards_fill" and "forwards_fill". Defaults to "forwards_fill".
+        numeric_impute_strategy (string): Impute strategy to use for numeric columns. Valid values include
+            "backwards_fill", "forwards_fill", and "interpolate". Defaults to "interpolate".
+        target_impute_strategy (string): Impute strategy to use for the target column. Valid values include
+            "backwards_fill", "forwards_fill", and "interpolate". Defaults to "forwards_fill".
+        random_seed (int): Seed for the random number generator. Defaults to 0.
+
+    Raises:
+        ValueError: If categorical_impute_strategy, numeric_impute_strategy, or target_impute_strategy is not one of the valid values.
+    """
+
+    modifies_features = True
+    modifies_target = True
+    training_only = True
+
+    name = "Time Series Imputer"
+    hyperparameter_ranges = {
+        "categorical_impute_strategy": ["backwards_fill", "forwards_fill"],
+        "numeric_impute_strategy": ["backwards_fill", "forwards_fill", "interpolate"],
+        "target_impute_strategy": ["backwards_fill", "forwards_fill", "interpolate"],
+    }
+    """{
+        "categorical_impute_strategy": ["backwards_fill", "forwards_fill"],
+        "numeric_impute_strategy": ["backwards_fill", "forwards_fill", "interpolate"],
+        "target_impute_strategy": ["backwards_fill", "forwards_fill", "interpolate"],
+    }"""
+    _valid_categorical_impute_strategies = set(["backwards_fill", "forwards_fill"])
+    _valid_numeric_impute_strategies = set(
+        ["backwards_fill", "forwards_fill", "interpolate"],
+    )
+    _valid_target_impute_strategies = set(
+        ["backwards_fill", "forwards_fill", "interpolate"],
+    )
+
+    # Incompatibility: https://github.com/alteryx/evalml/issues/4001
+    # TODO: Remove when support is added https://github.com/alteryx/evalml/issues/4014
+    _integer_nullable_incompatibilities = ["X", "y"]
+    _boolean_nullable_incompatibilities = ["X", "y"]
+
+    def __init__(
+        self,
+        categorical_impute_strategy="forwards_fill",
+        numeric_impute_strategy="interpolate",
+        target_impute_strategy="forwards_fill",
+        random_seed=0,
+        **kwargs,
+    ):
+        if categorical_impute_strategy not in self._valid_categorical_impute_strategies:
+            raise ValueError(
+                f"{categorical_impute_strategy} is an invalid parameter. Valid categorical impute strategies are {', '.join(self._valid_numeric_impute_strategies)}",
+            )
+        elif numeric_impute_strategy not in self._valid_numeric_impute_strategies:
+            raise ValueError(
+                f"{numeric_impute_strategy} is an invalid parameter. Valid numeric impute strategies are {', '.join(self._valid_numeric_impute_strategies)}",
+            )
+        elif target_impute_strategy not in self._valid_target_impute_strategies:
+            raise ValueError(
+                f"{target_impute_strategy} is an invalid parameter. Valid target column impute strategies are {', '.join(self._valid_target_impute_strategies)}",
+            )
+
+        parameters = {
+            "categorical_impute_strategy": categorical_impute_strategy,
+            "numeric_impute_strategy": numeric_impute_strategy,
+            "target_impute_strategy": target_impute_strategy,
+        }
+        parameters.update(kwargs)
+        self._all_null_cols = None
+        self._forwards_cols = None
+        self._backwards_cols = None
+        self._interpolate_cols = None
+        self._impute_target = None
+        super().__init__(
+            parameters=parameters,
+            component_obj=None,
+            random_seed=random_seed,
+        )
+
+    def fit(self, X, y=None):
+        """Fits imputer to data.
+
+        'None' values are converted to np.nan before imputation and are treated as the same.
+        If a value is missing at the beginning or end of a column, that value will be imputed using
+        backwards fill or forwards fill as necessary, respectively.
+
+        Args:
+            X (pd.DataFrame, np.ndarray): The input training data of shape [n_samples, n_features]
+            y (pd.Series, optional): The target training data of length [n_samples]
+
+        Returns:
+            self
+        """
+        X = infer_feature_types(X)
+
+        nan_ratio = X.ww.describe().loc["nan_count"] / X.shape[0]
+        self._all_null_cols = nan_ratio[nan_ratio == 1].index.tolist()
+
+        def _filter_cols(impute_strat, X):
+            """Function to return which columns of the dataset to impute given the impute strategy."""
+            cols = []
+            if self.parameters["categorical_impute_strategy"] == impute_strat:
+                if self.parameters["numeric_impute_strategy"] == impute_strat:
+                    cols = list(X.columns)
+                else:
+                    cols = list(X.ww.select(exclude=["numeric"]).columns)
+            elif self.parameters["numeric_impute_strategy"] == impute_strat:
+                cols = list(X.ww.select(include=["numeric"]).columns)
+
+            X_cols = [col for col in cols if col not in self._all_null_cols]
+            if len(X_cols) > 0:
+                return X_cols
+
+        self._forwards_cols = _filter_cols("forwards_fill", X)
+        self._backwards_cols = _filter_cols("backwards_fill", X)
+        self._interpolate_cols = _filter_cols("interpolate", X)
+
+        if y is not None:
+            y = infer_feature_types(y)
+            if y.isnull().any():
+                self._impute_target = self.parameters["target_impute_strategy"]
+
+        return self
+
+    def transform(self, X, y=None):
+        """Transforms data X by imputing missing values using specified timeseries-specific strategies. 'None' values are converted to np.nan before imputation and are treated as the same.
+
+        Args:
+            X (pd.DataFrame): Data to transform.
+            y (pd.Series, optional): Optionally, target data to transform.
+
+        Returns:
+            pd.DataFrame: Transformed X and y
+        """
+        if len(self._all_null_cols) == X.shape[1]:
+            df = pd.DataFrame(index=X.index)
+            df.ww.init()
+            return df, y
+        X = infer_feature_types(X)
+        if y is not None:
+            y = infer_feature_types(y)
+
+        X_not_all_null = X.ww.drop(self._all_null_cols)
+        X_schema = X_not_all_null.ww.schema
+        X_schema = X_schema.get_subset_schema(
+            subset_cols=X_schema._filter_cols(
+                exclude=["IntegerNullable", "BooleanNullable", "AgeNullable"],
+            ),
+        )
+
+        if self._forwards_cols is not None:
+            X_forward = X.ww[self._forwards_cols]
+            imputed = X_forward.pad()
+            imputed.bfill(inplace=True)  # Fill in the first value, if missing
+            X_not_all_null[X_forward.columns] = imputed
+
+        if self._backwards_cols is not None:
+            X_backward = X.ww[self._backwards_cols]
+            imputed = X_backward.bfill()
+            imputed.pad(inplace=True)  # Fill in the last value, if missing
+            X_not_all_null[X_backward.columns] = imputed
+
+        if self._interpolate_cols is not None:
+            X_interpolate = X.ww[self._interpolate_cols]
+            # TODO: Revert when pandas introduces Float64 dtype
+            imputed = X_interpolate.astype(
+                float,
+            ).interpolate()  # Cast to float because Int64 not handled
+            imputed.bfill(inplace=True)  # Fill in the first value, if missing
+            X_not_all_null[X_interpolate.columns] = imputed
+        X_not_all_null.ww.init(schema=X_schema)
+
+        y_imputed = pd.Series(y)
+        if y is not None and len(y) > 0:
+            if self._impute_target == "forwards_fill":
+                y_imputed = y.pad()
+                y_imputed.bfill(inplace=True)
+            elif self._impute_target == "backwards_fill":
+                y_imputed = y.bfill()
+                y_imputed.pad(inplace=True)
+            elif self._impute_target == "interpolate":
+                # TODO: Revert when pandas introduces Float64 dtype
+                y_imputed = y.astype(float).interpolate()
+                y_imputed.bfill(inplace=True)
+            y_imputed = ww.init_series(y_imputed)
+
+        return X_not_all_null, y_imputed
+
+    def _handle_nullable_types(self, X=None, y=None):
+        """Transforms X and y to remove any incompatible nullable types for the time series imputer when the interpolate method is used.
+
+        Args:
+            X (pd.DataFrame, optional): Input data to a component of shape [n_samples, n_features].
+                May contain nullable types.
+            y (pd.Series, optional): The target of length [n_samples]. May contain nullable types.
+
+        Returns:
+            X, y with any incompatible nullable types downcasted to compatible equivalents when interpolate is used. Is NoOp otherwise.
+        """
+        if self._impute_target == "interpolate":
+            # For BooleanNullable, we have to avoid Categorical columns
+            # since the category dtype also has incompatibilities with linear interpolate, which is expected
+            if isinstance(y.ww.logical_type, BooleanNullable):
+                y = ww.init_series(y, Double)
+            else:
+                _, y = super()._handle_nullable_types(None, y)
+        if self._interpolate_cols is not None:
+            X, _ = super()._handle_nullable_types(X, None)
+
+        return X, y
