93c3b3868355a81ed671c9b24ebe70fe5e6c6da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:53 +0000

    Add package mage-ai/0.8.30/mage_ai-0.8.30-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.8.30-py3-none-any.whl/mage_ai/data_integrations/destinations/constants.py
@@ -0,0 +1,14 @@
+DESTINATIONS = [
+    dict(name='BigQuery'),
+    dict(name='Delta Lake S3'),
+    dict(
+        module_name='MSSQL',
+        name='Microsoft SQL Server',
+        uuid='mssql',
+    ),
+    dict(name='MySQL'),
+    dict(name='PostgreSQL'),
+    dict(name='Redshift'),
+    dict(name='Snowflake'),
+    dict(name='Trino'),
+]
