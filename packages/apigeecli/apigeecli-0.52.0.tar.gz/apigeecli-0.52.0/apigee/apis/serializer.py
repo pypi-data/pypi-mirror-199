844ee1ed992bea708dc8a51bec34a31d32d8fb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:12 +0000

    Add package apigeecli/0.52.0/apigeecli-0.52.0.tar.gz

---
+++ packages/apigeecli/apigeecli-0.52.0.tar.gz/apigeecli-0.52.0/apigee/apis/serializer.py
@@ -0,0 +1,44 @@
+import json
+
+from apigee.utils import remove_last_items_from_list, run_func_on_iterable
+
+
+class ApisSerializer:
+    @staticmethod
+    def filter_deployed_revisions(details):
+        return list(
+            set(
+                run_func_on_iterable(
+                    details, lambda d: d["revision"], state_op="extend"
+                )
+            )
+        )
+
+    @staticmethod
+    def filter_deployment_details(details):
+        return run_func_on_iterable(
+            details["environment"],
+            lambda d: {
+                "name": d["name"],
+                "revision": [revision["name"] for revision in d["revision"]],
+            },
+        )
+
+    @staticmethod
+    def filter_undeployed_revisions(revisions, deployed, save_last=0):
+        undeployed = [int(rev) for rev in revisions if rev not in set(deployed)]
+        return remove_last_items_from_list(sorted(undeployed), save_last)
+
+    @staticmethod
+    def serialize_details(apis, format, prefix=None):
+        resp = apis
+        if format == "text":
+            return apis.text
+        apis = apis.json()
+        if prefix:
+            apis = [api for api in apis if api.startswith(prefix)]
+        if format == "dict":
+            return apis
+        elif format == "json":
+            return json.dumps(apis)
+        return resp
---
+++ packages/apigeecli/apigeecli-0.52.0.tar.gz/apigeecli-0.52.0/setup.py
@@ -0,0 +1,52 @@
+import os
+
+from setuptools import find_packages, setup
+
+from apigee import APP
+from apigee import __version__ as version
+from apigee import description
+
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name=APP,
+    version=version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/darumatic/apigee-cli",
+    author="Matthew Delotavo",
+    author_email="matthew.t.delotavo@gmail.com",
+    license="Apache license 2.0",
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+    ],
+    entry_points={"console_scripts": ["apigee=apigee.__main__:main"]},
+    install_requires=[
+        "click",
+        "click-aliases",
+        "click-option-group",
+        "colorama",
+        "coverage",
+        "gitpython",
+        "pudb",
+        "pyjwt",
+        "pyotp",
+        "python-gnupg>=0.3.5",
+        "requests",
+        "tabulate",
+        "tqdm",
+    ],
+    project_urls={"Documentation": "https://darumatic.github.io/apigee-cli/index.html"},
+    python_requires=">=3.8",
+    packages=find_packages(),
+)
