117fb5dcb6853283c38e79524c2b51fbfde9e5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:13 +0000

    Add package datachain/0.1.0/datachain-0.1.0.tar.gz

---
+++ packages/datachain/datachain-0.1.0.tar.gz/datachain-0.1.0/datachain/__init__.py
@@ -0,0 +1,8 @@
+"""@Author: Rayane AMROUCHE
+"""
+
+from datachain.core.common import Data
+from datachain.core.common import Pipe
+from datachain.core.common import DataCache
+from datachain.core.common import DataSource
+from datachain.core.common import DataManager
