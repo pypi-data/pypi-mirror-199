cd4ed74ab70e684290095f21b87cfae80a24ee39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:44:52 +0000

    Add package pyrtout/1.0.1/pyrtout-1.0.1.tar.gz

---
+++ packages/pyrtout/pyrtout-1.0.1.tar.gz/pyrtout-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.1"
+DESCRIPTION = "Timeout & retry functions in Python with a single line of code"
+with open("README.md", encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="pyrtout",
+    version=VERSION,
+    packages=find_packages("python-timeout", exclude=("tests",)),
+    author="radioactive11",
+    author_email="roy.arijit@icloud.com",
+    url="https://github.com/radioactive11/python-timeout",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords=["python", "timeout", "retry", "kill", "signal", "unix"],
+)
