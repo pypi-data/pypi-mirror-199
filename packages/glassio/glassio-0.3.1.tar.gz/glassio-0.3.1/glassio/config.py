ba136bc6c5f1e14d4b2e4b99b04406c73bdd80c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:37 +0000

    Add package glassio/0.3.1/glassio-0.3.1.tar.gz

---
+++ packages/glassio/glassio-0.3.1.tar.gz/glassio-0.3.1/glassio/config.py
@@ -0,0 +1,73 @@
+import tomllib
+from typing import Literal, Optional, Self
+from pydantic import BaseModel, Field
+from datetime import timedelta
+
+_DEFAULT_TIMEOUT = timedelta(seconds=1, milliseconds=100)
+_DEFAULT_COOKIE_TTL = timedelta(days=90)
+
+
+class SessionConfig(BaseModel):
+    cookie: str = "_gassio_session_id"
+    ttl: timedelta = _DEFAULT_COOKIE_TTL
+
+
+class DatabaseConfig(BaseModel):
+    host: str = "127.0.0.1"
+    port: int = Field(3000, gt=0, lt=65536)
+    dbname: str = "glassiodb"
+    timeout: timedelta = _DEFAULT_TIMEOUT
+
+    @property
+    def uri(self) -> str:
+        return f"mongodb://{self.host}:{self.port}/{self.dbname}"
+
+
+class LogConfig(BaseModel):
+    level: Literal["critical", "error", "warning", "info", "debug"] = "debug"
+    filename: Optional[str] = None
+    stdout: bool = True
+
+
+class QueueConfig(BaseModel):
+    type: Literal["mongo"] = "mongo"
+
+
+class MongoQueueConfig(BaseModel):
+    tasks_collection: str = "glassio_tasks"
+    keep_done_tasks_for: timedelta = timedelta(days=30)
+    cleanup_interval: timedelta = timedelta(hours=1)
+
+
+class OAuthConfig(BaseModel):
+    id: str
+    secret: str
+    authorize_url: str
+
+
+class WebConfig(BaseModel):
+    host: str = "127.0.0.1"
+    port: int = Field(3000, gt=0, lt=65536)
+
+
+class GeneralConfig(BaseModel):
+    documents_per_page: int = 20
+    environment: Literal["development", "testing", "staging", "production"] = "development"
+    enable_passwords: bool = True
+
+
+class BaseConfig(BaseModel):
+    session: SessionConfig = Field(default_factory=SessionConfig)
+    database: DatabaseConfig = Field(default_factory=DatabaseConfig)
+    logging: LogConfig = Field(default_factory=LogConfig)
+    oauth: Optional[OAuthConfig] = Field(None)
+    web: WebConfig = Field(default_factory=WebConfig)
+    general: GeneralConfig = Field(default_factory=GeneralConfig)
+    queue: QueueConfig = Field(default_factory=QueueConfig)
+    mongo_queue: MongoQueueConfig = Field(default_factory=MongoQueueConfig)
+
+    @classmethod
+    def parse(cls, filename) -> Self:
+        with open(filename, "rb") as f:
+            config = tomllib.load(f)
+        return cls(**config)
---
+++ packages/glassio/glassio-0.3.1.tar.gz/glassio-0.3.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="glassio",
+    version="0.3.1",
+    description="a micro webframework based on fastapi and motor",
+    url="https://gitlab.com/viert/glassio",
+    author="Pavel Vorobyev",
+    author_email="aquavitale@yandex.ru",
+    license="MIT",
+    packages=[pkg for pkg in find_packages() if pkg.startswith("glassio")],
+    include_package_data=True,
+    package_data={"glassio": ["*.txt", "*.py", "*.tmpl"]},
+    install_requires=[
+        "fastapi[all]",
+        "uvicorn",
+        "pymongo",
+        "motor",
+        "pydantic",
+        "ipython",
+        "requests",
+        "bcrypt",
+    ],
+    entry_points={"console_scripts": ["glassio=glassio.command:main",]},
+)
