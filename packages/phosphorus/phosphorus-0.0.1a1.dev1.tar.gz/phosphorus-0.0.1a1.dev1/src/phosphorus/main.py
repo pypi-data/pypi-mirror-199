052b1df6cff28a211fa10b05436743338c9b796d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:05 +0000

    Add package phosphorus/0.0.1a1.dev1/phosphorus-0.0.1a1.dev1.tar.gz

---
+++ packages/phosphorus/phosphorus-0.0.1a1.dev1.tar.gz/phosphorus-0.0.1a1.dev1/src/phosphorus/main.py
@@ -0,0 +1,9 @@
+msg="""
+This command is still in the planning phase.
+This version will be deleted, as soon as there is a working alpha version
+Please don't pin or bookmark this release.
+"""
+
+
+def main() -> None:
+    print(msg.strip())
