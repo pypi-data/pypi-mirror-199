9894d16cef966c00e2ccdf063af338f1d44e2280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:51 +0000

    Add package oops-all-itertools/2.0.1/oops-all-itertools-2.0.1.tar.gz

---
+++ packages/oops-all-itertools/oops-all-itertools-2.0.1.tar.gz/oops-all-itertools-2.0.1/docs/conf.py
@@ -0,0 +1,35 @@
+import sys, os
+
+# add one directory up to the path so we can get the library itself
+sys.path.insert(0, os.path.abspath('..'))
+
+import oops_all_itertools
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Oops All Itertools'
+copyright = '2023, Lonnen'
+author = 'Lonnen'
+version = oops_all_itertools.__version__
+release = version
+exclude_patterns = ['_build']
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
