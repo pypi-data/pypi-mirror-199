1a374e04cf03eaf3d7d64e1ce59396af1a9e93c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:59 +0000

    Add package pygwalker/0.1.6.2a0/pygwalker-0.1.6.2a0-py3-none-any.whl

---
+++ packages/pygwalker/pygwalker-0.1.6.2a0-py3-none-any.whl/pygwalker/__init__.py
@@ -0,0 +1,19 @@
+__name__ = 'pygwalker'
+from .__version__ import __version__
+
+# from .base import *
+
+# with open(os.path.join(HERE, 'templates', 'graphic-walker.umd.js'), 'r') as f:
+#     gwalker_js = "const process={env:{NODE_ENV:'production'} };" + f.read()
+
+# def loadJs():
+#         display(HTML(f"""<head>
+#             <script>{gwalker_js}</script></head>"""))
+#             # <meta charset="UTF-8">
+#             # <meta http-equiv="X-UA-Compatible" content="IE=edge">
+#             # <meta name="viewport" content="width=device-width, initial-scale=1.0">
+# loadJs()
+
+from .gwalker import to_html, walk, GWalker
+from .utils.check_update import check_update
+check_update()
---
+++ packages/pygwalker/pygwalker-0.1.6.2a0-py3-none-any.whl/pygwalker/templates/dist/interfaces/index.d.ts
@@ -0,0 +1,6 @@
+import type { IGWProps } from '../../../graphic-walker/packages/graphic-walker/dist/App';
+export interface IAppProps extends IGWProps {
+    version?: string;
+    hashcode?: string;
+    visSpec?: string;
+}
