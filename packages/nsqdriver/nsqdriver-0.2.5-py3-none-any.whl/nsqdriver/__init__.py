60e8836d60bf7dd6633fd3c255631d3023746af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:35 +0000

    Add package nsqdriver/0.2.5/nsqdriver-0.2.5-py3-none-any.whl

---
+++ packages/nsqdriver/nsqdriver-0.2.5-py3-none-any.whl/nsqdriver/__init__.py
@@ -0,0 +1,7 @@
+from .NS_MCI import Driver as MCIDriver
+from .NS_QSYNC import Driver as QSYNCDriver
+
+version_pack = (0, 2, 5)
+
+__version__ = '.'.join(str(_) for _ in version_pack)
+__all__ = ['MCIDriver', 'QSYNCDriver', '__version__']
