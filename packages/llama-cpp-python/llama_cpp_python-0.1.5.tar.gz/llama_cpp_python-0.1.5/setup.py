2e38bc03f14fb666f87d170eeb24da1a05c6aa26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:10 +0000

    Add package llama-cpp-python/0.1.5/llama_cpp_python-0.1.5.tar.gz

---
+++ packages/llama-cpp-python/llama_cpp_python-0.1.5.tar.gz/llama_cpp_python-0.1.5/setup.py
@@ -0,0 +1,11 @@
+from skbuild import setup
+
+setup(
+    name="llama_cpp_python",
+    description="A Python wrapper for llama.cpp",
+    version="0.1.5",
+    author="Andrei Betlen",
+    author_email="abetlen@gmail.com",
+    license="MIT",
+    packages=["llama_cpp"],
+)
