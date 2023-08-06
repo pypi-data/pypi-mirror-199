21b2970702b10b0a41c42af5d622cc5120d5cdf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:36 +0000

    Add package PyMieSim/0.10.3/PyMieSim-0.10.3-cp310-cp310-macosx_10_13_x86_64.whl

---
+++ packages/PyMieSim/PyMieSim-0.10.3-cp310-cp310-macosx_10_13_x86_64.whl/PyMieSim/examples/detector/plot_LP02.py
@@ -0,0 +1,24 @@
+"""
+
+LP02 Mode detector
+==================
+
+"""
+
+from PyMieSim.detector import LPmode
+
+detector = LPmode(
+    mode_number="3-1",
+    rotation=0.,
+    sampling=700,
+    NA=0.6,
+    gamma_offset=0,
+    phi_offset=40,
+    coupling_mode='Point'
+)
+
+figure = detector.plot()
+
+figure.show(save_directory='LP31.png', window_size=(1200, 600))
+
+# -
