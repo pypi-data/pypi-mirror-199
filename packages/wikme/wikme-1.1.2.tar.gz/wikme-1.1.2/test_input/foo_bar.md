f98ae5988491bfe8ecf787ea65e4e18732e14d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:54 +0000

    Add package wikme/1.1.2/wikme-1.1.2.tar.gz

---
+++ packages/wikme/wikme-1.1.2.tar.gz/wikme-1.1.2/test_input/foo_bar.md
@@ -0,0 +1,1 @@
+This foo loves to [[Bar Baz]], [[bar baz]], [[bar baz|and also to bar baz]]!
---
+++ packages/wikme/wikme-1.1.2.tar.gz/wikme-1.1.2/wikme/__init__.py
@@ -0,0 +1,6 @@
+"""
+Please refer to the documentation provided in the README.md,
+which can be found at wikme's PyPI URL: https://pypi.org/project/wikme/
+"""
+
+__version__ = "1.1.2"
