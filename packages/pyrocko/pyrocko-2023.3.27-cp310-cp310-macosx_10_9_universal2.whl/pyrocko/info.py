d9ad8fb4446dea7928c740597cb99e1fc79b76b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:00 +0000

    Add package pyrocko/2023.3.27/pyrocko-2023.3.27-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/pyrocko/pyrocko-2023.3.27-cp310-cp310-macosx_10_9_universal2.whl/pyrocko/info.py
@@ -0,0 +1,6 @@
+# This module is automatically created from setup.py
+git_sha1 = '6745f3907740b16584fd749f8187264804375396'
+local_modifications = False
+version = '2023.03.27'
+long_version = 'pyrocko-2023.03.27-6745f3907740b16584fd749f8187264804375396-2023-03-28_13:03:05'  # noqa
+installed_date = '2023-03-28_13:03:05'
