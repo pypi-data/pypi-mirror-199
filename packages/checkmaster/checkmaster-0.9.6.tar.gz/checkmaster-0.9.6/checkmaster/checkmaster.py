ae7f3ace6655b7fc677f21a3cc8caf3ad507ef6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:30 +0000

    Add package checkmaster/0.9.6/checkmaster-0.9.6.tar.gz

---
+++ packages/checkmaster/checkmaster-0.9.6.tar.gz/checkmaster-0.9.6/checkmaster/checkmaster.py
@@ -0,0 +1,62 @@
+import importlib
+import logging
+
+from copy import deepcopy
+
+logger = logging.getLogger(__name__)
+
+
+class CheckMaster:
+    def __init__(self, conf, log_style="label", tags=None, remediations=True):
+        self.conf = conf
+        self.result = deepcopy(conf)
+        self.tags = tags
+        self.log_style = log_style
+        self.remediations = remediations
+
+    def load_plugin(self, plugin_name):
+        n1, _, n2 = plugin_name.rpartition(".")
+        module = importlib.import_module(n1)
+        func = getattr(module, n2)
+        return func
+
+    def _check(self, func_name, func, rule, tags):
+        if self.tags and rule.get("tag") not in tags:
+            return
+
+        _check = func(**rule)
+        if self.log_style == "label":
+            msg = f"{rule.get('label') or rule}"
+        else:
+            msg = f"{func_name} {rule}"
+        if not _check:
+            _level = rule.get("level", "error")
+            getattr(logger, _level)(msg)
+            if self.remediations:
+                _rem = rule.get("remediation") or "no remediation message"
+                print(f"  {chr(746)} {_rem}")
+            if _level == "warning":
+                return _level
+            else:
+                return "false"
+        else:
+            logger.info(msg)
+            return "true"
+
+    def start(self):
+        statuses = {}
+        for _func, rules in self.result.items():
+            func = self.load_plugin(_func)
+            if isinstance(rules, dict):
+                _stat = self._check(_func, func, rules, self.tags)
+
+            elif isinstance(rules, list):
+                for rule in rules:
+                    _stat = self._check(_func, func, rule, self.tags)
+            else:
+                raise NotImplementedError(_func)
+            statuses[_func] = _stat
+        if "false" in statuses.values():
+            return False
+        else:
+            return True
---
+++ packages/checkmaster/checkmaster-0.9.6.tar.gz/checkmaster-0.9.6/checkmaster/sockets.py
@@ -0,0 +1,51 @@
+import logging
+import psutil
+import socket
+
+LOCAL_ADDRESS = ["127.0.0.1", "0.0.0.0", "::"]
+logger = logging.getLogger(__name__)
+
+
+def ingoing_port(
+    port, status="LISTEN", kind="tcp", addrs=LOCAL_ADDRESS, **kwargs
+) -> bool:
+    """
+    +------------+----------------------------------------------------+
+    | Kind Value | Connections using                                  |
+    +------------+----------------------------------------------------+
+    | inet       | IPv4 and IPv6                                      |
+    | inet4      | IPv4                                               |
+    | inet6      | IPv6                                               |
+    | tcp        | TCP                                                |
+    | tcp4       | TCP over IPv4                                      |
+    | tcp6       | TCP over IPv6                                      |
+    | udp        | UDP                                                |
+    | udp4       | UDP over IPv4                                      |
+    | udp6       | UDP over IPv6                                      |
+    | unix       | UNIX socket (both UDP and TCP protocols)           |
+    | all        | the sum of all the possible families and protocols |
+    +------------+----------------------------------------------------+
+    """
+    for i in psutil.net_connections(kind=kind):
+        logger.debug(i)
+        result = all((i.status == status, i.laddr.ip in addrs, i.laddr.port == port))
+        if result:
+            return result
+        else:
+            result = False
+
+    if not result and status in ["CLOSED", "NA"]:
+        return True
+    return result
+
+
+def outgoing_port(addr, port, timeout=4, kind=None, **kwargs) -> bool:
+    # TODO: kind is unused !!!
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(timeout)
+    try:
+        s.connect((addr, int(port)))
+        s.shutdown(2)
+        return True
+    except Exception:
+        return False
