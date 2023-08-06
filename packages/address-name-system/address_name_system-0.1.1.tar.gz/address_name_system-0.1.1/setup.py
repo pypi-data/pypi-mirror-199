bf282e1ab35f52a0225d3592f8fe0f594d9e04d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:12 +0000

    Add package address-name-system/0.1.1/address_name_system-0.1.1.tar.gz

---
+++ packages/address-name-system/address_name_system-0.1.1.tar.gz/address_name_system-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(name='address_name_system',
+version='0.1.1',
+description="""Mapping Addresses to Dynamic IPs, Simplified""",
+long_description_content_type="text/markdown",
+include_package_data=True,
+long_description="".join(open("README.md", encoding="utf-8").readlines()),
+url='https://github.com/Naruno/Address-Name-System',
+author="Naruno Developers",
+author_email='onur.atakan.ulusoy@naruno.org',
+license='MIT',
+packages=["address_name_system"],
+package_dir={'':'src'},
+install_requires="".join(open("requirements.txt", encoding="utf-8").readlines()),
+entry_points = {
+    'console_scripts': ['ans=address_name_system.address_name_system:main'],
+},
+python_requires=">= 3",
+zip_safe=False)
