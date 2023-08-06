8871c9091000067f3250e724516ae7a75c1622bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:35 +0000

    Add package gpt-sh/0.1.0/gpt-sh-0.1.0.tar.gz

---
+++ packages/gpt-sh/gpt-sh-0.1.0.tar.gz/gpt-sh-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+setup(
+    name='gpt-sh',
+    version='0.1.0',
+    description="A somewhat more advanced version of Shell GPT",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Timo Wang",
+    author_email="ntwang1994@gmail.com",
+    packages=["."],
+    entry_points={
+        "console_scripts": [
+            "gpt-sh = main:main"
+        ],
+    },
+    install_requires=[
+        "shell_gpt",
+    ]
+)
