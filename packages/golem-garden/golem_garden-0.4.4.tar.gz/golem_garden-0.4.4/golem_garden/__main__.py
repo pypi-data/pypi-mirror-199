5e3e0513fdecedd98fee1fc5beea33fd90e888af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:47 +0000

    Add package golem-garden/0.4.4/golem_garden-0.4.4.tar.gz

---
+++ packages/golem-garden/golem_garden-0.4.4.tar.gz/golem_garden-0.4.4/golem_garden/__main__.py
@@ -0,0 +1,10 @@
+from golem_garden.user_interface.command_line_interface import chat_with_golem
+
+
+def main(cli: bool = True):
+    if cli:
+        chat_with_golem()
+
+
+if __name__ == "__main__":
+    main()
