391d4e92cee0f58f0e1a20a8ef01d4b15bad7947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:50 +0000

    Add package WTTI/0.1.0/WTTI-0.1.0.tar.gz

---
+++ packages/WTTI/WTTI-0.1.0.tar.gz/WTTI-0.1.0/README.md
@@ -0,0 +1,2 @@
+# WTTI
+Webpage Text Transformation Interface，將網頁文章內容以結構化方式儲存，以方便後續分析應用。
---
+++ packages/WTTI/WTTI-0.1.0.tar.gz/WTTI-0.1.0/wtti/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.0"
+
+from .Content import Content # noqa: F401
+from .Text import Text # noqa: F401
