772f402ccd20bd243446fe7f06b8db0ee87dc72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:09 +0000

    Add package pyTONPublicAPI/0.2.0/pyTONPublicAPI-0.2.0.tar.gz

---
+++ packages/pyTONPublicAPI/pyTONPublicAPI-0.2.0.tar.gz/pyTONPublicAPI-0.2.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import setup
+from io import open
+import re
+
+def read(filename):
+    with open(filename, encoding='utf-8') as file:
+        return file.read()
+
+with open('pyTONPublicAPI/version.py', 'r', encoding='utf-8') as f:  # Credits: LonamiWebs
+    version = re.search(r"^__version__\s*=\s*'(.*)'.*$",
+                        f.read(), flags=re.MULTILINE).group(1)
+
+setup(name='pyTONPublicAPI',
+      version=version,
+      description='Python TON.sh Public API',
+      long_description=read('README.md'),
+      long_description_content_type="text/markdown",
+      author='Badiboy',
+      url='https://github.com/Badiboy/pyTONPublicAPI',
+      packages=['pyTONPublicAPI'],
+      requires=['requests'],
+      license='MIT license',
+      keywords="TON Open Network API",
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: MIT License',
+      ],
+)
