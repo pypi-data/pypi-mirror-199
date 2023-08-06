20ac549b535bd182be6af60d2e959cd26553e2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:14 +0000

    Add package pyauthenticator/0.0.7/pyauthenticator-0.0.7-py3-none-any.whl

---
+++ packages/pyauthenticator/pyauthenticator-0.0.7-py3-none-any.whl/pyauthenticator/__main__.py
@@ -0,0 +1,49 @@
+"""
+Generate two factor authentication codes on the command line
+"""
+import argparse
+from pyauthenticator.share import (
+    list_services,
+    load_config,
+    generate_qrcode,
+    add_service,
+    get_two_factor_code,
+)
+
+
+def command_line_parser():
+    """
+    Main function primarly used for the command line interface
+    """
+    parser = argparse.ArgumentParser(prog="pyauthenticator")
+    config_dict = load_config()
+    parser.add_argument(
+        "service",
+        help="Service to generate optauth code for. Available services are: "
+        + str(list_services(config_dict=config_dict)),
+    )
+    parser.add_argument(
+        "-qr",
+        "--qrcode",
+        action="store_true",
+        help="Generate qrcode as <service.png> file.",
+    )
+    parser.add_argument(
+        "-a",
+        "--add",
+        help="Add service by providing the qrcode png file as additional argument.",
+    )
+    args = parser.parse_args()
+    if args.qrcode:
+        generate_qrcode(key=args.service, config_dict=config_dict)
+    elif args.add:
+        add_service(
+            key=args.service, qrcode_png_file_name=args.add, config_dict=config_dict
+        )
+        print(args.service, "added.")
+    else:
+        print(get_two_factor_code(key=args.service, config_dict=config_dict))
+
+
+if __name__ == "__main__":
+    command_line_parser()
