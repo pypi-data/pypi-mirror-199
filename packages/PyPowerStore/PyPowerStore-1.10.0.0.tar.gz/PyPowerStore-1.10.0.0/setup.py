21b16fe8748b568344a2d164c5d412e99d687691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:07 +0000

    Add package PyPowerStore/1.10.0.0/PyPowerStore-1.10.0.0.tar.gz

---
+++ packages/PyPowerStore/PyPowerStore-1.10.0.0.tar.gz/PyPowerStore-1.10.0.0/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2019, Dell Technologies
+
+"""Setup file for PowerStore SDK"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(name='PyPowerStore',
+      version='1.10.0.0',
+      description='Python Library for Dell PowerStore',
+      author='Ansible Team at Dell',
+      author_email='ansible.team@dell.com',
+      install_requires=[
+        'urllib3>=1.26.7',
+        'requests>=2.23.0'
+      ],
+      url='https://github.com/dell/python-powerstore',
+      packages=['PyPowerStore', 'PyPowerStore.utils'],
+      )
