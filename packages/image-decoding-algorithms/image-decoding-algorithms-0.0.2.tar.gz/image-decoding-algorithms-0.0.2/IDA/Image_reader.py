c6801d068efce02f5755979fdc23751533e8b523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:07 +0000

    Add package image-decoding-algorithms/0.0.2/image-decoding-algorithms-0.0.2.tar.gz

---
+++ packages/image-decoding-algorithms/image-decoding-algorithms-0.0.2.tar.gz/image-decoding-algorithms-0.0.2/IDA/Image_reader.py
@@ -0,0 +1,50 @@
+from typing import Tuple, Union, List
+class Image_reading_class(object):
+
+    def __init__(self, path, input_mode="rb"):
+        self.image = open(path, mode=input_mode)
+
+    def read(self, len: int = 1, mode: str = 'str') -> Union[int, str, Tuple[int, int]]:
+        if mode == 'str':
+            return self.read_str(len)
+        elif mode == 'long':
+            return self.read_two_bytes_as_long_dec()
+        elif mode == 'dec':
+            return self.read_1_dec()
+        else:
+            return self.read_byte_as_2_dec()
+
+    def read_str(self, len: int) -> str:
+        return self.image.read(len)
+
+    def read_byte_as_2_dec(self) -> Tuple[int, int]:
+        byte = format(self.read_1_dec(), '08b')
+        return int(byte[:4], base=2), int(byte[4:], base=2)
+
+    def read_two_bytes_as_long_dec(self) -> int:
+        dec = 0
+        mult = 8 #right formula would be (len-1)*8 but since len always = 2, it is a constant
+        for i in range(2):
+            dec += self.read_1_dec() << mult
+            mult -= 8
+        return dec
+
+    def read_1_dec(self) -> int:
+        return ord(self.image.read(1))
+
+    def read_all(self) -> Tuple[str, ...]:
+        return self.image.read()
+
+    def close(self):
+        self.image.close()
+
+
+class Image(object):
+
+    def __init__(self, ImageWidth: int, ImageHeight: int, ImageBitDepth: int,
+                                                        ImageChannelsAmount: int, ImageData: List[List[List[int]]]):
+        self.width = ImageWidth
+        self.height = ImageHeight
+        self.bit_depth = ImageBitDepth
+        self.channels_amount = ImageChannelsAmount
+        self.data = ImageData
---
+++ packages/image-decoding-algorithms/image-decoding-algorithms-0.0.2.tar.gz/image-decoding-algorithms-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+setup(
+    name = "image-decoding-algorithms",
+    version = "0.0.2",
+    author = "Victor Andrievskiy",
+    author_email = "superavb222@gmail.com",
+    description= "Scripts for decoding an image",
+    long_description= "Scripts for decoding an image. Myself have written a decoding algorithm for JFIF, baseline encoding mode, used skimage for everything else.",
+    packages=find_packages(),
+    install_requires = ['scikit-image', 'numpy'],
+    url = "https://github.com/gejirz/image-decoding-algorithms",
+    keywords=['python', 'image decoding', 'JFIF', 'jpeg', 'decoding', 'baseline encoding'],
+    license= "MIT License"
+)
