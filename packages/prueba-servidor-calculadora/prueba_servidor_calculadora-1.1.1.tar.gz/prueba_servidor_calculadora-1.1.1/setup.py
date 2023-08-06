61ec2cd171a5645c97235f9a83ae46f72c848c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:42 +0000

    Add package prueba-servidor-calculadora/1.1.1/prueba_servidor_calculadora-1.1.1.tar.gz

---
+++ packages/prueba-servidor-calculadora/prueba_servidor_calculadora-1.1.1.tar.gz/prueba_servidor_calculadora-1.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="prueba_servidor_calculadora",
+    version="1.1.1",
+    author="Tu Nombre",
+    author_email="tu_correo@ejemplo.com",
+    description="Una simple calculadora de Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tu_usuario/mi-calculadora",
+    packages=["prueba_servidor_calculadora"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "py-expression-eval"
+    ],
+    entry_points={
+        'console_scripts': ['mi_calculadora = calculadora:main']
+},
+)
