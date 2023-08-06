dfabee7702d73662aeb8979f935807a24e713b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:48 +0000

    Add package keras-core/0.0.0/keras-core-0.0.0.tar.gz

---
+++ packages/keras-core/keras-core-0.0.0.tar.gz/keras-core-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="keras-core",
+    version="0.0.0",
+    description=(
+        "The Return of the King"
+    ),
+    author="Francois Chollet",
+    author_email="francois.chollet@gmail.com",
+    packages=find_packages(),
+)
