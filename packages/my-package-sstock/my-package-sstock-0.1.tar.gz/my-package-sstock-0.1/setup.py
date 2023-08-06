362b771af27ec00c1cff4b39de2a3b5041234ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:37 +0000

    Add package my-package-sstock/0.1/my-package-sstock-0.1.tar.gz

---
+++ packages/my-package-sstock/my-package-sstock-0.1.tar.gz/my-package-sstock-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my-package-sstock",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Add your package dependencies here
+    ],
+    author="Usman G Farooqui",
+    author_email="data.usac@outlook.com",
+    description="Testing safety stock",
+    url="",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
